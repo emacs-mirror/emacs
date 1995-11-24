e1e41781d7f077f6e19e339c8afd0cbaf9a13919
David Moore <dsm@harlequin.com>
1995-11-24 14:29:40 +0000

    New unit
    Stack scanning partly separated from threads.
    
    Copied from Perforce
     Change: 15049
     ServerID: perforce.ravenbrook.com

---
+++ mps/src/ss.h
@@ -0,0 +1,40 @@
+/*  impl.h.ss
+ *              STACK SCANNING
+ *
+ *  $HopeName: MMsrc/!than.h(trunk.1)$
+ *
+ *  Copyright (C) 1995 Harlequin Group, all rights reserved
+ *
+ *  Provides a function for scanning the stack and registers
+ *  
+ */
+
+#ifndef ss_h
+#define ss_h
+
+#include "std.h"
+#include "trace.h"
+#include "ref.h"
+
+
+/*  == StackScan ==
+ *  
+ *  StackScan scans the current stack between the 
+ *  stackBot and the current top of stack. It also fixes
+ *  any roots which may be in registers.  The fixing is done at the
+ *  given rank.
+ *  
+ *  See the specific implementation for the exact registers which
+ *  are scanned.
+ *
+ *  The word pointed to by stackBot is fixed if the stack
+ *  is by convention empty, and not fixed if it is full.
+ *  Where empty means sp points to first free word beyond the top of
+ *  stack.  Full means sp points to the top of stack itself.
+ */
+
+
+extern Error StackScan(Addr *stackBot, Trace trace, RefRank rank);
+
+
+#endif /* ss_h */
