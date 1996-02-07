60f8c11b8c9f4d7fdae4535e942de81b93c54851
Richard Brooksby <rb@ravenbrook.com>
1996-02-07 11:36:27 +0000

    Correcting va_start error.
    Adding some checks.
    
    Copied from Perforce
     Change: 15186
     ServerID: perforce.ravenbrook.com

--- mps/src/mpsi.c
+++ mps/src/mpsi.c
@@ -16,34 +16,10 @@
 #include <stddef.h>
 
 
-/* Interface Checking */
-
-#ifdef DEBUG_ASSERT      /* @@@@ do this? */
-#define TARGET_MPSI_ASSERT
-#endif
-
-#ifdef TARGET_MPSI_ASSERT
-#define mpsi_aver(_cond)        AVER(_cond) 
-#else
-#define mpsi_aver(_cond)        NOOP
-#endif
-
-
-/* Version Information */
-
-static mps_ver_s ver_s =
-{
-  "Harlequin Memory Pool System\n"
-  "Copyright 1996 Harlequin Group, all rights reserved.\n",
-  (unsigned)1,
-  (unsigned)0,
-  (unsigned)0   /* @@@@ Calculate from build somehow. */
-};
-
-
 /* Check consistency of interface mappings. */
 
-#ifdef TARGET_MPSI_ASSERT
+#ifdef DEBUG_ASSERT
+
 static Bool mpsi_check(void)
 {
   /* Check that external and internal result codes match. */
@@ -68,22 +44,26 @@ static Bool mpsi_check(void)
   AVER(sizeof(mps_word_t) == sizeof(Addr));
 
   /* Check ap_s/ApStruct compatibility by hand */
+  AVER(sizeof(mps_ap_s) == sizeof(ApStruct));
   AVER(offsetof(mps_ap_s, init) == offsetof(ApStruct, init));
   AVER(offsetof(mps_ap_s, alloc) == offsetof(ApStruct, alloc));
   AVER(offsetof(mps_ap_s, limit) == offsetof(ApStruct, limit));
 
+  /* Check ss_s/ScanStateStruct compatibility by hand */
+  AVER(sizeof(mps_ss_s) == sizeof(ScanStateStruct));
+  AVER(offsetof(mps_ss_s, fix) == offsetof(ScanStateStruct, fix));
+  AVER(offsetof(mps_ss_s, w0) == offsetof(ScanStateStruct, zoneShift));
+  AVER(offsetof(mps_ss_s, w1) == offsetof(ScanStateStruct, condemned));
+  AVER(offsetof(mps_ss_s, w2) == offsetof(ScanStateStruct, summary));
+
   return TRUE;
 }
-#endif
 
-mps_ver_t mps_ver(void)
-{
-  return &ver_s;
-}
+#endif /* DEBUG_ASSERT */
 
 mps_assert_t mps_assert_install(mps_assert_t handler)
 {
-  mpsi_aver(handler != NULL);
+  AVER(handler != NULL);
   return AssertInstall(handler);
 }
 
@@ -97,15 +77,15 @@ mps_res_t mps_space_create(mps_space_t *mps_space_o)
   Space *spaceReturn = (Space *)mps_space_o;
   /* This is the first real call that the client will have to make, */
   /* so check static consistency here. */
-  mpsi_aver(mpsi_check());
-  mpsi_aver(spaceReturn != NULL);
+  AVER(mpsi_check());
+  AVER(spaceReturn != NULL);
   return SpaceCreate(spaceReturn);
 }
 
 void mps_space_destroy(mps_space_t mps_space)
 {
   Space space = (Space)mps_space;
-  mpsi_aver(ISVALID(Space, space));
+  AVER(ISVALID(Space, space));
   SpaceDestroy(space);
 }
 
@@ -114,19 +94,19 @@ mps_res_t mps_form_create_A(mps_form_t *mps_form_o,
                             mps_form_A_t mps_form_A)
 {
   Format *formatReturn = (Format *)mps_form_o;
-  Error res;
-
-  res = FormatCreate(formatReturn,
-                     (Space)mps_space,
-                     (Addr)mps_form_A->align,
-                     (FormatScanMethod)mps_form_A->scan,
-                     (FormatSkipMethod)mps_form_A->skip,
-                     (FormatMoveMethod)mps_form_A->fwd,
-                     (FormatIsMovedMethod)mps_form_A->isfwd,
-                     (FormatCopyMethod)mps_form_A->copy);
-  return res;
+  Error e;
+  AVER(mps_form_A != NULL);
+  e = FormatCreate(formatReturn,
+                   (Space)mps_space,
+                   (Addr)mps_form_A->align,
+                   (FormatScanMethod)mps_form_A->scan,
+                   (FormatSkipMethod)mps_form_A->skip,
+                   (FormatMoveMethod)mps_form_A->fwd,
+                   (FormatIsMovedMethod)mps_form_A->isfwd,
+                   (FormatCopyMethod)mps_form_A->copy);
+  return e;
 }
-                              
+
 void mps_form_destroy(mps_form_t mps_form)
 {
   FormatDestroy((Format)mps_form);
@@ -140,17 +120,17 @@ mps_res_t mps_pool_create(mps_pool_t *mps_pool_o,
   Space space = (Space)mps_space;
   PoolClass class = (PoolClass)mps_class;
   va_list args;
-  Error res;
+  Error e;
 
-  mpsi_aver(poolReturn != NULL);
-  mpsi_aver(ISVALID(Space, space));
-  mpsi_aver(ISVALID(PoolClass, class));
+  AVER(poolReturn != NULL);
+  AVER(ISVALID(Space, space));
+  AVER(ISVALID(PoolClass, class));
 
-  va_start(args, size);
-  res = PoolCreateV(poolReturn, class, space, args);
+  va_start(args, mps_class);
+  e = PoolCreateV(poolReturn, class, space, args);
   va_end(args);
 
-  return res;
+  return e;
 }
 
 mps_res_t mps_pool_create_v(mps_pool_t *mps_pool_o,
@@ -161,21 +141,21 @@ mps_res_t mps_pool_create_v(mps_pool_t *mps_pool_o,
   Pool *poolReturn = (Pool *)mps_pool_o;
   Space space = (Space)mps_space;
   PoolClass class = (PoolClass)mps_class;
-  Error res;
+  Error e;
 
-  mpsi_aver(poolReturn != NULL);
-  mpsi_aver(ISVALID(Space, space));
-  mpsi_aver(ISVALID(PoolClass, class));
+  AVER(poolReturn != NULL);
+  AVER(ISVALID(Space, space));
+  AVER(ISVALID(PoolClass, class));
 
-  res = PoolCreateV(poolReturn, class, space, args);
+  e = PoolCreateV(poolReturn, class, space, args);
 
-  return res;
+  return e;
 }
 
 void mps_pool_destroy(mps_pool_t mps_pool)
 {
   Pool pool = (Pool)mps_pool;
-  mpsi_aver(ISVALID(Pool, pool));
+  AVER(ISVALID(Pool, pool));
   PoolDestroy(pool);
 }
 
@@ -184,17 +164,18 @@ mps_res_t mps_alloc(mps_addr_t *p_o,
                     size_t size, ...)
 {
   Pool pool = (Pool)mps_pool;
-  Error res;
+  Error e;
 
-  mpsi_aver(p_o != NULL);
-  mpsi_aver(ISVALID(Pool, pool));
-  mpsi_aver(size > 0);
+  AVER(p_o != NULL);
+  AVER(ISVALID(Pool, pool));
+  AVER(size > 0);
   /* Note: class may allow unaligned size. */
 
-  /* varargs ignored */
-  res = PoolAlloc((Addr *)p_o, pool, size);
+  /* Varargs are ignored at the moment -- none of the pool */
+  /* implementations use them, and they're not passed through. */
+  e = PoolAlloc((Addr *)p_o, pool, size);
   
-  return res;
+  return e;
 }
 
 mps_res_t mps_alloc_v(mps_addr_t *p_o,
@@ -202,26 +183,27 @@ mps_res_t mps_alloc_v(mps_addr_t *p_o,
                       size_t size, va_list args)
 {
   Pool pool = (Pool)mps_pool;
-  Error res;
+  Error e;
 
-  mpsi_aver(p_o != NULL);
-  mpsi_aver(ISVALID(Pool, pool));
-  mpsi_aver(size > 0);
+  AVER(p_o != NULL);
+  AVER(ISVALID(Pool, pool));
+  AVER(size > 0);
   UNUSED(args);
   /* Note: class may allow unaligned size. */
 
-  res = PoolAlloc((Addr *)p_o, pool, size);
-  return res;
+  /* Varargs are ignored at the moment -- none of the pool */
+  /* implementations use them, and they're not passed through. */
+  e = PoolAlloc((Addr *)p_o, pool, size);
+  return e;
 }
 
 void mps_free(mps_pool_t mps_pool, mps_addr_t p, size_t size)
 {
   Pool pool = (Pool)mps_pool;
 
-  mpsi_aver(ISVALID(Pool, pool));
-  mpsi_aver(size > 0);
+  AVER(ISVALID(Pool, pool));
+  AVER(size > 0);
   /* Note: class may allow unaligned size. */
-  /* @@@@ Check that p belongs to pool? */
 
   PoolFree(pool, (Addr)p, size);
 }
@@ -231,15 +213,17 @@ mps_res_t mps_ap_create(mps_ap_t *mps_ap_o, mps_pool_t mps_pool, ...)
   Ap *apReturn = (Ap *)mps_ap_o;
   Pool pool = (Pool)mps_pool;
   Buffer buf;
-  Error res;
+  Error e;
 
-  mpsi_aver(apReturn != NULL);
-  mpsi_aver(ISVALID(Pool, pool));
+  AVER(apReturn != NULL);
+  AVER(ISVALID(Pool, pool));
 
-  /* varargs ignored */
-  res = BufferCreate(&buf, pool);
-  if(res != MPS_RES_OK)
-    return res;
+  /* Varargs are ignored at the moment -- none of the pool */
+  /* implementations use them, and they're not passed through. */
+  e = BufferCreate(&buf, pool);
+  if(e != ErrSUCCESS)
+    return e;
+
   *apReturn = BufferAp(buf);
   return MPS_RES_OK;
 }
@@ -251,15 +235,18 @@ mps_res_t mps_ap_create_v(mps_ap_t *mps_ap_o,
   Ap *apReturn = (Ap *)mps_ap_o;
   Pool pool = (Pool)mps_pool;
   Buffer buf;
-  Error res;
+  Error e;
 
-  mpsi_aver(apReturn != NULL);
-  mpsi_aver(ISVALID(Pool, pool));
+  AVER(apReturn != NULL);
+  AVER(ISVALID(Pool, pool));
   UNUSED(args);
 
-  res = BufferCreate(&buf, pool);
-  if(res != MPS_RES_OK)
-    return res;
+  /* Varargs are ignored at the moment -- none of the pool */
+  /* implementations use them, and they're not passed through. */
+  e = BufferCreate(&buf, pool);
+  if(e != ErrSUCCESS)
+    return e;
+
   *apReturn = BufferAp(buf);
   return MPS_RES_OK;
 }
@@ -267,7 +254,7 @@ mps_res_t mps_ap_create_v(mps_ap_t *mps_ap_o,
 void mps_ap_destroy(mps_ap_t mps_ap)
 {
   Buffer buf = BufferOfAp((Ap)mps_ap);
-  mpsi_aver(ISVALID(Buffer, buf));
+  AVER(ISVALID(Buffer, buf));
   BufferDestroy(buf);
 }
 
@@ -275,10 +262,10 @@ mps_res_t (mps_reserve)(mps_addr_t *p_o, mps_ap_t mps_ap, size_t size)
 {
   Buffer buf = BufferOfAp((Ap)mps_ap);
 
-  mpsi_aver(p_o != NULL);
-/*  mpsi_aver(ISVALID(Buffer, buf));  can't do this - thread safe */
-  mpsi_aver(size > 0);
-  mpsi_aver(IsAligned(BufferPool(buf)->alignment, size));
+  AVER(p_o != NULL);
+  AVER(ISVALID(Buffer, buf));
+  AVER(size > 0);
+  AVER(IsAligned(BufferPool(buf)->alignment, size));
     
   return (BufferReserve)((Addr *)p_o, buf, size);
 }
@@ -287,9 +274,9 @@ mps_bool_t (mps_commit)(mps_ap_t mps_ap, mps_addr_t p, size_t size)
 {
   Buffer buf = BufferOfAp((Ap)mps_ap);
 
-/*  mpsi_aver(ISVALID(Buffer, buf));  can't do this - thread safe */
-  mpsi_aver(size > 0);
-  mpsi_aver(IsAligned(BufferPool(buf)->alignment, size));
+  AVER(ISVALID(Buffer, buf));
+  AVER(size > 0);
+  AVER(IsAligned(BufferPool(buf)->alignment, size));
 
   return (BufferCommit)(buf, (Addr)p, size);
 }
@@ -297,15 +284,15 @@ mps_bool_t (mps_commit)(mps_ap_t mps_ap, mps_addr_t p, size_t size)
 mps_res_t mps_ap_fill(mps_addr_t *p_o, mps_ap_t mps_ap, size_t size)
 {
   Buffer buf = BufferOfAp((Ap)mps_ap);
-  Error res;
+  Error e;
 
-  mpsi_aver(p_o != NULL);
-/*  mpsi_aver(ISVALID(Buffer, buf));  can't do this - thread safe */
-  mpsi_aver(size > 0);
-  mpsi_aver(IsAligned(BufferPool(buf)->alignment, size));
+  AVER(p_o != NULL);
+  AVER(ISVALID(Buffer, buf));
+  AVER(size > 0);
+  AVER(IsAligned(BufferPool(buf)->alignment, size));
 
-  res = BufferFill((Addr *)p_o, buf, size);
-  return res;
+  e = BufferFill((Addr *)p_o, buf, size);
+  return e;
 }
 
 mps_bool_t mps_ap_trip(mps_ap_t mps_ap, mps_addr_t p, size_t size)
@@ -313,9 +300,9 @@ mps_bool_t mps_ap_trip(mps_ap_t mps_ap, mps_addr_t p, size_t size)
   Buffer buf = BufferOfAp((Ap)mps_ap);
   Bool b;
 
-/*  mpsi_aver(ISVALID(Buffer, buf));  can't do this - thread safe */
-  mpsi_aver(size > 0);
-  mpsi_aver(IsAligned(BufferPool(buf)->alignment, size));
+  AVER(ISVALID(Buffer, buf));
+  AVER(size > 0);
+  AVER(IsAligned(BufferPool(buf)->alignment, size));
 
   b = BufferTrip(buf, (Addr)p, size);
   return b;
@@ -324,20 +311,22 @@ mps_bool_t mps_ap_trip(mps_ap_t mps_ap, mps_addr_t p, size_t size)
 mps_res_t mps_root_create(mps_root_t *mps_root_o,
                           mps_space_t mps_space,
                           mps_rank_t mps_rank,
+                          mps_rm_t mps_rm,
                           mps_root_scan_t mps_root_scan,
                           void *p, size_t s)
 {
   Root *rootReturn = (Root *)mps_root_o;
   Space space = (Space)mps_space;
   RefRank rank = (RefRank)mps_rank;
-  Error res;
 
-  mpsi_aver(rootReturn != NULL);
-  mpsi_aver(ISVALID(Space, space));
-  mpsi_aver(mps_root_scan != NULL);
-  res = RootCreate(rootReturn, space, rank, /* mode */ 0,
-    (Error (*)(ScanState, void *, size_t))mps_root_scan, p, s);
-  return res;
+  AVER(rootReturn != NULL);
+  AVER(ISVALID(Space, space));
+  AVER(mps_root_scan != NULL);
+  AVER(mps_rm = (mps_rm_t)0);
+
+  /* The root mode is ignored. */
+  return RootCreate(rootReturn, space, rank,
+                    (RootScanMethod)mps_root_scan, p, s);
 }
 
 mps_res_t mps_root_create_table(mps_root_t *mps_root_o,
@@ -349,16 +338,16 @@ mps_res_t mps_root_create_table(mps_root_t *mps_root_o,
   Root *rootReturn = (Root *)mps_root_o;
   Space space = (Space)mps_space;
   RefRank rank = (RefRank)mps_rank;
-  Error res;
 
-  mpsi_aver(mps_root_o != NULL);
-  mpsi_aver(ISVALID(Space, space));
-  mpsi_aver(base != NULL);
-  mpsi_aver((unsigned long)size > 0);
-  mpsi_aver(IsAligned(sizeof(mps_addr_t), size)); /* ?? */
-  res = RootCreateTable(rootReturn, space, rank,
-      /* mode */ 0, (Addr *)base, (Addr *)base + size);
-  return res;
+  AVER(mps_root_o != NULL);
+  AVER(ISVALID(Space, space));
+  AVER(base != NULL);
+  AVER((unsigned long)size > 0);
+  /* Note, size is the length of the array at base, not */
+  /* the size in bytes.  However, RootCreateTable expects */
+  /* base and limit pointers.  Be careful. */
+  return RootCreateTable(rootReturn, space, rank,
+                         (Addr *)base, (Addr *)base + size);
 }
 
 mps_res_t mps_root_create_form(mps_root_t *mps_root_o,
@@ -376,18 +365,45 @@ mps_res_t mps_root_create_form(mps_root_t *mps_root_o,
 mps_res_t mps_root_create_reg(mps_root_t *mps_root_o,
                               mps_space_t mps_space,
                               mps_rank_t mps_rank,
+                              mps_rm_t mps_rm,
                               mps_thr_t mps_thr,
                               mps_reg_scan_t mps_reg_scan,
                               void *reg_scan_p)
 {
-  NOTREACHED;
-  return MPS_RES_UNIMPL;
+  Root *rootReturn = (Root *)mps_root_o;
+  Space space = (Space)mps_space;
+  RefRank rank = (RefRank)mps_rank;
+  Thread thread = (Thread)mps_thr;
+
+  AVER(mps_root_o != NULL);
+  AVER(ISVALID(Space, space));
+  AVER(ISVALID(Thread, thread));
+  AVER(mps_reg_scan != NULL);
+
+  /* At present, we only support register scanning using our */
+  /* own ambiguous register and stack scanning method. */
+  AVER(mps_reg_scan == mps_stack_scan_ambig);
+  AVER(reg_scan_p != NULL); /* stackBot */
+  AVER(rank == MPS_RANK_AMBIG);
+  AVER(mps_rm == (mps_rm_t)0);
+
+  return RootCreateReg(rootReturn, space, rank, thread,
+                       (RootScanRegMethod)mps_reg_scan,
+                       reg_scan_p);
+}
+
+mps_res_t mps_stack_scan_ambig(mps_ss_t mps_ss,
+                               mps_reg_t mps_reg, void *p)
+{
+  ScanState ss = (ScanState)mps_ss;
+  Thread thread = (Thread)mps_reg;
+  return ThreadScan(ss, thread, p);
 }
 
 void mps_root_destroy(mps_root_t mps_root)
 {
   Root root = (Root)mps_root;
-  mpsi_aver(ISVALID(Root, root));
+  AVER(ISVALID(Root, root));
   RootDestroy(root);
 }
 
@@ -404,12 +420,9 @@ mps_res_t mps_thread_reg(mps_thr_t *mps_thr_o,
 {
   Thread *threadReturn = (Thread *)mps_thr_o;
   Space space = (Space)mps_space;
-  Error res;
-
-  mpsi_aver(mps_thr_o != NULL);
-  mpsi_aver(ISVALID(Space, space));
-  res = ThreadRegister(threadReturn, space, NULL); /* @@@@ wrong */
-  return res;
+  AVER(mps_thr_o != NULL);
+  AVER(ISVALID(Space, space));
+  return ThreadRegister(threadReturn, space);
 }
 
 void mps_thread_dereg(mps_thr_t mps_thr)
@@ -468,11 +481,11 @@ mps_msg_handler_t mps_msg_handler(mps_space_t mps_space,
 
 mps_res_t mps_fix(mps_ss_t mps_ss, mps_addr_t *ref_io)
 {
-  mps_res_t e;
+  mps_res_t res;
 
   MPS_SCAN_BEGIN(mps_ss) {
-  e = MPS_FIX(mps_ss, ref_io);
+    res = MPS_FIX(mps_ss, ref_io);
   } MPS_SCAN_END(mps_ss);
 
-  return e;
+  return res;
 }
