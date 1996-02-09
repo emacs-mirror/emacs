316f18ad5b9d8e175685b770f976248a53062839
Richard Brooksby <rb@ravenbrook.com>
1996-02-09 14:06:13 +0000

    New unit
    MPS Interfaces for Pools
    
    Copied from Perforce
     Change: 15234
     ServerID: perforce.ravenbrook.com

---
+++ mps/src/mpscmv.h
@@ -0,0 +1,14 @@
+/* impl.h.mpscmv: MEMORY POOL SYSTEM CLASS "MV"
+ *
+ * $HopeName$
+ * Copyright (C) 1996 Harlequin Group, all rights reserved.
+ */
+
+#ifndef mpscmv_h
+#define mpscmv_h
+
+#include "mps.h"
+
+extern mps_class_t mps_class_mv(void);
+
+#endif /* mpscmv_h */
