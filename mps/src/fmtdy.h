77c3dbde34a60393604d641829dfc8355591835c
Richard Brooksby <rb@ravenbrook.com>
1996-02-09 13:45:06 +0000

    Getting dylan format working
    Copied from Perforce
     Change: 15229
     ServerID: perforce.ravenbrook.com

--- mps/src/fmtdy.h
+++ mps/src/fmtdy.h
@@ -1,19 +1,20 @@
-/* impl.h.formdyl: DYLAN OBJECT FORMAT
+/* impl.h.fmtdy: DYLAN OBJECT FORMAT
  *
- * $HopeName$
+ * $HopeName: MMsrc!fmtdy.h(trunk.1) $
  * Copyright (C) 1996 Harlequin Group, all rights reserved
  */
 
-#ifndef formdyl_h
-#define formdyl_h
+#ifndef fmtdy_h
+#define fmtdy_h
 
 #include "mps.h"
 
-extern mps_form_A_s *dylan_form_A(void);
-extern mps_res_t dylan_init(mps_addr_t base, size_t size,
+extern mps_fmt_A_s *dylan_fmt_A(void);
+extern mps_res_t dylan_init(mps_addr_t addr, size_t size,
                             mps_addr_t *refs, size_t nr_refs);
-extern void dylan_write(mps_addr_t base,
+extern void dylan_write(mps_addr_t addr,
                         mps_addr_t *refs, size_t nr_refs);
-extern mps_addr_t dylan_read(mps_addr_t base);
+extern mps_addr_t dylan_read(mps_addr_t addr);
+extern mps_bool_t dylan_check(mps_addr_t addr);
 
-#endif /* formdyl_h */
+#endif /* fmtdy_h */
