20906965513e4beb3492223b1f2a75b474ddd729
David Jones <drj@ravenbrook.com>
1996-02-13 10:47:04 +0000

    New unit
    MPS interface to PoolClassLO
    
    Copied from Perforce
     Change: 15256
     ServerID: perforce.ravenbrook.com

---
+++ mps/src/mpsclo.h
@@ -0,0 +1,15 @@
+/* impl.h.mpscmv: MEMORY POOL SYSTEM CLASS "LO"
+ *
+ * $HopeName$
+ *
+ * Copyright (C) 1996 Harlequin Group, all rights reserved.
+ */
+
+#ifndef mpsclo_h
+#define mpsclo_h
+
+#include "mps.h"
+
+extern mps_class_t mps_class_lo(void);
+
+#endif /* mpsclo_h */
