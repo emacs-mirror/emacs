f980f2c1467b63f78cb6183e73a73b6e284e4f4a
David Jones <drj@ravenbrook.com>
1996-06-07 15:58:35 +0100

    New unit
    in support of irr4cc
    
    Copied from Perforce
     Change: 15833
     ServerID: perforce.ravenbrook.com

---
+++ mps/src/ssan.c
@@ -0,0 +1,15 @@
+/* impl.c.ssan: ANSI STACK SCANNER
+ *
+ * $HopeName$
+ *
+ * This module provides zero functionality.  It exists to feed the
+ * linker (prevent linker errors).
+ */
+
+#include "mpm.h"
+#include "ss.h"
+
+Res StackScan(ScanState ss, Addr *stackBot)
+{
+  return ResUNIMPL;
+}
