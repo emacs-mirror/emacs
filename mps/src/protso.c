d6074a7106fad2e5da47a26e9a6d1b8845170d43
David Jones <drj@ravenbrook.com>
1996-06-12 15:15:35 +0100

    New unit
    sospgc
    
    Copied from Perforce
     Change: 15845
     ServerID: perforce.ravenbrook.com

---
+++ mps/src/protso.c
@@ -0,0 +1,186 @@
+/*  impl.c.protso
+ *
+ *                  PROTECTION FOR Solaris
+ *
+ *  $HopeName: MMsrc!protso.c(MMdevel_protoposm_1.2) $
+ *
+ *  Copyright (C) 1995 Harlequin Group, all rights reserved
+ *
+ */
+
+#include "mpm.h"
+#include <limits.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <siginfo.h>
+#include <sys/mman.h>
+
+#ifndef MPS_OS_SO
+#error "protso.c is Solaris specific, but MPS_OS_SO is not set"
+#endif
+
+SRCID(protso, "$HopeName$");
+
+/* Fix up unprototyped system calls.  */
+/* Note that these are not fixed up by std.h because that only fixes */
+/* up discrepancies with ANSI. */
+
+extern int getpagesize(void);
+extern pid_t getpid(void);
+extern int kill(pid_t, int);
+
+/* Crap that can't be included via "open sesame" */
+/* definitions for the sa_flags field */
+/* Where is the source for this?  (which header files / man pages) @@ */
+#define	SA_SIGINFO	0x00000008
+/*
+ * SIGSEGV signal codes
+ */
+
+#define	SEGV_MAPERR	1	/* address not mapped to object */
+#define	SEGV_ACCERR	2	/* invalid permissions */
+#define	NSIGSEGV	2
+
+
+
+/* The previously-installed signal action, as returned by */
+/* sigaction(3).  See ProtSetup. */
+
+static struct sigaction sigNext;
+
+
+/*  == Protection Signal Handler ==
+ *
+ *  This is the signal handler installed by ProtSetup to deal with
+ *  protection faults.  It is installed on the SIGSEGV signal.
+ *  It decodes the protection fault details from the signal context
+ *  and passes them to SpaceAccess, which attempts to handle the
+ *  fault and remove its cause.  If the fault is handled, then
+ *  the handler returns and execution resumes.  If it isn't handled,
+ *  then sigHandle does its best to pass the signal on to the
+ *  previously installed signal handler (sigNext).
+ *
+ *  .sigh.addr: We assume that the OS decodes the address to something
+ *  sensible
+ *  .sigh.limit: We throw away the limit information.
+ */
+ 
+static void sigHandle(int sig, siginfo_t *info, void *context)
+{
+  AVER(sig == SIGSEGV);
+  AVER(info != NULL);
+
+  if(info->si_code == SEGV_ACCERR) {
+    AccessSet mode;
+    Addr base, limit;
+
+    /* We can't determine the access mode (read, write, etc.) */
+    /* under Solaris without decoding the faulting instruction. */
+    /* Don't bother, yet.  We can do this if necessary. */
+
+    mode = AccessREAD | AccessWRITE;
+
+    /* We assume that the access is for one word at the address. */
+    /* (Nb. ldd has to be dword aligned,
+     * hence cannot cross a page boundary) */
+
+    base = (Addr)info->si_addr;
+    limit = AddrAdd(base, (Size)sizeof(Addr));
+
+    /* Offer each protection structure the opportunity to handle the */
+    /* exception.  If it succeeds, then allow the mutator to continue. */
+
+    if(SpaceAccess(base, mode))
+      return;
+  }
+
+  /* The exception was not handled by any known protection structure, */
+  /* so throw it to the previously installed handler. */
+
+  /* @@ This is really weak.
+   * Need to implement rest of the contract of sigaction */
+  (*sigNext.sa_handler)(sig, info, context);
+}
+
+
+/*  == Global Protection Setup ==
+ *
+ *  Under Solaris, the global setup involves installing a signal handler
+ *  on SIGSEGV to catch and handle protection faults (see sigHandle).
+ *  The previous handler is recorded so that it can be reached from
+ *  sigHandle if it fails to handle the fault.
+ *
+ *  NOTE: There are problems with this approach:
+ *    1. we can't honor the wishes of the sigvec(2) entry for the
+ *       previous handler,
+ *    2. what if this thread is suspended just after calling signal(3)?
+ *       The sigNext variable will never be initialized!
+ */
+
+void ProtSetup(void)
+{
+  struct sigaction sa;
+  int result;
+
+  sa.sa_handler = sigHandle;
+  sa.sa_flags = SA_SIGINFO;
+
+  result = sigaction(SIGSEGV, &sa, &sigNext);
+  AVER(result == 0);
+}
+
+
+/*  == Set Protection ==
+ *
+ *  This is just a thin veneer on top of mprotect(2).
+ */
+
+void ProtSet(Addr base, Addr limit, AccessSet mode)
+{
+  int flags;
+
+  AVER(sizeof(int) == sizeof(Addr));
+  AVER(base < limit);
+  AVER(base != 0);
+  AVER(AddrOffset(base, limit) <= INT_MAX);	/* should be redundant */
+
+  flags = PROT_READ | PROT_WRITE | PROT_EXEC;
+  if((mode & AccessREAD) != 0)
+    flags &= ~PROT_READ;
+  if((mode & AccessWRITE) != 0)
+    flags &= ~PROT_WRITE;
+
+  if(mprotect((caddr_t)base, (int)AddrOffset(base, limit), flags) != 0)
+    NOTREACHED;
+}
+
+
+/* ProtSync -- synchronize protection settings with hardware
+ *
+ * This does nothing under Solaris.
+ */
+
+void ProtSync(Space space)
+{
+  NOOP;
+}
+
+
+
+/*  == Protection Trampoline ==
+ *
+ *  The protection trampoline is trivial under Solaris, as there is nothing
+ *  that needs to be done in the dynamic context of the mutator in order
+ *  to catch faults.  (Contrast this with Win32 Structured Exception
+ *  Handling.)
+ */
+
+void ProtTramp(void **resultReturn, void *(*f)(void *, size_t),
+               void *p, size_t s)
+{
+  AVER(f != NULL);
+  AVER(resultReturn != NULL);
+
+  *resultReturn = (*f)(p, s);
+}
---
+++ mps/src/vmso.c
@@ -0,0 +1,260 @@
+/* impl.c.vmso: VIRTUAL MEMORY MAPPING FOR SOLARIS 2.x
+ *
+ * $HopeName: MMsrc!vmsu.c(trunk.10) $
+ * Copyright (C) 1995 Harlequin Group, all rights reserved
+ *
+ * Design: design.mps.vm
+ *
+ * This is the implementation of the virtual memory mapping interface
+ * (vm.h) for Solaris 2.x
+ *
+ * mmap(2) is used to reserve address space by creating a mapping to
+ * /etc/passwd with page access none.  mmap(2) is used to map pages
+ * onto store by creating a copy-on-write mapping to /dev/zero.
+ *
+ * Experiments have shown that attempting to reserve address space
+ * by mapping /dev/zero results in swap being reserved.  This
+ * appears to be a bug, so we work round it by using /etc/passwd,
+ * the only file we can think of which is pretty much guaranteed
+ * to be around.
+ *
+ * .assume.not-last: The implementation of VMCreate assumes that
+ *   mmap() will not choose a region which contains the last page
+ *   in the address space, so that the limit of the mapped area
+ *   is representable.
+ *
+ * .assume.size: The maximum size of the reserved address space
+ *   is limited by the range of "int".  This will probably be half
+ *   of the address space.
+ *
+ * .assume.mmap.err: ENOMEM is the only error we really expect to
+ *   get from mmap.  The others are either caused by invalid params
+ *   or features we don't use.  See mmap(2) for details.
+ */
+
+#include "mpm.h"
+
+#ifndef MPS_OS_SO
+#error "vmso.c is Solaris 2.x specific, but MPS_OS_SO is not set"
+#endif
+
+#include <sys/types.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/errno.h>
+
+SRCID(vmso, "$HopeName: MMsrc!vmsu.c(trunk.10) $");
+
+
+/* Fix up unprototyped system calls.  */
+/* Note that these are not fixed up by std.h because that only fixes */
+/* up discrepancies with ANSI. */
+
+extern int close(int fd);
+extern int munmap(caddr_t addr, size_t len);
+extern int getpagesize(void);
+
+#define SpaceVM(space)  (&(space)->arenaStruct.vmStruct)
+
+Align VMAlign(void)
+{
+  Align align;
+
+  align = (Align)getpagesize();
+  AVER(SizeIsP2(align));
+
+  return align;
+}
+
+
+Bool VMCheck(VM vm)
+{
+  CHECKS(VM, vm);
+  CHECKL(vm->zero_fd >= 0);
+  CHECKL(vm->none_fd >= 0);
+  CHECKL(vm->zero_fd != vm->none_fd);
+  CHECKL(vm->base != 0);
+  CHECKL(vm->limit != 0);
+  CHECKL(vm->base < vm->limit);
+  CHECKL(vm->mapped <= vm->reserved);
+  CHECKL(SizeIsP2(vm->align));
+  CHECKL(AddrIsAligned(vm->base, vm->align));
+  CHECKL(AddrIsAligned(vm->limit, vm->align));
+  return TRUE;
+}
+
+
+Res VMCreate(Space *spaceReturn, Size size)
+{
+  caddr_t addr;
+  Align align;
+  int zero_fd;
+  int none_fd;
+  Space space;
+  VM vm;
+
+  align = VMAlign();
+
+  AVER(spaceReturn != NULL);
+  AVER(SizeIsAligned(size, align));
+  AVER(size != 0);
+  AVER(size <= INT_MAX); /* see .assume.size */
+
+  zero_fd = open("/dev/zero", O_RDONLY);
+  if(zero_fd == -1)
+    return ResFAIL;
+  none_fd = open("/etc/passwd", O_RDONLY);
+  if(none_fd == -1) {
+    close(zero_fd);
+    return ResFAIL;
+  }
+
+  /* Map in a page to store the descriptor on. */
+  addr = mmap((caddr_t)0, SizeAlignUp(sizeof(SpaceStruct), align),
+              PROT_READ | PROT_WRITE, MAP_PRIVATE,
+              zero_fd, (off_t)0);
+  if((int)addr == -1) {
+    int e = errno;
+    AVER(e == ENOMEM); /* .assume.mmap.err */
+    close(none_fd);
+    close(zero_fd);
+    if(e == ENOMEM)
+      return ResMEMORY;
+    else
+      return ResFAIL;
+  }
+  space = (Space)addr;
+  vm = SpaceVM(space);
+
+  vm->zero_fd = zero_fd;
+  vm->none_fd = none_fd;
+  vm->align = align;
+
+  /* See .assume.not-last. */
+  addr = mmap((caddr_t)0, size, PROT_NONE, MAP_SHARED, none_fd, (off_t)0);
+  if((int)addr == -1) {
+    int e = errno;
+    AVER(e == ENOMEM); /* .assume.mmap.err */
+    close(none_fd);
+    close(zero_fd);
+    if(e == ENOMEM)
+      return ResRESOURCE;
+    else
+      return ResFAIL;
+  }
+
+  vm->base = (Addr)addr;
+  vm->limit = AddrAdd(vm->base, size);
+  vm->reserved = size;
+  vm->mapped = (Size)0;
+
+  vm->sig = VMSig;
+
+  AVERT(VM, vm);
+
+  *spaceReturn = space;
+  return ResOK;
+}
+
+
+void VMDestroy(Space space)
+{
+  int r;
+  VM vm = SpaceVM(space);
+
+  AVERT(VM, vm);
+  AVER(vm->mapped == (Size)0);
+
+  /* This appears to be pretty pointless, since the space descriptor */
+  /* page is  about to vanish completely.  However, munmap might fail */
+  /* for some reason, and this would ensure that it was still */
+  /* discovered if sigs were being checked. */
+  vm->sig = SigInvalid;
+
+  close(vm->zero_fd);
+  close(vm->none_fd);
+  r = munmap((caddr_t)vm->base, (int)AddrOffset(vm->base, vm->limit));
+  AVER(r == 0);
+  r = munmap((caddr_t)space,
+             (int)SizeAlignUp(sizeof(SpaceStruct), vm->align));
+  AVER(r == 0);
+}
+
+
+Addr VMBase(Space space)
+{
+  VM vm = SpaceVM(space);
+  AVERT(VM, vm);
+  return vm->base;
+}
+
+Addr VMLimit(Space space)
+{
+  VM vm = SpaceVM(space);
+  AVERT(VM, vm);
+  return vm->limit;
+}
+
+
+Res VMMap(Space space, Addr base, Addr limit)
+{
+  VM vm = SpaceVM(space);
+  Size size;
+
+  AVERT(VM, vm);
+  AVER(sizeof(int) == sizeof(Addr));
+  AVER(base < limit);
+  AVER(base >= vm->base);
+  AVER(limit <= vm->limit);
+  AVER(AddrOffset(base, limit) <= INT_MAX);
+  AVER(AddrIsAligned(base, vm->align));
+  AVER(AddrIsAligned(limit, vm->align));
+
+  /* Map /dev/zero onto the area with a copy-on-write policy.  This */
+  /* effectively populates the area with zeroed memory. */
+
+  size = AddrOffset(base, limit);
+
+  if((int)mmap((caddr_t)base, (int)size,
+               PROT_READ | PROT_WRITE | PROT_EXEC,
+               MAP_PRIVATE | MAP_FIXED,
+               vm->zero_fd, (off_t)0) == -1) {
+    AVER(errno == ENOMEM); /* .assume.mmap.err */
+    return ResMEMORY;
+  }
+
+  vm->mapped += size;
+
+  return ResOK;
+}
+
+
+void VMUnmap(Space space, Addr base, Addr limit)
+{
+  VM vm = SpaceVM(space);
+  Size size;
+  caddr_t addr;
+
+  AVERT(VM, vm);
+  AVER(sizeof(int) == sizeof(Addr));
+  AVER(base < limit);
+  AVER(base >= vm->base);
+  AVER(limit <= vm->limit);
+  AVER(AddrIsAligned(base, vm->align));
+  AVER(AddrIsAligned(limit, vm->align));
+
+  /* Map /etc/passwd onto the area, allowing no access.  This */
+  /* effectively depopulates the area from memory, but keeps */
+  /* it "busy" as far as the OS is concerned, so that it will not */
+  /* be re-used by other calls to mmap which do not specify MAP_FIXED. */
+
+  size = AddrOffset(base, limit);
+
+  addr = mmap((caddr_t)base, (int)size,
+              PROT_NONE, MAP_SHARED, vm->none_fd, (off_t)0);
+  AVER((int)addr != -1);
+
+  vm->mapped -= size;
+}
+
