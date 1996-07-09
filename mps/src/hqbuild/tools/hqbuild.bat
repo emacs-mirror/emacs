1a69bb7a433908cd3f47ff4bb52bfdb32f17fdba
David Jones <drj@ravenbrook.com>
1996-07-09 14:53:17 +0100

    Setting up path include lib env vars
    Copied from Perforce
     Change: 15952
     ServerID: perforce.ravenbrook.com

--- mps/src/hqbuild/tools/hqbuild.bat
+++ mps/src/hqbuild/tools/hqbuild.bat
@@ -1,7 +1,12 @@
 @REM impl.bat.hqbuild
-@REM $HopeName: MMsrc!tools:swbuild.bat(trunk.1) $
+@REM $HopeName: MMsrc!hqbuild:tools:hqbuild.bat(trunk.1) $
 @REM Copyright (C) 1996 Harlequin Group, all rights reserved
 @REM Called by SWIG autobuild system
 @
+@REM we expect whatcom to have set MSVCNT and possibly MSMASM
+IF NOT %MSMASM%X == X SET PATH=%MSMASM%\bin;%PATH%
+SET PATH=%MSVCNT%\bin;%PATH%
+SET LIB=%MSVCNT%\lib
+SET INCLUDE=%MSVCNT%\include
 @REM Silently throw away all but first argument
 nmake /f %1.nmk
