9f859882951822e943fb58d0fe2309e75be26679
Richard Brooksby <rb@ravenbrook.com>
1996-08-07 10:56:40 +0100

    Pre-review polishing
    Copied from Perforce
     Change: 15965
     ServerID: perforce.ravenbrook.com

--- mps/src/mpsi.c
+++ mps/src/mpsi.c
@@ -1,7 +1,17 @@
 /* impl.c.mpsi: MEMORY POOL SYSTEM INTERFACE LAYER
  *
- * $HopeName: MMsrc!mpsi.c(trunk.14) $
+ * $HopeName: MMsrc!mpsi.c(trunk.15) $
  * Copyright (C) 1996 Harlequin Group, all rights reserved.
+ *
+ * PURPOSE
+ *
+ * .purpose: This code bridges between the MPS interface to C,
+ * impl.h.mpsi, and the internal MPM interfaces, as defined by
+ * impl.h.mpm.  .purpose.check: It performs checking of the C client's
+ * usage of the MPS Interface.  .purpose.thread: It excludes multiple
+ * threads from the MPM by locking the Space (see .thread-safety).
+ *
+ * NOTES
  *
  * .thread-safety: Most calls through this interface lock the space
  * and therefore make the MPM single-threaded.  In order to do this
@@ -9,18 +19,66 @@
  * as ThreadSpace() must therefore be callable when the space is
  * _not_ locked.  These methods are tagged with the tag of this note.
  *
- * .lock-free: Certain functions inside the mps are thread-safe and do
+ * .lock-free: Certain functions inside the MPM are thread-safe and do
  * not need to be serialized by using locks.  They are tagged with this
  * the tag of this note.
+ *
+ * .form: Almost all functions in this implementation simply cast their
+ * arguments to the equivalent internal types, and cast results back
+ * to the external type, where necessary.  Only exceptions are noted
+ * in comments.
+ *
+ * TRANSGRESSIONS (rule.impl.trans)
+ *
+ * .check.protocol: (rule.impl.req) More could be done in this code to
+ * check that protocols are obeyed by the client.  It probably doesn't
+ * meet checking requirements.
+ *
+ * .varargs: (rule.universal.complete) The varags passed to mps_alloc(_v)
+ * and mps_ap_create(_v) are ignored at the moment.  None of the pool
+ * implementations use them, and they're not passed through.
+ *
+ * .poll: (rule.universal.complete) Various allocation methods call
+ * SpacePoll to allow the MPM to "steal" CPU time and get on with
+ * background tasks such as incremental GC.  This mechanism hasn't been
+ * well thought out.
+ *
+ * .root-mode: (rule.universal.complete) The root "mode", which specifies
+ * things like the protectability of roots, is ignored at present.  This
+ * is because the MPM doesn't ever try to protect them.  In future, it
+ * will.
+ *
+ * .reg-scan: (rule.universal.complete) At present, we only support
+ * register scanning using our own ambiguous register and stack scanning
+ * method, mps_stack_scan_ambig.  This may never change, but the way the
+ * interface is designed allows for the possibility of change.
+ *
+ * .msg.unimpl: (rule.universal.complete) Aynchronous messages from the
+ * MPS to the client are not implemented.
  */
 
 #include "mpm.h"
 #include "mps.h"
 
-SRCID(mpsi, "$HopeName: MMsrc!mpsi.c(trunk.14) $");
+SRCID(mpsi, "$HopeName: MMsrc!mpsi.c(trunk.15) $");
 
 
-/* Check consistency of interface mappings. */
+/* mpsi_check -- check consistency of interface mappings
+ *
+ * .check.purpose: The mpsi_check function attempts to check whether
+ * the defintions in impl.h.mpsi match the equivalent definition in
+ * the MPM.  It is checking the assumptions made in the other functions
+ * in this implementation.
+ *
+ * .check.macros: The CHECK* macros use some C trickery to attempt to
+ * verify that certain types and fields are equivalent.  The do not
+ * do a complete job.  This trickery is justified by the security gained
+ * in knowing that impl.h.mps matches the MPM.  See also
+ * mail.richard.1996-08-07.09-49.  [This paragraph is intended to
+ * satisfy rule.impl.trick.]
+ *
+ * .check.empty: Note that mpsi_check compiles away to almost nothing.
+ */
 
 #define CHECKLVALUE(lv1, lv2) \
   (sizeof((lv1) = (lv2)), sizeof((lv2) = (lv1)), TRUE)
@@ -85,6 +143,7 @@ static Bool mpsi_check(void)
   return TRUE;
 }
 
+
 mps_assert_t mps_assert_install(mps_assert_t handler)
 {
   AVER(handler != NULL);
@@ -96,25 +155,32 @@ mps_assert_t mps_assert_default(void)
   return AssertDefault();
 }
 
+
 mps_res_t mps_space_create_wmem(mps_space_t *mps_space_o,
-     				mps_addr_t base, size_t size)
+                                mps_addr_t base, size_t size)
 {
   Space *spaceReturn = (Space *)mps_space_o;
+
   /* This is the first real call that the client will have to make, */
   /* so check static consistency here. */
   AVER(mpsi_check());
+
   AVER(spaceReturn != NULL);
   AVER(base != NULL);
+
   return SpaceCreate(spaceReturn, (Addr)base, (Size)size);
 }
 
 mps_res_t mps_space_create(mps_space_t *mps_space_o)
 {
   Space *spaceReturn = (Space *)mps_space_o;
+
   /* This is the first real call that the client will have to make, */
   /* so check static consistency here. */
   AVER(mpsi_check());
+
   AVER(spaceReturn != NULL);
+
   return SpaceCreate(spaceReturn, (Addr)0, (Size)0);
 }
 
@@ -124,6 +190,15 @@ void mps_space_destroy(mps_space_t mps_space)
   AVERT(Space, space);
   SpaceDestroy(space);
 }
+
+
+/* mps_fmt_create_A -- create an object format of variety A
+ *
+ * .fmt.create.A.purpose: This function converts an object format
+ * spec of variant "A" into an MPM Format object.  See
+ * design.mps.interface.c.fmt.extend for justification of the
+ * way that the format structure is declared as "mps_fmt_A".
+ */ 
 
 mps_res_t mps_fmt_create_A(mps_fmt_t *mps_fmt_o,
                            mps_space_t mps_space,
@@ -156,6 +231,7 @@ void mps_fmt_destroy(mps_fmt_t mps_fmt)
   FormatDestroy((Format)mps_fmt);
   SpaceLeave(space);
 }
+
 
 mps_res_t mps_pool_create(mps_pool_t *mps_pool_o,
                           mps_space_t mps_space,
@@ -216,6 +292,7 @@ void mps_pool_destroy(mps_pool_t mps_pool)
   SpaceLeave(space);
 }
 
+
 mps_res_t mps_alloc(mps_addr_t *p_o,
                     mps_pool_t mps_pool,
                     size_t size, ...)
@@ -226,16 +303,15 @@ mps_res_t mps_alloc(mps_addr_t *p_o,
 
   SpaceEnter(space);
 
-  /* Give the space the opportunity to steal CPU time. */
-  SpacePoll(space);
+  SpacePoll(space);                     /* .poll */
 
   AVER(p_o != NULL);
   AVERT(Pool, pool);
   AVER(size > 0);
-  /* Note: class may allow unaligned size. */
+  /* Note: class may allow unaligned size, see */
+  /* design.mps.class-interface.alloc.size.align. */
 
-  /* Varargs are ignored at the moment -- none of the pool */
-  /* implementations use them, and they're not passed through. */
+  /* See .varargs. */
   res = PoolAlloc((Addr *)p_o, pool, size);
 
   SpaceLeave(space);
@@ -252,17 +328,16 @@ mps_res_t mps_alloc_v(mps_addr_t *p_o,
 
   SpaceEnter(space);
 
-  /* Give the space the opportunity to steal CPU time. */
-  SpacePoll(space);
+  SpacePoll(space);                     /* .poll */
 
   AVER(p_o != NULL);
   AVERT(Pool, pool);
   AVER(size > 0);
   UNUSED(args);
-  /* Note: class may allow unaligned size. */
+  /* Note: class may allow unaligned size, see */
+  /* design.mps.class-interface.alloc.size.align. */
 
-  /* Varargs are ignored at the moment -- none of the pool */
-  /* implementations use them, and they're not passed through. */
+  /* See .varargs. */
   res = PoolAlloc((Addr *)p_o, pool, size);
 
   SpaceLeave(space);
@@ -278,7 +353,8 @@ void mps_free(mps_pool_t mps_pool, mps_addr_t p, size_t size)
 
   AVERT(Pool, pool);
   AVER(size > 0);
-  /* Note: class may allow unaligned size. */
+  /* Note: class may allow unaligned size, see */
+  /* design.mps.class-interface.alloc.size.align. */
 
   PoolFree(pool, (Addr)p, size);
   SpaceLeave(space);
@@ -301,8 +377,7 @@ mps_res_t mps_ap_create(mps_ap_t *mps_ap_o, mps_pool_t mps_pool,
   AVER(apReturn != NULL);
   AVERT(Pool, pool);
 
-  /* Varargs are ignored at the moment -- none of the pool */
-  /* implementations use them, and they're not passed through. */
+  /* See .varargs. */
   va_start(args, mps_rank);
   res = BufferCreate(&buf, pool, rank);
   va_end(args);
@@ -330,8 +405,7 @@ mps_res_t mps_ap_create_v(mps_ap_t *mps_ap_o, mps_pool_t mps_pool,
   AVERT(Pool, pool);
   UNUSED(args);
 
-  /* Varargs are ignored at the moment -- none of the pool */
-  /* implementations use them, and they're not passed through. */
+  /* See .varargs. */
   res = BufferCreate(&buf, pool, rank);
   if(res != ResOK)
     return res;
@@ -353,14 +427,18 @@ void mps_ap_destroy(mps_ap_t mps_ap)
   SpaceLeave(space);
 }
 
+
+/* mps_reserve -- allocate store in preparation for initialization
+ *
+ * .reserve.call: mps_reserve does not call BufferReserve, but instead
+ * uses the in-line macro from impl.h.mps.  This is so that it calls
+ * mps_ap_fill and thence SpacePoll (.poll).  The consistency checks here
+ * are the ones which can be done outside the MPM.  See also .commit.call.
+ */
+
 mps_res_t (mps_reserve)(mps_addr_t *p_o, mps_ap_t mps_ap, size_t size)
 {
   mps_res_t res;
-
-  /* mps_reserve does not call BufferReserve, but instead uses the */
-  /* in-line macro from impl.h.mps.  This is so that it calls mps_ap_fill */
-  /* and thence SpacePoll.  The consistency checks here are the ones */
-  /* which can be done outside the interface. */
 
   AVER(p_o != NULL);
   AVER(size > 0);
@@ -372,13 +450,18 @@ mps_res_t (mps_reserve)(mps_addr_t *p_o, mps_ap_t mps_ap, size_t size)
   return res;
 }
 
+
+/* mps_commit -- commit initialized object, finishing allocation
+ *
+ * .commit.call: mps_commit does not call BufferCommit, but instead
+ * uses the in-line commit macro from impl.h.mps.  This is so that it
+ * calls mps_ap_trip and thence SpacePoll in future (.poll).  The
+ * consistency checks here are the ones which can be done outside the
+ * MPM.  See also .reserve.call.
+ */
+
 mps_bool_t (mps_commit)(mps_ap_t mps_ap, mps_addr_t p, size_t size)
 {
-  /* mps_commit does not call BufferCommit, but instead uses the */
-  /* in-line commit macro from impl.h.mps.  This is so that it calls */
-  /* mps_ap_trip.  The consistency checks here are the ones which can be */
-  /* done outside the interface. */
-
   AVER(mps_ap != NULL);
   AVER(p != NULL);
   AVER(size > 0);
@@ -388,6 +471,13 @@ mps_bool_t (mps_commit)(mps_ap_t mps_ap, mps_addr_t p, size_t size)
   return mps_commit(mps_ap, p, size);
 }
 
+
+/* mps_ap_fill -- called by mps_reserve when an AP hasn't enough space
+ *
+ * .ap.fill.internal: Note that mps_ap_fill should never be "called"
+ * directly by the client code.  It is invoked by the mps_reserve macro.
+ */
+
 mps_res_t mps_ap_fill(mps_addr_t *p_o, mps_ap_t mps_ap, size_t size)
 {
   Buffer buf = BufferOfAP((AP)mps_ap);
@@ -396,8 +486,7 @@ mps_res_t mps_ap_fill(mps_addr_t *p_o, mps_ap_t mps_ap, size_t size)
 
   SpaceEnter(space);
 
-  /* Give the space the opportunity to steal CPU time. */
-  SpacePoll(space);
+  SpacePoll(space);                     /* .poll */
 
   AVER(p_o != NULL);
   AVERT(Buffer, buf);
@@ -409,6 +498,13 @@ mps_res_t mps_ap_fill(mps_addr_t *p_o, mps_ap_t mps_ap, size_t size)
   SpaceLeave(space);
   return res;
 }
+
+
+/* mps_trip -- called by mps_commit when an AP is tripped
+ *
+ * .ap.trip.internal: Note that mps_ap_trip should never be "called"
+ * directly by the client code.  It is invoked by the mps_commit macro.
+ */
 
 mps_bool_t mps_ap_trip(mps_ap_t mps_ap, mps_addr_t p, size_t size)
 {
@@ -426,6 +522,7 @@ mps_bool_t mps_ap_trip(mps_ap_t mps_ap, mps_addr_t p, size_t size)
   SpaceLeave(space);
   return b;
 }
+
 
 mps_res_t mps_root_create(mps_root_t *mps_root_o,
                           mps_space_t mps_space,
@@ -446,7 +543,7 @@ mps_res_t mps_root_create(mps_root_t *mps_root_o,
   AVER(mps_root_scan != NULL);
   AVER(mps_rm == (mps_rm_t)0);
 
-  /* The root mode is ignored. */
+  /* See .root-mode. */
   res = RootCreate(rootReturn, space, rank,
                  (RootScanMethod)mps_root_scan, p, s);
   SpaceLeave(space);
@@ -470,10 +567,12 @@ mps_res_t mps_root_create_table(mps_root_t *mps_root_o,
   AVERT(Space, space);
   AVER(base != NULL);
   AVER((unsigned long)size > 0);
+
   /* Note, size is the length of the array at base, not */
   /* the size in bytes.  However, RootCreateTable expects */
   /* base and limit pointers.  Be careful. */
-  /* The root mode is ignored. */
+
+  /* See .root-mode. */
   res = RootCreateTable(rootReturn, space, rank,
                         (Addr *)base, (Addr *)base + size);
   SpaceLeave(space);
@@ -498,7 +597,8 @@ mps_res_t mps_root_create_fmt(mps_root_t *mps_root_o,
   AVER(scan != NULL);
   AVER(base != NULL);
   AVER(base < limit);
-  /* The root mode is ignored. */
+
+  /* See .root-mode. */
   return RootCreateFmt(rootReturn, space, rank, scan,
                        (Addr)base, (Addr)limit);
 }
@@ -523,20 +623,24 @@ mps_res_t mps_root_create_reg(mps_root_t *mps_root_o,
   AVERT(Space, space);
   AVERT(Thread, thread);
   AVER(mps_reg_scan != NULL);
-
-  /* At present, we only support register scanning using our */
-  /* own ambiguous register and stack scanning method. */
-  AVER(mps_reg_scan == mps_stack_scan_ambig);
+  AVER(mps_reg_scan == mps_stack_scan_ambig); /* .reg.scan */
   AVER(reg_scan_p != NULL); /* stackBot */
   AVER(rank == MPS_RANK_AMBIG);
   AVER(mps_rm == (mps_rm_t)0);
 
+  /* See .root-mode. */
   res = RootCreateReg(rootReturn, space, rank, thread,
                     (RootScanRegMethod)mps_reg_scan,
                     reg_scan_p);
   SpaceLeave(space);
   return res;
 }
+
+
+/* mps_stack_scan_ambig -- scan the thread state ambiguously
+ *
+ * See .reg-scan.
+ */
 
 mps_res_t mps_stack_scan_ambig(mps_ss_t mps_ss,
                                mps_reg_t mps_reg, void *p)
@@ -545,6 +649,7 @@ mps_res_t mps_stack_scan_ambig(mps_ss_t mps_ss,
   Thread thread = (Thread)mps_reg;
   return ThreadScan(ss, thread, p);
 }
+
 
 void mps_root_destroy(mps_root_t mps_root)
 {
@@ -556,13 +661,18 @@ void mps_root_destroy(mps_root_t mps_root)
   SpaceLeave(space);
 }
 
+
 void (mps_tramp)(void **r_o,
                  void *(*f)(void *p, size_t s),
                  void *p,
                  size_t s)
 {
+  /* No parameter validation is possible.  We place no constraints on */
+  /* the value of these parameters. */
+
   ProtTramp(r_o, f, p, s);
 }
+
 
 mps_res_t mps_thread_reg(mps_thr_t *mps_thr_o,
                          mps_space_t mps_space)
@@ -599,7 +709,12 @@ void mps_ld_reset(mps_ld_t mps_ld, mps_space_t mps_space)
   SpaceLeave(space);
 }
 
-/* .lock-free */
+
+/* mps_ld_add -- add a reference to a location dependency
+ *
+ * See .lock-free.
+ */
+
 void mps_ld_add(mps_ld_t mps_ld, mps_space_t mps_space, mps_addr_t addr)
 {
   Space space = (Space)mps_space;
@@ -607,7 +722,12 @@ void mps_ld_add(mps_ld_t mps_ld, mps_space_t mps_space, mps_addr_t addr)
   LDAdd(ld, space, (Addr)addr);
 }
 
-/* .lock-free */
+
+/* mps_ld_isstale -- check whether a location dependency is "stale"
+ *
+ * See .lock-free.
+ */
+
 mps_bool_t mps_ld_isstale(mps_ld_t mps_ld,
                           mps_space_t mps_space,
                           mps_addr_t addr)
@@ -636,7 +756,9 @@ mps_word_t mps_collections(mps_space_t mps_space)
   return SpaceEpoch(space);  /* thread safe: see impl.h.space.epoch.ts */
 }
 
-/* @@@@ not done from here on */
+
+/* Unimplemented -- see .msg.unimpl */
+
 mps_bool_t mps_msg_next(mps_space_t mps_space,
                         mps_mc_t *mps_mc_o,
                         void **msg_data_o)
@@ -645,6 +767,9 @@ mps_bool_t mps_msg_next(mps_space_t mps_space,
   return FALSE;
 }
 
+
+/* Unimplemented -- see .msg.unimpl */
+
 mps_bool_t mps_msg_peek(mps_space_t mps_space,
                     mps_mc_t *mps_mc_o,
                     void **msg_data_o)
@@ -652,6 +777,9 @@ mps_bool_t mps_msg_peek(mps_space_t mps_space,
   NOTREACHED;
   return FALSE;
 }
+
+
+/* Unimplemented -- see .msg.unimpl */
 
 mps_msg_handler_t mps_msg_handler(mps_space_t mps_space,
                                   mps_msg_handler_t mps_handler)
