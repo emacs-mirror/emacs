793522fac160faac15268ca069e63b23bf9a9af0
Gavin Matthews <grm@ravenbrook.com>
1996-11-13 15:08:42 +0000

    Review.h.mpslib.2
    Copied from Perforce
     Change: 16304
     ServerID: perforce.ravenbrook.com

--- mps/src/mpslib.h
+++ mps/src/mpslib.h
@@ -1,15 +1,24 @@
 /* impl.h.mpslib: HARLEQUIN MEMORY POOL SYSTEM LIBRARY INTERFACE
  *
- * $HopeName: MMsrc!mpslib.h(MMdevel_lib.3) $
+ * $HopeName: MMsrc!mpslib.h(trunk.2) $
  * Copyright (C) 1996 Harlequin Group, all rights reserved.
  *
  * .readership: MPS client application developers, MPS developers.
- * .sources: design.mps.lib
+ * .sources: design.mps.lib, design.mps.exec-env in Memory Management 
+ *   Information System
+ * .doc: Full documentation not yet available.
+ * .purpose: The purpose of this file is to declare the functions and types
+ *   required for the MPS downward library interface, and also to include those
+ *   headers permitted to free-standing implementations (see .free-standing),
+ *   except float.h.
  */
 
 #ifndef mpslib_h
 #define mpslib_h
 
+/* .free-standing: These three headers may be used in free-standing 
+ * implementations.  See standard.ansic section 4 and design.mps.exec-env.
+ */
 #include <stdarg.h>
 #include <limits.h>
 #include <stddef.h>
