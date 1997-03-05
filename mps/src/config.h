4e30e0f81aafe18cf3c2a866034a52f81b72965e
Richard Brooksby <rb@ravenbrook.com>
1997-03-05 16:04:30 +0000

    New unit
    Merging MMdevel_config
    
    Copied from Perforce
     Change: 17181
     ServerID: perforce.ravenbrook.com

---
+++ mps/src/config.h
@@ -0,0 +1,99 @@
+/* impl.h.config: MPS CONFIGURATION
+ *
+ * Copyright (C) 1997 Harlequin Group, all rights reserved.
+ * $HopeName: MMsrc!config.h(MMdevel_config.1) $
+ */
+
+#ifndef config_h
+#define config_h
+
+/* Platform Configuration */
+
+#include "mpstd.h"
+
+
+/* Variety Configuration
+ *
+ * Convert CONFIG_VAR_* defined on compiler command line into
+ * internal configuration parameters.  See design.mps.config.var.
+ */
+
+#if defined(CONFIG_VAR_DF)
+#define MPS_VAR_DF
+#define ASSERT_MPSI		/* impl.c.mpsi */
+#define ASSERT_MPM 		/* impl.h.mpm */
+#define CHECK_DEEP       	/* impl.h.assert */
+#define CHECK_ASSERT    	/* impl.h.assert */
+#elif defined(CONFIG_VAR_DP)    /* debug, partial checking */
+#define MPS_VAR_DP
+#define ASSERT_MPSI
+#define ASSERT_MPM
+#define CHECK_SHALLOW
+#define CHECK_ASSERT     	/* impl.h.assert */
+#elif defined(CONFIG_VAR_DS)    /* debug, sig checking only */
+#define MPS_VAR_DS
+#define ASSERT_MPSI
+#define ASSERT_MPM
+#elif defined(CONFIG_VAR_RO)
+#define MPS_VAR_RO
+#else
+#error "No target variety configured."
+#endif
+
+
+/* Product Configuration
+ *
+ * Convert CONFIG_PROD_* defined on compiler command line into
+ * internal configuration parameters.  See design.mps.config.prod.
+ */
+
+#if defined(CONFIG_PROD_EPCORE)
+#define MPS_PROD_EPCORE
+#define VM_RM			/* impl.h.mpmst.vm */
+#elif defined(CONFIG_PROD_DYLAN)
+#define MPS_PROD_DYLAN
+#elif defined(CONFIG_PROD_MPS)
+#define MPS_PROD_MPS
+#else
+#error "No target product configured."
+#endif
+
+
+/* Space Configuration -- see impl.c.space */
+
+#define SPACE_CONTROL_EXTENDBY  ((Size)4096)
+#define SPACE_CONTROL_AVGSIZE   ((Size)32)
+#define SPACE_CONTROL_MAXSIZE   ((Size)65536)
+#define SPACE_POLL_MAX          ((Size)262144)
+#define SPACE_LD_LENGTH         ((Size)4)
+
+
+/* Arena Configuration -- see impl.c.arena* */
+
+#define ARENA_SIZE              ((Size)64<<20)
+#define ARENA_ASSOC             ((Size)2)
+#define ARENA_POOL              ((int)0)
+#define ARENA_CLASS             ((int)1)
+#define ARENA_JUNKBYTE          ((unsigned char)0xA9)
+#define ARENA_ANSI_ALIGN        ((Align)4096)
+#define ARENA_ANSI_ZONESHIFT    ((Shift)20)
+
+
+/* Shield Configuration -- see impl.c.shield */
+
+#define SHIELD_CACHE_SIZE ((Size)2)
+
+
+/* VM Configuration -- see impl.c.vm* */
+
+#define VMAN_ALIGN              ((Align)4096)
+#define VM_JUNKBYTE             ((unsigned char)0xA9)
+#define VMRM_ALIGN		((Align)4096)
+
+
+/* Tracer Configuration -- see impl.c.trace */
+
+#define TRACE_MAX               ((Size)1)
+
+
+#endif /* config_h */
