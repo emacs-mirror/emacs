fbff19516e1a29db31df963f8b9c8dc7946f3fda
Richard Tucker <rit@harlequin.com>
1997-03-21 13:00:01 +0000

    New unit
    I said so
    
    Copied from Perforce
     Change: 17340
     ServerID: perforce.ravenbrook.com

---
+++ mps/qa/function/1.c
@@ -0,0 +1,21 @@
+/* test create a space and then destroy it
+   language c
+   link testlib.o
+*/
+
+#include "testlib.h"
+
+static void test(void)
+{
+ mps_space_t space;
+
+ cdie(mps_space_create(&space), "create space");
+ mps_space_destroy(space);
+}
+
+int main(void)
+{
+ easy_tramp(test);
+ pass();
+ return 0;
+}
