8cd9a978a0d2e5270f44b700eaef2d4109775f57
David Jones <drj@ravenbrook.com>
1997-04-07 08:57:02 +0100

    Fixing bad merge
    Copied from Perforce
     Change: 17377
     ServerID: perforce.ravenbrook.com

--- mps/src/hqbuild/tools/hqbuild.bat
+++ mps/src/hqbuild/tools/hqbuild.bat
@@ -1,10 +1,6 @@
 @REM impl.bat.hqbuild
-<<<<<<< /u/nickb/mm/merge/trunk/src/hqbuild:tools:hqbuild.bat
-@REM $HopeName: MMsrc!hqbuild:tools:hqbuild.bat(trunk.2) $
-=======
-@REM $HopeName: MMsrc!hqbuild:tools:hqbuild.bat(MMdevel_sw_eq.2) $
->>>>>>> 1.2.5.2
-@REM Copyright (C) 1996 Harlequin Group, all rights reserved
+@REM $HopeName: MMsrc!hqbuild:tools:hqbuild.bat(trunk.3) $
+@REM Copyright (C) 1996,1997 Harlequin Group, all rights reserved
 @REM Called by SWIG autobuild system
 @
 @REM we expect whatcom to have set MSVCNT and possibly MSMASM
@@ -12,5 +8,5 @@ IF NOT %MSMASM%X == X SET PATH=%MSMASM%\bin;%PATH%
 SET PATH=%MSVCNT%\bin;%PATH%
 SET LIB=%MSVCNT%\lib
 SET INCLUDE=%MSVCNT%\include
-@REM Silently throw away all but first argument
+@REM First argument is expected to be platform code, rest we pass on
 nmake /f %1.nmk %2 %3 %4 %5 %6 %7 %8 %9
