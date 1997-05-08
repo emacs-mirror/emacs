c5c7d4a13c3f35cd080dae90aa91c9b2d94730ed
Gavin Matthews <grm@ravenbrook.com>
1997-05-08 13:45:51 +0100

    Fix cast in call to mps_fix
    Copied from Perforce
     Change: 18061
     ServerID: perforce.ravenbrook.com

--- mps/src/mpsicv.c
+++ mps/src/mpsicv.c
@@ -1,6 +1,6 @@
 /* impl.c.mpsicv: MPSI COVERAGE TEST
  *
- * $HopeName: MMsrc!mpsicv.c(trunk.8) $
+ * $HopeName: MMsrc!mpsicv.c(trunk.9) $
  * Copyright (C) 1996 Harlequin Group, all rights reserved
  */
 
@@ -98,7 +98,7 @@ static void ap_create_v_test(mps_pool_t pool, mps_rank_t rank, ...)
 static mps_res_t root_single(mps_ss_t ss, void *p, size_t s)
 {
   testlib_unused(s);
-  return mps_fix(ss, (mps_addr_t)p);
+  return mps_fix(ss, (mps_addr_t *)p);
 }
 
 static void *test(void *arg, size_t s)
