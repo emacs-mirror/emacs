338ed2dac6dab5375c4f91604538a6d9be29a83b
David Jones <drj@ravenbrook.com>
1997-05-14 13:11:03 +0100

    New unit
    Copied from Perforce
     Change: 18166
     ServerID: perforce.ravenbrook.com

---
+++ mps/src/mpscawl.h
@@ -0,0 +1,14 @@
+/* impl.h.mpscaawl: MEMORY POOL SYSTEM CLASS "AWL"
+ *
+ * $HopeName$
+ * Copyright (C) 1997 The Harlequin Group.  All rights reserved.
+ */
+
+#ifndef mpscawl_h
+#define mpscawl_h
+
+#include "mps.h"
+
+extern mps_class_t mps_class_awl(void);
+
+#endif /* mpscawl_h */
