193a8ca5c0d8e7319e4ad0c8b711d9293c6d9969
David Jones <drj@ravenbrook.com>
1997-05-15 13:09:42 +0100

    Making it actually do some testing
    Copied from Perforce
     Change: 18172
     ServerID: perforce.ravenbrook.com

--- mps/src/awlut.c
+++ mps/src/awlut.c
@@ -1,6 +1,6 @@
 /* impl.c.awlut: POOL CLASS AWL UNIT TEST
  *
- * $HopeName$
+ * $HopeName: MMsrc!awlut.c(trunk.1) $
  * Copyright (C) 1997 The Harlequin Group Limited.  All rights reserved.
  *
  * READERSHIP
@@ -16,6 +16,7 @@
 #include "mps.h"
 #include "mpscawl.h"
 #include "mpsclo.h"
+#include "fmtdy.h"
 #include "testlib.h"
 
 static void *test(void *v, size_t s)
@@ -23,13 +24,30 @@ static void *test(void *v, size_t s)
   mps_space_t space;
   mps_pool_t leafpool;
   mps_pool_t tablepool;
+  mps_fmt_t dylanfmt;
   mps_ap_t leafap, exactap, weakap;
 
   space = (mps_space_t)v;
 
-  leafpool = tablepool = NULL;
-  leafap = exactap = weakap = NULL;
-  (void)(leafpool, tablepool, leafap, exactap, weakap, 0);
+  die(mps_fmt_create_A(&dylanfmt, space, dylan_fmt_A()),
+      "Format Create\n");
+  die(mps_pool_create(&leafpool, space, mps_class_lo(), dylanfmt),
+      "Leaf Pool Create\n");
+  die(mps_pool_create(&tablepool, space, mps_class_awl(), dylanfmt),
+      "Table Pool Create\n");
+  die(mps_ap_create(&leafap, leafpool, MPS_RANK_EXACT),
+      "Leaf AP Create\n");
+  die(mps_ap_create(&exactap, tablepool, MPS_RANK_EXACT),
+      "Exact AP Create\n");
+  die(mps_ap_create(&weakap, tablepool, MPS_RANK_WEAK),
+      "Weak AP Create\n");
+
+  mps_ap_destroy(weakap);
+  mps_ap_destroy(exactap);
+  mps_ap_destroy(leafap);
+  mps_pool_destroy(tablepool);
+  mps_pool_destroy(leafpool);
+  mps_fmt_destroy(dylanfmt);
 
   return NULL;
 }
