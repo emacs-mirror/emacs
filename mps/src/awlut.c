a810ab987cf19c41e9ea4dc9fa359123b91ee905
David Jones <drj@ravenbrook.com>
1997-06-18 09:32:37 +0100

    Making test more testy
    Copied from Perforce
     Change: 18354
     ServerID: perforce.ravenbrook.com

--- mps/src/awlut.c
+++ mps/src/awlut.c
@@ -1,6 +1,6 @@
 /* impl.c.awlut: POOL CLASS AWL UNIT TEST
  *
- * $HopeName: MMsrc!awlut.c(trunk.2) $
+ * $HopeName: MMsrc!awlut.c(trunk.3) $
  * Copyright (C) 1997 The Harlequin Group Limited.  All rights reserved.
  *
  * READERSHIP
@@ -20,25 +20,52 @@
 #include "testlib.h"
 
 #include <string.h>
+#include <assert.h>
+#include <stdio.h>
+
+
+#define TABLE_SLOTS 50
+#define ITERATIONS 5000
+#define CHATTER 100
+/* The number that a half of all numbers generated from rnd are less
+ * than.  Hence, probability a-half, or P a-half */
+/* see impl.h.testlib */
+#define P_A_HALF (1024uL*1024uL*1024uL - 1)     /* 2^30 - 1 */
+
+static mps_word_t bogus_class;
+
 
 static mps_word_t wrapper_wrapper[] = {
   (mps_word_t)wrapper_wrapper,	/* wrapper */
-  0,				/* class */
-  3<<2||2,			/* F */
+  (mps_word_t)&bogus_class,	/* class */
+  3<<2|2,			/* F */
   1<<(MPS_WORD_WIDTH - 8),	/* V */
-  1<<2||1,			/* VL */
+  1<<2|1,			/* VL */
   1				/* patterns */
 };
 
 
 static mps_word_t string_wrapper[] = {
   (mps_word_t)wrapper_wrapper,	/* wrapper */
-  0,				/* class */
+  (mps_word_t)&bogus_class,	/* class */
   0,				/* F */
-  1<<(MPS_WORD_WIDTH - 8),	/* V */
+  1<<(MPS_WORD_WIDTH - 8)|3<<3|4,	/* V */
   1				/* VL */
 };
 
+static mps_word_t table_wrapper[] = {
+  (mps_word_t)wrapper_wrapper,  /* wrapper */
+  (mps_word_t)&bogus_class,	/* class */
+  1<<2|1,			/* F */
+  1<<(MPS_WORD_WIDTH - 8)|2,	/* V */
+  1				/* VL */
+};
+
+#define DYLAN_ALIGN 4 /* depends on value defined in fmtdy.c */
+
+/* create a dylan string object (byte vector) whose contents
+ * are the string s (including the terminating NUL)
+ * .assume.dylan-obj */
 static mps_word_t *alloc_string(char *s, mps_ap_t ap)
 {
   size_t l;
@@ -50,6 +77,7 @@ static mps_word_t *alloc_string(char *s, mps_ap_t ap)
   /* number of words * sizeof word */
   objsize = (2 + (l+sizeof(mps_word_t)-1)/sizeof(mps_word_t)) *
 	    sizeof(mps_word_t);
+  objsize = (objsize + DYLAN_ALIGN-1)/DYLAN_ALIGN*DYLAN_ALIGN;
   do {
     size_t i;
     char *s2;
@@ -57,7 +85,7 @@ static mps_word_t *alloc_string(char *s, mps_ap_t ap)
     die(mps_reserve(&p, ap, objsize), "Reserve Leaf\n");
     object = p;
     object[0] = (mps_word_t)string_wrapper;
-    object[1] = l << 2 || 1;
+    object[1] = l << 2 | 1;
     s2 = (char *)&object[2];
     for(i = 0; i < l; ++i) {
       s2[i] = s[i];
@@ -66,11 +94,112 @@ static mps_word_t *alloc_string(char *s, mps_ap_t ap)
   return object;
 }
 
+/* create a table with n variable slots
+ * .assume.dylan-obj
+ */
+static mps_word_t *alloc_table(unsigned long n, mps_ap_t ap)
+{
+  size_t objsize;
+  void *p;
+  mps_word_t *object;
+  objsize = (3 + n) * sizeof(mps_word_t);
+  objsize = (objsize + MPS_PF_ALIGN-1)/MPS_PF_ALIGN*MPS_PF_ALIGN;
+  do {
+    unsigned long i;
+
+    die(mps_reserve(&p, ap, objsize), "Reserve Table\n");
+    object = p;
+    object[0] = (mps_word_t)table_wrapper;
+    object[1] = 0;
+    object[2] = n << 2 | 1;
+    for(i = 0; i < n; ++i) {
+      object[3+i] = 0;
+    }
+  } while(!mps_commit(ap, p, objsize));
+  return object;
+}
+
+/* gets the nth slot from a table
+ * .assume.dylan-obj
+ */
+static mps_word_t *table_slot(mps_word_t *table, unsigned long n)
+{
+  return (mps_word_t *)table[3+n];
+}
+
+/* sets the nth slot in a table
+ * .assume.dylan-obj
+ */
+static void set_table_slot(mps_word_t *table,
+			   unsigned long n, mps_word_t *p)
+{
+  assert(table[0] = (mps_word_t)table_wrapper);
+  table[3+n] = (mps_word_t)p;
+}
+
+/* links two tables together via their link slot
+ * (1st fixed part slot)
+ */
+static void table_link(mps_word_t *t1, mps_word_t *t2)
+{
+  assert(t1[0] == (mps_word_t)table_wrapper);
+  assert(t2[0] == (mps_word_t)table_wrapper);
+  t1[1] = (mps_word_t)t2;
+  t2[1] = (mps_word_t)t1;
+}
+
 static void test(mps_ap_t leafap, mps_ap_t exactap, mps_ap_t weakap)
 {
-  mps_word_t *spong;
+  mps_word_t *weaktable;
+  mps_word_t *exacttable;
+  mps_word_t *preserve[TABLE_SLOTS];	/* preserves objects in the weak */
+				        /* table by referring to them */
+  unsigned long i, j;
 
-  spong = alloc_string("spong", leafap);
+  exacttable = alloc_table(TABLE_SLOTS, exactap);
+  weaktable = alloc_table(TABLE_SLOTS, weakap);
+  table_link(exacttable, weaktable);
+
+  for(i = 0; i < TABLE_SLOTS; ++i) {
+    mps_word_t *string;
+    if(rnd() < P_A_HALF) {
+      string = alloc_string("iamalive", leafap);
+      preserve[i] = string;
+    } else {
+      string = alloc_string("iamdead", leafap);
+      preserve[i] = 0;
+    }
+    set_table_slot(weaktable, i, string);
+  }
+
+  for(j = 0; j < ITERATIONS; ++j) {
+    for(i = 0; i < TABLE_SLOTS; ++i) {
+      mps_word_t *string;
+
+      string = alloc_string("spong", leafap);
+      set_table_slot(exacttable, i, string);
+    }
+  }
+
+  for(i = 0; i < TABLE_SLOTS; ++i) {
+    if(preserve[i] == 0) {
+      if(table_slot(weaktable, i)) {
+	fprintf(stdout,
+		"Strongly unreachable weak table entry found, "
+	        "slot %lu.\n",
+		i);
+      } else {
+	if(table_slot(exacttable, i) != 0) {
+	  fprintf(stdout,
+		  "Weak table entry deleted, but corresponding "
+		  "exact table entry not deleted, slot %lu.\n",
+		  i);
+	}
+      }
+    }
+  }
+
+  puts("A okay\n");
 }
 
 
