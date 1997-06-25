e0d579624f6f41bba7f6a7cebdb2344c6ca89624
David Jones <drj@ravenbrook.com>
1997-06-25 12:30:20 +0100

    Edits for review.impl.c.bt.4
    Copied from Perforce
     Change: 18379
     ServerID: perforce.ravenbrook.com

--- mps/src/bt.c
+++ mps/src/bt.c
@@ -1,6 +1,6 @@
 /* impl.c.bt: BIT TABLES
  *
- * $HopeName: MMsrc!bt.c(trunk.3) $
+ * $HopeName: MMsrc!bt.c(trunk.4) $
  * Copyright (C) 1997 Harlequin Group, all rights reserved
  *
  * READERSHIP
@@ -13,101 +13,132 @@
  *
  * PURPOSE
  *
- * The Bit Tables modules is provides centrally functions for
- * manipulating tables of bits.  Such tables are often used to provide some
- * sort of mapping or set functionality (for example, a table of mark bits
- * in a mark and sweep pool may use bit tables).
+ * .purpose: see design.mps.bt
  */
 
 
 #include "mpm.h"
 
-SRCID(bt, "$HopeName: MMsrc!bt.c(trunk.3) $");
+SRCID(bt, "$HopeName: MMsrc!bt.c(trunk.4) $");
 
 
+/* design.mps.bt.fun.size */
 Size BTSize(unsigned long n)
 {
+  /* check that the expression used in rounding up doesn't overflow */
   AVER(n+MPS_WORD_WIDTH-1 > n);
 
   return (n+MPS_WORD_WIDTH-1)/MPS_WORD_WIDTH*sizeof(Word);
 }
   
+
+/* design.mps.bt.fun.get */
 int (BTGet)(BT t, Index i)
 {
   AVER(t != NULL);
+  AVER(AddrIsAligned((Addr)t, sizeof *t));
   /* Can't check i */
 
+  /* see macro in impl.h.mpm */
   return BTGet(t, i);
 }
   
+
+/* design.mps.bt.fun.set */
 void (BTSet)(BT t, Index i)
 {
   AVER(t != NULL);
+  AVER(AddrIsAligned((Addr)t, sizeof *t));
   /* Can't check i */
 
+  /* see macro in impl.h.mpm */
   BTSet(t, i);
 }
 
+
+/* design.mps.bt.fun.res */
 void (BTRes)(BT t, Index i)
 {
   AVER(t != NULL);
+  AVER(AddrIsAligned((Addr)t, sizeof *t));
   /* Can't check i */
 
+  /* see macro in impl.h.mpm */
   BTRes(t, i);
 }
 
+
+/* design.mps.bt.fun.set-range */
 void BTSetRange(BT t, Index i, Index j)
 {
   AVER(t != NULL);
+  AVER(AddrIsAligned((Addr)t, sizeof *t));
   AVER(i < j);
 
-  for( ; i < j; ++i) {
+  while(i < j) {
     BTSet(t, i);
+    ++i;
   }
 }
 
+
+/* design.mps.bt.fun.res-range */
 void BTResRange(BT t, Index i, Index j)
 {
   AVER(t != NULL);
+  AVER(AddrIsAligned((Addr)t, sizeof *t));
   AVER(i < j);
 
-  for( ; i < j; ++i) {
+  while(i < j) {
     BTRes(t, i);
+    ++i;
   }
 }
 
-Bool BTFindResRange(Index *iReturn, Index *jReturn,
-                    BT t, unsigned long s, unsigned long n)
+
+/* design.mps.bt.fun.find-res-range */
+Bool BTFindResRange(Index *baseIndexReturn, Index *limitIndexReturn,
+                    BT table, unsigned long tableSize,
+		    unsigned long runLength)
 {
-  unsigned long i = 0;
+  Index i = 0;
 
-  AVER(iReturn != NULL);
-  AVER(jReturn != NULL);
-  AVER(t != NULL);
-  AVER(n <= s);
-  AVER(n > 0);
+  AVER(baseIndexReturn != NULL);
+  AVER(limitIndexReturn != NULL);
+  AVER(table != NULL);
+  AVER(AddrIsAligned((Addr)table, sizeof *table));
+  AVER(runLength <= tableSize);
+  AVER(runLength > 0);
 
-  /* remember, s is the size of the table */
-  while(i < s) {
-    if(!BTGet(t, i)) {
-      unsigned long j = i;
+  /* design.mps.bt.fun.find-res.outer-loop */
+  while(i < tableSize) {
+    if(!BTGet(table, i)) {
+      /* design.mps.bt.fun.find-res.enter */
+      /* i now marks the beginning of a run */
+      Index j = i;
+
       do {
+	/* design.mps.bt.fun.find-res.inner-loop */
         ++j;
-      } while(j < s && !BTGet(t, j));
-      if(j - i >= n) {
+      } while(j < tableSize && !BTGet(table, j));
+      /* j now marks the end of a run */
+      if(j - i >= runLength) {
+	/* design.mps.bt.fun.find-res.success */
         /* found sufficiently long run */
-        *jReturn = j;
-        *iReturn = i;
+        *limitIndexReturn = j;
+        *baseIndexReturn = i;
         return TRUE;
       }
+      /* design.mps.bt.fun.find-res.continue */
+      /* wasn't long enough */
+      AVER(i < j);
       i = j;
-      continue;         /* necessary, consider j == s */
+    } else {
+      /* design.mps.bt.fun.find-res.outer-loop */
+      ++i;
     }
-    /* wasn't long enough */
-    ++i;
   }
-  AVER(i == s);
+  AVER(i == tableSize);
 
   return FALSE;
 }
-
