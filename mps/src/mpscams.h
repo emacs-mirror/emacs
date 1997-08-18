87857bcba293f5ab25e661ad2ea3cee9b2ff5f69
Nick Barnes <nb@ravenbrook.com>
1997-08-18 12:19:13 +0100

    New unit
    ** No reason given. **
    
    Copied from Perforce
     Change: 18626
     ServerID: perforce.ravenbrook.com

---
+++ mps/src/mpscams.h
@@ -0,0 +1,14 @@
+/* impl.h.mpscams: MEMORY POOL SYSTEM CLASS "AMS"
+ *
+ * $HopeName: MMsrc!mpscams.h(MMdevel_poolams.1) $
+ * Copyright (C) 1997 Harlequin Group, all rights reserved.
+ */
+
+#ifndef mpscams_h
+#define mpscams_h
+
+#include "mps.h"
+
+extern mps_class_t mps_class_ams(void);
+
+#endif /* mpscamc_h */
