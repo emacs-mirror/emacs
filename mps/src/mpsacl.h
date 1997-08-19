a8fb1917e24803fcd073d7139d24dbc55760aa28
Pekka Pirinen <pekka@harlequin.com>
1997-08-19 19:34:01 +0100

    New unit
    new arena class
    
    Copied from Perforce
     Change: 18641
     ServerID: perforce.ravenbrook.com

---
+++ mps/src/mpsacl.h
@@ -0,0 +1,16 @@
+/* impl.h.mpsacl: MEMORY POOL SYSTEM ARENA CLASS "CL"
+ *
+ * $HopeName: MMsrc!mpsacl.h(MMdevel_config_thread.1) $
+ * Copyright (C) 1997 The Harlequin Group.  All rights reserved.
+ */
+
+#ifndef mpsacl_h
+#define mpsacl_h
+
+#include "mps.h"
+
+
+extern mps_arena_class_t mps_arena_class_cl(void);
+
+
+#endif /* mpsacl_h */
