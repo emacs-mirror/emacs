3024c6d1a5201ebc04e5e9ce278a19430a498e79
Pekka Pirinen <pekka@harlequin.com>
1997-08-19 19:34:01 +0100

    New unit
    new arena class
    
    Copied from Perforce
     Change: 18641
     ServerID: perforce.ravenbrook.com

---
+++ mps/src/mpsavm.h
@@ -0,0 +1,16 @@
+/* impl.h.mpsavm: MEMORY POOL SYSTEM ARENA CLASS "VM"
+ *
+ * $HopeName: MMsrc!mpsavm.h(MMdevel_config_thread.1) $
+ * Copyright (C) 1997 The Harlequin Group.  All rights reserved.
+ */
+
+#ifndef mpsavm_h
+#define mpsavm_h
+
+#include "mps.h"
+
+
+extern mps_arena_class_t mps_arena_class_vm(void);
+
+
+#endif /* mpsavm_h */
