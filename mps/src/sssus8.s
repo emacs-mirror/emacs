ba2ada3a6a880125cea3551da58f0dd2f5123072
Gavin Matthews <grm@ravenbrook.com>
1997-08-27 17:27:19 +0100

    Merge mmdevel_annotation and fix stack scanning bugs
    Copied from Perforce
     Change: 18682
     ServerID: perforce.ravenbrook.com

--- mps/src/sssus8.s
+++ mps/src/sssus8.s
@@ -1,13 +1,17 @@
-!  impl.s.sssusp
+!  impl.s.sssus8
 !
 !                      STACK SCANNING
 !
-!  $HopeName: MMsrc!sssusp.s(trunk.2) $
+!  $HopeName: MMsrc!sssos9sc.S(trunk.1) $
 !
-!  Copyright (C) 1996 Harlequin Group, all rights reserved
+!  Copyright (C) 1995 Harlequin Group, all rights reserved
 !
 !  This scans the stack and the preserved integer registers.
 !  See design.mps.thread-manager
+!
+!  This file is identical to impl.s.sssos8, except for the
+!  leading underscore convention on C names.  They should be
+!  changed in parallel.
 !
 !  The non-global registers are preserved into the stackframe
 !  by the "ta 3" instruction.  This leaves the global registers.
@@ -32,7 +36,7 @@ _StackScan:               !(ss, stackBot)
 
   mov %i0,%o0             !ss
   sub %fp,24,%o1          !stackTop (base)
-  call _TraceScanAreaTagged     !(ss,stackTop,stackBot) returns e
+  call _TraceScanArea     !(stackTop,stackBot,trace,rank) returns e
   mov %i1,%o2          !ds!stackBot (limit)
 
   ret
