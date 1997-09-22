64b176d1c8a462159127cd119977c546d7a9ac7c
Richard Tucker <rit@harlequin.com>
1997-09-22 12:32:50 +0100

    New test (prev was a placeholder)
    Copied from Perforce
     Change: 18783
     ServerID: perforce.ravenbrook.com

--- mps/qa/function/38.c
+++ mps/qa/function/38.c
@@ -1,30 +1,53 @@
 /* TEST_HEADER
- summary = mps_ld_merge
+ summary = test of location dependencies
  language = c
- link = testlib.o awlfmt.o
+ link = testlib.o rankfmt.o
 END_HEADER
 */
 
 #include "testlib.h"
 #include "mpscawl.h"
-#include "awlfmt.h"
+#include "mpscmv.h"
+#include "mpscamc.h"
+#include "rankfmt.h"
+
+#define MAXLDS 100
 
 void *stackpointer;
 
-static void test(void)
-{
- mps_space_t space;
- mps_pool_t pool;
- mps_thr_t thread;
- mps_root_t root;
+mps_space_t space;
+static mycell *obj_table[MAXLDS];
+static mps_ld_t lds[MAXLDS];
 
+static void checklds(void) {
+ int i;
+
+ for (i=0; i < MAXLDS; i++) {
+  if (obj_table[i]->data.copycount != 0) {
+   asserts(mps_ld_isstale(lds[i], space, (mps_addr_t) obj_table[i]),
+    "%d isn't stale but should be", i);
+   if (ranint(4) == 0) {
+    obj_table[i]->data.copycount = 0;
+    mps_ld_reset(lds[i], space);
+    comment("reset %d", i);
+    mps_ld_add(lds[i], space, (mps_addr_t) obj_table[i]);
+   }
+  }
+ }
+}
+
+static void test(void) {
+ mps_pool_t poolmv, poolawl, poolamc;
+ mps_thr_t thread;
+ mps_root_t root0, root1, root2;
+
+ mps_addr_t p, q;
  mps_fmt_t format;
- mps_ap_t ap;
+ mps_ap_t apawl, apamc;
 
  mycell *a, *b, *c, *d, *e, *f, *g;
 
- int i;
- int j;
+ int i,j;
 
  RC;
 
@@ -33,72 +56,106 @@ static void test(void)
  cdie(mps_thread_reg(&thread, space), "register thread");
 
  cdie(
-  mps_root_create_reg(&root, space, MPS_RANK_AMBIG, 0, thread,
+  mps_root_create_table(&root0, space, MPS_RANK_AMBIG, 0, &exfmt_root, 1),
+  "create exfmt root");
+
+ cdie(
+  mps_root_create_table(&root2, space, MPS_RANK_EXACT, 0,
+                        (mps_addr_t *)obj_table, MAXLDS),
+  "create table root");
+
+ cdie(
+  mps_root_create_reg(&root1, space, MPS_RANK_AMBIG, 0, thread,
    mps_stack_scan_ambig, stackpointer, 0),
-  "create root");
+  "create register and stack root");
 
  cdie(
   mps_fmt_create_A(&format, space, &fmtA),
   "create format");
 
  cdie(
-  mps_pool_create(&pool, space, mps_class_awl(), format),
-  "create pool");
+  mps_pool_create(&poolawl, space, mps_class_awl(), format),
+  "create awl pool");
 
  cdie(
-  mps_ap_create(&ap, pool, MPS_RANK_EXACT),
+  mps_pool_create(&poolmv, space, mps_class_mv(), 0x4000, 128, 0x4000),
+  "create mv pool");
+
+ cdie(
+  mps_ap_create(&apawl, poolawl, MPS_RANK_EXACT),
   "create ap");
 
- for (j=1; j<100; j++)
- {
-  comment("%i of 100.", j);
-  UC;
-  a = allocone(ap, 5, 1);
-  b = a;
-  c = a;
-  d = a;
-  e = a;
-  f = a;
-  g = a;
+ /* first we'll use only pool classes MV and AWL. So LDs shouldn't
+    go stale at all.
+ */
 
-  for (i=1; i<1000; i++)
-  {
-  UC;
-   c = allocone(ap, 1000, 1);
-   if (ranint(8) == 0) d = c;
-   if (ranint(8) == 0) e = c;
-   if (ranint(8) == 0) f = c;
-   if (ranint(8) == 0) g = c;
-  UC;
-   setref(b, 0, c);
-  UC;
-   setref(c, 1, d);
-  UC;
-   setref(c, 2, e);
-  UC;
-   setref(c, 3, f);
-  UC;
-   setref(c, 4, g);
-  UC;
-   b = c;
+ b = allocone(apawl, 5, MPS_RANK_EXACT);
+
+ for (i=0; i < MAXLDS; i++) {
+  comment("%d", i);
+  mps_alloc(&p, poolmv, sizeof(mps_ld_s));
+  a = allocone(apawl, 5, MPS_RANK_EXACT);
+  setref(a, 0, b);
+  b = a;
+  a = allocdumb(apawl, 256*1024, MPS_RANK_EXACT);
+  comment("alloc");
+  lds[i] = p;
+  mps_ld_reset(lds[i], space);
+  comment("reset");
+  if (i>0) {
+   mps_ld_add(lds[i], space, (mps_addr_t) b);
+  }
+  comment("add");
+ }
+
+ for (i=0; i < MAXLDS; i++) {
+  comment("%d", i);
+  asserts(mps_ld_isstale(lds[i], space, p) == 0,
+          "%d stale but shouldn't be", i);
+ }
+
+ cdie(
+  mps_pool_create(&poolamc, space, mps_class_amc(), format),
+  "create amc pool");
+
+ cdie(
+  mps_ap_create(&apamc, poolamc, MPS_RANK_EXACT),
+  "create ap");
+
+/* allocate MAXLDS objects, and make each LD depend on the corresponding
+   object
+*/
+
+ for (i=0; i < MAXLDS; i++) {
+  comment("%d", i);
+  obj_table[i] = allocone(apamc, ranint(100), MPS_RANK_EXACT);
+  mps_ld_add(lds[i], space, (mps_addr_t) obj_table[i]);
+ }
+
+ for (i=0; i < 1000; i++) {
+  comment("%d of 1000", i);
+  checklds();
+  for (j=0; j < 8; j++) {
+   a = allocdumb(apamc, 32*1024, MPS_RANK_EXACT);
   }
  }
- DC;
- DMC;
 
- checkfrom(a);
+ mps_ap_destroy(apawl);
+ mps_ap_destroy(apamc);
+ comment("Destroyed aps.");
 
- mps_ap_destroy(ap);
- comment("Destroyed ap.");
-
- mps_pool_destroy(pool);
- comment("Destroyed pool.");
+ mps_pool_destroy(poolmv);
+ mps_pool_destroy(poolamc);
+ mps_pool_destroy(poolawl);
+ comment("Destroyed pools.");
 
  mps_fmt_destroy(format);
  comment("Destroyed format.");
 
- mps_root_destroy(root);
- comment("Destroyed root.");
+ mps_root_destroy(root0);
+ mps_root_destroy(root1);
+ mps_root_destroy(root2);
+ comment("Destroyed roots.");
 
  mps_thread_dereg(thread);
  comment("Deregistered thread.");
