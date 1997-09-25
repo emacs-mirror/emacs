d69cdef8cc0ecc7e1ce045d2e892dca50f11792b
Pekka Pirinen <pekka@harlequin.com>
1997-09-25 21:02:05 +0100

    Note unused parameters
    Copied from Perforce
     Change: 18841
     ServerID: perforce.ravenbrook.com

--- mps/src/mpsiw3.c
+++ mps/src/mpsiw3.c
@@ -2,16 +2,17 @@
  *
  *       WIN32 MEMORY POOL SYSTEM INTERFACE LAYER EXTRAS
  *
- *  $HopeName: MMsrc!mpsint.c(MMdevel_restr.2) $
+ *  $HopeName: MMsrc!mpsiw3.c(trunk.3) $
  *
- *  Copyright (C) 1996 Harlequin Group, all rights reserved
+ *  Copyright (C) 1996, 1997 Harlequin Group, all rights reserved
  */
 
 #include "mpm.h"
 #include "mps.h"
-#include <windows.h>
 
-SRCID(mpsint, "$HopeName: MMsrc!mpsint.c(MMdevel_restr.2) $");
+#include "mpswin.h"
+
+SRCID(mpsint, "$HopeName: MMsrc!mpsiw3.c(trunk.3) $");
 
 /* This is defined in protnt.c */
 extern LONG ProtSEHfilter(LPEXCEPTION_POINTERS info);
@@ -26,5 +27,6 @@ LONG mps_SEH_filter(LPEXCEPTION_POINTERS info,
 
 void mps_SEH_handler(void *p, size_t s)
 {
+  UNUSED(p); UNUSED(s);
   NOTREACHED;
 }
