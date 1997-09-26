51da15d56fce6dcbfb317335963ddfdecbb03b77
Richard Tucker <rit@harlequin.com>
1997-09-26 15:04:45 +0100

    Add grotesque interface version (arena instead of space)
    Copied from Perforce
     Change: 18847
     ServerID: perforce.ravenbrook.com

--- mps/qa/test/testlib/versind.h
+++ mps/qa/test/testlib/versind.h
@@ -5,9 +5,16 @@
    valid versions are:
     MO : modern -- i.e. as in thursday afternoon
     OS : oldstyle -- dylan.incr.patch.11
+    GR : grotesque -- dylan.honeybee (space -> arena)
 */
+#if defined(MMQA_VERS_GR)
 
-#if defined(MMQA_VERS_MO)
+#include "grotesq.h"
+
+#elif defined(MMQA_VERS_MO)
+
+#include "modern.h"
+
 #elif defined(MMQA_VERS_OS)
 
 #include "oldstyle.h"
