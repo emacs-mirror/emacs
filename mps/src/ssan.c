4fe7fcd8802a0d8ec13fc0be6342787d695b64e4
Pekka Pirinen <pekka@harlequin.com>
1997-10-24 18:52:53 +0100

    Suppress some warnings
    Copied from Perforce
     Change: 18914
     ServerID: perforce.ravenbrook.com

--- mps/src/ssan.c
+++ mps/src/ssan.c
@@ -1,6 +1,6 @@
 /* impl.c.ssan: ANSI STACK SCANNER
  *
- * $HopeName$
+ * $HopeName: MMsrc!ssan.c(trunk.1) $
  *
  * This module provides zero functionality.  It exists to feed the
  * linker (prevent linker errors).
@@ -9,7 +9,9 @@
 #include "mpm.h"
 #include "ss.h"
 
+
 Res StackScan(ScanState ss, Addr *stackBot)
 {
+  UNUSED(ss); UNUSED(stackBot);
   return ResUNIMPL;
 }
