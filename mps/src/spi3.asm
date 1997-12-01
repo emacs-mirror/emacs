1356f1bfb75a5f9e45569f862eded64b24418217
David Jones <drj@ravenbrook.com>
1997-12-01 15:48:21 +0000

    New unit
    stack probe for intel
    
    Copied from Perforce
     Change: 19031
     ServerID: perforce.ravenbrook.com

---
+++ mps/src/spi3.asm
@@ -0,0 +1,30 @@
+;  impl.asm.spi3
+;
+;                    STACK PROBE
+;
+;  $HopeName$
+;
+;  Copyright (C) 1997 Harlequin Group PLC, all rights reserved
+;
+;  This function reads a location that is probeDepth words beyond
+;  the current stack pointer.  On intel platforms the stack grows
+;  downwards so this means reading from a location with a lesser address.
+;
+;  The registers edi, esi, ebx are the registers defined to be preserved
+;  across function calls, so we do not use those.
+
+.386
+.model flat
+.code
+
+_StackProbe proc public ; (Word probeDepth)
+  push ebp              ; frame pointer
+  mov  ebp,esp
+  mov  eax, [ebp+08]
+  neg  eax
+  mov  eax, [esp+eax*4]   ; do the actual probe
+  leave
+  ret                   ; return
+_StackProbe endp
+
+end
