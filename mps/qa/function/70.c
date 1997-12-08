32440839034b8b4b9a6da65cd367481af42eab79
Richard Tucker <rit@harlequin.com>
1997-12-08 17:28:52 +0000

    New unit
    new test
    
    Copied from Perforce
     Change: 19072
     ServerID: perforce.ravenbrook.com

---
+++ mps/qa/function/70.c
@@ -0,0 +1,35 @@
+/* TEST_HEADER
+ summary = create a space and see how much mem is taken
+ language = c
+ link = testlib.o
+END_HEADER
+*/
+
+#include "testlib.h"
+
+static void test(void)
+{
+ mps_space_t space;
+
+ comment("Entered trampoline");
+ pause(10);
+ cdie(mps_space_create(&space), "create space");
+ comment("Created space");
+ report("committed", "%ul", mps_arena_committed(space));
+ report("reserved",  "%ul", mps_arena_reserved(space));
+ pause(10);
+ mps_space_destroy(space);
+ comment("Destroyed space");
+ pause(10);
+}
+
+int main(void)
+{
+ comment("Started");
+ pause(10);
+ easy_tramp(test);
+ comment("Left trampoline");
+ pause(10);
+ pass();
+ return 0;
+}
