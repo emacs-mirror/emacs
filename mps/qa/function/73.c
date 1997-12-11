08fe643258fa64dd77a11404483c9830a1d1db9f
Richard Tucker <rit@harlequin.com>
1997-12-11 12:52:11 +0000

    New unit
    new test
    
    Copied from Perforce
     Change: 19095
     ServerID: perforce.ravenbrook.com

---
+++ mps/qa/function/73.c
@@ -0,0 +1,90 @@
+/* TEST_HEADER
+ summary = AWL pool should get collected (request.dylan.170322)
+ language = c
+ link = testlib.o rankfmt.o
+END_HEADER
+*/
+
+#include "testlib.h"
+#include "mpscawl.h"
+#include "rankfmt.h"
+
+void *stackpointer;
+
+mps_space_t space;
+
+static void test(void) {
+ mps_pool_t poolawl;
+ mps_thr_t thread;
+ mps_root_t root0, root1;
+
+ mps_fmt_t format;
+ mps_ap_t apawl;
+
+ mycell *a;
+
+ long int j;
+
+ cdie(mps_space_create(&space), "create space");
+
+ cdie(mps_thread_reg(&thread, space), "register thread");
+
+ cdie(
+  mps_root_create_reg(&root0, space, MPS_RANK_AMBIG, 0, thread,
+   mps_stack_scan_ambig, stackpointer, 0),
+  "create root");
+ 
+ cdie(
+  mps_root_create_table(&root1, space, MPS_RANK_AMBIG, 0, &exfmt_root, 1),
+  "create table root");
+
+ cdie(
+  mps_fmt_create_A(&format, space, &fmtA),
+  "create format");
+
+ cdie(
+  mps_pool_create(&poolawl, space, mps_class_awl(), format),
+  "create pool");
+
+ cdie(
+  mps_ap_create(&apawl, poolawl, MPS_RANK_EXACT),
+  "create ap");
+
+/* alloc lots in an AWL pool; it should be collected away */
+
+ for(j=0; j<1000; j++) {
+  a = allocdumb(apawl, 1024ul*1024, MPS_RANK_EXACT);
+ }
+
+/* (total allocated is 1000 M) */
+
+ mps_root_destroy(root0);
+ mps_root_destroy(root1);
+ comment("Destroyed roots.");
+
+ mps_ap_destroy(apawl);
+ comment("Destroyed ap.");
+
+ mps_pool_destroy(poolawl);
+ comment("Destroyed pool.");
+
+ mps_fmt_destroy(format);
+ comment("Destroyed format.");
+
+ mps_thread_dereg(thread);
+ comment("Deregistered thread.");
+
+ mps_space_destroy(space);
+ comment("Destroyed space.");
+
+ pass();
+
+}
+
+int main(void) {
+ void *m;
+ stackpointer=&m; /* hack to get stack pointer */
+
+ easy_tramp(test);
+ return 0;
+}
