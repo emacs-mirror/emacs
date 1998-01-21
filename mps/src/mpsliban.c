4f719071960b39838f81c2399c679e200759f782
Gavin Matthews <grm@ravenbrook.com>
1998-01-21 12:33:29 +0000

    Change.dylan.170473; don't cast null
    Copied from Perforce
     Change: 19250
     ServerID: perforce.ravenbrook.com

--- mps/src/mpsliban.c
+++ mps/src/mpsliban.c
@@ -1,6 +1,6 @@
 /* impl.c.mpsliban: HARLEQUIN MEMORY POOL SYSTEM LIBRARY INTERFACE (ANSI)
  *
- * $HopeName: MMsrc!mpsliban.c(trunk.6) $
+ * $HopeName: MMsrc!mpsliban.c(trunk.7) $
  * Copyright (C) 1996,1997 Harlequin Group, all rights reserved.
  *
  * PURPOSE
@@ -93,10 +93,11 @@ mps_clock_t mps_clock(void)
 unsigned long mps_lib_telemetry_control(void)
 {
   char *s;
+  char **null = NULL;
 
   s = getenv("MPS_TELEMETRY_CONTROL");
   if(s != NULL)
-    return strtoul(s, (char **)NULL, 0);
+    return strtoul(s, null, 0);
   else
     return 0;
 }
