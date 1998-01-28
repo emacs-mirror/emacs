bb70efdb904fcebb5b7a4b2c1f0473bd90f7951f
David Jones <drj@ravenbrook.com>
1998-01-28 16:39:03 +0000

    Adding walker
    Copied from Perforce
     Change: 19283
     ServerID: perforce.ravenbrook.com

--- mps/src/poolams.c
+++ mps/src/poolams.c
@@ -1,6 +1,6 @@
 /* impl.c.poolams: AUTOMATIC MARK & SWEEP POOL CLASS
  *
- * $HopeName: MMsrc!poolams.c(trunk.15) $
+ * $HopeName: MMsrc!poolams.c(trunk.16) $
  * Copyright (C) 1997, 1998 The Harlequin Group Limited.  All rights reserved.
  * 
  * .readership: any MPS developer.
@@ -17,7 +17,7 @@
 #include "mpm.h"
 #include "mpscams.h"
 
-SRCID(poolams, "$HopeName: MMsrc!poolams.c(trunk.15) $");
+SRCID(poolams, "$HopeName: MMsrc!poolams.c(trunk.16) $");
 
 
 #define AMSSig          ((Sig)0x519A3599) /* SIGnature AMS */
@@ -1259,6 +1259,7 @@ static PoolClassStruct PoolClassAMSStruct = {
   AMSReclaim,                /* reclaim */
   AMSBenefit,                /* benefit */
   PoolCollectAct,            /* act */
+  PoolNoWalk,                /* implement this @@@@ */
   AMSDescribe,               /* describe */
   PoolClassSig               /* impl.h.mpm.class.end-sig */
 };
