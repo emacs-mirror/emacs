6ba4127bfa3220c747ae5c665c765802897a63aa
David Jones <drj@ravenbrook.com>
1998-04-16 16:25:34 +0100

    New unit
    vanilla protection mutator context module
    
    Copied from Perforce
     Change: 19412
     ServerID: perforce.ravenbrook.com

---
+++ mps/src/prmcan.c
@@ -0,0 +1,37 @@
+/* impl.c.prmcan: PROTECTION MUTATOR CONTEXT (ANSI)
+ *
+ * $HopeName$
+ * Copyright (C) 1998. Harlequin Group plc. All rights reserved.
+ *
+ * READERSHIP
+ *
+ * .readership: Any MPS developer.
+ *
+ * DESIGN
+ *
+ * .design: See design.mps.prot for the generic design of the interface
+ * which is implemented in this module including the contracts for the
+ * functions.
+ *
+ * PURPOSE
+ *
+ * .purpose: This module implements the part of the protection module
+ * that implements the MutatorFaultContext type.  In this ANSI version
+ * none of the functions have a useful implementation.
+ */
+
+#include "mpm.h"
+
+Bool ProtCanStepInstruction(MutatorFaultContext context)
+{
+  UNUSED(context);
+
+  return FALSE;
+}
+
+Res ProtStepInstruction(MutatorFaultContext context)
+{
+  UNUSED(context);
+
+  return ResUNIMPL;
+}
