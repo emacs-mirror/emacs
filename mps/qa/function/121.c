875d7ad0e6457dd8c3b165fd2940317dbbd09041
Richard Tucker <rit@harlequin.com>
1998-06-26 10:27:52 +0100

    New unit
    new test
    
    Copied from Perforce
     Change: 19718
     ServerID: perforce.ravenbrook.com

---
+++ mps/qa/function/121.c
@@ -0,0 +1,43 @@
+/* $HopeName: MMQA_test_function!119.c(trunk.1) $
+TEST_HEADER
+ summary = very small arenas
+ language = c
+ link = testlib.o
+OUTPUT_SPEC
+ result = pass
+END_HEADER
+*/
+
+#include "testlib.h"
+#include "mpsavm.h"
+#include "mpscmv.h"
+
+void *stackpointer;
+
+mps_arena_t arena;
+mps_thr_t thread;
+mps_pool_t pool;
+mps_pool_t pools[100];
+
+static void test(void) {
+
+ int i;
+ for (i=64; i>0; i--) {
+  comment("Trying arena of %dKB.", i);
+  cdie(mps_arena_create(&arena, mps_arena_class_vm(), (size_t) (1024*i)),
+   "create arena");
+  cdie(mps_thread_reg(&thread, arena), "register thread");
+  mps_thread_dereg(thread);
+  mps_arena_destroy(arena);
+ }
+}
+
+int main(void)
+{
+ void *m;
+ stackpointer=&m; /* hack to get stack pointer */
+
+ easy_tramp(test);
+ pass();
+ return 0;
+}
