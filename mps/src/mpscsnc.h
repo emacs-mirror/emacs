c3edb052ba521c3c80e4c642f52644fcf4df8799
Tony Mann <tony@harlequin.com>
1998-10-16 09:21:27 +0100

    New unit
    change.dylan.jackdaw.160122: add stack pool
    
    Copied from Perforce
     Change: 20249
     ServerID: perforce.ravenbrook.com

---
+++ mps/src/mpscsnc.h
@@ -0,0 +1,14 @@
+/* impl.h.mpscasnc: MEMORY POOL SYSTEM CLASS "SNC"
+ *
+ * $HopeName: MMsrc!mpscsnc.h(trunk.1) $
+ * Copyright (C) 1997 The Harlequin Group.  All rights reserved.
+ */
+
+#ifndef mpscsnc_h
+#define mpscsnc_h
+
+#include "mps.h"
+
+extern mps_class_t mps_class_snc(void);
+
+#endif /* mpscsnc_h */
