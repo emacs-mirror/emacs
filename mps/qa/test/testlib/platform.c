fb6b2917ec48838509bcf1529e7660f0c7bd8c8c
Richard Tucker <rit@harlequin.com>
1998-10-26 15:21:45 +0000

    Catch memory access errors on w3 (with seh)
    Copied from Perforce
     Change: 20281
     ServerID: perforce.ravenbrook.com

--- mps/qa/test/testlib/platform.c
+++ mps/qa/test/testlib/platform.c
@@ -1,8 +1,9 @@
-/* $HopeName$
+/* $HopeName: MMQA_harness!testlib:platform.c(trunk.2) $
 */
 
 #include "mps.h"
 #include "platform.h"
+#include "testlib.h"
 
 #ifdef MPS_OS_SU
 
@@ -10,6 +11,34 @@ void *memmove(void *to, void *from, size_t bytes)
 {
  bcopy((char *)from, (char *)to, (int)bytes);
  return to;
+}
+
+#endif
+
+#ifdef MPS_OS_W3
+
+LONG mySEHFilter(LPEXCEPTION_POINTERS info) {
+ LPEXCEPTION_RECORD er;
+ int write;
+ unsigned long address;
+
+ er = info->ExceptionRecord;
+
+ if (er->ExceptionCode == EXCEPTION_ACCESS_VIOLATION) {
+  write = er->ExceptionInformation[0];
+  address = er->ExceptionInformation[1];
+  report("memoryerror", "true");
+  if (write == 1) {
+   report("memoryop", "write");
+  } else { /* write == 0 */
+   report("memoryop", "read");
+  }
+  report("memoryaddr", "%ld", address);
+  myabort();
+ }
+
+ /* otherwise don't interfere */
+ return EXCEPTION_CONTINUE_SEARCH;
 }
 
 #endif
