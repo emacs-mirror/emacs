5b0ba8c57bb8b0bbf8967e6c8d4cf4e360e36193
Pekka Pirinen <pekka@harlequin.com>
1998-10-29 13:53:48 +0000

    Mps_telemetry_flush
    Copied from Perforce
     Change: 20316
     ServerID: perforce.ravenbrook.com

--- mps/src/mpsi.c
+++ mps/src/mpsi.c
@@ -1,6 +1,6 @@
 /* impl.c.mpsi: MEMORY POOL SYSTEM C INTERFACE LAYER
  *
- * $HopeName: MMsrc!mpsi.c(trunk.61) $
+ * $HopeName: MMsrc!mpsi.c(trunk.62) $
  * Copyright (C) 1997. Harlequin Group plc. All rights reserved.
  *
  * .purpose: This code bridges between the MPS interface to C,
@@ -52,7 +52,7 @@
 #include "mps.h"
 #include "mpsavm.h" /* only for mps_space_create */
 
-SRCID(mpsi, "$HopeName: MMsrc!mpsi.c(trunk.61) $");
+SRCID(mpsi, "$HopeName: MMsrc!mpsi.c(trunk.62) $");
 
 
 /* mpsi_check -- check consistency of interface mappings
@@ -1463,6 +1463,12 @@ mps_word_t mps_telemetry_intern(const char *label)
 void mps_telemetry_label(mps_addr_t addr, mps_word_t intern_id)
 {
   EventLabelAddr((Addr)addr, (Word)intern_id);
+}
+
+void mps_telemetry_flush(void)
+{
+  /* Telemetry does its own concurrency control, so none here. */
+  (void)EventSync();
 }
 
 
