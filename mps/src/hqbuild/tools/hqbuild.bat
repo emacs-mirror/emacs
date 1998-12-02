9b9a5eaff8f338fe7d05ff1bb89fb9dce909cbb4
Pekka Pirinen <pekka@harlequin.com>
1998-12-02 15:20:18 +0000

    Update vars for 5.0
    Copied from Perforce
     Change: 20379
     ServerID: perforce.ravenbrook.com

--- mps/src/hqbuild/tools/hqbuild.bat
+++ mps/src/hqbuild/tools/hqbuild.bat
@@ -1,12 +1,17 @@
-@REM impl.bat.hqbuild
-@REM $HopeName: MMsrc!hqbuild:tools:hqbuild.bat(trunk.3) $
-@REM Copyright (C) 1996,1997 Harlequin Group, all rights reserved
+@REM impl.bat.hqbuild:
+@REM $HopeName: MMsrc!hqbuild:tools:hqbuild.bat(trunk.4) $
+@REM Copyright (C) 1996-1998 Harlequin Group plc.  All rights reserved.
 @REM Called by SWIG autobuild system
 @
-@REM we expect whatcom to have set MSVCNT and possibly MSMASM
+@REM we expect whatcom to have set MSVCNT and possibly MSMASM and MSTOOLS
 IF NOT %MSMASM%X == X SET PATH=%MSMASM%\bin;%PATH%
-SET PATH=%MSVCNT%\bin;%PATH%
-SET LIB=%MSVCNT%\lib
-SET INCLUDE=%MSVCNT%\include
+SET PATH=%MSVCNT%\..\sharedide\bin\ide;%MSVCNT%\..\sharedide\bin;%MSVCNT%\bin;%PATH%
+SET INCLUDE=%MSVCNT%\include;%MSVCNT%\mfc\include;%INCLUDE%
+SET LIB=%MSVCNT%\lib;%MSVCNT%\mfc\lib;%LIB%
+IF %MSTOOLS%X == X GOTO nomstools
+SET PATH=%MSTOOLS%\bin;%PATH%
+SET INCLUDE=%MSTOOLS%\include;%INCLUDE%
+SET LIB=%MSTOOLS%\lib;%LIB%
+:nomstools
 @REM First argument is expected to be platform code, rest we pass on
 nmake /f %1.nmk %2 %3 %4 %5 %6 %7 %8 %9
