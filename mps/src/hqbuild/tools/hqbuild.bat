653a0cfa48d3b94308767c749a4e95b0b47ecf20
Pekka Pirinen <pekka@harlequin.com>
1998-12-02 17:28:02 +0000

    Remove the obsolete mstools stuff
    Copied from Perforce
     Change: 20380
     ServerID: perforce.ravenbrook.com

--- mps/src/hqbuild/tools/hqbuild.bat
+++ mps/src/hqbuild/tools/hqbuild.bat
@@ -1,5 +1,5 @@
-@REM impl.bat.hqbuild:
-@REM $HopeName: MMsrc!hqbuild:tools:hqbuild.bat(trunk.4) $
+@REM impl.bat.hqbuild: setup for SWIG autobuild system
+@REM $HopeName: MMsrc!hqbuild:tools:hqbuild.bat(trunk.5) $
 @REM Copyright (C) 1996-1998 Harlequin Group plc.  All rights reserved.
 @REM Called by SWIG autobuild system
 @
@@ -8,10 +8,5 @@ IF NOT %MSMASM%X == X SET PATH=%MSMASM%\bin;%PATH%
 SET PATH=%MSVCNT%\..\sharedide\bin\ide;%MSVCNT%\..\sharedide\bin;%MSVCNT%\bin;%PATH%
 SET INCLUDE=%MSVCNT%\include;%MSVCNT%\mfc\include;%INCLUDE%
 SET LIB=%MSVCNT%\lib;%MSVCNT%\mfc\lib;%LIB%
-IF %MSTOOLS%X == X GOTO nomstools
-SET PATH=%MSTOOLS%\bin;%PATH%
-SET INCLUDE=%MSTOOLS%\include;%INCLUDE%
-SET LIB=%MSTOOLS%\lib;%LIB%
-:nomstools
 @REM First argument is expected to be platform code, rest we pass on
 nmake /f %1.nmk %2 %3 %4 %5 %6 %7 %8 %9
