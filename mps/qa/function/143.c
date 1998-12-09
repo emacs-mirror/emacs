67becfaba9917342cf89aa5a1e40f238344f483f
Richard Tucker <rit@harlequin.com>
1998-12-09 11:27:01 +0000

    Change specified assertcond
    (has changed to textual message)
    
    Copied from Perforce
     Change: 20400
     ServerID: perforce.ravenbrook.com

--- mps/qa/function/143.c
+++ mps/qa/function/143.c
@@ -1,13 +1,12 @@
 /* 
 TEST_HEADER
- id = $HopeName$
+ id = $HopeName: MMQA_test_function!143.c(trunk.2) $
  summary = EPDR fenceposting check: free
  language = c
  link = testlib.o
 OUTPUT_SPEC
  assert = true
- assertfile = .\dbgpool.c
- assertcond = FenceCheck(debug, pool, old, size)
+ assertcond = fencepost check on free
 END_HEADER
 */
 
