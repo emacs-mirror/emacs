724a8d6825902e54205dfdca1f25fb3cc6261d50
Pekka Pirinen <pekka@harlequin.com>
1999-01-14 20:44:36 +0000

    New unit
    Easier to include these SW stubs in mmsw, if they're here
    
    Copied from Perforce
     Change: 20472
     ServerID: perforce.ravenbrook.com

--- mps/src/arenavmx.c
+++ mps/src/arenavmx.c
@@ -1,43 +1,28 @@
 /* impl.c.arenavmx: STUB FOR ARENAVM
  *
- * $HopeName: MMsrc!arenavmx.c(trunk.1) $
- * Copyright (C) 1998 Harlequin Group plc.  All rights reserved.
+ * $HopeName$
+ * Copyright (C) 1999 Harlequin Group plc.  All rights reserved.
  *
- * This is a stub-file, providing stub-functions to take the place of 
- * functions exported from impl.c.arenavm.  Calling a stub-function 
- * causes a run-time assertion.
- *
- * DESIGN
- *
- * .design.interim:
- * mail.richardk.1998-04-17.14-52.1.req.unimpl.cli-interface.run-assert
- * See also impl.c.arenavm.
- * 
- * .design.filename: The suffix "x" on this filename is to designate it 
- * as containing stubs.  This is a bit lame...
- *
- *
- * IMPROVEMENTS
- *
- * .improve.reaction.ifdef: Clients might want one of a range of 
- * reactions to calling a stub-function 
- * (mail.richardk.1998-04-17.14-52.1.req.unimpl.cli-interface).  
- * This variation could be controlled by a config.h define.
+ * .purpose: This file provides stub functions to take the place of VM
+ * arena functions on platforms where it doesn't exist.  Calling a stub
+ * function causes a run-time assertion.  This file will be included in
+ * SW libraries to allow linking against these symbols (don't ask).
  */
 
 
 #include "mpm.h"
 #include "mpsavm.h"
 
-SRCID(arenavmx, "$HopeName: MMsrc!arenavmx.c(trunk.1) $");
+SRCID(arenavmx, "$HopeName$");
 
-/* mps_arena_class_vm -- return the arena class VM
- * Run-time asserting stub-function.
- * The purpose is to allow links against this symbol to succeed.
- * Don't actually call it -- it will cause a run-time assert.
- */
 
 mps_arena_class_t mps_arena_class_vm(void)
+{
+  NOTREACHED;
+  return (mps_arena_class_t)NULL;
+}
+
+mps_arena_class_t mps_arena_class_vmnz(void)
 {
   NOTREACHED;
   return (mps_arena_class_t)NULL;
