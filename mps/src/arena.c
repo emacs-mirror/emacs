ca03a142aa710dd9f260e72827cdfe9262845859
David Jones <drj@ravenbrook.com>
1999-02-03 11:44:01 +0000

    Bug: setting spare commit limit to 0, change.epcore.chub.160196, merging from brisling
    Copied from Perforce
     Change: 20543
     ServerID: perforce.ravenbrook.com

--- mps/src/arena.c
+++ mps/src/arena.c
@@ -1,6 +1,6 @@
 /* impl.c.arena: ARENA IMPLEMENTATION
  *
- * $HopeName: MMsrc!arena.c(MMdevel_drj_arena_hysteresis.3) $
+ * $HopeName: MMsrc!arena.c(trunk.61) $
  * Copyright (C) 1998. Harlequin Group plc. All rights reserved.
  *
  * .readership: Any MPS developer
@@ -36,7 +36,7 @@
 #include "poolmrg.h"
 #include "mps.h"
 
-SRCID(arena, "$HopeName: MMsrc!arena.c(MMdevel_drj_arena_hysteresis.3) $");
+SRCID(arena, "$HopeName: MMsrc!arena.c(trunk.61) $");
 
 
 /* Forward declarations */
@@ -1394,7 +1394,7 @@ Size ArenaSpareCommitLimit(Arena arena)
 void ArenaSetSpareCommitLimit(Arena arena, Size limit)
 {
   AVERT(Arena, arena);
-  AVER(0 < limit);
+  /* Can't check limit, as all possible values are allowed. */
 
   arena->spareCommitLimit = limit;
   if(arena->spareCommitLimit < arena->spareCommitted) {
