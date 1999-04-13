cf6057a4a7af119c37749ae4e2ccadfc1be04dcf
Pekka Pirinen <pekka@harlequin.com>
1999-04-13 13:43:11 +0100

    Add more explanation
    Copied from Perforce
     Change: 20717
     ServerID: perforce.ravenbrook.com

--- mps/src/arenavmx.c
+++ mps/src/arenavmx.c
@@ -1,19 +1,22 @@
-/* impl.c.arenavmx: STUB FOR ARENAVM
+/* impl.c.arenavmx: STUBS FOR ARENAVM
  *
- * $HopeName$
+ * $HopeName: MMsrc!arenavmx.c(trunk.3) $
  * Copyright (C) 1999 Harlequin Group plc.  All rights reserved.
  *
- * .purpose: This file provides stub functions to take the place of VM
- * arena functions on platforms where it doesn't exist.  Calling a stub
- * function causes a run-time assertion.  This file will be included in
- * SW libraries to allow linking against these symbols (don't ask).
+ * .purpose: This file is not properly part of the MPS.  It is a
+ * convenience file for EP-Core who do _not_ wish to get a link error,
+ * when they link to a VM arena function on a platform where it isn't
+ * supported (see req.epcore.link.no-error).
+ *
+ * .stub: This file provides stub functions for the VM arena class
+ * functions.  Calling any of them causes a run-time assertion.
  */
 
 
 #include "mpm.h"
 #include "mpsavm.h"
 
-SRCID(arenavmx, "$HopeName$");
+SRCID(arenavmx, "$HopeName: MMsrc!arenavmx.c(trunk.3) $");
 
 
 mps_arena_class_t mps_arena_class_vm(void)
