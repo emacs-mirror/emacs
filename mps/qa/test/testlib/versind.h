453ffbe1bf7713b83c87e627c65970a81ae1c68d
Richard Tucker <rit@harlequin.com>
1999-05-04 15:28:21 +0100

    Use auto-detect symbols instead of interface_version
    Copied from Perforce
     Change: 20774
     ServerID: perforce.ravenbrook.com

--- mps/qa/test/testlib/versind.h
+++ mps/qa/test/testlib/versind.h
@@ -1,40 +1,86 @@
-/* $HopeName: MMQA_harness!testlib:versind.h(trunk.4) $
+/* $HopeName: MMQA_harness!testlib:versind.h(trunk.5) $
 versind.h
-   lead appropriate header files to do version-specific
-   interface things
+   This file used to read header files depending on the
+   value of the MPS_INTERFACE_VERSION parameter (aka MMQA_VERS_XX).
+   Now it works using MMQA_SYMBOL_xxx tests.
 
-   valid versions are:
+   Almost all the existing functionality can be done just as well
+   this way. The only difference is the change in parameters
+   between 'oldstyle' and 'modern', which is no longer relevant.
+   We don't need to test oldstyle MMs any more.
+
+   For the record, the old versions were:
     MO : modern -- i.e. as in thursday afternoon
     OS : oldstyle -- dylan.incr.patch.11
     GR : grotesque -- dylan.honeybee (space -> arena)
     BQ : baroque -- dylan.meadowlark (have to include mpsw3.h)
     HU : humanist -- dylan.kinglet (spare committed fund)
-
-  preXX.h makes new tests run with MMs before XX
-  postXX.h makes old tests run with MMs from XX onwards
-
-  Each postXX.h begins by including the previous one.
-  Each preXX.h ends by including the next one.
 */
 
-#if defined(MMQA_VERS_OS)
-#include "preMO.h"
+/* In release.dylan.honeybee, space was renamed arena in lots of functions
+   We'll use the "destroy" functions to test which versions are available
+*/
 
-#elif defined(MMQA_VERS_MO)
-#include "preGR.h"
-#include "postMO.h"
-
-#elif defined(MMQA_VERS_GR)
-#include "preBQ.h"
-#include "postGR.h"
-
-#elif defined(MMQA_VERS_BQ)
-#include "preHU.h"
-#include "postBQ.h"
-
+#ifdef MMQA_DEFINED_mps_arena_destroy
+#ifdef MMQA_DEFINED_mps_space_destroy
+/* bizarrely, both versions seem to be present! */
 #else
-#include "postHU.h"
+/* need to define "space" variants */
+#define mps_space_clamp(a)     mps_arena_clamp(a)
+#define mps_space_release(a)   mps_arena_release(a)
+#define mps_space_park(a)      mps_arena_park(a)
+#define mps_space_collect(a)   mps_arena_collect(a)
+#define mps_space_destroy(a)   mps_arena_destroy(a)
+#define mps_space_reserved(a)  mps_arena_reserved(a)
+#define mps_space_committed(a) mps_arena_committed(a)
 
 #endif
+#else
+#ifdef MMQA_DEFINED_mps_space_destroy
+/* need to define "arena" variants */
+#define mps_arena_clamp(a)     mps_space_clamp(a)
+#define mps_arena_release(a)   mps_space_release(a)
+#define mps_arena_park(a)      mps_space_park(a)
+#define mps_arena_collect(a)   mps_space_collect(a)
+#define mps_arena_destroy(a)   mps_space_destroy(a)
+#define mps_arena_reserved(a)  mps_space_reserved(a)
+#define mps_arena_committed(a) mps_space_committed(a)
+#else
+/* Neither exists -- error! */
+#error Could find either mps_arena_destroy or mps_space_destroy
+#endif
+#endif
 
+/* Before release.epcore.brisling / release.dylan.kinglet
+   there was no spare committed fund
+*/
+
+#ifndef MMQA_DEFINED_mps_arena_spare_committed
+#define mps_arena_spare_committed(a) ((size_t) 0)
+#endif
+
+#ifndef MMQA_DEFINED_mps_arena_space_commit_limit
+#define mps_arena_spare_commit_limit(a) ((size_t) 0)
+#endif
+
+#ifndef MMQA_DEFINED_mps_arena_spare_commit_limit_set
+#define mps_arena_spare_commit_limit_set(a, l) \
+ asserts(0, \
+ "MPS interface versions before HU do not support the spare committed fund")
+#endif
+
+/*
+  The following defs can't be done automatically. I don't think
+  this is a problem, but here they are just in case.
+
+  Before modern interface;
+   - no closure arguments for mps_root_create_reg
+   - no rank argument for mps_ap_create (ever)
+
+  #define mps_root_create_reg(ro, sp, ra, rm, thr, scan, p, _s) \
+   (mps_root_create_reg)(ro, sp, ra, rm, thr, scan, p)
+
+  #define mps_ap_create(a, p, _r) \
+   (mps_ap_create)(a, p)
+*/
 
