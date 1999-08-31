78063c35740b8533ffaa8875e24f8a2dcab17cba
Tony Mann <tony@harlequin.com>
1999-08-31 14:32:28 +0100

    Change.mps.dylan.kinglet.160272:
    Platform specific details of locks and threads appear in shared header files
    
    Copied from Perforce
     Change: 20903
     ServerID: perforce.ravenbrook.com

--- mps/src/lockcov.c
+++ mps/src/lockcov.c
@@ -1,30 +1,45 @@
 /* impl.c.lockcov
  *                     LOCK COVERAGE TEST
  *
- * $HopeName: MMsrc!lockcov.c(trunk.2) $
+ * $HopeName: MMsrc!lockcov.c(trunk.3) $
  */
 
 #include "mpm.h"
+#include <stdlib.h>             /* for malloc & free */
 
 int main(void){
-  LockStruct a,b;
+  Lock a = malloc(LockSize());
+  Lock b = malloc(LockSize());
 
-  LockInit(&a);
-  LockInit(&b);
-  LockClaim(&a);
-  LockClaimRecursive(&b);
-  LockReleaseMPM(&a);
-  LockClaimRecursive(&b);
-  LockFinish(&a);
-  LockReleaseRecursive(&b);
-  LockReleaseRecursive(&b);
-  LockFinish(&b);
-  LockInit(&a);
-  LockClaim(&a);
-  LockClaimRecursive(&a);
-  LockReleaseRecursive(&a);
-  LockReleaseMPM(&a);
-  LockFinish(&a);
+  AVER(a != NULL);
+  AVER(b != NULL);
+
+  LockInit(a);
+  LockInit(b);
+  LockClaimGlobal();
+  LockClaim(a);
+  LockClaimRecursive(b);
+  LockClaimGlobalRecursive();
+  LockReleaseGlobal();
+  LockClaimGlobal();
+  LockReleaseMPM(a);
+  LockClaimGlobalRecursive();
+  LockReleaseGlobal();
+  LockClaimRecursive(b);
+  LockFinish(a);
+  LockReleaseRecursive(b);
+  LockReleaseRecursive(b);
+  LockFinish(b);
+  LockInit(a);
+  LockClaim(a);
+  LockClaimRecursive(a);
+  LockReleaseGlobalRecursive();
+  LockReleaseRecursive(a);
+  LockReleaseMPM(a);
+  LockFinish(a);
+  LockReleaseGlobalRecursive();
+  free(a);
+  free(b);
 
   return 0;
 }
