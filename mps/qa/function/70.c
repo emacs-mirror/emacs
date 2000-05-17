84669568cc3a5536e64e7bd187798aa44f50943c
Pekka Pirinen <pekka@harlequin.com>
2000-05-17 14:59:45 +0100

    Rename pause to mmqa_pause
    Copied from Perforce
     Change: 21197
     ServerID: perforce.ravenbrook.com

--- mps/qa/function/70.c
+++ mps/qa/function/70.c
@@ -1,6 +1,6 @@
 /* 
 TEST_HEADER
- id = $HopeName$
+ id = $HopeName: MMQA_test_function!70.c(trunk.3) $
  summary = create a space and see how much mem is taken
  language = c
  link = testlib.o
@@ -9,29 +9,31 @@ END_HEADER
 
 #include "testlib.h"
 
+
 static void test(void)
 {
  mps_space_t space;
 
  comment("Entered trampoline");
- pause(10);
+ mmqa_pause(10);
  cdie(mps_space_create(&space), "create space");
  comment("Created space");
  report("committed", "%ul", mps_arena_committed(space));
  report("reserved",  "%ul", mps_arena_reserved(space));
- pause(10);
+ mmqa_pause(10);
  mps_space_destroy(space);
  comment("Destroyed space");
- pause(10);
+ mmqa_pause(10);
 }
+
 
 int main(void)
 {
  comment("Started");
- pause(10);
+ mmqa_pause(10);
  easy_tramp(test);
  comment("Left trampoline");
- pause(10);
+ mmqa_pause(10);
  pass();
  return 0;
 }
