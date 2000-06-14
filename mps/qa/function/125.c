71494360fc2241757c4181eaf02a5f98978e8c90
Pekka Pirinen <pekka@harlequin.com>
2000-06-14 18:10:06 +0100

    Add missing cast
    Copied from Perforce
     Change: 21274
     ServerID: perforce.ravenbrook.com

--- mps/qa/function/125.c
+++ mps/qa/function/125.c
@@ -1,6 +1,6 @@
 /* 
 TEST_HEADER
- id = $HopeName$
+ id = $HopeName: MMQA_test_function!125.c(trunk.1) $
  summary = extensible arena test (small objects)
  language = c
  link = testlib.o rankfmt.o
@@ -12,11 +12,13 @@ END_HEADER
 #include "mpsavm.h"
 #include "rankfmt.h"
 
+
 void *stackpointer;
+
 
 static void test(void)
 {
- mps_space_t space;
+ mps_arena_t arena;
  mps_pool_t pool;
  mps_thr_t thread;
  mps_root_t root, root1;
@@ -28,43 +30,39 @@ static void test(void)
 
  int j;
 
-/* create an arena that can't grow beyond 1 M */
+ /* create an arena that can't grow beyond 1 Mb */
 
- cdie(mps_arena_create(&space, mps_arena_class_vm(), (size_t) (1024*1024*1)),
-  "create arena");
+ cdie(mps_arena_create(&arena, mps_arena_class_vm(), (size_t) (1024*1024*1)),
+      "create arena");
 
- cdie(mps_thread_reg(&thread, space), "register thread");
+ cdie(mps_thread_reg(&thread, arena), "register thread");
 
- cdie(
-  mps_root_create_reg(&root, space, MPS_RANK_AMBIG, 0, thread,
-   mps_stack_scan_ambig, stackpointer, 0),
-  "create root");
+ cdie(mps_root_create_reg(&root, arena, MPS_RANK_AMBIG, 0, thread,
+                          mps_stack_scan_ambig, stackpointer, 0),
+      "create root");
 
- cdie(
-  mps_root_create_table(&root1,space,MPS_RANK_AMBIG,0,&exfmt_root,1),
-  "create table root");
+ cdie(mps_root_create_table(&root1, arena, MPS_RANK_AMBIG, 0,
+                            (mps_addr_t*)&exfmt_root, 1),
+      "create table root");
 
- cdie(
-  mps_fmt_create_A(&format, space, &fmtA),
-  "create format");
+ cdie(mps_fmt_create_A(&format, arena, &fmtA),
+      "create format");
 
- cdie(
-  mps_pool_create(&pool, space, mps_class_amc(), format),
-  "create pool");
+ cdie(mps_pool_create(&pool, arena, mps_class_amc(), format),
+      "create pool");
 
- cdie(
-  mps_ap_create(&ap, pool, MPS_RANK_EXACT),
-  "create ap");
+ cdie(mps_ap_create(&ap, pool, MPS_RANK_EXACT),
+      "create ap");
 
  comment("ready");
 
  b = allocone(ap, 2, MPS_RANK_EXACT);
 
-/* allocate lots of little objects */
+ /* allocate lots of little objects */
 
- while (mps_arena_committed(space) < 1024ul*1024ul*20) {
+ while (mps_arena_committed(arena) < 1024ul*1024ul*20) {
   comment("reserved %ld, committed %ld",
-   mps_arena_reserved(space), mps_arena_committed(space));
+   mps_arena_reserved(arena), mps_arena_committed(arena));
   for (j=0; j<10000; j++) {
    a = allocone(ap, 2, MPS_RANK_EXACT);
    setref(a, 0, b);
@@ -72,7 +70,7 @@ static void test(void)
    a = allocone(ap, 1, MPS_RANK_EXACT);
    setref(b, 1, a);
   }
-  mps_arena_collect(space);
+  mps_arena_collect(arena);
  }
 
  mps_ap_destroy(ap);
@@ -91,10 +89,10 @@ static void test(void)
  mps_thread_dereg(thread);
  comment("Deregistered thread.");
 
- mps_space_destroy(space);
- comment("Destroyed space.");
-
+ mps_arena_destroy(arena);
+ comment("Destroyed arena.");
 }
+
 
 int main(void)
 {
