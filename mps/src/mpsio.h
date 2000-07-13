62f956a38db14b45151ba6ae6ad6577366d049de
Pekka Pirinen <pekka@harlequin.com>
2000-07-13 19:40:18 +0100

    Update copyright (change.mps.configura.eval.170562); remove parameter names
    Copied from Perforce
     Change: 21332
     ServerID: perforce.ravenbrook.com

--- mps/src/mpsio.h
+++ mps/src/mpsio.h
@@ -1,14 +1,10 @@
 /* impl.h.mpsio: HARLEQUIN MEMORY POOL SYSTEM I/O INTERFACE
  *
- * $HopeName: MMsrc!mpsio.h(MMdevel_event.2) $
- * Copyright (C) 1996 Harlequin Group, all rights reserved.
+ * $HopeName: MMsrc!mpsio.h(trunk.2) $
+ * Copyright (C) 1996 Harlequin Limited.  All rights reserved.
  *
- * .readership: MPS client application developers, MPS developers.
+ * .readership: For MPS client application developers, MPS developers.
  * .sources: design.mps.io
- *
- * TRANSGRESSIONS (rule.impl.trans)
- *
- * There's no way this meets all the reqiurements yet.
  */
 
 #ifndef mpsio_h
@@ -16,12 +12,14 @@
 
 #include "mps.h"	/* for mps_res_t */
 
+
 typedef struct mps_io_s *mps_io_t;
 
-extern mps_res_t mps_io_create(mps_io_t *mps_io_r);
-extern void mps_io_destroy(mps_io_t mps_io);
+extern mps_res_t mps_io_create(mps_io_t *);
+extern void mps_io_destroy(mps_io_t);
 
-extern mps_res_t mps_io_write(mps_io_t mps_io, void *mps_buf, size_t mps_size);
-extern mps_res_t mps_io_flush(mps_io_t mps_io);
+extern mps_res_t mps_io_write(mps_io_t, void *, size_t);
+extern mps_res_t mps_io_flush(mps_io_t);
+
 
 #endif /* mpsio_h */
