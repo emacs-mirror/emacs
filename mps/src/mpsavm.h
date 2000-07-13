0b292d7eaecfcdb667ec1caeb9665fbaca8ab0d2
Pekka Pirinen <pekka@harlequin.com>
2000-07-13 18:38:36 +0100

    Update copyright (change.mps.configura.eval.170562)
    Copied from Perforce
     Change: 21326
     ServerID: perforce.ravenbrook.com

--- mps/src/mpsavm.h
+++ mps/src/mpsavm.h
@@ -1,7 +1,7 @@
 /* impl.h.mpsavm: MEMORY POOL SYSTEM ARENA CLASS "VM"
  *
- * $HopeName: MMsrc!mpsavm.h(trunk.2) $
- * Copyright (C) 1997 The Harlequin Group.  All rights reserved.
+ * $HopeName: MMsrc!mpsavm.h(trunk.3) $
+ * Copyright (C) 1998 Harlequin Limited.  All rights reserved.
  */
 
 #ifndef mpsavm_h
