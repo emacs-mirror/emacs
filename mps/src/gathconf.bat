60bfc48072e579796ea98b91f2dee48585e8a451
Pekka Pirinen <pekka@harlequin.com>
2000-07-25 18:36:48 +0100

    New unit
    Batch file to gather all the release files for Configura
    
    Copied from Perforce
     Change: 21382
     ServerID: perforce.ravenbrook.com

---
+++ mps/src/gathconf.bat
@@ -0,0 +1,32 @@
+@rem impl.bat.gathconf: GATHERING A RELEASE FOR CONFIGURA
+@rem
+@rem $HopeName$
+@rem Copyright (C) 2000 Harlequin Limited.  All rights reserved.
+
+mkdir release
+mkdir release\include
+mkdir release\lib
+mkdir release\lib\w3i3
+mkdir release\lib\w3i3\we
+mkdir release\lib\w3i3\ce
+mkdir release\src
+copy mps.h release\include
+copy mpsavm.h release\include
+copy mpsacl.h release\include
+copy mpscamc.h release\include
+copy mpscams.h release\include
+copy mpsclo.h release\include
+copy mpscmv.h release\include
+copy mpscmvff.h release\include
+copy mpscsnc.h release\include
+copy mpsio.h release\include
+copy mpslib.h release\include
+copy mpstd.h release\include
+copy mpsw3.h release\include
+copy mpswin.h release\include
+copy w3i3mv\we\mps_conf.lib release\lib\w3i3\we
+copy w3i3mv\ce\mps_conf.lib release\lib\w3i3\ce
+copy w3i3mv\we\mpsplan.lib release\lib\w3i3\we
+copy w3i3mv\ce\mpsplan.lib release\lib\w3i3\ce
+copy mpsliban.c release\src
+copy mpsioan.c release\src
