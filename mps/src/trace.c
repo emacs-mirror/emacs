0ec8d8bd01acf6a98473de680d47cb1c179c8b34
Pekka Pirinen <pekka@harlequin.com>
2000-08-14 20:24:06 +0100

    Reverse traceworkfactor
    Copied from Perforce
     Change: 21419
     ServerID: perforce.ravenbrook.com

--- mps/src/trace.c
+++ mps/src/trace.c
@@ -1,6 +1,6 @@
 /* impl.c.trace: GENERIC TRACER IMPLEMENTATION
  *
- * $HopeName: MMsrc!trace.c(trunk.90) $
+ * $HopeName: MMsrc!trace.c(trunk.91) $
  * Copyright (C) 2000 Harlequin Limited.  All rights reserved.
  *
  * .design: design.mps.trace.
@@ -8,7 +8,7 @@
 
 #include "mpm.h"
 
-SRCID(trace, "$HopeName: MMsrc!trace.c(trunk.90) $");
+SRCID(trace, "$HopeName: MMsrc!trace.c(trunk.91) $");
 
 
 /* Types
@@ -367,8 +367,8 @@ Serial TraceTopGen = 2;
 
 double TraceGen0IncrementalityMultiple = 0.5;
 double TraceEphemeralMortality = 0.85;
-double TraceTopGenMortality = 0.2;
-double TraceWorkFactor = 4.0;
+double TraceTopGenMortality = 0.51;
+double TraceWorkFactor = 0.25;
 
 
 /* TraceSetWhiteUnion
