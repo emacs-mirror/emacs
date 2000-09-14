5723c6699601af372f55b116a3ba6797743aa49e
Pekka Pirinen <pekka@harlequin.com>
2000-09-14 16:32:03 +0100

    Replace avers; clean up
    Copied from Perforce
     Change: 21506
     ServerID: perforce.ravenbrook.com

--- mps/src/lockutw3.c
+++ mps/src/lockutw3.c
@@ -1,55 +1,59 @@
-/* impl.c.lockutw3: LOCK COVERAGE TEST
+/* impl.c.lockutw3: LOCK UTILIZATION TEST
  *
- * $HopeName: MMsrc!lockutw3.c(trunk.7) $
+ * $HopeName$
+ * Copyright (C) 1997 Harlequin Limited.  All rights reserved.
  */
 
 #include "mpm.h"
 #include "testlib.h"
 
+#include "mpswin.h"
+
+
 #ifndef MPS_OS_W3
 #error "Relies on Win32 threads"
 #endif
 
-#include "mpswin.h"
 
-SRCID(lockutw3, "$HopeName: MMsrc!lockutw3.c(trunk.7) $");
-
+#define nTHREADS 4
 
 static Lock lock;
-unsigned long shared,tmp;
+unsigned long shared, tmp;
 
 
 void incR(unsigned long i)
 {
   LockClaimRecursive(lock);
-  if(i<100) {
+  if (i < 100) {
     while(i--) {
-      tmp=shared;
-      shared=tmp+1;
+      tmp = shared;
+      shared = tmp + 1;
     }
   } else {
-    incR(i>>1);
-    incR(i+1>>1);
+    incR(i >> 1);
+    incR(i+1 >> 1);
   }
   LockReleaseRecursive(lock);
 }
 
+
 void inc(unsigned long i)
 {
   incR(i+1>>1);
-  i>>=1;
-  while(i){
+  i >>= 1;
+  while (i) {
     LockClaim(lock);
-    if(i>10000){
+    if (i > 10000) {
       incR(5000);
-      i-=5000;
+      i -= 5000;
     }
-    tmp=shared;
-    shared=tmp+1;
+    tmp = shared;
+    shared = tmp+1;
     i--;
     LockReleaseMPM(lock);
   }
 }
+
 
 #define COUNT 100000l
 DWORD WINAPI thread0(void *p)
@@ -59,30 +63,31 @@ DWORD WINAPI thread0(void *p)
   return 0;
 }
 
+
 int main(void)
 {
   DWORD id;
   HANDLE t[10];
-  unsigned i,nthreads;
-
-  nthreads = 4;
+  unsigned i;
 
   lock = malloc(LockSize());
-  AVER(lock != NULL);
+  Insist(lock != NULL);
 
   LockInit(lock);
 
   shared = 0;
 
-  for(i=0;i<nthreads;i++)
-    t[i] = CreateThread(NULL,0,thread0,NULL,0,&id);
+  for(i = 0; i < nTHREADS; i++)
+    t[i] = CreateThread(NULL, 0, thread0, NULL, 0, &id);
 
-  for(i=0;i<nthreads;i++)
+  for(i = 0; i < nTHREADS; i++)
     WaitForSingleObject(t[i], INFINITE);
 
-  AVER(shared == nthreads*COUNT);
+  Insist(shared == nTHREADS*COUNT);
 
   LockFinish(lock);
 
+  fflush(stdout); /* synchronize */
+  fprintf(stderr, "\nConclusion:  Failed to find any defects.\n");
   return 0;
 }
