c8356c6a79864eaa4b64e69ca1e2e6ee4f8c8b01
Pekka Pirinen <pekka@harlequin.com>
2000-09-26 18:32:36 +0100

    Make segsummary a field accessor (change.mps.epcore.chub.160303)
    Copied from Perforce
     Change: 21547
     ServerID: perforce.ravenbrook.com

--- mps/src/mpmtypes.h
+++ mps/src/mpmtypes.h
@@ -1,6 +1,6 @@
 /* impl.h.mpmtypes: MEMORY POOL MANAGER TYPES
  *
- * $HopeName: MMsrc!mpmtypes.h(trunk.81) $
+ * $HopeName: MMsrc!mpmtypes.h(trunk.82) $
  * Copyright (C) 1999 Harlequin Limited.  All rights reserved.
  *
  * .design: design.mps.type
@@ -144,7 +144,6 @@ typedef void (*SegSetWhiteMethod)(Seg seg, TraceSet white);
 typedef void (*SegSetRankSetMethod)(Seg seg, RankSet rankSet);
 typedef void (*SegSetRankSummaryMethod)(Seg seg, RankSet rankSet, 
                                         RefSet summary);
-typedef RefSet (*SegSummaryMethod)(Seg seg);
 typedef void (*SegSetSummaryMethod)(Seg seg, RefSet summary);
 typedef Buffer (*SegBufferMethod)(Seg seg);
 typedef void (*SegSetBufferMethod)(Seg seg, Buffer buffer);
