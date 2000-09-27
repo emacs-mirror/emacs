a7180e30412cfca4009ef777cfc58859b702e67f
Pekka Pirinen <pekka@harlequin.com>
2000-09-27 18:53:38 +0100

    Srcid
    Copied from Perforce
     Change: 21553
     ServerID: perforce.ravenbrook.com

--- mps/src/prmcan.c
+++ mps/src/prmcan.c
@@ -1,19 +1,11 @@
 /* impl.c.prmcan: PROTECTION MUTATOR CONTEXT (ANSI)
  *
- * $HopeName$
- * Copyright (C) 1998. Harlequin Group plc. All rights reserved.
- *
- * READERSHIP
- *
- * .readership: Any MPS developer.
- *
- * DESIGN
+ * $HopeName: MMsrc!prmcan.c(trunk.1) $
+ * Copyright (C) 1998 Harlequin Limited.  All rights reserved.
  *
  * .design: See design.mps.prot for the generic design of the interface
  * which is implemented in this module including the contracts for the
  * functions.
- *
- * PURPOSE
  *
  * .purpose: This module implements the part of the protection module
  * that implements the MutatorFaultContext type.  In this ANSI version
@@ -22,12 +14,20 @@
 
 #include "mpm.h"
 
+SRCID(prmcan, "$HopeName$");
+
+
+/* ProtCanStepInstruction -- can the current instruction be single-stepped */
+
 Bool ProtCanStepInstruction(MutatorFaultContext context)
 {
   UNUSED(context);
 
   return FALSE;
 }
+
+
+/* ProtStepInstruction -- step over instruction by modifying context */
 
 Res ProtStepInstruction(MutatorFaultContext context)
 {
