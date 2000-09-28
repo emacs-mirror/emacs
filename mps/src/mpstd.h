6f64074924fb8caf5f59ef8dec5fd1139b9aac52
Pekka Pirinen <pekka@harlequin.com>
2000-09-28 20:47:50 +0100

    Return to correct alignments
    Copied from Perforce
     Change: 21555
     ServerID: perforce.ravenbrook.com

--- mps/src/mpstd.h
+++ mps/src/mpstd.h
@@ -1,6 +1,6 @@
 /* impl.h.mpstd: HARLEQUIN MEMORY POOL SYSTEM TARGET DETECTION
  *
- * $HopeName: MMsrc!mpstd.h(trunk.24) $
+ * $HopeName: MMsrc!mpstd.h(trunk.25) $
  * Copyright (C) 2000 Harlequin Limited.  All rights reserved.
  *
  * Detect the target platform using predefined preprocessor symbols
@@ -8,25 +8,24 @@
  * documentation, or, in the case of GCC, from the compiler itself.
  * References to the documentation appear above each detection line.
  *
- * .hack.align:  All alignments have been hacked to be at least 8.
- * This is a short term fix to meet req.epcore.attr.align.  (some
- * of them really ought to be 8, others should be smaller)
- *
  * .macos.ppc.align: MacOS / PowerPC requires 8 bytes alignment (in
- * general).  See arch.pp.mac and arch.pp.align.
+ * general).  See "Mac OS Runtime Architecture", table 4-2.
  */
 
 #ifndef mpstd_h
 #define mpstd_h
+
 
 /* Irix 5/6 man cc and man abi.  We can't check for _ABIO32 (see
  * os.i5), as we have to support Irix 5.2, which doesn't define it.  We
  * check the value of _MIPS_FPSET, as it is defined across all Irix 5
  * and 6 platforms, and on Irix 6 distinguishes O32 from the other two
  * ABIs.  When we support the other ABIs, we need a new OS name for
- * them.  See analysis.irix-cpp. */
+ * them.  Alignment from testing.
+ */
 
-#if defined(__sgi) && defined(__unix) && defined(__mips) && defined(_SYSTYPE_SVR4) && (_MIPS_FPSET == 16)
+#if defined(__sgi) && defined(__unix) && defined(__mips) \
+    && defined(_SYSTYPE_SVR4) && (_MIPS_FPSET == 16)
 #define MPS_PF_I5M2CC
 #define MPS_PF_STRING   "i5m2cc"
 #define MPS_OS_I5
@@ -35,11 +34,12 @@
 #define MPS_T_WORD      unsigned long
 #define MPS_WORD_WIDTH  32
 #define MPS_WORD_SHIFT  5
-#define MPS_PF_ALIGN    8 /* .hack.align */
+#define MPS_PF_ALIGN    8
 
-/* See analysis.irix-cpp */
+/* See above.  Alignment from testing. */
 
-#elif defined(__sgi) && defined(__unix) && defined(__mips) && defined(_SYSTYPE_SVR4) && defined(_ABIN32)
+#elif defined(__sgi) && defined(__unix) && defined(__mips) \
+      && defined(_SYSTYPE_SVR4) && defined(_ABIN32)
 #define MPS_PF_IAM4CC
 #define MPS_PF_STRING   "iam4cc"
 #define MPS_OS_IA
@@ -48,10 +48,9 @@
 #define MPS_T_WORD      unsigned long
 #define MPS_WORD_WIDTH  32
 #define MPS_WORD_SHIFT  5
-#define MPS_PF_ALIGN    8 /* .hack.align */
+#define MPS_PF_ALIGN    8
 
-/* winnt.h (D:\packages\msvc20\include\winnt.h on aaron) */
-/* really ought to check this more thoroughly */
+/* winnt.h from MS VC 2.0 */
 
 #elif defined(_MSC_VER) && defined(_WIN32) && defined(_M_ALPHA)
 #define MPS_PF_W3ALMV
@@ -62,10 +61,9 @@
 #define MPS_T_WORD      unsigned long
 #define MPS_WORD_WIDTH  32
 #define MPS_WORD_SHIFT  5
-#define MPS_PF_ALIGN    8 /* .hack.align */
+#define MPS_PF_ALIGN    4
 
-/* winnt.h (D:\packages\msvc20\include\winnt.h on aaron) */
-/* really ought to check this more thoroughly */
+/* winnt.h from MS VC 2.0 */
 
 #elif defined(_MSC_VER) && defined(_WIN32) && defined(_M_PPC)
 #define MPS_PF_W3PPMV
@@ -76,10 +74,14 @@
 #define MPS_T_WORD      unsigned long
 #define MPS_WORD_WIDTH  32
 #define MPS_WORD_SHIFT  5
-#define MPS_PF_ALIGN    8 /* .hack.align */
+#define MPS_PF_ALIGN    4
 
-/* Visual C++ 2.0, Books Online, C/C++ Book, Preprocessor Reference, */
-/* Chapter 1: The Preprocessor, Macros, Predefined Macros. */
+/* Visual C++ 2.0, Books Online, C/C++ Book, Preprocessor Reference,
+ * Chapter 1: The Preprocessor, Macros, Predefined Macros.
+ * Alignment of 4 would work, but the MS library uses 8 bytes for
+ * doubles and __int64, so we choose that.  The actual granularity of
+ * VC malloc is 16!
+ */
 
 #elif defined(_MSC_VER) && defined(_WIN32) && defined(_M_IX86)
 #define MPS_PF_W3I3MV
@@ -90,10 +92,11 @@
 #define MPS_T_WORD      unsigned long
 #define MPS_WORD_WIDTH  32
 #define MPS_WORD_SHIFT  5
-#define MPS_PF_ALIGN    8 /* .hack.align */
+#define MPS_PF_ALIGN    8
 
-/* MW C/C++/ASM Lang Ref (CW9), pp184-186.  Metrowerks does not document */
-/* a way to determine the OS -- we assume MacOS 7.  */
+/* MW C/C++/ASM Lang Ref (CW9), pp. 184-186.  Metrowerks does not document
+ * a way to determine the OS -- we assume MacOS 7.
+ */
 
 #elif defined(__MWERKS__) && __MC68K__ == 1
 #define MPS_PF_S760MW
@@ -104,10 +107,11 @@
 #define MPS_T_WORD      unsigned long
 #define MPS_WORD_WIDTH  32
 #define MPS_WORD_SHIFT  5
-#define MPS_PF_ALIGN    8 /* .hack.align */
+#define MPS_PF_ALIGN    1
 
-/* MW C/C++/ASM Lang Ref (CW9), pp184-186.  Metrowerks does not document */
-/* a way to determine the OS -- we assume MacOS 7.  */
+/* MW C/C++/ASM Lang Ref (CW9), pp. 184-186.  Metrowerks does not document
+ * a way to determine the OS -- we assume MacOS 7.
+ */
 
 #elif defined(__MWERKS__) && __POWERPC__ == 1
 #define MPS_PF_S7PPMW
@@ -120,10 +124,11 @@
 #define MPS_WORD_SHIFT  5
 #define MPS_PF_ALIGN    8 /* .macos.ppc.align */
 
-/* 1. MPW 3.0 C Ref, p. 43.                                             */
-/* 2. MPW SC/SCpp C/C++ Compiler for 68k Macintosh, p 3-60.             */
-/* These are the two MPW 68k compilers. They do not define anything     */
-/* which lets us determine the system version.                          */
+/* 1. MPW 3.0 C Ref, p. 43.
+ * 2. MPW SC/SCpp C/C++ Compiler for 68k Macintosh, p 3-60.
+ * These are the two MPW 68k compilers.  They do not define anything
+ * which lets us determine the system version.
+ */
 
 #elif defined(m68k) && (defined (applec) || defined(__SC__))
 #define MPS_PF_S760AC
@@ -134,12 +139,13 @@
 #define MPS_T_WORD      unsigned long
 #define MPS_WORD_WIDTH  32
 #define MPS_WORD_SHIFT  5
-#define MPS_PF_ALIGN    8 /* .hack.align */
+#define MPS_PF_ALIGN    1
 
-/* 1. C++/C Compiler for Macintosh with PowerPC, p 3-36.                */
-/* 2. MPW MrC/MrCpp C/C++ Compiler for Power Macintosh, p 3-57.         */
-/* These are the two MPW PowerPC compilers. They do not define anything */
-/* which lets us determine the system version.                          */
+/* 1. C++/C Compiler for Macintosh with PowerPC, p 3-36.
+ * 2. MPW MrC/MrCpp C/C++ Compiler for Power Macintosh, p 3-57.
+ * These are the two MPW PowerPC compilers.  They do not define anything
+ * which lets us determine the system version.
+ */
 
 #elif defined(__PPCC__) || (defined(__MRC__) && defined(__POWERPC))
 #define MPS_PF_S7PPAC
@@ -154,8 +160,8 @@
 
 /* GCC 2.5.8, gcc -E -dM, (__SVR4 indicates Solaris) */
 
-#elif defined(__sun__) && defined(__sparc__) && defined(__GNUC__) && \
-      !defined(__svr4__)
+#elif defined(__sun__) && defined(__sparc__) && defined(__GNUC__) \
+      && !defined(__svr4__)
 #define MPS_PF_SUS8GC
 #define MPS_PF_STRING   "sus8gc"
 #define MPS_OS_SU
@@ -168,8 +174,8 @@
 
 /* LCC 3.4 (ish), man page */
 
-#elif defined(sun) && defined(sparc) && defined(__LCC__) && \
-      !defined(__svr4__)
+#elif defined(sun) && defined(sparc) && defined(__LCC__) \
+      && !defined(__svr4__)
 #define MPS_PF_SUS8LC
 #define MPS_PF_STRING   "sus8lc"
 #define MPS_OS_SU
@@ -182,8 +188,8 @@
 
 /* GCC 2.5.8, gcc -E -dM */
 
-#elif defined(__sun__) && defined(__sparc__) && defined(__GNUC__) && \
-      defined(__svr4__)
+#elif defined(__sun__) && defined(__sparc__) && defined(__GNUC__) \
+      && defined(__svr4__)
 #define MPS_PF_SOS8GC
 #define MPS_PF_STRING   "sos8gc"
 #define MPS_OS_SO
@@ -194,11 +200,13 @@
 #define MPS_WORD_SHIFT  5
 #define MPS_PF_ALIGN    8
 
-/* SunPro C, man cc (confirmed by grep). Note that this doesn't
+/* SunPro C, man cc (confirmed by grep).  Note that this doesn't
  * actually nail down UltraSPARCs; there are no compiler predefined
- * macros for that. */
+ * macros for that.
+ */
 
-#elif defined(__sun) && defined(__SUNPRO_C) && defined(__SVR4) && defined(__sparc)
+#elif defined(__sun) && defined(__SUNPRO_C) && defined(__SVR4) \
+      && defined(__sparc)
 
 #define MPS_PF_SOS9SC
 #define MPS_PF_STRING   "sos9sc"
@@ -224,7 +232,7 @@
 #define MPS_WORD_SHIFT  6
 #define MPS_PF_ALIGN    8
 
-/* From the cc(1) man page on schiele */
+/* From the cc(1) man page */
 
 #elif defined(__osf__) && defined(__alpha) && defined(__DECC)
 #define MPS_PF_O1ALCC
@@ -238,10 +246,13 @@
 #define MPS_WORD_SHIFT  6
 #define MPS_PF_ALIGN    8
 
-/* GCC 2.6.3, gcc -E -dM */
+/* GCC 2.6.3, gcc -E -dM
+ * The actual granularity of GNU malloc is 8, but field alignments are
+ * all 4.
+ */
 
 #elif defined(__linux__) && defined(__i386__) && defined(__GNUC__)
-#define MPS_PF_LII3GC
+#define MPS_PF_LII4GC
 #define MPS_PF_STRING   "lii4gc"
 #define MPS_OS_LI
 #define MPS_ARCH_I4
@@ -249,10 +260,9 @@
 #define MPS_T_WORD      unsigned long
 #define MPS_WORD_WIDTH  32
 #define MPS_WORD_SHIFT  5
-#define MPS_PF_ALIGN    8 /* .hack.align */
+#define MPS_PF_ALIGN    4
 
 /* GCC 2.7.2, gcc -E -dM */
-/* platform.lippgc knocked up very quickly by drj */
 
 #elif defined(__linux__) && defined(__PPC__) && defined(__GNUC__)
 #define MPS_PF_LIPPGC
@@ -263,7 +273,7 @@
 #define MPS_T_WORD      unsigned long
 #define MPS_WORD_WIDTH  32
 #define MPS_WORD_SHIFT  5
-#define MPS_PF_ALIGN    8 /* .hack.align; maybe it should be 8, who knows? */
+#define MPS_PF_ALIGN    8 /* @@@@ not tested */
 
 #else
 #error "Unable to detect target platform"
