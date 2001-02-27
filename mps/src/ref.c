be96f24d3ef9f1764bb6951d8381b39fefe16e37
Pekka Pirinen <pekka@harlequin.com>
2001-02-27 19:12:28 +0000

    Remove doc
    Copied from Perforce
     Change: 21630
     ServerID: perforce.ravenbrook.com

--- mps/src/ref.c
+++ mps/src/ref.c
@@ -1,30 +1,19 @@
 /* impl.c.ref: REFERENCES
  *
- * $HopeName: MMsrc!ref.c(trunk.9) $
- * Copyright (C) 1999.  Harlequin Limited.  All rights reserved.
+ * $HopeName: MMsrc!ref.c(trunk.10) $
+ * Copyright (C) 1999 Harlequin Limited.  All rights reserved.
  *
- * .def.ref: Ref is an alias for Addr which can be used to document
- * where references are being passed.
+ * .purpose: Implement operations on Ref, RefSet, and Rank.
  *
- * .def.refset: RefSet is a conservative approximation to a set of
- * references.  It is conservative in the sense that if isMember returns
- * FALSE for a reference then it is guaranteed that the reference was
- * not added into the RefSet (i.e., accumulating a pointer guarantees
- * that isMember will return TRUE for that reference), but the converse
- * is not true (i.e., isMember can return TRUE for references that have
- * not been accumulated).
- *
- * RefSets are designed to provide a very fast method for Add and
- * for IsMember.  Add is used to implement reference summaries,
- * which provide a remembered set.  IsMember is used to inline part
- * of the Fix function, and provide good discrimination of the
- * white set.  It is expected that the discrimination provided
- * will be useful for distinguishing segments and groups of segments.
+ * .design: See design.mps.ref and design.mps.refset.
  */
 
 #include "mpm.h"
 
-SRCID(ref, "$HopeName: MMsrc!ref.c(trunk.9) $");
+SRCID(ref, "$HopeName: MMsrc!ref.c(trunk.10) $");
+
+
+/* RankCheck -- check a rank value */
 
 Bool RankCheck(Rank rank)
 {
@@ -33,6 +22,8 @@ Bool RankCheck(Rank rank)
   return TRUE;
 }
 
+
+/* RankSetCheck -- check a rank set */
 
 Bool RankSetCheck(RankSet rankSet)
 {
@@ -71,22 +62,20 @@ RefSet RefSetOfRange(Arena arena, Addr base, Addr limit)
   zbase = (Word)base >> arena->zoneShift;
   zlimit = (((Word)limit-1) >> arena->zoneShift) + 1;
 
-  if(zlimit - zbase >= MPS_WORD_WIDTH)        /* .rsor.univ */
+  if (zlimit - zbase >= MPS_WORD_WIDTH)        /* .rsor.univ */
     return RefSetUNIV;
 
   zbase  &= MPS_WORD_WIDTH - 1;
   zlimit &= MPS_WORD_WIDTH - 1;
 
-  if(zbase < zlimit)                      /* .rsor.swap */
+  if (zbase < zlimit)                      /* .rsor.swap */
     return ((RefSet)1<<zlimit) - ((RefSet)1<<zbase);
   else
     return ~(((RefSet)1<<zbase) - ((RefSet)1<<zlimit));
 }
 
 
-/* RefSetOfSeg -- calculate the reference set of segment addresses
- *
- */
+/* RefSetOfSeg -- calculate the reference set of segment addresses */
 
 RefSet RefSetOfSeg(Arena arena, Seg seg)
 {
