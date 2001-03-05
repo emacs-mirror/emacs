c100719ca461c038f6a022588921b8c458a46a40
Pekka Pirinen <pekka@harlequin.com>
2001-03-05 20:47:15 +0000

    Convert to chains
    Copied from Perforce
     Change: 21654
     ServerID: perforce.ravenbrook.com

--- mps/qa/function/11.c
+++ mps/qa/function/11.c
@@ -1,6 +1,6 @@
 /* 
 TEST_HEADER
- id = $HopeName: MMQA_test_function!11.c(trunk.5) $
+ id = $HopeName: MMQA_test_function!11.c(trunk.6) $
  summary = lots of allocation to provoke mmap error in sunos
  language = c
  link = myfmt.o testlib.o
@@ -16,7 +16,15 @@ END_HEADER
 #include "mpsavm.h"
 #include "myfmt.h"
 
+
+#define genCOUNT (3)
+
+static mps_gen_param_s testChain[genCOUNT] = {
+  { 6000, 0.90 }, { 8000, 0.65 }, { 16000, 0.50 } };
+
+
 void *stackpointer;
+
 
 static void test(void)
 {
@@ -26,6 +34,7 @@ static void test(void)
  mps_root_t root;
 
  mps_fmt_t format;
+ mps_chain_t chain;
  mps_ap_t ap;
 
  mycell *a,*b,*c;
@@ -37,7 +46,6 @@ static void test(void)
       "create arena");
 
  cdie(mps_thread_reg(&thread, arena), "register thread");
-
  cdie(
   mps_root_create_reg(&root, arena, MPS_RANK_AMBIG, 0, thread,
    mps_stack_scan_ambig, stackpointer, 0),
@@ -46,12 +54,12 @@ static void test(void)
  cdie(
   mps_fmt_create_A(&format, arena, &fmtA),
   "create format");
+ cdie(mps_chain_create(&chain, arena, genCOUNT, testChain), "chain_create");
 
  formatcomments = 0;
 
- cdie(
-  mps_pool_create(&pool, arena, mps_class_amc(), format),
-  "create pool");
+ cdie(mmqa_pool_create_chain(&pool, arena, mps_class_amc(), format, chain),
+      "create pool");
 
  cdie(
   mps_ap_create(&ap, pool, MPS_RANK_EXACT),
@@ -75,23 +83,13 @@ static void test(void)
  }
 
  mps_ap_destroy(ap);
- comment("Destroyed ap.");
-
  mps_pool_destroy(pool);
- comment("Destroyed pool.");
-
+ mps_chain_destroy(chain);
  mps_fmt_destroy(format);
- comment("Destroyed format.");
-
  mps_root_destroy(root);
- comment("Destroyed root.");
-
  mps_thread_dereg(thread);
- comment("Deregistered thread.");
-
  mps_arena_destroy(arena);
  comment("Destroyed space.");
-
 }
 
 
