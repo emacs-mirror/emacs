6803b1878cd568c76c479f4f59a9ba654deb2be9
Pekka Pirinen <pekka@harlequin.com>
2001-03-09 16:25:51 +0000

    Convert to chains
    Copied from Perforce
     Change: 21690
     ServerID: perforce.ravenbrook.com

--- mps/qa/function/63.c
+++ mps/qa/function/63.c
@@ -1,6 +1,6 @@
 /* 
 TEST_HEADER
- id = $HopeName$
+ id = $HopeName: MMQA_test_function!63.c(trunk.4) $
  summary = loops wthin an AMC pool
  language = c
  link = testlib.o awlfmt.o
@@ -9,18 +9,28 @@ END_HEADER
 
 #include "testlib.h"
 #include "mpscamc.h"
+#include "mpsavm.h"
 #include "awlfmt.h"
+
+
+#define genCOUNT (3)
+
+static mps_gen_param_s testChain[genCOUNT] = {
+  { 6000, 0.90 }, { 8000, 0.65 }, { 16000, 0.50 } };
+
 
 void *stackpointer;
 
+
 static void test(void)
 {
- mps_space_t space;
+ mps_arena_t arena;
  mps_pool_t poolamc1;
  mps_thr_t thread;
  mps_root_t root;
 
  mps_fmt_t format;
+ mps_chain_t chain;
  mps_ap_t ap1;
 
  mycell *a, *b;
@@ -30,63 +40,49 @@ static void test(void)
 
  RC;
 
- cdie(mps_space_create(&space), "create space");
+ cdie(mps_arena_create(&arena, mps_arena_class_vm(), mmqaArenaSIZE),
+      "create arena");
 
- cdie(mps_thread_reg(&thread, space), "register thread");
+ die(mps_thread_reg(&thread, arena), "register thread");
+ die(mps_root_create_reg(&root, arena, MPS_RANK_AMBIG, 0, thread,
+                         mps_stack_scan_ambig, stackpointer, 0),
+     "create root");
 
- cdie(
-  mps_root_create_reg(&root, space, MPS_RANK_AMBIG, 0, thread,
-   mps_stack_scan_ambig, stackpointer, 0),
-  "create root");
+ die(mps_fmt_create_A(&format, arena, &fmtA), "create format");
+ cdie(mps_chain_create(&chain, arena, genCOUNT, testChain), "chain_create");
 
- cdie(
-  mps_fmt_create_A(&format, space, &fmtA),
-  "create format");
-
- cdie(
-  mps_pool_create(&poolamc1, space, mps_class_amc(), format),
-  "create pool");
+ die(mmqa_pool_create_chain(&poolamc1, arena, mps_class_amc(), format, chain),
+     "create pool(1)");
 
  cdie(
   mps_ap_create(&ap1, poolamc1, MPS_RANK_EXACT),
   "create ap");
 
- for (j=1; j<100; j++)
- {
+ for (j = 1; j < 100; j++) {
   comment("%i of 100.", j);
 
-  for (i=1; i<10000; i++)
-  {
-  UC;
+  for (i = 1; i < 10000; i++) {
+   UC;
    a = allocone(ap1, 2, 1);
    b = allocone(ap1, 2, 1);
    setref(a, 0, b);
    setref(b, 0, a);
-  UC;
+   UC;
   }
- DC;
- DMC;
+  DC;
+  DMC;
  }
 
  mps_ap_destroy(ap1);
- comment("Destroyed aps.");
-
  mps_pool_destroy(poolamc1);
- comment("Destroyed pools.");
-
+ mps_chain_destroy(chain);
  mps_fmt_destroy(format);
- comment("Destroyed format.");
-
  mps_root_destroy(root);
- comment("Destroyed root.");
-
  mps_thread_dereg(thread);
- comment("Deregistered thread.");
-
- mps_space_destroy(space);
- comment("Destroyed space.");
-
+ mps_arena_destroy(arena);
+ comment("Destroyed arena.");
 }
+
 
 int main(void)
 {
