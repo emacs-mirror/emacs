0747c97cff27c7d36c1312f2819ae6ede1b298dd
Pekka Pirinen <pekka@harlequin.com>
2001-03-09 12:59:14 +0000

    Convert to chains
    Copied from Perforce
     Change: 21681
     ServerID: perforce.ravenbrook.com

--- mps/qa/function/40.c
+++ mps/qa/function/40.c
@@ -1,6 +1,6 @@
 /* 
 TEST_HEADER
- id = $HopeName: MMQA_test_function!40.c(trunk.4) $
+ id = $HopeName: MMQA_test_function!40.c(trunk.5) $
  summary =  check tagged roots are scanned correctly
  language = c
  link = testlib.o exfmt.o
@@ -9,20 +9,29 @@ END_HEADER
 
 #include "testlib.h"
 #include "mpscamc.h"
+#include "mpsavm.h"
 #include "exfmt.h"
+
+
+#define genCOUNT (3)
+
+static mps_gen_param_s testChain[genCOUNT] = {
+  { 6000, 0.90 }, { 8000, 0.65 }, { 16000, 0.50 } };
+
 
 void *stackpointer;
 
 
 static void test(void)
 {
- mps_space_t space;
+ mps_arena_t arena;
  mps_pool_t pool;
  mps_thr_t thread;
  mps_root_t root;
  mps_root_t root2;
 
  mps_fmt_t format;
+ mps_chain_t chain;
  mps_ap_t ap;
 
  mycell *z[100];
@@ -30,37 +39,24 @@ static void test(void)
  int i;
 
  formatcomments = 1;
-
-
  RC;
 
- cdie(mps_space_create(&space), "create space");
+ cdie(mps_arena_create(&arena, mps_arena_class_vm(), mmqaArenaSIZE),
+      "create arena");
 
- cdie(mps_thread_reg(&thread, space), "register thread");
+ cdie(mps_thread_reg(&thread, arena), "register thread");
 
-/*
- cdie(
-  mps_root_create_reg(&root, space, MPS_RANK_AMBIG, 0, thread,
-   mps_stack_scan_ambig, stackpointer, 0),
-  "create root");
-*/
+ die(mps_root_create_table_masked(&root, arena, MPS_RANK_EXACT,
+                                  0, (mps_addr_t*)&z[0], 100, 0x4),
+     "create table root");
+ die(mps_root_create_table(&root2, arena, MPS_RANK_AMBIG, 0, &exfmt_root, 1),
+     "create exfmt root");
 
- cdie(
-  mps_root_create_table_masked(&root, space, MPS_RANK_EXACT,
-   0, (mps_addr_t*)&z[0], 100, 0x4),
-  "create table root");
+ die(mps_fmt_create_A(&format, arena, &fmtA), "create format");
+ cdie(mps_chain_create(&chain, arena, genCOUNT, testChain), "chain_create");
 
- cdie(
-  mps_root_create_table(&root2, space, MPS_RANK_AMBIG, 0, &exfmt_root, 1),
-  "create exfmt root");
-
- cdie(
-  mps_fmt_create_A(&format, space, &fmtA),
-  "create format");
-
- cdie(
-  mps_pool_create(&pool, space, mps_class_amc(), format),
-  "create pool");
+ die(mmqa_pool_create_chain(&pool, arena, mps_class_amc(), format, chain),
+     "create pool(amc)");
 
  cdie(
   mps_ap_create(&ap, pool, MPS_RANK_EXACT),
@@ -70,7 +66,7 @@ static void test(void)
   comment("%i of 10.", i);
   UC;
   z[i] = allocone(ap, 1, 1);
-  if (i % 8 == 0) { z[i] = (mycell *) ((int) z[i] + 4); } /* error to scan this! */
+  if (i % 8 == 0) { z[i] = (mycell *) ((int)z[i] + 4); } /* error to scan this! */
  }
 
  for (i=0; i<1000; i++) {
@@ -80,23 +76,15 @@ static void test(void)
  DMC;
 
  mps_ap_destroy(ap);
- comment("Destroyed ap.");
-
  mps_pool_destroy(pool);
- comment("Destroyed pool.");
-
+ mps_chain_destroy(chain);
  mps_fmt_destroy(format);
- comment("Destroyed format.");
-
  mps_root_destroy(root);
  mps_root_destroy(root2);
  comment("Destroyed roots.");
-
  mps_thread_dereg(thread);
- comment("Deregistered thread.");
-
- mps_space_destroy(space);
- comment("Destroyed space.");
+ mps_arena_destroy(arena);
+ comment("Destroyed arena.");
 }
 
 
