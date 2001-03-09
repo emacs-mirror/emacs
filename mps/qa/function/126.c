f3217adb557851c9a022eea4c4e6e63e91ed091a
Pekka Pirinen <pekka@harlequin.com>
2001-03-09 20:30:34 +0000

    Convert to chains
    Copied from Perforce
     Change: 21708
     ServerID: perforce.ravenbrook.com

--- mps/qa/function/126.c
+++ mps/qa/function/126.c
@@ -1,6 +1,6 @@
 /* 
 TEST_HEADER
- id = $HopeName: MMQA_test_function!126.c(trunk.1) $
+ id = $HopeName: MMQA_test_function!126.c(trunk.2) $
  summary = extensible arena test (huge objects)
  language = c
  link = testlib.o rankfmt.o
@@ -11,6 +11,12 @@ END_HEADER
 #include "mpscamc.h"
 #include "mpsavm.h"
 #include "rankfmt.h"
+
+
+#define genCOUNT (3)
+
+static mps_gen_param_s testChain[genCOUNT] = {
+  { 6000, 0.90 }, { 8000, 0.65 }, { 16000, 0.50 } };
 
 
 void *stackpointer;
@@ -24,17 +30,16 @@ static void test(void)
  mps_root_t root, root1;
 
  mps_fmt_t format;
+ mps_chain_t chain;
  mps_ap_t ap;
 
  mycell *b;
 
  /* create an arena that can't grow beyond 1 Mb */
-
  cdie(mps_arena_create(&arena, mps_arena_class_vm(), (size_t) (1024*1024*1)),
       "create arena");
 
  cdie(mps_thread_reg(&thread, arena), "register thread");
-
  cdie(mps_root_create_reg(&root, arena, MPS_RANK_AMBIG, 0, thread,
                           mps_stack_scan_ambig, stackpointer, 0),
       "create root");
@@ -45,9 +50,10 @@ static void test(void)
 
  cdie(mps_fmt_create_A(&format, arena, &fmtA),
       "create format");
+ cdie(mps_chain_create(&chain, arena, genCOUNT, testChain), "chain_create");
 
- cdie(mps_pool_create(&pool, arena, mps_class_amc(), format),
-      "create pool");
+ die(mmqa_pool_create_chain(&pool, arena, mps_class_amc(), format, chain),
+     "create pool");
 
  cdie(mps_ap_create(&ap, pool, MPS_RANK_EXACT),
       "create ap");
@@ -76,21 +82,12 @@ static void test(void)
          mps_arena_reserved(arena), mps_arena_committed(arena));
 
  mps_ap_destroy(ap);
- comment("Destroyed ap.");
-
  mps_pool_destroy(pool);
- comment("Destroyed pool.");
-
+ mps_chain_destroy(chain);
  mps_fmt_destroy(format);
- comment("Destroyed format.");
-
  mps_root_destroy(root);
  mps_root_destroy(root1);
- comment("Destroyed roots.");
-
  mps_thread_dereg(thread);
- comment("Deregistered thread.");
-
  mps_arena_destroy(arena);
  comment("Destroyed arena.");
 }
