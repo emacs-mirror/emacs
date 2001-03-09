ce313fa2c4015801c1381c6cfdd8fd14b9038bea
Pekka Pirinen <pekka@harlequin.com>
2001-03-09 20:16:42 +0000

    Convert to chains
    Copied from Perforce
     Change: 21706
     ServerID: perforce.ravenbrook.com

--- mps/qa/function/118.c
+++ mps/qa/function/118.c
@@ -1,6 +1,6 @@
 /* 
 TEST_HEADER
- id = $HopeName: MMQA_test_function!118.c(trunk.4) $
+ id = $HopeName: MMQA_test_function!118.c(trunk.5) $
  summary = Collect with a fully initialised (but not committed) buffer
  language = c
  link = testlib.o
@@ -11,10 +11,17 @@ END_HEADER
 #include "mpscamc.h"
 #include "mpsavm.h"
 
+
+#define genCOUNT (3)
+
+static mps_gen_param_s testChain[genCOUNT] = {
+  { 6000, 0.90 }, { 8000, 0.65 }, { 16000, 0.50 } };
+
 #define testArenaSIZE     ((size_t)64<<20)
 /* objSIZE should be such that when this size is requested in a reserve */
 /* the buffer gets filled with exactly this much memory */
 #define objSIZE           8192
+
 
 static mps_ap_t ap;
 
@@ -48,7 +55,8 @@ static void simple_pad(mps_addr_t addr, size_t size)
 {
 }
 
-static void simple_copy(mps_addr_t obj, mps_addr_t to) {
+static void simple_copy(mps_addr_t obj, mps_addr_t to)
+{
  error("copy method not implemented in this test");
 }
 
@@ -88,6 +96,7 @@ static void test(void)
   mps_arena_t arena;
   mps_thr_t thread;
   mps_fmt_t format;
+  mps_chain_t chain;
   mps_pool_t pool;
   unsigned long i;
 
@@ -97,9 +106,10 @@ static void test(void)
   die(mps_thread_reg(&thread, arena), "thread_reg");
 
   die(mps_fmt_create_A(&format, arena, &simple_fmt_A), "fmt_create");
+  die(mps_chain_create(&chain, arena, genCOUNT, testChain), "chain_create");
 
-  die(mps_pool_create(&pool, arena, mps_class_amc(), format),
-      "pool_create(amc)");
+  die(mmqa_pool_create_chain(&pool, arena, mps_class_amc(), format, chain),
+      "create pool");
 
   die(mps_ap_create(&ap, pool, MPS_RANK_EXACT), "BufferCreate");
   die(mps_ap_create(&busy_ap, pool, MPS_RANK_EXACT), "BufferCreate");
@@ -120,6 +130,7 @@ static void test(void)
   mps_ap_destroy(busy_ap);
   mps_ap_destroy(ap);
   mps_pool_destroy(pool);
+  mps_chain_destroy(chain);
   mps_fmt_destroy(format);
   mps_thread_dereg(thread);
   mps_arena_destroy(arena);
