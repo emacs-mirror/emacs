1b2bfa9fe879a06d1342bf1296773007ad4989af
Pekka Pirinen <pekka@harlequin.com>
2001-03-14 13:14:07 +0000

    Space->arena
    Copied from Perforce
     Change: 21732
     ServerID: perforce.ravenbrook.com

--- mps/src/protan.c
+++ mps/src/protan.c
@@ -1,26 +1,28 @@
 /* impl.c.protan: ANSI MEMORY PROTECTION
  *
- * $HopeName: MMsrc!protan.c(trunk.7) $
- * Copyright (C) 1997 The Harlequin Group Limited.  All rights reserved.
+ * $HopeName: MMsrc!protan.c(trunk.8) $
+ * Copyright (C) 1997 Harlequin Limited.  All rights reserved.
  *
- * READERSHIP
- *
- * .readership: Any MPS developer
  *
  * DESIGN
  *
  * design.mps.protan
- *
  */
 
 #include "mpm.h"
 
-SRCID(protan, "$HopeName: MMsrc!protan.c(trunk.7) $");
+SRCID(protan, "$HopeName: MMsrc!protan.c(trunk.8) $");
+
+
+/* ProtSetup -- global protection setup */
 
 void ProtSetup(void)
 {
   NOOP;
 }
+
+
+/* ProtSet -- set the protection for a page */
 
 void ProtSet(Addr base, Addr limit, AccessSet pm)
 {
@@ -31,35 +33,42 @@ void ProtSet(Addr base, Addr limit, AccessSet pm)
   NOOP;
 }
 
-/* design.mps.protan.fun.sync */
-void ProtSync(Space space)
+
+/* ProtSync -- synchronize protection settings with hardware
+ *
+ * See design.mps.protan.fun.sync.
+ */
+
+void ProtSync(Arena arena)
 {
   Bool synced;
 
-  AVERT(Space, space);
+  AVERT(Arena, arena);
 
   do {
     Seg seg;
 
     synced = TRUE;
-    if(SegFirst(&seg, space)) {
+    if (SegFirst(&seg, arena)) {
       Addr base;
       do {
 	base = SegBase(seg);
-	if(SegPM(seg) != AccessSetEMPTY) {   /* design.mps.protan.fun.sync.seg */
-	  ShieldEnter(space);
-	  TraceSegAccess(space, seg, SegPM(seg));
-	  ShieldLeave(space);
+	if (SegPM(seg) != AccessSetEMPTY) { /* design.mps.protan.fun.sync.seg */
+	  ShieldEnter(arena);
+	  TraceSegAccess(arena, seg, SegPM(seg));
+	  ShieldLeave(arena);
 	  synced = FALSE;
 	}
-      } while(SegNext(&seg, space, base));
+      } while(SegNext(&seg, arena, base));
     }
   } while(!synced);
 }
 
-void ProtTramp(void **rReturn,
-                void *(*f)(void *, size_t),
-                void *p, size_t s)
+
+/* ProtTramp -- protection trampoline */
+
+void ProtTramp(void **rReturn, void *(*f)(void *, size_t),
+               void *p, size_t s)
 {
   AVER(rReturn != NULL);
   AVER(FUNCHECK(f));
