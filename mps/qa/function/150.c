b5681dacb1515d9783bb74edd0a5ce9e8e1b3a13
Pekka Pirinen <pekka@harlequin.com>
2001-03-21 18:58:26 +0000

    Amcz also needs a chain
    Copied from Perforce
     Change: 21766
     ServerID: perforce.ravenbrook.com

--- mps/qa/function/150.c
+++ mps/qa/function/150.c
@@ -1,6 +1,6 @@
 /* 
 TEST_HEADER
- id = $HopeName: MMQA_test_function!150.c(trunk.5) $
+ id = $HopeName: MMQA_test_function!150.c(trunk.6) $
  summary = finalization and collection stats
  language = c
  link = testlib.o rankfmt.o
@@ -18,6 +18,12 @@ END_HEADER
 #include "mpscamc.h"
 #include "mpsavm.h"
 #include "rankfmt.h"
+
+
+#define genCOUNT (3)
+
+static mps_gen_param_s testChain[genCOUNT] = {
+  { 6000, 0.90 }, { 8000, 0.65 }, { 16000, 0.50 } };
 
 
 void *stackpointer;
@@ -41,14 +47,16 @@ int qhd = 0;
 int qtl = 0;
 
 
-static void nq(mps_message_t mess) {
+static void nq(mps_message_t mess)
+{
  mqueue[qhd] = mess;
  qhd = (qhd+1) % 10000;
  asserts(qhd != qtl, "No space in message queue.");
 }
 
 
-static int qmt(void) {
+static int qmt(void)
+{
  if (qhd == qtl) {
   return 1;
  } else {
@@ -57,7 +65,8 @@ static int qmt(void) {
 }
 
 
-static int dq(mps_message_t *mess) {
+static int dq(mps_message_t *mess)
+{
  if (qhd == qtl) {
   return 0;
  } else {
@@ -68,7 +77,8 @@ static int dq(mps_message_t *mess) {
 }
 
 
-static void process_mess(mps_message_t message, int faction, mps_addr_t *ref) {
+static void process_mess(mps_message_t message, int faction, mps_addr_t *ref)
+{
  mps_addr_t ffref;
 
  switch (faction) {
@@ -94,7 +104,8 @@ static void process_mess(mps_message_t message, int faction, mps_addr_t *ref) {
 }
 
 
-static void qpoll(mycell **ref, int faction) {
+static void qpoll(mycell **ref, int faction)
+{
  mps_message_t message;
 
  if (dq(&message)) {
@@ -103,7 +114,8 @@ static void qpoll(mycell **ref, int faction) {
 }
 
 
-static void process_stats(mps_message_t message) {
+static void process_stats(mps_message_t message)
+{
  report("collect", "true");
  report("collect_live", "%ld",
         mps_message_gc_live_size(arena, message));
@@ -115,7 +127,8 @@ static void process_stats(mps_message_t message) {
 }
 
 
-static void messagepoll(mycell **ref, int faction) {
+static void messagepoll(mycell **ref, int faction)
+{
  mps_message_t message;
 
  if (mps_message_get(&message, arena, mps_message_type_finalization())) {
@@ -131,11 +144,13 @@ static void messagepoll(mycell **ref, int faction) {
 #define clear(type, var) (*((volatile type*)&(var)) = NULL)
 
 
-static void test(void) {
+static void test(void)
+{
  mps_pool_t poolamc, poolawl, poollo;
  mps_thr_t thread;
  mps_root_t root0, root1;
 
+ mps_chain_t chain;
  mps_fmt_t format;
  mps_ap_t apamc, apawl, aplo;
 
@@ -147,7 +162,6 @@ static void test(void) {
       "create arena");
 
  cdie(mps_thread_reg(&thread, arena), "register thread");
-
  cdie(mps_root_create_reg(&root0, arena, MPS_RANK_AMBIG, 0, thread,
                           mps_stack_scan_ambig, stackpointer, 0),
       "create root");
@@ -158,24 +172,25 @@ static void test(void) {
 
  cdie(mps_fmt_create_A(&format, arena, &fmtA),
       "create format");
+ cdie(mps_chain_create(&chain, arena, genCOUNT, testChain), "chain_create");
 
- cdie(mps_pool_create(&poolamc, arena, mps_class_amc(), format),
-      "create pool");
+ die(mmqa_pool_create_chain(&poolamc, arena, mps_class_amc(), format, chain),
+     "create pool(amc)");
+
+ die(mmqa_pool_create_chain(&poollo, arena, mps_class_amcz(), format, chain),
+     "create pool(amcz)");
 
  cdie(mps_pool_create(&poolawl, arena, mps_class_awl(), format),
-      "create pool");
-
- cdie(mps_pool_create(&poollo, arena, mps_class_amcz(), format),
-      "create pool");
+      "create pool(awl)");
 
  cdie(mps_ap_create(&apawl, poolawl, MPS_RANK_WEAK),
-      "create ap");
+      "create ap(awl)");
 
  cdie(mps_ap_create(&apamc, poolamc, MPS_RANK_EXACT),
-      "create ap");
+      "create ap(amc)");
  
  cdie(mps_ap_create(&aplo, poollo, MPS_RANK_EXACT),
-      "create ap");
+      "create ap(amcz)");
 
  mps_message_type_enable(arena, mps_message_type_finalization());
  mps_message_type_enable(arena, mps_message_type_gc());
@@ -292,20 +307,16 @@ static void test(void) {
  mps_pool_destroy(poolamc);
  mps_pool_destroy(poolawl);
  mps_pool_destroy(poollo);
- comment("Destroyed pools.");
-
+ mps_chain_destroy(chain);
  mps_fmt_destroy(format);
- comment("Destroyed format.");
-
  mps_thread_dereg(thread);
- comment("Deregistered thread.");
-
  mps_arena_destroy(arena);
  comment("Destroyed arena.");
 }
 
 
-int main(void) {
+int main(void)
+{
  void *m;
  stackpointer=&m; /* hack to get stack pointer */
 
