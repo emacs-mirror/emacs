ad70f63d30be3b75e42a2a053ad91d329e92519e
Pekka Pirinen <pekka@harlequin.com>
2001-04-12 15:13:01 +0100

    Stackprobe type
    Copied from Perforce
     Change: 21790
     ServerID: perforce.ravenbrook.com

--- mps/src/span.c
+++ mps/src/span.c
@@ -1,24 +1,24 @@
 /* impl.c.span: ANSI STACK PROBE
  *
- * $HopeName: MMsrc!span.c(trunk.1) $
+ * $HopeName: MMsrc!span.c(trunk.2) $
  * Copyright (C) 1997 Harlequin Limited.  All rights reserved.
  *
  * PURPOSE
  *
  * .purpose: The purpose of the ANSI Stack Probe is to provide a
- * non-functional implementation of the Stack Probe interface.
- * Stack Probe has a function implementation on platforms where the
+ * non-functional implementation of the StackProbe interface.
+ * StackProbe has a function implementation on platforms where the
  * MPS takes some special action to avoid stack overflow.
  */
 
 #include "mpm.h"
 
-SRCID(span, "$HopeName: MMsrc!span.c(trunk.1) $");
+SRCID(span, "$HopeName: MMsrc!span.c(trunk.2) $");
 
 
 /* StackProbe -- probe above the stack to provoke early stack overflow */
 
-void StackProbe(Word depth)
+void StackProbe(Size depth)
 {
   AVER(depth == 0);
   NOOP;
