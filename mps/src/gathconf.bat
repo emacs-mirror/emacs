91c139a007b0856335e7ee3f2d712ecd54f70bdd
Nick Barnes <nb@ravenbrook.com>
2001-10-04 16:06:58 +0100

    Make a debug-enabled build with the current version of msvc.
    Copied from Perforce
     Change: 22967
     ServerID: perforce.ravenbrook.com

--- mps/src/gathconf.bat
+++ mps/src/gathconf.bat
@@ -9,6 +9,8 @@ mkdir release\lib
 mkdir release\lib\w3i3
 mkdir release\lib\w3i3\we
 mkdir release\lib\w3i3\ce
+mkdir release\lib\w3i3\wi
+mkdir release\lib\w3i3\ci
 mkdir release\src
 copy mps.h release\include
 copy mpsavm.h release\include
@@ -28,5 +30,9 @@ copy w3i3mv\we\mps_conf.lib release\lib\w3i3\we
 copy w3i3mv\ce\mps_conf.lib release\lib\w3i3\ce
 copy w3i3mv\we\mpsplan.lib release\lib\w3i3\we
 copy w3i3mv\ce\mpsplan.lib release\lib\w3i3\ce
+copy w3i3mv\wi\mps_conf.lib release\lib\w3i3\wi
+copy w3i3mv\ci\mps_conf.lib release\lib\w3i3\ci
+copy w3i3mv\wi\mpsplan.lib release\lib\w3i3\wi
+copy w3i3mv\ci\mpsplan.lib release\lib\w3i3\ci
 copy mpsliban.c release\src
 copy mpsioan.c release\src
