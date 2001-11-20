46585b32dbfc5e42fc22abda54b1e52087add584
Stefan Monnier <monnier@iro.umontreal.ca>
2001-11-20 14:21:15 +0000

    (cperl-perldoc): Require man before binding `manual-program'.

--- lisp/progmodes/cperl-mode.el
+++ lisp/progmodes/cperl-mode.el
@@ -6770,6 +6770,7 @@ We suppose that the regexp is scanned already."
                    (error "No perldoc args given")
                  default-entry)
              input))))
+  (require 'man)
   (let* ((is-func (and
 		   (string-match "^[a-z]+$" word)
 		   (string-match (concat "^" word "\\>")
@@ -6777,7 +6778,6 @@ We suppose that the regexp is scanned already."
 				  'cperl-short-docs
 				  'variable-documentation))))
 	 (manual-program (if is-func "perldoc -f" "perldoc")))
-    (require 'man)
     (Man-getpage-in-background word)))
 
 (defun cperl-perldoc-at-point ()
