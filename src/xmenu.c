4fe1842d060c06dcbff3da8fc203053f349e5a9f
Richard M. Stallman <rms@gnu.org>
2002-01-04 22:18:17 +0000

    (menu_highlight_callback): Hide any tooltip window.

--- src/xmenu.c
+++ src/xmenu.c
@@ -1192,6 +1192,8 @@ menu_highlight_callback (widget, id, call_data)
   f = menubar_id_to_frame (id);
   if (f)
     {
+      Fx_hide_tip ();
+
       XSETFRAME (frame, f);
       kbd_buffer_store_help_event (frame, help);
     }
