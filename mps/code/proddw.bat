b09650f47c87cc0e4fb902fc8bab115a5da6d964
Richard Brooksby <rb@ravenbrook.com>
2002-06-06 11:44:23 +0100

    Adding licenses to source code files.
    Copied from Perforce
     Change: 29836
     ServerID: perforce.ravenbrook.com

--- mps/code/proddw.bat
+++ mps/code/proddw.bat
@@ -1,7 +1,8 @@
 @rem impl.bat.proddw
 @rem Script that automates building and collating a dylan product
-rem $Id$
-rem Copyright (c) 2001 Ravenbrook Limited.
+@rem $Id$
+@rem Copyright (c) 2001 Ravenbrook Limited.
+
 nmake /f w3i3mv.nmk VARIETY=ci mmdw.lib mpsplan.lib
 nmake /f w3i3mv.nmk VARIETY=hi mmdw.lib mpsplan.lib
 nmake /f w3i3mv.nmk VARIETY=he mmdw.lib mpsplan.lib
@@ -39,3 +40,43 @@ copy w3i3mv\he\mpsplan.lib dylan\mps\lib\w3i3\he
 copy w3i3mv\wi\mpsplan.lib dylan\mps\lib\w3i3\wi
 copy mpsliban.c dylan\mps\src
 copy mpsioan.c dylan\mps\src
+
+@rem C. COPYRIGHT AND LICENSE
+@rem
+@rem Copyright (C) 2001-2002 Ravenbrook Limited <http://www.ravenbrook.com/>.
+@rem All rights reserved.  This is an open source license.  Contact
+@rem Ravenbrook for commercial licensing options.
+@rem 
+@rem Redistribution and use in source and binary forms, with or without
+@rem modification, are permitted provided that the following conditions are
+@rem met:
+@rem 
+@rem 1. Redistributions of source code must retain the above copyright
+@rem notice, this list of conditions and the following disclaimer.
+@rem 
+@rem 2. Redistributions in binary form must reproduce the above copyright
+@rem notice, this list of conditions and the following disclaimer in the
+@rem documentation and/or other materials provided with the distribution.
+@rem 
+@rem 3. Redistributions in any form must be accompanied by information on how
+@rem to obtain complete source code for this software and any accompanying
+@rem software that uses this software.  The source code must either be
+@rem included in the distribution or be available for no more than the cost
+@rem of distribution plus a nominal fee, and must be freely redistributable
+@rem under reasonable conditions.  For an executable file, complete source
+@rem code means the source code for all modules it contains. It does not
+@rem include source code for modules or files that typically accompany the
+@rem major components of the operating system on which the executable file
+@rem runs.
+@rem 
+@rem THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+@rem IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+@rem TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+@rem PURPOSE, OR NON-INFRINGEMENT, ARE DISCLAIMED. IN NO EVENT SHALL THE
+@rem COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+@rem INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+@rem NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+@rem USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+@rem ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+@rem (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+@rem THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
