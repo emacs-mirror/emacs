6f2d725ddf011801c7e1610123271e8ccafa3f17
Juanma Barranquero <lekktu@gmail.com>
2002-07-03 11:46:24 +0000

    Fix typo.

--- lisp/emacs-lisp/lisp-mode.el
+++ lisp/emacs-lisp/lisp-mode.el
@@ -719,7 +719,7 @@ is the buffer position of the start of the containing expression."
     (parse-partial-sexp (point) calculate-lisp-indent-last-sexp 0 t)
     (if (and (elt state 2)
              (not (looking-at "\\sw\\|\\s_")))
-        ;; car of form doesn't seem to be a a symbol
+        ;; car of form doesn't seem to be a symbol
         (progn
           (if (not (> (save-excursion (forward-line 1) (point))
                       calculate-lisp-indent-last-sexp))
