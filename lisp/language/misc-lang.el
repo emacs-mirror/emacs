bd6435af742e87a79ebb8508b5c91410b74e463a
Dave Love <fx@gnu.org>
2002-07-09 14:41:09 +0000

    ("UTF-8"): New language environment.

--- lisp/language/misc-lang.el
+++ lisp/language/misc-lang.el
@@ -2,6 +2,7 @@
 
 ;; Copyright (C) 1995 Electrotechnical Laboratory, JAPAN.
 ;; Licensed to the Free Software Foundation.
+;; Copyright (C) 2002 Free Software Foundation, Inc.
 
 ;; Keywords: multilingual, character set, coding system
 
@@ -40,6 +41,29 @@
 	 (documentation . "\
 IPA is International Phonetic Alphabet for English, French, German
 and Italian.")))
+
+(set-language-info-alist
+ "UTF-8" `((coding-system mule-utf-8)
+	   (coding-priority mule-utf-8)
+	   (setup-function
+	    . (lambda ()
+		;; Use Unicode font under Windows.  Jason Rumney fecit.
+		(if (and (fboundp 'w32-add-charset-info)
+			 (not (boundp 'w32-unicode-charset-defined)))
+		    (w32-add-charset-info "iso10646-1" 'w32-charset-ansi t))))
+;; Is this appropriate?
+;; 	   (exit-function
+;; 	    . (lambda ()
+;; 		(if (and (fboundp 'w32-add-charset-info)
+;; 			 (not (boundp 'w32-unicode-charset-defined)))
+;; 		    (setq w32-charset-info-alist
+;; 			  (delete (assoc "iso10646-1")
+;; 				  w32-charset-info-alist)))))
+	   (input-method . "rfc1345")	; maybe not the best choice
+	   (documentation . "\
+This language environment is a generic one for a subset of the Unicode
+character set encoded in UTF-8."))
+ nil)
 
 (provide 'misc-lang)
 
