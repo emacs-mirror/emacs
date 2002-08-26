20357168ed2f021118cafa125fa9e9185391d4c6
Kim F. Storm <storm@cua.dk>
2002-08-26 10:16:51 +0000

    (make_terminal_frame) [CANNOT_DUMP]: Initialize foreground and background colors. From Joe Buehler (tiny change).

--- src/frame.c
+++ src/frame.c
@@ -486,6 +486,10 @@ make_terminal_frame ()
   make_mac_terminal_frame (f);
 #else
   f->output_data.x = &tty_display;
+#ifdef CANNOT_DUMP
+  FRAME_FOREGROUND_PIXEL(f) = FACE_TTY_DEFAULT_FG_COLOR;
+  FRAME_BACKGROUND_PIXEL(f) = FACE_TTY_DEFAULT_BG_COLOR;
+#endif
 #endif /* macintosh */
 #endif /* WINDOWSNT */
 #endif /* MSDOS */
