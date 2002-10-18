bb0a812529bd416bbd73ff0d9373275015ffacad
Nick Barnes <nb@ravenbrook.com>
2002-10-18 14:29:20 +0100

    Add a new test, for finalization, demonstrating the value of proxies.
    Copied from Perforce
     Change: 34507
     ServerID: perforce.ravenbrook.com

--- mps/code/fmtdytst.h
+++ mps/code/fmtdytst.h
@@ -18,6 +18,14 @@ extern mps_bool_t dylan_check(mps_addr_t addr);
 extern void dylan_pad(mps_addr_t addr, size_t size);
 extern int dylan_wrapper_check(mps_word_t *w);
 
+extern mps_res_t make_dylan_vector(mps_word_t *v, mps_ap_t ap, size_t slots);
+
+#define DYLAN_VECTOR_SLOT(o,n) (((mps_word_t *) (o))[(n)+2])
+
+#define DYLAN_INT(n) (((n) << 2) | 1)
+
+#define DYLAN_INT_INT(d)  ((d) >> 2)
+
 #endif /* fmtdy_h */
 
 
@@ -26,18 +34,18 @@ extern int dylan_wrapper_check(mps_word_t *w);
  * Copyright (C) 2001-2002 Ravenbrook Limited <http://www.ravenbrook.com/>.
  * All rights reserved.  This is an open source license.  Contact
  * Ravenbrook for commercial licensing options.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright
  * notice, this list of conditions and the following disclaimer.
- * 
+ *
  * 2. Redistributions in binary form must reproduce the above copyright
  * notice, this list of conditions and the following disclaimer in the
  * documentation and/or other materials provided with the distribution.
- * 
+ *
  * 3. Redistributions in any form must be accompanied by information on how
  * to obtain complete source code for this software and any accompanying
  * software that uses this software.  The source code must either be
@@ -48,7 +56,7 @@ extern int dylan_wrapper_check(mps_word_t *w);
  * include source code for modules or files that typically accompany the
  * major components of the operating system on which the executable file
  * runs.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
  * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
  * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
