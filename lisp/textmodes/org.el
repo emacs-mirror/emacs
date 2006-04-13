6d5ef7fe091a255e50a26df40c638cc09145efa1
Carsten Dominik <dominik@science.uva.nl>
2006-04-13 16:33:06 +0000

    *** empty log message ***

--- lisp/textmodes/org.el
+++ lisp/textmodes/org.el
@@ -13238,7 +13238,7 @@ With optional NODE, go directly to that node."
   ;; through to `fill-paragraph' when appropriate.
   (set (make-local-variable 'fill-paragraph-function) 'org-fill-paragraph)
   ;; Adaptive filling: To get full control, first make sure that
-  ;; `adaptive-fill-regexp' never matches.  Then install our won matcher.
+  ;; `adaptive-fill-regexp' never matches.  Then install our own matcher.
   (set (make-local-variable 'adaptive-fill-regexp) "\000")
   (set (make-local-variable 'adaptive-fill-function)
        'org-adaptive-fill-function))
