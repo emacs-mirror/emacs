6dcbe583ce72896aef569f197eb1142609009371
David Jones <drj@ravenbrook.com>
2007-03-01 18:56:07 +0000

    Mps: no register scanner on mactel for mpsicv.
    Copied from Perforce
     Change: 161872
     ServerID: perforce.ravenbrook.com

--- mps/code/mpsicv.c
+++ mps/code/mpsicv.c
@@ -450,12 +450,22 @@ static void *test(void *arg, size_t s)
 
 #define TEST_ARENA_SIZE              ((size_t)16<<20)
 
+/* Hacky.  Sorry.
+ * REG_ROOT is defined to 1 when we can use a register root.
+ * Otherwise it is defined to 0.
+ */
+#if !defined(CONFIG_PROD_EPCORE) && !defined(MPS_PF_XCI3GC)
+#define REG_ROOT 1
+#else
+#define REG_ROOT 0
+#endif
+
 
 int main(int argc, char **argv)
 {
   mps_arena_t arena;
   mps_thr_t thread;
-#if !defined(CONFIG_PROD_EPCORE) && !defined(CONFIG_PF_XCPPGC)
+#if REG_ROOT
   mps_root_t reg_root;
 #endif
   void *r;
@@ -467,7 +477,7 @@ int main(int argc, char **argv)
       "arena_create");
   die(mps_thread_reg(&thread, arena), "thread_reg");
 
-#if !defined(CONFIG_PROD_EPCORE) && !defined(CONFIG_PF_XCPPGC)
+#if REG_ROOT
   die(mps_root_create_reg(&reg_root, arena,
                           MPS_RANK_AMBIG, (mps_rm_t)0,
                           thread, &mps_stack_scan_ambig,
@@ -477,7 +487,7 @@ int main(int argc, char **argv)
 
   (mps_tramp)(&r, test, arena, 0);  /* non-inlined trampoline */
   mps_tramp(&r, test, arena, 0);
-#if !defined(CONFIG_PROD_EPCORE) && !defined(CONFIG_PF_XCPPGC)
+#if REG_ROOT
   mps_root_destroy(reg_root);
 #endif 
   mps_thread_dereg(thread);
