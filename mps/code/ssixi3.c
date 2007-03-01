7046c284311cce114a349dbf6a04aaa34b5990a5
David Jones <drj@ravenbrook.com>
2007-03-01 19:14:47 +0000

    Mps: stack scanner for darwin (os x).
    Copied from Perforce
     Change: 161873
     ServerID: perforce.ravenbrook.com

---
+++ mps/code/ssixi3.c
@@ -0,0 +1,109 @@
+/* sslii3.c: UNIX/INTEL STACK SCANNING
+ *
+ * $Id$
+ * Copyright (c) 2001 Ravenbrook Limited.  See end of file for license.
+ *
+ *  This scans the stack and fixes the registers which may contain
+ *  roots.  See <design/thread-manager/>
+ *
+ *  This code was originally developed and tested on Linux, and then
+ *  copied to the FreeBSD and Darwin (OS X) operating systems where it
+ *  also seems to work.  Note that on FreeBSD and Darwin it has not
+ *  been indepently verified with respect to any ABI documentation.
+ *
+ *  This code appears is common to more than one Unix implementation on
+ *  Intel hardware (but is not portable Unix code).
+ *
+ *  The registers edi, esi, ebx are the registers defined to be preserved
+ *  across function calls and therefore may contain roots.
+ *  These are pushed on the stack for scanning.
+ *
+ * SOURCES
+ *
+ * .source.callees.saves: Set of callee-saved registers taken from
+ * CALL_USED_REGISTERS in <gcc-sources>/config/i386/i386.h.
+ *
+ * ASSUMPTIONS
+ *
+ * .assume.align: The stack pointer is assumed to be aligned on a word
+ * boundary.
+ *
+ * .assume.asm.stack: The compiler must not do wacky things with the
+ * stack pointer around a call since we need to ensure that the
+ * callee-save regs are visible during TraceScanArea.
+ *
+ * .assume.asm.order: The volatile modifier should prevent movement
+ * of code, which might break .assume.asm.stack.
+ *
+ */
+
+
+#include "mpm.h"
+
+SRCID(sslii3, "$Id$");
+
+
+/* .assume.asm.order */
+#define ASMV(x) __asm__ volatile (x)
+
+
+Res StackScan(ScanState ss, Addr *stackBot)
+{
+  Addr *stackTop;
+  Res res;
+
+  /* .assume.asm.stack */
+  ASMV("push %ebx");    /* These registers are callee-saved */
+  ASMV("push %esi");    /* and so may contain roots.  They are pushed */
+  ASMV("push %edi");    /* for scanning.  See .source.callees.saves. */
+  ASMV("mov %%esp, %0" : "=r" (stackTop) :);    /* stackTop = esp */
+
+  AVER(AddrIsAligned((Addr)stackTop, sizeof(Addr)));  /* .assume.align */
+  res = TraceScanArea(ss, stackTop, stackBot);
+
+  ASMV("add $12, %esp");  /* pop 3 regs to restore the stack pointer */
+
+  return res;
+}
+
+
+/* C. COPYRIGHT AND LICENSE
+ *
+ * Copyright (C) 2001-2002 Ravenbrook Limited <http://www.ravenbrook.com/>.
+ * All rights reserved.  This is an open source license.  Contact
+ * Ravenbrook for commercial licensing options.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * 3. Redistributions in any form must be accompanied by information on how
+ * to obtain complete source code for this software and any accompanying
+ * software that uses this software.  The source code must either be
+ * included in the distribution or be available for no more than the cost
+ * of distribution plus a nominal fee, and must be freely redistributable
+ * under reasonable conditions.  For an executable file, complete source
+ * code means the source code for all modules it contains. It does not
+ * include source code for modules or files that typically accompany the
+ * major components of the operating system on which the executable file
+ * runs.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE, OR NON-INFRINGEMENT, ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
