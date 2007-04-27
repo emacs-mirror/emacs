bebe0ee45ad0837bdd354ebff95fa72dc7272253
Richard Kistruck <rhsk@ravenbrook.com>
2007-04-27 15:37:16 +0100

    Mps br/diag: variety.ii: now has diagnostics, and no logging (pro tem);
    mpm.h: DIAG() et al under control of DIAGNOSTICS;
    diag.c: under control of DIAGNOSTICS, plus add header and copyright.
    
    Copied from Perforce
     Change: 162214
     ServerID: perforce.ravenbrook.com

--- mps/code/diag.c
+++ mps/code/diag.c
@@ -1,11 +1,16 @@
-/*$Header$
-  Implementation of diagnostic interface.
-*/
+/* diag.c: MEMORY POOL MANAGER DIAGNOSTICS
+ *
+ * $Id$
+ * Copyright (c) 2007 Ravenbrook Limited.  See end of file for license.
+ *
+ */
 
 #include <stdarg.h>
 
 #include "mpm.h"
 #include "mpslib.h" /* for mps_lib_stdout */
+
+#if defined(DIAGNOSTICS)
 
 Bool DiagEnabledGlobal = TRUE;
 
@@ -21,3 +26,51 @@ mps_lib_FILE *DiagStream(void)
 }
 
 
+#elif defined(DIAGNOSTICS_NONE)
+
+#else
+
+#error "No diagnostics configured."
+
+#endif
+
+/* C. COPYRIGHT AND LICENSE
+ *
+ * Copyright (C) 2001-2003 Ravenbrook Limited <http://www.ravenbrook.com/>.
+ * All rights reserved.  This is an open source license.  Contact
+ * Ravenbrook for commercial licensing options.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * 3. Redistributions in any form must be accompanied by information on how
+ * to obtain complete source code for this software and any accompanying
+ * software that uses this software.  The source code must either be
+ * included in the distribution or be available for no more than the cost
+ * of distribution plus a nominal fee, and must be freely redistributable
+ * under reasonable conditions.  For an executable file, complete source
+ * code means the source code for all modules it contains. It does not
+ * include source code for modules or files that typically accompany the
+ * major components of the operating system on which the executable file
+ * runs.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE, OR NON-INFRINGEMENT, ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
