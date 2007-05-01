6419d790082d62bdc68fc04674b6a3e06d5c5646
Richard Kistruck <rhsk@ravenbrook.com>
2007-05-01 12:31:22 +0100

    Mps br/diag: w3build.bat: only we, hi, di, ci, and rearrange to reduce interleaving of different deliverables.
    Copied from Perforce
     Change: 162244
     ServerID: perforce.ravenbrook.com

--- mps/code/w3build.bat
+++ mps/code/w3build.bat
@@ -7,24 +7,12 @@
 
 rmdir /q/s w3i3mv
 nmake /f w3i3mv.nmk VARIETY=we mps.lib mpsplan.lib mpsplcb.lib mpsdy.dll
-nmake /f w3i3mv.nmk VARIETY=wi mps.lib mpsplan.lib mpsplcb.lib mpsdy.dll
-nmake /f w3i3mv.nmk VARIETY=he mps.lib mpsplan.lib mpsplcb.lib mpsdy.dll
 nmake /f w3i3mv.nmk VARIETY=hi mps.lib mpsplan.lib mpsplcb.lib mpsdy.dll
 nmake /f w3i3mv.nmk VARIETY=di mps.lib mpsplan.lib mpsplcb.lib mpsdy.dll
-nmake /f w3i3mv.nmk VARIETY=ce mps.lib mpsplan.lib mpsplcb.lib mpsdy.dll
 nmake /f w3i3mv.nmk VARIETY=ci mps.lib mpsplan.lib mpsplcb.lib mpsdy.dll
 mkdir release
+
 mkdir release\include
-mkdir release\lib
-mkdir release\lib\w3i3
-mkdir release\lib\w3i3\we
-mkdir release\lib\w3i3\he
-mkdir release\lib\w3i3\ce
-mkdir release\lib\w3i3\wi
-mkdir release\lib\w3i3\hi
-mkdir release\lib\w3i3\di
-mkdir release\lib\w3i3\ci
-mkdir release\src
 copy mps.h release\include
 copy mpsavm.h release\include
 copy mpsacl.h release\include
@@ -42,42 +30,37 @@ copy mpstd.h release\include
 copy mpsw3.h release\include
 copy mpswin.h release\include
 
-copy w3i3mv\we\mps.lib release\lib\w3i3\we
-copy w3i3mv\he\mps.lib release\lib\w3i3\he
-copy w3i3mv\ce\mps.lib release\lib\w3i3\ce
-copy w3i3mv\we\mpsplan.lib release\lib\w3i3\we
-copy w3i3mv\he\mpsplan.lib release\lib\w3i3\he
-copy w3i3mv\ce\mpsplan.lib release\lib\w3i3\ce
-copy w3i3mv\we\mpsplcb.lib release\lib\w3i3\we
-copy w3i3mv\he\mpsplcb.lib release\lib\w3i3\he
-copy w3i3mv\ce\mpsplcb.lib release\lib\w3i3\ce
-copy w3i3mv\we\mpsdy.dll release\lib\w3i3\we
-copy w3i3mv\he\mpsdy.dll release\lib\w3i3\he
-copy w3i3mv\ce\mpsdy.dll release\lib\w3i3\ce
-copy w3i3mv\we\mpsdy.lib release\lib\w3i3\we
-copy w3i3mv\he\mpsdy.lib release\lib\w3i3\he
-copy w3i3mv\ce\mpsdy.lib release\lib\w3i3\ce
+mkdir release\lib
+mkdir release\lib\w3i3
 
-copy w3i3mv\wi\mps.lib release\lib\w3i3\wi
+mkdir release\lib\w3i3\we
+copy w3i3mv\we\mps.lib release\lib\w3i3\we
+copy w3i3mv\we\mpsplan.lib release\lib\w3i3\we
+copy w3i3mv\we\mpsplcb.lib release\lib\w3i3\we
+copy w3i3mv\we\mpsdy.dll release\lib\w3i3\we
+copy w3i3mv\we\mpsdy.lib release\lib\w3i3\we
+
+mkdir release\lib\w3i3\hi
 copy w3i3mv\hi\mps.lib release\lib\w3i3\hi
-copy w3i3mv\di\mps.lib release\lib\w3i3\di
-copy w3i3mv\ci\mps.lib release\lib\w3i3\ci
-copy w3i3mv\wi\mpsplan.lib release\lib\w3i3\wi
 copy w3i3mv\hi\mpsplan.lib release\lib\w3i3\hi
-copy w3i3mv\di\mpsplan.lib release\lib\w3i3\di
-copy w3i3mv\ci\mpsplan.lib release\lib\w3i3\ci
-copy w3i3mv\wi\mpsplcb.lib release\lib\w3i3\wi
 copy w3i3mv\hi\mpsplcb.lib release\lib\w3i3\hi
-copy w3i3mv\di\mpsplcb.lib release\lib\w3i3\di
-copy w3i3mv\ci\mpsplcb.lib release\lib\w3i3\ci
-copy w3i3mv\wi\mpsdy.dll release\lib\w3i3\wi
 copy w3i3mv\hi\mpsdy.dll release\lib\w3i3\hi
-copy w3i3mv\di\mpsdy.dll release\lib\w3i3\di
-copy w3i3mv\ci\mpsdy.dll release\lib\w3i3\ci
-copy w3i3mv\wi\mpsdy.lib release\lib\w3i3\wi
 copy w3i3mv\hi\mpsdy.lib release\lib\w3i3\hi
+
+mkdir release\lib\w3i3\di
+copy w3i3mv\di\mps.lib release\lib\w3i3\di
+copy w3i3mv\di\mpsplan.lib release\lib\w3i3\di
+copy w3i3mv\di\mpsplcb.lib release\lib\w3i3\di
+copy w3i3mv\di\mpsdy.dll release\lib\w3i3\di
 copy w3i3mv\di\mpsdy.lib release\lib\w3i3\di
+
+mkdir release\lib\w3i3\ci
+copy w3i3mv\ci\mps.lib release\lib\w3i3\ci
+copy w3i3mv\ci\mpsplan.lib release\lib\w3i3\ci
+copy w3i3mv\ci\mpsplcb.lib release\lib\w3i3\ci
+copy w3i3mv\ci\mpsdy.dll release\lib\w3i3\ci
 copy w3i3mv\ci\mpsdy.lib release\lib\w3i3\ci
 
+mkdir release\src
 copy mpsliban.c release\src
 copy mpsioan.c release\src
