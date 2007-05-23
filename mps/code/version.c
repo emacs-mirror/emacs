2e979e4340705fab11858123d6d1c3ac99315d25
Richard Kistruck <rhsk@ravenbrook.com>
2007-05-23 16:56:01 +0100

    Mps br/diag: release exp-162399
    Copied from Perforce
     Change: 162399
     ServerID: perforce.ravenbrook.com

--- mps/code/version.c
+++ mps/code/version.c
@@ -29,7 +29,7 @@ SRCID(version, "$Id$");
  * (Note: before 2006-02-01 the style was "release.epcore.chub")
  */
 
-#define MPS_RELEASE "exp-162395"
+#define MPS_RELEASE "exp-162399"
 
 
 /* MPSCopyrightNotice -- copyright notice for the binary
