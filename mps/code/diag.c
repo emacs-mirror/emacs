bb326c6b66143d38bff02157758197463ec11bd3
Richard Kistruck <rhsk@ravenbrook.com>
2007-08-09 12:11:29 +0100

    Mps br/diagtag: writef (mpm.c) uses new stream_fputc/s(), but still use mps_lib_file*
    and mps_lib_EOF.
    diag.c:
      Diag is a buffer capable of holding a diagnostic.
      FilterStream can buffer and filter diagnostics.
      Rule is a selection rule for filtering diagnostics.
      FilterOutput does output (but does not apply the rules yet).
      DiagStream can be switched to either FilterStream() or mps_lib_stdout().  
    
    Copied from Perforce
     Change: 163067
     ServerID: perforce.ravenbrook.com

--- mps/code/diag.c
+++ mps/code/diag.c
@@ -11,6 +11,198 @@
 #include "mpslib.h" /* for mps_lib_stdout */
 
 
+static mps_lib_FILE *FilterStream(void);
+static int FilterStream_fputc(int c, mps_lib_FILE *stream);
+static int FilterStream_fputs(const char *s, mps_lib_FILE *stream);
+
+
+/* Stream -- output to FilterStream or to a real mps_lib_FILE stream
+ *
+ * There are only two functions and two destinations; a full class 
+ * hierarchy would be overkill!  RHSK 2007-08-08.
+ */
+
+int stream_fputc(int c, mps_lib_FILE *stream)
+{
+  if(stream == FilterStream())
+    return FilterStream_fputc(c, stream);
+  else
+    return mps_lib_fputc(c, stream);
+}
+
+int stream_fputs(const char *s, mps_lib_FILE *stream)
+{
+  if(stream == FilterStream())
+    return FilterStream_fputs(s, stream);
+  else
+    return mps_lib_fputs(s, stream);
+}
+
+
+/* Diag -- buffer to store a diagnostic
+ *
+ */
+
+enum {DIAG_BUF_SIZE = 10000};
+
+typedef struct DiagStruct {
+  /* @@ needs sig */
+  const char  *tag;
+  Count n;
+  char buf[DIAG_BUF_SIZE];
+} *Diag;
+
+
+/* FilterStream -- capable of filtering diagnostics
+ *
+ * This is not really an mps_lib_FILE*; it is a single global instance 
+ * of a DiagStruct.
+ *
+ * Output is stored in a buffer in a DiagStruct, to be filtered and 
+ * output when complete.
+ */
+
+struct DiagStruct FilterDiagGlobal;
+
+static mps_lib_FILE *FilterStream(void)
+{
+  return (mps_lib_FILE*)&FilterDiagGlobal;
+}
+
+static mps_lib_FILE *FilterUnderlyingStream(void)
+{
+  return mps_lib_stdout;
+}
+
+typedef struct RuleStruct {
+  const char *action;
+  const char *tag;
+  const char *para;
+  const char *line;
+  int state;
+} *Rule;
+
+struct RuleStruct RulesGlobal[] = {
+  { "+", "TraceStart", "*", "*" }
+};
+
+static void FilterOutput(Diag diag, Rule rules)
+{
+  Res res;
+  Index i;
+  Bool newline = TRUE;
+  
+  if(diag->tag == NULL)
+    diag->tag = "(...no tag...)";
+
+  res = WriteF(FilterUnderlyingStream(), "\nMPS.$S {\n", diag->tag, NULL);
+  AVER(res == ResOK);
+
+  for(i = 0; i < diag->n; i++) {
+    char c;
+    int r;
+
+    if(newline) {
+      r = stream_fputc(' ', FilterUnderlyingStream());
+      AVER(r != mps_lib_EOF);
+      newline = FALSE;
+    }
+    
+    c = diag->buf[i];
+    r = stream_fputc(c, FilterUnderlyingStream());
+    AVER(r != mps_lib_EOF);
+    
+    if(c == '\n') {
+      newline = TRUE;
+    }
+  }
+
+  res = WriteF(FilterUnderlyingStream(), "} MPS.$S\n", diag->tag, NULL);
+  AVER(res == ResOK);
+}
+
+static void FilterStream_TagBegin(mps_lib_FILE *stream, const char *tag)
+{
+  Diag diag;
+  diag = (Diag)stream;
+  /* AVERT(Diag, diag) */
+  
+  AVER(diag->tag == NULL);
+
+  /* @@ when all diags are tagged, the buffer must be empty */
+  /* @@ but for now, as a courtesy... */
+  if(diag->n > 0) {
+    FilterOutput(diag, NULL);
+    diag->n = 0;
+  }
+
+  AVER(diag->n == 0);
+  diag->tag = tag;
+}
+
+static void FilterStream_TagEnd(mps_lib_FILE *stream, const char *tag)
+{
+  Diag diag;
+  diag = (Diag)stream;
+  /* AVERT(Diag, diag) */
+
+  AVER(diag->tag != NULL);
+  /* AVER(strequal(diag->tag, tag)); */
+
+  /* Output the diag */
+  FilterOutput(diag, &RulesGlobal[0]);
+
+  diag->tag = NULL;
+  diag->n = 0;
+}
+
+static int FilterStream_fputc(int c, mps_lib_FILE *stream)
+{
+  Diag diag;
+  
+  AVER(c != mps_lib_EOF);
+  AVER(stream == FilterStream());
+
+  diag = (Diag)stream;
+  /* AVERT(Diag, diag) */
+  /* @@ when all diags are tagged: AVER(diag->tag != NULL); */
+
+  AVER(diag->n + 1 <= sizeof(diag->buf));
+  if (diag->n + 1 > sizeof(diag->buf))
+    return mps_lib_EOF;
+  
+  /* add c to buffer */
+  diag->buf[diag->n++] = c;
+  return c;
+}
+
+static int FilterStream_fputs(const char *s, mps_lib_FILE *stream)
+{
+  Diag diag;
+  Count l;
+  Index i;
+  
+  AVER(s);
+  AVER(stream == FilterStream());
+
+  diag = (Diag)stream;
+  /* AVERT(Diag, diag) */
+  /* @@ when all diags are tagged: AVER(diag->tag != NULL); */
+
+  l = StringLength(s);
+  
+  AVER(diag->n + l <= sizeof(diag->buf));
+  if (diag->n + l > sizeof(diag->buf))
+    return mps_lib_EOF;
+
+  /* add s to buffer */
+  for (i = 0; i < l; i++) {
+    diag->buf[diag->n++] = s[i];
+  }
+  return 1;
+}
+
+
 /* DIAG_WITH_STREAM_AND_WRITEF -- Diagnostic output channel
  *
  * Only used for DIAG_WITH_STREAM_AND_WRITEF; see config.h.
@@ -25,45 +217,64 @@ Bool DiagIsOn(void)
 
 mps_lib_FILE *DiagStream(void)
 {
-  return mps_lib_stdout;
+  if(0) {
+    return FilterStream();
+  } else {
+    return mps_lib_stdout;
+  }
 }
 
-const char *DiagTagGlobal = NULL;
-
-static void DiagTagBegin(const char *tag)
+static void DiagTagBegin(mps_lib_FILE *stream, const char *tag)
 {
-  Res res;
+  AVER(stream);
+  AVER(tag);
 
-  AVER(DiagTagGlobal == NULL);
-  DiagTagGlobal = tag;
-  res = WriteF(DiagStream(), "MPS.$S { ", tag, NULL);
-  AVER(res == ResOK);
+  if(stream == FilterStream()) {
+    FilterStream_TagBegin(stream, tag);
+  } else {
+    Res res;
+    res = WriteF(stream, "MPS.$S {\n", tag, NULL);
+    AVER(res == ResOK);
+  }
 }
 
-static void DiagTagEnd(const char *tag)
+static void DiagTagEnd(mps_lib_FILE *stream, const char *tag)
 {
-  Res res;
+  AVER(stream);
+  AVER(tag);
 
-  AVER(DiagTagGlobal != NULL);
-  /* AVER(strequal(DiagTagGlobal, tag)); */
-  res = WriteF(DiagStream(), "} MPS.$S\n", tag, NULL);
-  DiagTagGlobal = NULL;
-  AVER(DiagTagGlobal == NULL);
+  if(stream == FilterStream()) {
+    FilterStream_TagEnd(stream, tag);
+  } else {
+    Res res;
+    res = WriteF(stream, "} MPS.$S\n", tag, NULL);
+    AVER(res == ResOK);
+  }
 }
+
+
+/* Diag*F functions -- interface for general MPS code (via macros)
+ *
+ * These function manage TagBegin/End, and WriteF the text to 
+ * DiagStream().
+ *
+ * Direct writing to DiagStream() is also permitted (eg. from a
+ * Describe method).
+ */
 
 void DiagSingleF(const char *tag, ...)
 {
   va_list args;
   Res res;
 
-  DiagTagBegin(tag);
+  DiagTagBegin(DiagStream(), tag);
 
   va_start(args, tag);
   res = WriteF_v(DiagStream(), args);
   AVER(res == ResOK);
   va_end(args);
 
-  DiagTagEnd(tag);
+  DiagTagEnd(DiagStream(), tag);
 }
 
 void DiagFirstF(const char *tag, ...)
@@ -71,7 +282,7 @@ void DiagFirstF(const char *tag, ...)
   va_list args;
   Res res;
 
-  DiagTagBegin(tag);
+  DiagTagBegin(DiagStream(), tag);
 
   va_start(args, tag);
   res = WriteF_v(DiagStream(), args);
@@ -97,7 +308,7 @@ void DiagMoreF(const char *firstformat, ...)
 
 void DiagEnd(const char *tag)
 {
-  DiagTagEnd(tag);
+  DiagTagEnd(DiagStream(), tag);
 }
 
 extern void diag_test(void);
