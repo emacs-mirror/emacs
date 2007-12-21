3830b161c4eb615dbf7b5c552abda253ee7f7c43
Richard Kistruck <rhsk@ravenbrook.com>
2007-12-21 15:36:58 +0000

    Mps exp-163669
    Copied from Perforce
     Change: 163669
     ServerID: perforce.ravenbrook.com

--- mps/code/version.c
+++ mps/code/version.c
@@ -29,7 +29,7 @@ SRCID(version, "$Id$");
  * (Note: before 2006-02-01 the style was "release.epcore.chub")
  */
 
-#define MPS_RELEASE "release/1.108.0"
+#define MPS_RELEASE "exp-163669"
 
 
 /* MPSCopyrightNotice -- copyright notice for the binary
