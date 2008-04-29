87c5b1791f2393646a083fad65cd6d84b4344e51
Richard Kistruck <rhsk@ravenbrook.com>
2008-04-29 15:10:30 +0100

    Mps exp-164852
    Copied from Perforce
     Change: 164852
     ServerID: perforce.ravenbrook.com

--- mps/code/version.c
+++ mps/code/version.c
@@ -29,7 +29,7 @@ SRCID(version, "$Id$");
  * (Note: before 2006-02-01 the style was "release.epcore.chub")
  */
 
-#define MPS_RELEASE "exp-164845"
+#define MPS_RELEASE "exp-164852"
 
 
 /* MPSCopyrightNotice -- copyright notice for the binary
