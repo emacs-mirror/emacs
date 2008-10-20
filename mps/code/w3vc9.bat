fc9c9ef7b337ad45bfe09a5790a6497c80084e58
Richard Kistruck <rhsk@ravenbrook.com>
2008-10-20 16:51:50 +0100

    Mps br/vc9: new w3i3m9 clone-platform:
    Regular changes w3i3mv ->w3i3m9
    
    Copied from Perforce
     Change: 166504
     ServerID: perforce.ravenbrook.com

--- mps/code/w3vc9.bat
+++ mps/code/w3vc9.bat
@@ -6,73 +6,50 @@
 @rem Copyright (C) 2005-2008 Ravenbrook Limited.  All rights reserved.
 
 
-@set mpsreleasename=exp-166419
+IF DEFINED mpsreleasename (
+@echo "Adding vc9 build into mpsreleasename=%mpsreleasename%"
+) ELSE (
+@echo "Error: mpsreleasename is not defined."
+@echo "You must run w3build.bat first, then run VC9's vcvars32.bat, then run this script."
+exit /b
+)
 
+rmdir /q/s w3i3m9
+nmake /f w3i3m9.nmk VARIETY=we mps.lib mpsplan.lib mpsplcb.lib mpsdy.dll
+nmake /f w3i3m9.nmk VARIETY=hi mps.lib mpsplan.lib mpsplcb.lib mpsdy.dll
+nmake /f w3i3m9.nmk VARIETY=di mps.lib mpsplan.lib mpsplcb.lib mpsdy.dll
+nmake /f w3i3m9.nmk VARIETY=ci mps.lib mpsplan.lib mpsplcb.lib mpsdy.dll
 
-rmdir /q/s w3i3mv
-nmake /f w3i3mv.nmk VARIETY=we mps.lib mpsplan.lib mpsplcb.lib mpsdy.dll
-nmake /f w3i3mv.nmk VARIETY=hi mps.lib mpsplan.lib mpsplcb.lib mpsdy.dll
-nmake /f w3i3mv.nmk VARIETY=di mps.lib mpsplan.lib mpsplcb.lib mpsdy.dll
-nmake /f w3i3mv.nmk VARIETY=ci mps.lib mpsplan.lib mpsplcb.lib mpsdy.dll
+mkdir %mpsreleasename%\lib\w3i3m9
 
-rmdir /q/s %mpsreleasename%
-mkdir %mpsreleasename%
+mkdir %mpsreleasename%\lib\w3i3m9\we
+@echo we-%mpsreleasename% > %mpsreleasename%\lib\w3i3m9\we\we-%mpsreleasename%.txt
+copy w3i3m9\we\mps.lib %mpsreleasename%\lib\w3i3m9\we
+copy w3i3m9\we\mpsplan.lib %mpsreleasename%\lib\w3i3m9\we
+copy w3i3m9\we\mpsplcb.lib %mpsreleasename%\lib\w3i3m9\we
+copy w3i3m9\we\mpsdy.dll %mpsreleasename%\lib\w3i3m9\we
+copy w3i3m9\we\mpsdy.lib %mpsreleasename%\lib\w3i3m9\we
 
-mkdir %mpsreleasename%\include
-copy mps.h %mpsreleasename%\include
-copy mpsavm.h %mpsreleasename%\include
-copy mpsacl.h %mpsreleasename%\include
-copy mpscamc.h %mpsreleasename%\include
-copy mpscams.h %mpsreleasename%\include
-copy mpscawl.h %mpsreleasename%\include
-copy mpsclo.h %mpsreleasename%\include
-copy mpscmv.h %mpsreleasename%\include
-copy mpscmvff.h %mpsreleasename%\include
-copy mpscsnc.h %mpsreleasename%\include
-copy mpsio.h %mpsreleasename%\include
-copy mpslib.h %mpsreleasename%\include
-copy mpslibcb.h %mpsreleasename%\include
-copy mpstd.h %mpsreleasename%\include
-copy mpsw3.h %mpsreleasename%\include
-copy mpswin.h %mpsreleasename%\include
+mkdir %mpsreleasename%\lib\w3i3m9\hi
+@echo hi-%mpsreleasename% > %mpsreleasename%\lib\w3i3m9\hi\hi-%mpsreleasename%.txt
+copy w3i3m9\hi\mps.lib %mpsreleasename%\lib\w3i3m9\hi
+copy w3i3m9\hi\mpsplan.lib %mpsreleasename%\lib\w3i3m9\hi
+copy w3i3m9\hi\mpsplcb.lib %mpsreleasename%\lib\w3i3m9\hi
+copy w3i3m9\hi\mpsdy.dll %mpsreleasename%\lib\w3i3m9\hi
+copy w3i3m9\hi\mpsdy.lib %mpsreleasename%\lib\w3i3m9\hi
 
-mkdir %mpsreleasename%\lib
-copy w3gen.def %mpsreleasename%\lib\mps-fns.def
+mkdir %mpsreleasename%\lib\w3i3m9\di
+@echo di-%mpsreleasename% > %mpsreleasename%\lib\w3i3m9\di\di-%mpsreleasename%.txt
+copy w3i3m9\di\mps.lib %mpsreleasename%\lib\w3i3m9\di
+copy w3i3m9\di\mpsplan.lib %mpsreleasename%\lib\w3i3m9\di
+copy w3i3m9\di\mpsplcb.lib %mpsreleasename%\lib\w3i3m9\di
+copy w3i3m9\di\mpsdy.dll %mpsreleasename%\lib\w3i3m9\di
+copy w3i3m9\di\mpsdy.lib %mpsreleasename%\lib\w3i3m9\di
 
-mkdir %mpsreleasename%\lib\w3i3
-
-mkdir %mpsreleasename%\lib\w3i3\we
-@echo we-%mpsreleasename% > %mpsreleasename%\lib\w3i3\we\we-%mpsreleasename%.txt
-copy w3i3mv\we\mps.lib %mpsreleasename%\lib\w3i3\we
-copy w3i3mv\we\mpsplan.lib %mpsreleasename%\lib\w3i3\we
-copy w3i3mv\we\mpsplcb.lib %mpsreleasename%\lib\w3i3\we
-copy w3i3mv\we\mpsdy.dll %mpsreleasename%\lib\w3i3\we
-copy w3i3mv\we\mpsdy.lib %mpsreleasename%\lib\w3i3\we
-
-mkdir %mpsreleasename%\lib\w3i3\hi
-@echo hi-%mpsreleasename% > %mpsreleasename%\lib\w3i3\hi\hi-%mpsreleasename%.txt
-copy w3i3mv\hi\mps.lib %mpsreleasename%\lib\w3i3\hi
-copy w3i3mv\hi\mpsplan.lib %mpsreleasename%\lib\w3i3\hi
-copy w3i3mv\hi\mpsplcb.lib %mpsreleasename%\lib\w3i3\hi
-copy w3i3mv\hi\mpsdy.dll %mpsreleasename%\lib\w3i3\hi
-copy w3i3mv\hi\mpsdy.lib %mpsreleasename%\lib\w3i3\hi
-
-mkdir %mpsreleasename%\lib\w3i3\di
-@echo di-%mpsreleasename% > %mpsreleasename%\lib\w3i3\di\di-%mpsreleasename%.txt
-copy w3i3mv\di\mps.lib %mpsreleasename%\lib\w3i3\di
-copy w3i3mv\di\mpsplan.lib %mpsreleasename%\lib\w3i3\di
-copy w3i3mv\di\mpsplcb.lib %mpsreleasename%\lib\w3i3\di
-copy w3i3mv\di\mpsdy.dll %mpsreleasename%\lib\w3i3\di
-copy w3i3mv\di\mpsdy.lib %mpsreleasename%\lib\w3i3\di
-
-mkdir %mpsreleasename%\lib\w3i3\ci
-@echo ci-%mpsreleasename% > %mpsreleasename%\lib\w3i3\ci\ci-%mpsreleasename%.txt
-copy w3i3mv\ci\mps.lib %mpsreleasename%\lib\w3i3\ci
-copy w3i3mv\ci\mpsplan.lib %mpsreleasename%\lib\w3i3\ci
-copy w3i3mv\ci\mpsplcb.lib %mpsreleasename%\lib\w3i3\ci
-copy w3i3mv\ci\mpsdy.dll %mpsreleasename%\lib\w3i3\ci
-copy w3i3mv\ci\mpsdy.lib %mpsreleasename%\lib\w3i3\ci
-
-mkdir %mpsreleasename%\src
-copy mpsliban.c %mpsreleasename%\src
-copy mpsioan.c %mpsreleasename%\src
+mkdir %mpsreleasename%\lib\w3i3m9\ci
+@echo ci-%mpsreleasename% > %mpsreleasename%\lib\w3i3m9\ci\ci-%mpsreleasename%.txt
+copy w3i3m9\ci\mps.lib %mpsreleasename%\lib\w3i3m9\ci
+copy w3i3m9\ci\mpsplan.lib %mpsreleasename%\lib\w3i3m9\ci
+copy w3i3m9\ci\mpsplcb.lib %mpsreleasename%\lib\w3i3m9\ci
+copy w3i3m9\ci\mpsdy.dll %mpsreleasename%\lib\w3i3m9\ci
+copy w3i3m9\ci\mpsdy.lib %mpsreleasename%\lib\w3i3m9\ci
