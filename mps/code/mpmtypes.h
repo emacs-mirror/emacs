b88d9e7cdda84b2c71d089c325ef232739e9df92
Richard Kistruck <rhsk@ravenbrook.com>
2008-11-11 16:57:30 +0000

    Mps br/timing: new type "clock", internal equivalent of mps_clock_t.
    Used for a field in messages.  (More uses to follow).
    
    Copied from Perforce
     Change: 166697
     ServerID: perforce.ravenbrook.com

--- mps/code/mpmtypes.h
+++ mps/code/mpmtypes.h
@@ -40,6 +40,7 @@ typedef unsigned Shift;                 /* <design/type/#shift> */
 typedef unsigned Serial;                /* <design/type/#serial> */
 typedef Addr Ref;                       /* <design/type/#ref> */
 typedef void *Pointer;                  /* <design/type/#pointer> */
+typedef unsigned long Clock;            /* processor time */
 
 typedef Word RefSet;                    /* design.mps.refset */
 typedef Word ZoneSet;                   /* design.mps.refset */
