4d55c060c8075d6069a1907f87af3de3179d6502
Richard Kistruck <rhsk@ravenbrook.com>
2009-02-05 15:48:58 +0000

    Mps br/timing poolamc.c: [back out previous broken code] revert to original poolamc.c
    Copied from Perforce
     Change: 167342
     ServerID: perforce.ravenbrook.com

--- mps/code/poolamc.c
+++ mps/code/poolamc.c
@@ -363,7 +363,6 @@ static Bool amcSegHasNailboard(Seg seg)
 
   type = *amcSegTypeP(seg);
   AVER(type == AMCPTypeNailboard || type == AMCPTypeGen);
-  AVER(type != AMCPTypeNailboard);  /* RHSK hack: nailboards disabled */
   return type == AMCPTypeNailboard;
 }
 
@@ -681,7 +680,6 @@ static Res amcGenDescribe(amcGen gen, mps_lib_FILE *stream)
 
 /* amcSegCreateNailboard -- create nailboard for segment */
 
-#if 0
 static Res amcSegCreateNailboard(Seg seg, Pool pool)
 {
   amcNailboard board;
@@ -722,7 +720,7 @@ failMarkTable:
 failAllocNailboard:
   return res;
 }
-#endif
+
 
 /* amcSegDestroyNailboard -- destroy the nailboard of a segment */
 
@@ -792,7 +790,6 @@ static Bool amcNailGetAndSetMark(Seg seg, Ref ref)
  * bits that correspond to client pointers for them.  We may assume
  * that the range is unmarked.
  */
-#if 0
 static void amcNailMarkRange(Seg seg, Addr base, Addr limit)
 {
   amcNailboard board;
@@ -818,7 +815,7 @@ static void amcNailMarkRange(Seg seg, Addr base, Addr limit)
   board->nails += ilimit - ibase;
   board->distinctNails += ilimit - ibase;
 }
-#endif
+
 
 /* amcNailRangeIsMarked -- check that a range in the board is marked
  *
@@ -1221,9 +1218,6 @@ static Res AMCWhiten(Pool pool, Trace trace, Seg seg)
         /* There is an active buffer, make sure it's nailed. */
         if(!amcSegHasNailboard(seg)) {
           if(SegNailed(seg) == TraceSetEMPTY) {
-#if 1
-            UNUSED(res);
-#else
             res = amcSegCreateNailboard(seg, pool);
             if(res != ResOK) {
               /* Can't create nailboard, don't condemn. */
@@ -1233,7 +1227,6 @@ static Res AMCWhiten(Pool pool, Trace trace, Seg seg)
               amcNailMarkRange(seg, BufferScanLimit(buffer),
                                BufferLimit(buffer));
             }
-#endif
             ++trace->nailCount;
             SegSetNailed(seg, TraceSetSingle(trace));
           } else {
@@ -1646,11 +1639,9 @@ Res AMCFix(Pool pool, ScanState ss, Seg seg, Ref *refIO)
     /* we will lose some pointer fixes because we introduced a */
     /* nailboard). */
     if(SegNailed(seg) == TraceSetEMPTY) {
-#if 0
       res = amcSegCreateNailboard(seg, pool);
       if(res != ResOK)
         return res;
-#endif
       ++ss->nailCount;
       SegSetNailed(seg, TraceSetUnion(SegNailed(seg), ss->traces));
     }
@@ -1798,11 +1789,9 @@ static Res AMCHeaderFix(Pool pool, ScanState ss, Seg seg, Ref *refIO)
     /* we will lose some pointer fixes because we introduced a */
     /* nailboard). */
     if(SegNailed(seg) == TraceSetEMPTY) {
-#if 0
       res = amcSegCreateNailboard(seg, pool);
       if(res != ResOK)
         return res;
-#endif
       ++ss->nailCount;
       SegSetNailed(seg, TraceSetUnion(SegNailed(seg), ss->traces));
     }
