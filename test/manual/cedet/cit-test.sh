d4d1014f127f40af827d16051778adbb2ce76d53
zappo <zappo@users.sourceforge.net>
2009-05-30 14:26:58 +0000

    Add -f toggle-debug-on-quit flag

--- test/manual/cedet/cit-test.sh
+++ test/manual/cedet/cit-test.sh
@@ -3,4 +3,4 @@
 EMACS=${EMACS:-emacs};
 
 rm -r /tmp/CEDET_INTEG*
-$EMACS -q -l ../common/cedet.el -l cit-load.el -f toggle-debug-on-error  -f cedet-integ-test
+$EMACS -q -l ../common/cedet.el -l cit-load.el -f toggle-debug-on-error  -f toggle-debug-on-quit -f cedet-integ-test
