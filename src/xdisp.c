612620d0b2a74747804bb161f0886ba3538163f3
Kenichi Handa <handa@m17n.org>
2009-06-11 02:34:34 +0000

    (x_get_glyph_overhangs): Fix calculation of right overhang for the static composition case.

--- src/xdisp.c
+++ src/xdisp.c
@@ -19915,8 +19915,8 @@ x_get_glyph_overhangs (glyph, f, left, right)
 
 	  if (cmp->rbearing > cmp->pixel_width)
 	    *right = cmp->rbearing - cmp->pixel_width;
-	  if (cmp->lbearing < 0);
-	  *left = - cmp->lbearing;
+	  if (cmp->lbearing < 0)
+	    *left = - cmp->lbearing;
 	}
       else
 	{
