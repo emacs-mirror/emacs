d58e8d8aa47fe57625c6e2e883d3cf8503dea318
zappo <zappo@users.sourceforge.net>
2009-08-08 22:15:31 +0000

    Reverse the exist status of Emacs.

--- test/manual/cedet/cit-test.sh
+++ test/manual/cedet/cit-test.sh
@@ -8,4 +8,11 @@ if [ -z $1 ]; then
 fi
 
 rm -r /tmp/CEDET_INTEG*
-$EMACS -q -l ../common/cedet.el -l cit-load.el -f toggle-debug-on-error  -f toggle-debug-on-quit -f cedet-integ-test-${STYLE}
+
+if $EMACS -q -l ../common/cedet.el -l cit-load.el -f toggle-debug-on-error  -f toggle-debug-on-quit -f cedet-integ-test-${STYLE}; then
+    # Reverse the meaning of a 0 exit status, as the user had to quit Emacs
+    # but on success, the program kills emacs with 1 (to be different.)
+    exit 1;
+else
+    exit 0;
+fi
