b76efc742abea79391b8dc5678df9e242d844d43
zappo <zappo@users.sourceforge.net>
2009-08-08 21:52:40 +0000

    Determine the STYLE of test to run. If none passed in, use Make.

--- test/manual/cedet/cit-test.sh
+++ test/manual/cedet/cit-test.sh
@@ -2,5 +2,10 @@
 
 EMACS=${EMACS:-emacs};
 
+STYLE=$1;
+if [ -z $1 ]; then
+    STYLE=Make
+fi
+
 rm -r /tmp/CEDET_INTEG*
-$EMACS -q -l ../common/cedet.el -l cit-load.el -f toggle-debug-on-error  -f toggle-debug-on-quit -f cedet-integ-test
+$EMACS -q -l ../common/cedet.el -l cit-load.el -f toggle-debug-on-error  -f toggle-debug-on-quit -f cedet-integ-test-${STYLE}
