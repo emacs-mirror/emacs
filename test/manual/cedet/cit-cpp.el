0141378a741abe12b59d757f9ced47570d02be4c
xscript <xscript@users.sourceforge.net>
2010-09-15 16:27:53 +0200

    Remove obsolete cvs-auto-updated 'X-RCS' line.

--- test/manual/cedet/cit-cpp.el
+++ test/manual/cedet/cit-cpp.el
@@ -3,7 +3,6 @@
 ;; Copyright (C) 2008, 2009, 2010 Eric M. Ludlam
 
 ;; Author: Eric M. Ludlam <eric@siege-engine.com>
-;; X-RCS: $Id: cit-cpp.el,v 1.13 2010-06-13 01:14:46 zappo Exp $
 
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License as
