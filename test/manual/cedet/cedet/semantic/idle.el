97673689be8979093ecb9a1f99f7d4f41eafaff8
xscript <xscript@users.sourceforge.net>
2011-05-15 22:16:47 +0200

    Synchronize cedet/semantic with Emacs.

---
+++ test/manual/cedet/cedet/semantic/idle.el
@@ -0,0 +1,7 @@
+(defun semantic-idle-pnf-test ()
+  "Test `semantic-idle-scheduler-work-parse-neighboring-files' and time it."
+  (interactive)
+  (let ((start (current-time))
+        (junk (semantic-idle-scheduler-work-parse-neighboring-files))
+        (end (current-time)))
+    (message "Work took %.2f seconds." (semantic.elapsed-time start end))))
