0781aac6ccf2e5167d763e28e83562dd5eaeb590
Eric Ludlam <zappo@gnu.org>
2011-08-20 12:58:58 -0400

    Add comments, require cedet-files.

--- test/manual/cedet/cedet-files-utests.el
+++ test/manual/cedet/cedet-files-utests.el
@@ -1,3 +1,31 @@
+;;; cedet-files-utests.el ---
+;;
+;; Copyright (C) 2011 Eric M. Ludlam
+;;
+;; Author: Eric M. Ludlam <eric@siege-engine.com>
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see http://www.gnu.org/licenses/.
+
+
+;;; Commentary:
+;;
+;;
+
+(require 'cedet-files)
+
+;;; Code:
+
 (defvar cedet-files-utest-list
   '(
     ( "/home/me/src/myproj/src/foo.c" . "!home!me!src!myproj!src!foo.c" )
@@ -34,3 +62,7 @@ converted into.")
           )
 
         ))))
+
+(provide 'cedet-files-utests)
+
+;;; cedet-files-utests.el ends here
