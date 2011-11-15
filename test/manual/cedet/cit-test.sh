d8f7f9c0a9bac299650d6a401f114f382d698121
David Engster <dengste@eml.cc>
2011-11-15 21:29:00 +0100

    * tests/cit-test.sh: Fix filename for integration test.

--- test/manual/cedet/cit-test.sh
+++ test/manual/cedet/cit-test.sh
@@ -11,8 +11,9 @@ fi
 OPTIONS=$2
 
 rm -r /tmp/CEDET_INTEG*
+
 cd ..
-$EMACS $OPTIONS -q -l cedet-load.el -l tests/cit-load.el -f toggle-debug-on-error  -f toggle-debug-on-quit -f cedet-integ-test-${STYLE}
+$EMACS $OPTIONS -q -l cedet-devel-load.el -l tests/cit-load.el -f toggle-debug-on-error  -f toggle-debug-on-quit -f cedet-integ-test-${STYLE}
 EXITCODE=$?
 
 if [ $EXITCODE -eq 0 ]; then
