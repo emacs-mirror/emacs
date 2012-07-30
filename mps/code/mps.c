58bd26ec77a3f0e57f521218b0503a5ed20df03f
Richard Brooksby <rb@ravenbrook.com>
2012-07-30 16:48:02 +0100

    Adding w3i3mv to mps.c, allowing simple builds.  this now works:
    cl /Feamcss.exe /DCONFIG_PROD_MPS /DCONFIG_VAR_CI amcss.c fmtdy.c fmtdytst.c fmtno.c mps.c testlib.c
    
    Copied from Perforce
     Change: 178777
     ServerID: perforce.ravenbrook.com

--- mps/code/mps.c
+++ mps/code/mps.c
@@ -119,6 +119,21 @@
 #include "span.c"       /* generic stack probe */
 #include "ssixi3.c"     /* Posix on 32-bit Intel stack scan */
 
+/* Windows on 32-bit Intel with Microsoft Visual Studio */
+
+#elif defined(MPS_PF_W3I3MV)
+
+#include "lockw3.c"     /* Windows locks */
+#include "thw3.c"       /* Windows threading */
+#include "thw3i3.c"     /* Windows on 32-bit Intel thread stack scan */
+#include "vmw3.c"       /* Windows virtual memory */
+#include "protw3.c"     /* Windows protection */
+#include "proti3.c"     /* Intel mutator context decoding */
+#include "prmci3w3.c"   /* Windows on 32-bit Intel mutator context */
+#include "ssw3i3.c"     /* Windows on 32-bit Intel scan scan */
+#include "spi3.c"       /* Intel stack probe */
+#include "mpsiw3.c"     /* Windows interface layer extras */
+
 #else
 
 #error "Unknown platform -- can't determine platform specific parts."
