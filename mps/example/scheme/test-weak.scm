7f34f0e8cb74fe2d28c59d1bedc2c25a3dfffeb3
Gareth Rees <gdr@ravenbrook.com>
2012-11-02 21:23:12 +0000

    Implement the global symbol table as a weak-value hash table from strings to symbols.
    Copied from Perforce
     Change: 180294
     ServerID: perforce.ravenbrook.com

--- mps/example/scheme/test-weak.scm
+++ mps/example/scheme/test-weak.scm
@@ -2,29 +2,51 @@
 
 (load "test-common.scm")
 
-;; First, check that all the hash tables behave as expected.
-
-(define (ht-test ht-fun hash-fun cmp-fun key)
-  (let* ((ht (ht-fun hash-fun cmp-fun))
-         (f (lambda (n) (equal? (hashtable-ref ht (key n) #f) n)))
-         (g (lambda (n) (hashtable-set! ht (key n) n)))
-         (r (range 25)))
-    (for-each g r)
-    (all (map f r))))
-
-(define (stringify n) (make-string n #\b))
-(check '(ht-test make-hashtable string-hash string=? stringify) #t)
-(check '(ht-test make-weak-key-hashtable string-hash string=? stringify) #t)
-(check '(ht-test make-weak-value-hashtable string-hash string=? stringify) #t)
-(check '(ht-test make-doubly-weak-hashtable string-hash string=? stringify) #t)
-(define (symbolize n) (string->symbol (make-string n #\a)))
-(check '(ht-test make-hashtable eq-hash eq? symbolize) #t)
-(define (identity n) n)
-(check '(ht-test make-hashtable eqv-hash eqv? identity) #t)
-
+(define (populate ht kvs)
+  (let ((f (lambda (kv) (hashtable-set! ht (car kv) (cdr kv)))))
+    (for-each f kvs)))
 
 ;; The MPS doesn't actually guarantee promptness of splatting. But we
 ;; have to test it somehow!
+
+(define (ht-test ht-fun hash cmp f1 f2 kvs)
+  (let* ((ht (ht-fun hash cmp))
+         (f (lambda (kv) (equal? (hashtable-ref ht (car kv) #f) (cdr kv)))))
+    (populate ht kvs)
+    (list (begin (gc) (all (map f kvs)))
+          (begin (for-each f1 kvs) (gc) (hashtable-size ht))
+          (begin (for-each f2 kvs) (gc) (hashtable-size ht)))))
+
+(define (dk kv) (set-car! kv #f))
+(define (dv kv) (set-cdr! kv #f))
+
+(check '(ht-test make-hashtable string-hash string=? dk dv
+                 '(("one" . 1) ("two" . 2) ("three" . 3)))
+       '(#t 3 3))
+
+(check '(ht-test make-weak-key-hashtable eq-hash eq? dk dv
+                 '((ONE . 1) (TWO . 2) (THREE . 3)))
+       '(#t 0 0))
+
+(check '(ht-test make-weak-key-hashtable eqv-hash eqv? dv dk
+                 '((1 . 1) (2 . 2) (3 . 3)))
+       '(#t 3 0))
+
+(check '(ht-test make-weak-value-hashtable string-hash string=? dk dv
+                 '(("one" . 1) ("two" . 2) ("three" . 3)))
+       '(#t 3 0))
+
+(check '(ht-test make-weak-value-hashtable string-hash string=? dv dk
+                 '(("one" . 1) ("two" . 2) ("three" . 3)))
+       '(#t 0 0))
+
+(check '(ht-test make-doubly-weak-hashtable eq-hash eq? dk dv
+                 '(("one" . 1) ("two" . 2) ("three" . 3)))
+       '(#t 0 0))
+
+(check '(ht-test make-doubly-weak-hashtable eqv-hash eqv? dv dk
+                 '((#\a . 1) (#\b . 2) (#\c . 3)))
+       '(#t 0 0))
 
 (write-string "All tests pass.")
 (newline)
