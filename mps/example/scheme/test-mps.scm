ec1375852d6d641e7870042fe718397d4cb38cee
Gareth Rees <gdr@ravenbrook.com>
2012-11-02 17:35:05 +0000

    Refactor test code now that we can load it.
    Copied from Perforce
     Change: 180290
     ServerID: perforce.ravenbrook.com

--- mps/example/scheme/test-mps.scm
+++ mps/example/scheme/test-mps.scm
@@ -1,25 +1,6 @@
 ;;; test-mps.scm -- tests for the MPS toy Scheme interpreter
 
-(define (check exp result)
-  (write-string "test: ") (write exp) (newline)
-  (write-string "expect: ") (write result) (newline)
-  (define actually (eval exp))
-  (write-string "got: ") (write actually) (newline)
-  (if (not (equal? actually result))
-    (error exp)))
-
-;; Return (f (f (f ... (f a) ... ))) with n invocations of f.
-(define (church n f a)
-  (if (eqv? n 0)
-      a
-      (church (- n 1) f (f a))))
-
-;; Call (f n) ... (f 2) (f 1).
-(define (repeat n f)
-  (if (eqv? n 0)
-      '()
-      (begin (f n)
-             (repeat (- n 1) f))))
+(load "test-common.scm")
 
 ;; Test recursion.
 (check '(church 1000 (lambda (a) (+ 1 a)) 0) 1000)
@@ -27,10 +8,7 @@
 ;; Create strings of various lengths.
 (check '(church 50 (lambda (s) (string-append s "x")) "") (make-string 50 #\x))
 
-(define (map f l) (if (null? l) '() (cons (f (car l)) (map f (cdr l)))))
-(define (all l) (if (null? l) #t (if (car l) (all (cdr l)) #f)))
-(define (range n) (if (eqv? n 0) '() (append (range (- n 1)) (list n))))
-
+;; Test map, range.
 (check '(map (lambda (x) (+ 1 x)) '(1 2 3)) '(2 3 4))
 (check '(range 5) '(1 2 3 4 5))
 (check '(map (lambda (x) (+ 1 x)) (range 10)) (cdr (range 11)))
@@ -39,9 +17,9 @@
 (define (ht-test ht key)
   (let* ((f (lambda (n) (equal? (hashtable-ref ht (key n) #f) n)))
          (g (lambda (n) (hashtable-set! ht (key n) n)))
-         (n 100))
-    (repeat n g)
-    (all (map f (range n)))))
+         (r (range 100)))
+    (for-each g r)
+    (all (map f r))))
 
 (define (stringify n) (make-string n #\b))
 (check '(ht-test (make-hashtable string-hash string=?) stringify) #t)
