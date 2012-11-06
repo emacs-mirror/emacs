6622746c68bd36f8463323b7a913c3aaab1599f1
Gareth Rees <gdr@ravenbrook.com>
2012-11-06 22:37:53 +0000

    Implement lots more r4rs.
    Copied from Perforce
     Change: 180377
     ServerID: perforce.ravenbrook.com

--- mps/example/scheme/test-common.scm
+++ mps/example/scheme/test-common.scm
@@ -1,5 +1,7 @@
 ;;; test-common.scm -- common definitions for the Scheme tests
 
+(load "r4rs.scm")
+
 (define (check exp result)
   (let ((actually (eval exp)))
     (if (not (equal? actually result))
@@ -10,14 +12,8 @@
           (error "failed!")))))
 
 ;; Return (f (f (f ... (f a) ... ))) with n invocations of f.
-(define (church n f a)
-  (if (eqv? n 0)
-      a
-      (church (- n 1) f (f a))))
-
-(define (map f l) (if (null? l) '() (cons (f (car l)) (map f (cdr l)))))
+(define (church n f a) (if (eqv? n 0) a (church (- n 1) f (f a))))
 (define (all l) (if (null? l) #t (if (car l) (all (cdr l)) #f)))
 (define (range n) (if (eqv? n 0) '() (append (range (- n 1)) (list n))))
-(define (for-each f l) (if (null? l) #f (begin (f (car l)) (for-each f (cdr l)))))
 (define (reduce f l a) (if (null? l) a (f (car l) (reduce f (cdr l) a))))
 (define (sum l) (reduce + l 0))
