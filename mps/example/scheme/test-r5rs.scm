fd03ed65aa855eae85e423383e25b691eb3bf835
Gareth Rees <gdr@ravenbrook.com>
2012-11-11 19:34:46 +0000

    Implement "do".
    Copied from Perforce
     Change: 180441
     ServerID: perforce.ravenbrook.com

--- mps/example/scheme/test-r5rs.scm
+++ mps/example/scheme/test-r5rs.scm
@@ -403,8 +403,8 @@
 
 ;;; do
 
-;; UNIMPL: (check '(do ((vec (make-vector 5)) (i 0 (+ i 1))) ((= i 5) vec) (vector-set! vec i i)) '#(0 1 2 3 4))
-;; UNIMPL: (check '(let ((x '(1 3 5 7 9))) (do ((x x (cdr x)) (sum 0 (+ sum (car x)))) ((null? x) sum))) '25)
+(check '(do ((vec (make-vector 5)) (i 0 (+ i 1))) ((= i 5) vec) (vector-set! vec i i)) '#(0 1 2 3 4))
+(check '(let ((x '(1 3 5 7 9))) (do ((x x (cdr x)) (sum 0 (+ sum (car x)))) ((null? x) sum))) '25)
 
 ;;; named let
 
