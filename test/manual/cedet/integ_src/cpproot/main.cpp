c856c2024c9aae4a69ef0887cdf41f05e508d280
Eric Ludlam <zappo@gnu.org>
2012-12-31 22:19:00 -0500

    New test file.

---
+++ test/manual/cedet/integ_src/cpproot/main.cpp
@@ -0,0 +1,38 @@
+/*
+ * Main program for testing ede/cpp-root project.
+ */
+
+#include "sppmacros.h"
+#include "test.h"
+
+int main(int argc, char *argv[])
+{
+
+  return 0;
+}
+
+#ifdef FEATURE1
+int feature1()
+{
+  return 1;
+}
+#endif
+
+#ifdef FEATURE2
+int feature2()
+{
+  return 2;
+}
+#endif
+
+#ifdef FEATURE3
+int feature3()
+{
+  return 3;
+}
+#endif
+
+int generic_feature()
+{
+  return 0;
+}
