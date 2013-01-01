adc318b874e0ee505648931c8f13b8ad6d80bf57
Eric Ludlam <zappo@gnu.org>
2012-12-31 22:19:15 -0500

    New test file.

---
+++ test/manual/cedet/integ_src/cpproot/sppmacros.h
@@ -0,0 +1,9 @@
+/*
+ * This source file contains macros to be used by :spp-files slot in the
+ * ede/cpp-root project type.
+ */
+
+#define FEATURE1 1
+#define FEATURE2 2
+
+/* #define FEATURE3 3 */
