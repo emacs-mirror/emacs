9d2c338b3de88fad8970cee91b11727e6dc4f0b5
Eric Ludlam <zappo@gnu.org>
2012-12-31 22:19:29 -0500

    New test file.

---
+++ test/manual/cedet/integ_src/cpproot/test.h
@@ -0,0 +1,18 @@
+/*
+ * This file contains a few misc prototypes for declarations in other parts
+ * of the cpp-root project type.
+ */
+
+#ifdef FEATURE1
+int feature1();
+#endif
+
+#ifdef FEATURE2
+int feature2();
+#endif
+
+#ifdef FEATURE3
+int feature3();
+#endif
+
+int generic_feature();
