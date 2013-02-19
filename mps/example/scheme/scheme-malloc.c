0ff56c73fd7fc199df1b93c457a4b4de109df24e
Richard Brooksby <rb@ravenbrook.com>
2013-02-19 16:52:44 +0000

    Fixing typos in comments.
    Copied from Perforce
     Change: 180959
     ServerID: perforce.ravenbrook.com

--- mps/example/scheme/scheme-malloc.c
+++ mps/example/scheme/scheme-malloc.c
@@ -1244,7 +1244,7 @@ static obj_t load(obj_t env, obj_t op_env, const char *filename) {
 
 /* eval_list -- evaluate list of expressions giving list of results
  *
- * eval_list evaluates a list of expresions and yields a list of their
+ * eval_list evaluates a list of expressions and yields a list of their
  * results, in order.  If the list is badly formed, an error is thrown
  * using the message given.
  */
