feb202556c7f9625a6381e395be1ac390ebe0d5d
Eric Ludlam <zappo@gnu.org>
2013-03-31 12:02:26 -0400

    Don't check contents after initial fil
    * test/manual/cedet/cit-texi.el: (cit-srecode-fill-texi): Do not check
     contents after initial fill.  See comment for why.

--- test/manual/cedet/cit-texi.el
+++ test/manual/cedet/cit-texi.el
@@ -1,6 +1,6 @@
 ;;; cit-texi.el --- testing Texinfo support.
 
-;; Copyright (C) 2008 Eric M. Ludlam
+;; Copyright (C) 2008, 2013 Eric M. Ludlam
 
 ;; Author: Eric M. Ludlam <eric@siege-engine.com>
 
@@ -63,8 +63,10 @@
 
   ;; 2 b) Test various templates.
 
-  (cit-srecode-fill-with-stuff "src/foodoc.texi" cit-doc-tags
-			       "NAME" "All about the FOO.")
+  ;; We can't test the inserted resutls because the empty insert creates a
+  ;; section that isn't part of cit-doc-tags, but is discovered by the validator.
+  (cit-srecode-fill-with-stuff-notest "src/foodoc.texi" cit-doc-tags
+				      "NAME" "All about the FOO.")
 
   (re-search-forward "@menu\n")
   (sit-for 0)
