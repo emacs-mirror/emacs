f91e2cedbc7b30be734a97cf3edf6f9250358bd0
Eric Ludlam <zappo@gnu.org>
2013-03-31 15:23:40 -0400

    New pretend system include for cpproot test.

---
+++ test/manual/cedet/integ_src/fauxsyslib/cpprootsys.h
@@ -0,0 +1,27 @@
+/** cpprootsys.h --- Pretend sys header for cpproot test.
+ *
+ * Copyright (C) 2013 Eric M. Ludlam
+ *
+ * Author: Eric M. Ludlam <eric@siege-engine.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+
+
+#ifndef CPPROOTSYS_H
+#define CPPROOTSYS_H 1
+
+#define CPPSYSFCN 1
+
+#endif // CPPROOTSYS_H
