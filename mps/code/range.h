a3e4f9f493f5948599819f1f844e26f1fe64359a
Gareth Rees <gdr@ravenbrook.com>
2013-05-21 13:20:29 +0100

    Add function versions of range{base,limit,size} macros: these can be useful when debugging.
    Copied from Perforce
     Change: 182029
     ServerID: perforce.ravenbrook.com

--- mps/code/range.h
+++ mps/code/range.h
@@ -30,6 +30,9 @@ extern void RangeFinish(Range range);
 extern Res RangeDescribe(Range range, mps_lib_FILE *stream);
 extern Bool RangeCheck(Range range);
 extern Bool RangeOverlap(Range range1, Range range2);
+extern Addr (RangeBase)(Range range);
+extern Addr (RangeLimit)(Range range);
+extern Size (RangeSize)(Range range);
 
 
 /* Types */
