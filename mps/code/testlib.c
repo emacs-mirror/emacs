91a43551327c20562195577997384c7ff1737b85
Gareth Rees <gdr@ravenbrook.com>
2013-05-23 20:10:36 +0100

    Ensure that the random seed is not lost in case of failure.
    Copied from Perforce
     Change: 182131
     ServerID: perforce.ravenbrook.com

--- mps/code/testlib.c
+++ mps/code/testlib.c
@@ -274,6 +274,7 @@ void randomize(int argc, char *argv[])
            argv[0], seed0);
     rnd_state_set(seed0);
   }
+  fflush(stdout); /* ensure seed is not lost in case of failure */
 }
 
 unsigned long rnd_state(void)
