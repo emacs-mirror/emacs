54e71b9f8219c48a6fe8ccbc27d40cde15f6a096
Gareth Rees <gdr@ravenbrook.com>
2013-05-25 15:45:26 +0100

    Several formats need a find-dependent function.
    Copied from Perforce
     Change: 182228
     ServerID: perforce.ravenbrook.com

--- mps/test/test/testlib/newfmt.h
+++ mps/test/test/testlib/newfmt.h
@@ -65,6 +65,7 @@ mycell *allocone(mps_ap_t ap, int size);
 mycell *allocdumb(mps_ap_t ap, size_t bytes);
 
 mps_addr_t getdata(mycell *obj);
+mps_addr_t getassociated(mps_addr_t addr);
 void setref(mycell *obj, int n, mycell *to);
 mycell *getref(mycell *obj, int n);
 
