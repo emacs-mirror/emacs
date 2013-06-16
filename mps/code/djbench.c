d02bc32f5f61e0cf00859596459550b784483b8d
Richard Brooksby <rb@ravenbrook.com>
2013-06-16 15:36:17 +0100

    Adding leader comment description, copyright, and license.
    Copied from Perforce
     Change: 182775
     ServerID: perforce.ravenbrook.com

--- mps/code/djbench.c
+++ mps/code/djbench.c
@@ -1,5 +1,14 @@
-/* djban.c -- "DJ" Benchmark on ANSI C library
+/* djbench.c -- "DJ" Benchmark on ANSI C library
+ *
  * $Id$
+ * Copyright 2013 Ravenbrook Limited.  See end of file for license.
+ *
+ * This is an allocation stress benchmark test for manual variable pools
+ * and also for stdlib malloc/free (for comparison).
+ *
+ * It repeatedly runs over an array of blocks and allocates or frees them
+ * with some probability, then frees all the remaining blocks at the end.
+ * This test can be iterated.
  */
 
 #include <time.h>
@@ -242,3 +251,45 @@ int main(int argc, char *argv[]) {
   
   return EXIT_SUCCESS;
 }
+
+
+/* C. COPYRIGHT AND LICENSE
+ *
+ * Copyright (c) 2001-2013 Ravenbrook Limited <http://www.ravenbrook.com/>.
+ * All rights reserved.  This is an open source license.  Contact
+ * Ravenbrook for commercial licensing options.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright
+ * notice, this list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * 
+ * 3. Redistributions in any form must be accompanied by information on how
+ * to obtain complete source code for this software and any accompanying
+ * software that uses this software.  The source code must either be
+ * included in the distribution or be available for no more than the cost
+ * of distribution plus a nominal fee, and must be freely redistributable
+ * under reasonable conditions.  For an executable file, complete source
+ * code means the source code for all modules it contains. It does not
+ * include source code for modules or files that typically accompany the
+ * major components of the operating system on which the executable file
+ * runs.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE, OR NON-INFRINGEMENT, ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDERS AND CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
