83c8dc03c7a91ada7bc10c43d41bae4b8732f9a4
David Engster <dengste@eml.cc>
2013-11-04 22:31:41 +0100

    Use random temporary directory for integration tests
    * tests/cedet-integ-test.el (cedet-integ-base): Use `make-temp-name'
      so that integrations tests can run in parallel.
      (cedet-integ-test-proj): Delete it after usage.
      (cit-check-compilation-for-error): Show compilation output in
      batch-mode.
    * tests/cit-test.sh: Do not remove temporary directory.

--- test/manual/cedet/cit-test.sh
+++ test/manual/cedet/cit-test.sh
@@ -12,6 +12,7 @@ OPTIONS=$2
 
 rm -r /tmp/CEDET_INTEG*
 
+cd ..
 $EMACS $OPTIONS -q -l cedet-devel-load.el -l tests/cit-load.el -f toggle-debug-on-error  -f toggle-debug-on-quit -f cedet-integ-test-${STYLE}
 EXITCODE=$?
 
