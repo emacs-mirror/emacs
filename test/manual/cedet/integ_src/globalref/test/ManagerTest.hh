0c4deea5f627db93d5e489c5b9154236df284835
Eric Ludlam <zappo@gnu.org>
2014-01-01 20:44:55 -0500

    Test files contributed by Barry OReilly
    * test/manual/cedet/integ_src/globalref/test/ManagerTest.hh: These
     test files contributed by Barry OReilly, modified for use in the
     automated tests for CEDET, and with additional comments.
     inc/Util.hh, src/Util.cc: Search symbol both in and outside of
     namespaces.

---
+++ test/manual/cedet/integ_src/globalref/test/ManagerTest.hh
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "Util.hh"
+
+//
+// This fcn uses the namespaces used in the test, but should not be
+// a search destination.
+//
+namespace play { namespace test {
+    class ManagerTest {
+    public:
+      ManagerTest();
+    };
+}}
