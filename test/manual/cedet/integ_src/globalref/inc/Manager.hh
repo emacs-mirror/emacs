dbfb32039935568f4d7171705fc654986b429d1d
Eric Ludlam <zappo@gnu.org>
2014-01-01 20:44:55 -0500

    Test files contributed by Barry OReilly
    These test files contributed by Barry OReilly, modified for use in the
    automated tests for CEDET, and with additional comments.  inc/Util.hh,
    src/Util.cc: Search symbol both in and outside of namespaces.

---
+++ test/manual/cedet/integ_src/globalref/inc/Manager.hh
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Util.h"
+
+//
+// This files shows use of the namespaces in Util.hh but with diff symbols.
+//
+namespace play { namespace prod {
+class Manager {
+   public:
+   explicit Manager(const int);
+};
+}}
