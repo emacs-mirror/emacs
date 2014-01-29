02962f64cabc35810bfd9ea1559367209cf739a6
Richard Brooksby <rb@ravenbrook.com>
2014-01-29 15:13:22 +0000

    Eliminating dodgy use of *segprefdefault() which could lead to implicit calls to memcpy.
    Copied from Perforce
     Change: 184217
     ServerID: perforce.ravenbrook.com

--- mps/code/mpm.h
+++ mps/code/mpm.h
@@ -626,6 +626,7 @@ extern Res ArenaNoExtend(Arena arena, Addr base, Size size);
 
 extern Bool SegPrefCheck(SegPref pref);
 extern SegPref SegPrefDefault(void);
+extern void SegPrefInit(SegPref pref);
 extern void SegPrefExpress(SegPref pref, SegPrefKind kind, void *p);
 
 extern void LocusInit(Arena arena);
