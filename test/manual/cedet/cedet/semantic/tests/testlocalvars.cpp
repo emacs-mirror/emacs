1c4ec31f34a619289209665461d35444dfb05f7c
David Engster <dengste@eml.cc>
2014-02-02 20:31:32 +0100

    Add test for parsing local variables
    * tests/cedet/semantic/test/manual/cedet/testlocalvars.cpp: New file.
    * tests/cedet/semantic/test/manual/cedet/Project.ede: Add above.
    * tests/cedet/semantic/test/manual/cedet/Makefile: Regenerate.
    * tests/cedet/semantic/ia-utest.el (semantic-ia-utest-file-list): Add
      new test file.

---
+++ test/manual/cedet/cedet/semantic/tests/testlocalvars.cpp
@@ -0,0 +1,33 @@
+// Test parsing of local variables
+
+class foo {
+  foo *member;
+  char[10] anArray;
+};
+
+void func()
+{
+  foo local1;
+  foo* local2 = localvar.member;
+  foo* local3 = new foo();
+  foo local4[10];
+  char local5[5] = {'1','2','3','4','5'};
+  char *local6 = "12345";
+  char local7 = local.anArray[0];
+
+  // Check that all of the above was parsed
+  local//-1-
+    ; //#1# ("local1" "local2" "local3" "local4" "local5" "local6" "local7")
+
+  local1.//-2-
+    ; //#2# ("anArray" "member")
+
+  local2->//-3-
+    ; //#3# ("anArray" "member")
+
+  local3->//-4-
+    ; //#4# ("anArray" "member")
+
+  local4[0].//-5-
+    ; //#5# ("anArray" "member")
+}
