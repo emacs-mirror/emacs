fcd691aaa3219d146cd2d6ef885a81c460ed25ab
Richard Brooksby <rb@ravenbrook.com>
2014-02-28 13:46:36 +0000

    Adding unzoned arena tests back, now that there is a method of asking for them.
    Copied from Perforce
     Change: 184602
     ServerID: perforce.ravenbrook.com

--- mps/code/apss.c
+++ mps/code/apss.c
@@ -199,12 +199,15 @@ int main(int argc, char *argv[])
   testInArena(arena, &fenceOptions);
   mps_arena_destroy(arena);
 
-#if 0 /* FIXME: Restore when arena can take an option */
-  die(mps_arena_create(&arena, mps_arena_class_vmnz(), 2*testArenaSIZE),
-      "mps_arena_create");
+  MPS_ARGS_BEGIN(args) {
+    MPS_ARGS_ADD(args, MPS_KEY_ARENA_SIZE, 2 * testArenaSIZE);
+    MPS_ARGS_ADD(args, MPS_KEY_ARENA_ZONED, FALSE);
+    MPS_ARGS_DONE(args);
+    die(mps_arena_create_k(&arena, mps_arena_class_vm(), args),
+        "mps_arena_create");
+  } MPS_ARGS_END(args);
   testInArena(arena, bothOptions);
   mps_arena_destroy(arena);
-#endif
 
   die(mps_arena_create(&arena, mps_arena_class_cl(),
                        testArenaSIZE, malloc(testArenaSIZE)),
