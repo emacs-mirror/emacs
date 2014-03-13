b2d35664df70bc330c76b5957a8be85ca4af3b5c
Richard Brooksby <rb@ravenbrook.com>
2014-03-13 15:00:37 +0000

    Apss test relies on non-zoned client arena to test tight memory condition, but client arena is zoned by default now.  fixed the test to set up a non-zoned client arena.
    Copied from Perforce
     Change: 184779
     ServerID: perforce.ravenbrook.com

--- mps/code/apss.c
+++ mps/code/apss.c
@@ -209,9 +209,13 @@ int main(int argc, char *argv[])
   testInArena(arena, bothOptions);
   mps_arena_destroy(arena);
 
-  die(mps_arena_create(&arena, mps_arena_class_cl(),
-                       testArenaSIZE, malloc(testArenaSIZE)),
-      "mps_arena_create");
+  MPS_ARGS_BEGIN(args) {
+    MPS_ARGS_ADD(args, MPS_KEY_ARENA_SIZE, testArenaSIZE);
+    MPS_ARGS_ADD(args, MPS_KEY_ARENA_ZONED, FALSE);
+    MPS_ARGS_ADD(args, MPS_KEY_ARENA_CL_BASE, malloc(testArenaSIZE));
+    die(mps_arena_create_k(&arena, mps_arena_class_cl(), args),
+        "mps_arena_create");
+  } MPS_ARGS_END(args);
   testInArena(arena, bothOptions);
   mps_arena_destroy(arena);
 
