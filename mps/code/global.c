298735312584b6ded132aeaacf593fb4b72149bc
Gareth Rees <gdr@ravenbrook.com>
2014-04-13 20:51:10 +0100

    Check rings before destroying the control pool, so that you get an assertion when a ring points into space that is about to be unmapped, instead of a crash after it has been unmapped.
    Copied from Perforce
     Change: 185495
     ServerID: perforce.ravenbrook.com

--- mps/code/global.c
+++ mps/code/global.c
@@ -397,25 +397,7 @@ void GlobalsFinish(Globals arenaGlobals)
   Arena arena;
   Rank rank;
   
-  /* Check that the tear-down is complete: that the client has
-   * destroyed all data structures associated with the arena. We do
-   * this *before* calling AVERT(Globals, arenaGlobals) because the
-   * AVERT will crash if there are any remaining data structures, and
-   * it is politer to assert than to crash. (The crash would happen
-   * because by this point in the code the control pool has been
-   * destroyed and so the address space containing all these rings has
-   * potentially been unmapped, and so RingCheck dereferences a
-   * pointer into that unmapped memory.) See job000652. */
   arena = GlobalsArena(arenaGlobals);
-  AVER(RingIsSingle(&arena->formatRing));
-  AVER(RingIsSingle(&arena->chainRing));
-  AVER(RingIsSingle(&arena->messageRing));
-  AVER(RingIsSingle(&arena->threadRing));
-  for(rank = 0; rank < RankLIMIT; ++rank)
-    AVER(RingIsSingle(&arena->greyRing[rank]));
-  AVER(RingIsSingle(&arenaGlobals->poolRing));
-  AVER(RingIsSingle(&arenaGlobals->rootRing));
-
   AVERT(Globals, arenaGlobals);
 
   STATISTIC_STAT(EVENT2(ArenaWriteFaults, arena,
@@ -445,6 +427,7 @@ void GlobalsPrepareToDestroy(Globals arenaGlobals)
   TraceId ti;
   Trace trace;
   Chain defaultChain;
+  Rank rank;
 
   AVERT(Globals, arenaGlobals);
 
@@ -499,6 +482,23 @@ void GlobalsPrepareToDestroy(Globals arenaGlobals)
     arena->finalPool = NULL;
     PoolDestroy(pool);
   }
+
+  /* Check that the tear-down is complete: that the client has
+   * destroyed all data structures associated with the arena. We do
+   * this here rather than in GlobalsFinish because by the time that
+   * is called, the control pool has been destroyed and so the address
+   * space containing all these rings has potentially been unmapped,
+   * and so RingCheck dereferences a pointer into that unmapped memory
+   * and we get a crash instead of an assertion. See job000652.
+   */
+  AVER(RingIsSingle(&arena->formatRing));
+  AVER(RingIsSingle(&arena->chainRing));
+  AVER(RingIsSingle(&arena->messageRing));
+  AVER(RingIsSingle(&arena->threadRing));
+  AVER(RingIsSingle(&arenaGlobals->rootRing));
+  for(rank = 0; rank < RankLIMIT; ++rank)
+    AVER(RingIsSingle(&arena->greyRing[rank]));
+  /* poolRing is not yet single (control pool etc. still exist) */
 }
 
 
