40b4bd4a43b9c66330da0b4b8b5bbf05cc43f25f
Eric Ludlam <zappo@gnu.org>
2014-04-20 14:07:58 -0400

    Adedd `ede-detect-utest'
    * test/manual/cedet/cedet-utests.el: (cedet/ede/detect-utest): New
     require.  (cedet-utest-test-alist): Add ede-detect-utest.

--- test/manual/cedet/cedet-utests.el
+++ test/manual/cedet/cedet-utests.el
@@ -31,6 +31,7 @@
 (require 'pulse-utest)
 (require 'cedet-files-utests)
 (require 'cedet-compat)
+(require 'cedet/ede/detect-utest)
 (require 'cedet/semantic/lex-utest)
 (require 'cedet/semantic/lex-spp-utest)
 (require 'cedet/semantic/utest-parse)
@@ -84,9 +85,7 @@
     ;;
     ;; EDE
     ;;
-
-    ;; @todo - Currently handled in the integration tests.  Need
-    ;;         some simpler unit tests here.
+    ("ede: project detection tests" . ede-detect-utest)
 
     ;;
     ;; SEMANTIC
