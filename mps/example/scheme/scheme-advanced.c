86f83bc0f37c03de5e61f014b48bc2a537407d3c
Gareth Rees <gdr@ravenbrook.com>
2014-06-12 11:28:41 +0100

    Must fflush stderr too--it's buffered on windows!
    Copied from Perforce
     Change: 186517
     ServerID: perforce.ravenbrook.com

--- mps/example/scheme/scheme-advanced.c
+++ mps/example/scheme/scheme-advanced.c
@@ -4401,6 +4401,7 @@ static int start(int argc, char *argv[])
       printf("%lu, %lu> ", (unsigned long)total,
              (unsigned long)mps_collections(arena));
       fflush(stdout);
+      fflush(stderr);
       obj = read(input);
       if(obj == obj_eof) break;
       obj = eval(env, op_env, obj);
