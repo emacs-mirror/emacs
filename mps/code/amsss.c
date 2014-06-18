80607070f7a1ed34914c136d4a272591ba6a4f52
Gareth Rees <gdr@ravenbrook.com>
2014-06-18 16:24:38 +0100

    Fix problems noted by rb in review <https://info.ravenbrook.com/mail/2014/06/18/14-40-10/0/>
    Copied from Perforce
     Change: 186669
     ServerID: perforce.ravenbrook.com

--- mps/code/amsss.c
+++ mps/code/amsss.c
@@ -208,7 +208,7 @@ int main(int argc, char *argv[])
 
   MPS_ARGS_BEGIN(args) {
     MPS_ARGS_ADD(args, MPS_KEY_ARENA_SIZE, testArenaSIZE);
-    MPS_ARGS_ADD(args, MPS_KEY_ARENA_GRAIN_SIZE, (size_t)1 << (rnd() % 14));
+    MPS_ARGS_ADD(args, MPS_KEY_ARENA_GRAIN_SIZE, rnd_grain(testArenaSIZE));
     die(mps_arena_create_k(&arena, mps_arena_class_vm(), args), "arena_create");
   } MPS_ARGS_END(args);
   die(mps_arena_commit_limit_set(arena, 2 * testArenaSIZE), "commit_limit_set");
