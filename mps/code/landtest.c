cbe81bf0deedae1b543acc87144572667661dc81
Gareth Rees <gdr@ravenbrook.com>
2014-10-10 11:21:22 +0100

    Remove call to deprecated macro mps_args_done.
    Copied from Perforce
     Change: 187171
     ServerID: perforce.ravenbrook.com

--- mps/code/landtest.c
+++ mps/code/landtest.c
@@ -500,7 +500,6 @@ extern int main(int argc, char *argv[])
         MPS_ARGS_ADD(piArgs, MPS_KEY_MFS_UNIT_SIZE, sizeof(CBSFastBlockStruct));
         MPS_ARGS_ADD(piArgs, MPS_KEY_EXTEND_BY, ArenaGrainSize(arena));
         MPS_ARGS_ADD(piArgs, MFSExtendSelf, i);
-        MPS_ARGS_DONE(piArgs);
         die(PoolInit(mfs, arena, PoolClassMFS(), piArgs), "PoolInit");
       } MPS_ARGS_END(piArgs);
 
