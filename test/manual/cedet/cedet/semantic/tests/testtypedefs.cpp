0a0dd2725479e5e19c58cfc27effa76041e8d321
Eric Ludlam <zappo@gnu.org>
2014-11-24 12:38:04 -0500

    Test typedef which has the keyword "class" included
    * test/manual/cedet/cedet/semantic/tests/testtypedefs.cpp:
     (NS2::Myclass, MyClassHandle, dixon): New test w/ a typedef that has
     keyword "class" included, instead of omitted as with other tests.

--- test/manual/cedet/cedet/semantic/tests/testtypedefs.cpp
+++ test/manual/cedet/cedet/semantic/tests/testtypedefs.cpp
@@ -118,3 +118,33 @@ int func(void)
      ; // #7# ( "field_a" "field_b" )
    return 0;
 }
+
+
+// ------------------
+// Example from Dixon Ryan
+
+
+namespace NS2 {
+   class MyClass {
+
+   public:
+      void myFunction() { }
+   };
+}
+
+typedef class NS2::MyClass* MyClassHandle;
+
+int dixon ( void ) {
+  MyClassHandle mch = getMyClassHandle();
+  NS2::MyClass* mcptr = getMyClassHandle();
+
+  mch-> // -8-
+    ; // #8# ( "myFunction" )
+
+  mcptr-> // -9-
+    ; // #9# ( "myFunction" )
+
+  deleteMyClassHandle(mch);
+
+  return 0;
+}
