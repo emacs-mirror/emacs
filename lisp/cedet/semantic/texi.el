ec4505da3f31a6e829c81e9d3a8555a9c6877c9e
Eric Ludlam <eric@siege-engine.com>
2015-04-11 19:18:01 -0400

    semantic: Accept FLAGS in texinfo-mode for analyzing completions
    * lisp/cedet/semantic/texi.el (semantic-analyze-possible-completions):
     Add support for FLAGS (currently ignored).

--- lisp/cedet/semantic/texi.el
+++ lisp/cedet/semantic/texi.el
@@ -412,12 +412,13 @@ Optional argument POINT is where to look for the environment."
   "List of commands that we might bother completing.")
 
 (define-mode-local-override semantic-analyze-possible-completions
-  texinfo-mode (context)
+  texinfo-mode (context &rest flags)
   "List smart completions at point.
 Since texinfo is not a programming language the default version is not
 useful.  Instead, look at the current symbol.  If it is a command
 do primitive texinfo built ins.  If not, use ispell to lookup words
-that start with that symbol."
+that start with that symbol.
+Any extra FLAGS are currently ignored."
   (let ((prefix (car (oref context :prefix)))
 	)
     (cond ((member 'function (oref context :prefixclass))
