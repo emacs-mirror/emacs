d07e012c3d094f56ba140316943a6bda85d6edf9
Eric Ludlam <eric@siege-engine.com>
2015-04-11 21:13:02 -0400

    New test files combining macros and EDE

---
+++ test/manual/cedet/cedet/semantic/testwithede/testedereplaced.c
@@ -0,0 +1,29 @@
+/** testedereplaced.c --- File equivalent to testede as if macros expanded.
+ *
+ * Copyright (C) 2015 Eric Ludlam
+ *
+ * Author: Eric Ludlam <zappo@ballista>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/.
+ */
+
+int A(int b);
+
+int B(int b);
+
+int C(int b);
+
+int D(int b);
+
+int E(int b);
