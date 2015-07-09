7f96e4d86b623c71ddb9d01ebfa9a4a58524d659
justbur <justin@burkett.cc>
2015-07-09 07:44:36 -0400

    Fix #22
    Check for keymap instead of not a symbol.

--- which-key.el
+++ which-key.el
@@ -196,8 +196,10 @@ bottom."
   "Fill which-key--buffer with key descriptions and reformat.
 Finally, show the buffer."
   (let ((prefix-keys (this-single-command-keys)))
+;;    (when (> (length prefix-keys) 0) (message "key: %s" (key-description prefix-keys)))
+;;    (when (> (length prefix-keys) 0) (message "key binding: %s" (key-binding prefix-keys)))
     (when (and (> (length prefix-keys) 0)
-               (not (symbolp (key-binding prefix-keys))))
+               (keymapp (key-binding prefix-keys)))
       (let* ((buf (current-buffer))
              ;; get formatted key bindings
              (fmt-width-cons (which-key/get-formatted-key-bindings buf prefix-keys))
