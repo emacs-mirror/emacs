f650559ccab02604dd26dcecbc9d7f75c0f18130
Gareth Rees <gdr@ravenbrook.com>
2015-08-10 20:29:21 +0100

    New test case 231: create/configure arena with too-small commit limit.
    Copied from Perforce
     Change: 188118
     ServerID: perforce.ravenbrook.com

---
+++ mps/test/function/231.c
@@ -0,0 +1,43 @@
+/* 
+TEST_HEADER
+ id = $Id$
+ summary = create/configure arena with too-small commit limit
+ language = c
+ link = testlib.o
+OUTPUT_SPEC
+ create1 = COMMIT_LIMIT
+ create2 = OK
+ configure = FAIL
+END_HEADER
+*/
+
+#include "testlib.h"
+#include "newfmt.h"
+
+static void test(void)
+{
+  mps_arena_t arena;
+
+  MPS_ARGS_BEGIN(args) {
+    MPS_ARGS_ADD(args, MPS_KEY_ARENA_COMMIT_LIMIT, 16 * 1024);
+    report_res("create1",
+               mps_arena_create_k(&arena, mps_arena_class_vm(), args));
+  } MPS_ARGS_END(args);
+
+  report_res("create2", 
+             mps_arena_create_k(&arena, mps_arena_class_vm(), mps_args_none));
+
+  MPS_ARGS_BEGIN(args) {
+    MPS_ARGS_ADD(args, MPS_KEY_ARENA_COMMIT_LIMIT, 16 * 1024);
+    report_res("configure", mps_arena_configure(arena, args));
+  } MPS_ARGS_END(args);
+
+  mps_arena_destroy(arena);
+}
+
+int main(void)
+{
+  easy_tramp(test);
+  pass();
+  return 0;
+}
