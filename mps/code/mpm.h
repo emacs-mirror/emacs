92c4d5a5749762826d76212e85434493971eec14
Gareth Rees <gdr@ravenbrook.com>
2015-09-03 13:01:55 +0100

    Introduce new type work representing a measure of work done by the collector. use this systematically to make the code clearer.
    Copied from Perforce
     Change: 188210
     ServerID: perforce.ravenbrook.com

--- mps/code/mpm.h
+++ mps/code/mpm.h
@@ -399,7 +399,7 @@ extern void TraceDestroyFinished(Trace trace);
 extern Res TraceAddWhite(Trace trace, Seg seg);
 extern Res TraceCondemnZones(Trace trace, ZoneSet condemnedSet);
 extern Res TraceStart(Trace trace, double mortality, double finishingTime);
-extern Size TracePoll(Globals globals);
+extern Work TracePoll(Globals globals);
 
 extern Rank TraceRankForAccess(Arena arena, Seg seg);
 extern void TraceSegAccess(Arena arena, Seg seg, AccessSet mode);
@@ -667,7 +667,7 @@ extern Res PolicyAlloc(Tract *tractReturn, Arena arena, LocusPref pref,
 extern Bool PolicyStartTrace(Trace *traceReturn, Arena arena);
 extern double PolicyCollectionTime(Arena arena);
 extern Bool PolicyPoll(Arena arena);
-extern Bool PolicyPollAgain(Arena arena, Clock start, Size tracedSize);
+extern Bool PolicyPollAgain(Arena arena, Clock start, Work tracedWork);
 
 
 /* Locus interface */
