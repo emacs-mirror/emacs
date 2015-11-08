1fece3ebd1514717b0b7f19cf06c9f59ffdb0041
Phillip Lord <phillip.lord@russet.org.uk>
2015-11-08 16:44:54 +0000

    Testing complete on move script.

---
+++ test/automated/move.sh
@@ -0,0 +1,92 @@
+mkdir lisp
+mkdir src
+mkdir lisp/emacs-lisp
+mkdir lisp/emacs-lisp/eieio-tests
+mkdir lisp/legacy
+mkdir lisp/gnus
+mkdir lisp/international
+mkdir lisp/vc
+
+
+mv add-log-tests.el lisp/vc
+mv advice-tests.el lisp/emacs-lisp/nadvice-tests.el
+mv auth-source-tests.el lisp/gnus
+mv auto-revert-tests.el lisp/autorevert-tests.el
+mv bytecomp-tests.el lisp/emacs-lisp
+mv calc-tests.el lisp/calc
+mv cl-generic-tests.el lisp/emacs-lisp
+mv cl-lib-tests.el lisp/emacs-lisp
+mv cmds-tests.el src
+mv coding-tests.el lisp/legacy
+mv comint-testsuite.el lisp/comint-tests.el
+mv compile-tests.el lisp/progmodes
+mv completion-tests.el lisp/minibuffer-tests.el
+mv core-elisp-tests.el lisp/legacy-tests
+mv data-tests.el src
+mv dbus-tests.el lisp/net
+mv decoder-tests.el lisp/legacy
+mv descr-text-test.el lisp/descr-text-tests.el
+mv eieio-test-methodinvoke.el lisp/emacs-lisp/eieio-tests
+mv eieio-test-persist.el lisp/emacs-lisp/eieio-tests
+mv eieio-tests.el lisp/emacs-lisp/eieio-tests
+mv electric-tests.el lisp
+mv elisp-mode-tests.el lisp/progmodes
+mv epg-tests.el lisp
+mv ert-tests.el lisp/emacs-lisp
+mv ert-x-tests.el lisp/emacs-lisp
+mv eshell.el lisp/eshell
+mv f90.el lisp/progmodes
+mv file-notify-tests.el lisp/filenotify-tests.el
+mv files.el lisp/legacy/files-tests.el
+mv finalizer-tests.el src
+mv flymake-tests.el lisp/progmodes
+mv fns-tests.el src
+mv font-parse-tests.el lisp/legacy
+mv generator-tests.el lisp/emacs-lisp
+mv gnus-tests.el lisp/gnus
+mv help-fns.el lisp/help-fns-tests.el
+mv icalendar-tests.el lisp/calendar
+mv imenu-test.el lisp/imenu-tests.el
+mv info-xref.el lisp/info-xref-tests.el
+mv inotify-test.el src/inotify-tests.el
+mv json-tests.el lisp
+mv let-alist.el lisp/emacs-lisp/let-alist-tests.el
+mv lexbind-tests.el lisp/legacy
+mv libxml-tests.el src/xml-tests.el
+mv man-tests.el lisp
+mv map-tests.el lisp/emacs-lisp
+mv message-mode-tests.el lisp/gnus/message-tests.el
+mv mule-util.el lisp/international/mule-util-tests.el
+mv newsticker-tests.el lisp/net
+mv occur-tests.el lisp/legacy
+mv package-test.el lisp/emacs-lisp/package-tests.el
+mv pcase-tests.el lisp/emacs-lisp
+mv print-tests.el src
+mv process-tests.el lisp/legacy
+mv python-tests.el lisp/progmodes
+mv reftex-tests.el lisp/textmodes
+mv regexp-tests.el lisp/emacs-lisp/regexp-opt-tests.el
+mv replace-tests.el lisp/
+mv ruby-mode-tests.el lisp/progmodes
+mv sasl-scram-rfc-tests.el lisp/net
+mv seq-tests.el lisp/emacs-lisp
+mv sgml-mode-tests.el lisp/textmodes
+mv simple-test.el lisp/simple-tests.el
+mv subr-tests.el lisp/subr-tests.el
+mv subr-x-tests.el lisp/emacs-lisp
+mv subword-tests.el lisp/progmodes
+mv syntax-tests.el lisp/legacy
+mv tabulated-list-test.el lisp/emacs-lisp
+mv textprop-tests.el lisp/legacy
+mv thingatpt.el lisp/thingatpt-tests.el
+mv thunk-tests.el lisp/emacs-lisp
+mv tildify-tests.el lisp/textmodes
+mv timer-tests.el lisp/emacs-lisp
+mv tramp-tests.el lisp/net
+mv undo-tests.el lisp/legacy
+mv url-future-tests.el lisp/url
+mv url-util-tests.el lisp/url
+mv vc-bzr.el lisp/vc/vc-bzr-tests.el
+mv vc-tests.el lisp/vc
+mv xml-parse-tests.el lisp/xml-tests.el
+mv zlib-tests.el src
