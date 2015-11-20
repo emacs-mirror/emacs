emacs-mirror/emacs

commit 9dd0ceaaabe6b4ac5f3268747b6103fb04d65e5f

Revert change in default of side-window-bottom
Not happy with the look of it yet, even though it's probably better to
avoid the echo area

which-key.el
@@ -164,7 +164,7 @@ and nil. Nil turns the feature off."
   :group 'which-key
   :type 'integer)
 
-(defcustom which-key-side-window-location 'bottom
+(defcustom which-key-side-window-location 'echo
   "Location of which-key popup when `which-key-popup-type' is side-window.
 Should be one of top, bottom, left or right. You can also specify
 a list of two locations, like (right bottom). In this case, the
@@ -565,7 +565,7 @@ bottom."
   (interactive)
   (setq which-key-popup-type 'side-window
         which-key-side-window-location 'bottom
-        which-key-show-prefix 'bottom))
+        which-key-show-prefix 'echo))
 
 ;;;###autoload
 (defun which-key-setup-minibuffer ()

README.org
@@ -1,17 +1,6 @@
 * which-key 
 [[http://melpa.org/#/which-key][http://melpa.org/packages/which-key-badge.svg]] [[http://stable.melpa.org/#/which-key][file:http://stable.melpa.org/packages/which-key-badge.svg]]
 ** What's New
-- Implemented =bottom= as an option for =which-key-show-prefix=, which will show
-  the key, page number, and next page key information on the last line of the
-  which-key buffer.
-- This is the new default for =which-key-setup-side-window-bottom=. The old
-  default, which is still available, used the echo area for this information.
-  The echo area is not a good default, because it's not possible to completely
-  control what shows there without doing something ugly, so occasionally the
-  which-key information will be overwritten. To restore the old default simply
-  #+BEGIN_SRC emacs-lisp
-  (setq which-key-show-prefix 'echo)
-  #+END_SRC
 - The function =which-key-show-top-level= was implemented by @iqbalansari
   (thanks!) to show top-level key bindings (those not behind a prefix). You can
   use =M-x which-key-show-top-level= to try it and bind it to a key if you like.

commit 3fceb99ee4300ad5bf711f7113872a1dabe22301

Fix mistake in reverting defaults
Ref 9dd0ceaaabe6b4ac5f3268747b6103fb04d65e5f

Accidentally changed the side window location instead of the show-prefix
one.

which-key.el
@@ -139,7 +139,7 @@ and have `which-key-special-key-face' applied to them."
   :group 'which-key
   :type 'string)
 
-(defcustom which-key-show-prefix 'bottom
+(defcustom which-key-show-prefix 'echo
   "Whether to and where to display the current prefix sequence.
 Possible choices are echo for echo area (the default), left, top
 and nil. Nil turns the feature off."