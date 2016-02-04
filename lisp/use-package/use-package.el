c5e98d87dae831d3ed8527bd806f6c4814db8f9b
Thierry Volpiatto <thierry.volpiatto@gmail.com>
2016-02-04 11:19:23 +0100

    Ensure package-install support a second argument
    * use-package.el (use-package-ensure-elpa): Do it.

--- lisp/use-package/use-package.el
+++ lisp/use-package/use-package.el
@@ -462,7 +462,9 @@ manually updated package."
   (if (package-installed-p package)
       t
     (if (or (assoc package package-archive-contents) no-refresh)
-        (package-install package t)
+        (if (boundp 'package-selected-packages)
+            (package-install package t)
+            (package-install package))
       (progn
         (package-refresh-contents)
         (use-package-ensure-elpa package t)))))
