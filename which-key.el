0c32f52bfce89850cb5a4aeb97b1d11be038ff17
justbur <justin@burkett.cc>
2016-05-16 21:06:23 -0400

    Update docs for new sort function

--- which-key.el
+++ which-key.el
@@ -245,8 +245,10 @@ a percentage out of the frame's height."
 are
 
 1. `which-key-key-order': by key (default)
-2. `which-key-description-order': by description
-3. `which-key-prefix-then-key-order': prefix (no prefix first) then key
+2. `which-key-key-order-alpha': by key using alphabetical order
+3. `which-key-description-order': by description
+4. `which-key-prefix-then-key-order': prefix (no prefix first) then key
+5. `which-key-local-then-key-order': local binding then key
 
 See the README and the docstrings for those functions for more
 information."
