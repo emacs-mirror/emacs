665d8197f059e40ca56c0ca8c4702285175a040c
Gareth Rees <gdr@ravenbrook.com>
2016-09-08 10:57:07 +0100

    Use area roots (instead of a thread root) to ensure that objects are finalized reliably even in the hot variety.
    Copied from Perforce
     Change: 192235
     ServerID: perforce.ravenbrook.com

--- mps/test/function/69.c
+++ mps/test/function/69.c
@@ -26,8 +26,7 @@ mps_arena_t arena;
 
 static void test(void) {
  mps_pool_t pool;
- mps_thr_t thread;
- mps_root_t root;
+ mps_root_t rootA, rootB;
 
  mps_fmt_t format;
  mps_chain_t chain;
@@ -42,11 +41,12 @@ static void test(void) {
 
  cdie(mps_arena_create(&arena, mps_arena_class_vm(), mmqaArenaSIZE),
       "create arena");
+ mps_arena_park(arena);
 
- die(mps_thread_reg(&thread, arena), "register thread");
- die(mps_root_create_reg(&root, arena, mps_rank_ambig(), 0, thread,
-                         mps_stack_scan_ambig, stackpointer, 0),
-     "create root");
+ die(mps_root_create_area(&rootA, arena, mps_rank_ambig(), (mps_rm_t)0,
+                          &a, &a + 1, mps_scan_area, NULL), "create root");
+ die(mps_root_create_area(&rootB, arena, mps_rank_ambig(), (mps_rm_t)0,
+                          &b, &b + 1, mps_scan_area, NULL), "create root");
 
  die(mps_fmt_create_A(&format, arena, &fmtA), "create format");
  cdie(mps_chain_create(&chain, arena, genCOUNT, testChain), "chain_create");
@@ -95,12 +95,12 @@ static void test(void) {
  mps_message_discard(arena, message);
 
  mps_arena_park(arena);
- mps_root_destroy(root);
+ mps_root_destroy(rootA);
+ mps_root_destroy(rootB);
  mps_ap_destroy(ap);
  mps_pool_destroy(pool);
  mps_chain_destroy(chain);
  mps_fmt_destroy(format);
- mps_thread_dereg(thread);
  mps_arena_destroy(arena);
  comment("Destroyed arena.");
 }
