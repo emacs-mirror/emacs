fc7fc42f13549fa78c1829c2e3ce05dea5019a6c
John Wiegley <johnw@newartisans.com>
2016-10-17 16:40:29 -0700

    Bump version to 2.3

--- lisp/use-package/use-package.el
+++ lisp/use-package/use-package.el
@@ -5,8 +5,8 @@
 ;; Author: John Wiegley <jwiegley@gmail.com>
 ;; Maintainer: John Wiegley <jwiegley@gmail.com>
 ;; Created: 17 Jun 2012
-;; Modified: 6 Jul 2016
-;; Version: 2.2
+;; Modified: 17 Oct 2016
+;; Version: 2.3
 ;; Package-Requires: ((bind-key "1.0") (diminish "0.44"))
 ;; Keywords: dotemacs startup speed config package
 ;; URL: https://github.com/jwiegley/use-package
