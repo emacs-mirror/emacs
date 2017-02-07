ea6f1dc5aacff2f3d909e410db05af01966555aa
Justin Burkett <justin@burkett.cc>
2017-02-07 10:55:14 -0500

    Version 2.0
    Increased major version for replacement list changes

--- which-key.el
+++ which-key.el
@@ -4,7 +4,7 @@
 
 ;; Author: Justin Burkett <justin@burkett.cc>
 ;; URL: https://github.com/justbur/emacs-which-key
-;; Version: 1.1.15
+;; Version: 2.0
 ;; Keywords:
 ;; Package-Requires: ((emacs "24.3"))
 
