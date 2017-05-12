046785e5b77858b2dca464d620a1e95b89e23322
Andrew G Cohen <cohen@andy.bu.edu>
2017-05-12 09:40:03 +0800

    Redo entry functions for making search groups
    * lisp/gnus/gnus-group.el (gnus-group-make-search-group):
    (gnus-group-read-ephemeral-search-group): Rename these functions to be
    more consistent with other group creation functions, and move to
    gnus-group.el.
    * lisp/gnus/nnir.el (nnir-make-specs): Refactor new function to ease
    search group creation.

--- lisp/gnus/gnus-group.el
+++ lisp/gnus/gnus-group.el
@@ -49,9 +49,6 @@
 (autoload 'gnus-agent-total-fetched-for "gnus-agent")
 (autoload 'gnus-cache-total-fetched-for "gnus-cache")
 
-(autoload 'gnus-group-make-search-group "nnselect")
-(autoload 'gnus-group-make-permanent-search-group "nnselect")
-
 (autoload 'gnus-cloud-upload-all-data "gnus-cloud")
 (autoload 'gnus-cloud-download-all-data "gnus-cloud")
 
@@ -672,8 +669,8 @@ simple manner."
   "D" gnus-group-enter-directory
   "f" gnus-group-make-doc-group
   "w" gnus-group-make-web-group
-  "G" gnus-group-make-search-group
-  "g" gnus-group-make-permanent-search-group
+  "G" gnus-group-read-ephemeral-search-group
+  "g" gnus-group-make-search-group
   "M" gnus-group-read-ephemeral-group
   "r" gnus-group-rename-group
   "R" gnus-group-make-rss-group
@@ -919,8 +916,8 @@ simple manner."
 	["Add the help group" gnus-group-make-help-group t]
 	["Make a doc group..." gnus-group-make-doc-group t]
 	["Make a web group..." gnus-group-make-web-group t]
+	["Read a search group..." gnus-group-read-ephemeral-search-group t]
 	["Make a search group..." gnus-group-make-search-group t]
-	["Make a permanent search group..." gnus-group-make-permanent-search-group t]
 	["Make a virtual group..." gnus-group-make-empty-virtual t]
 	["Add a group to a virtual..." gnus-group-add-to-virtual t]
 	["Make an ephemeral group..." gnus-group-read-ephemeral-group t]
@@ -3196,6 +3193,51 @@ mail messages or news articles in files that have numeric names."
      (gnus-group-real-name group)
      (list 'nndir (gnus-group-real-name group) (list 'nndir-directory dir)))))
 
+
+(autoload 'nnir-make-specs "nnir")
+(autoload 'gnus-group-topic-name "gnus-topic")
+
+;; Temporary to make group creation easier
+(defun gnus-group-make-search-group (nnir-extra-parms &optional specs)
+  (interactive "P")
+  (let ((name (read-string "Group name: " nil)))
+    (gnus-group-make-group
+     name
+     (list 'nnselect "nnselect")
+     nil
+     (list
+      (cons 'nnselect-specs
+	    (list
+	     (cons 'nnselect-function 'nnir-run-query)
+	     (cons 'nnselect-args
+		   (nnir-make-specs nnir-extra-parms specs))))))))
+
+(defun gnus-group-read-ephemeral-search-group (nnir-extra-parms	&optional specs)
+  "Create an nnselect group based on a search.  Prompt for a
+search query and determine the groups to search as follows: if
+called from the *Server* buffer search all groups belonging to
+the server on the current line; if called from the *Group* buffer
+search any marked groups, or the group on the current line, or
+all the groups under the current topic. Calling with a prefix-arg
+prompts for additional search-engine specific constraints. A
+non-nil `specs' arg must be an alist with `nnir-query-spec' and
+`nnir-group-spec' keys, and skips all prompting."
+  (interactive "P")
+  (gnus-group-read-ephemeral-group
+   (concat "nnselect-" (message-unique-id))
+   (list 'nnselect "nnselect")
+   nil
+   (cons (current-buffer) gnus-current-window-configuration)
+					;     nil
+   nil nil
+   (list
+    (cons 'nnselect-specs
+	  (list
+	   (cons 'nnselect-function 'nnir-run-query)
+	   (cons 'nnselect-args
+		 (nnir-make-specs nnir-extra-parms specs))))
+    (cons 'nnselect-artlist nil))))
+
 (defun gnus-group-add-to-virtual (n vgroup)
   "Add the current group to a virtual group."
   (interactive
@@ -4729,6 +4771,7 @@ you the groups that have both dormant articles and cached articles."
 		 (not (gnus-group-read-only-p group)))
 	(gnus-add-marked-articles
 	 group 'expire (list article))))))
+
 
 
 ;;;
