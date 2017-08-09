7c01092e65b2ea520af8ca36ee93cef49981d45a
Justin Burkett <justin@burkett.cc>
2017-08-08 20:05:37 -0400

    Make minor changes to commentary in which-key.el

--- which-key.el
+++ which-key.el
@@ -24,14 +24,15 @@
 
 ;;; Commentary:
 
-;; which-key is a minor mode for Emacs that displays the key bindings following
-;; your currently entered incomplete command (a prefix) in a popup. For example,
-;; after enabling the minor mode if you enter C-x and wait for the default of 1
-;; second the minibuffer will expand with all of the available key bindings that
-;; follow C-x (or as many as space allows given your settings). This includes
-;; prefixes like C-x 8 which are shown in a different face. Screenshots of what
-;; the popup will look like along with information about additional features can
-;; be found at https://github.com/justbur/emacs-which-key.
+;; which-key provides the minor mode which-key-mode for Emacs. The mode displays
+;; the key bindings following your currently entered incomplete command (a
+;; prefix) in a popup. For example, after enabling the minor mode if you enter
+;; C-x and wait for the default of 1 second the minibuffer will expand with all
+;; of the available key bindings that follow C-x (or as many as space allows
+;; given your settings). This includes prefixes like C-x 8 which are shown in a
+;; different face. Screenshots of what the popup will look like along with
+;; information about additional features can be found at
+;; https://github.com/justbur/emacs-which-key.
 ;;
 
 ;;; Code:
