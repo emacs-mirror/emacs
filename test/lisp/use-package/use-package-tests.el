3ea2d34219af6b23f0d8414a4d5b966f8bfd8e79
John Wiegley <johnw@newartisans.com>
2017-11-30 09:42:47 -0800

    Add stubs for future tests of all keywords

--- test/lisp/use-package/use-package-tests.el
+++ test/lisp/use-package/use-package-tests.el
@@ -25,10 +25,76 @@
 (require 'ert)
 (require 'use-package)
 
-(ert-deftest use-package-normalize-binder ()
+(ert-deftest use-package-test-recognize-function ()
+  (should (use-package--recognize-function 'sym))
+  (should (use-package--recognize-function #'sym))
+  (should (use-package--recognize-function (lambda () ...)))
+  (should (use-package--recognize-function '(lambda () ...)))
+  (should (use-package--recognize-function #'(lambda () ...)))
+
+  (should-not (use-package--recognize-function 1))
+  (should-not (use-package--recognize-function "Hello"))
+  (should-not (use-package--recognize-function '(nil . nil))))
+
+(ert-deftest use-package-test-normalize-function ()
+  (should (equal (use-package--normalize-function 'sym) 'sym))
+  (should (equal (use-package--normalize-function #'sym) 'sym))
+  (should (equal (use-package--normalize-function (lambda () ...)) (lambda () ...)))
+  (should (equal (use-package--normalize-function '(lambda () ...)) (lambda () ...)))
+  (should (equal (use-package--normalize-function #'(lambda () ...)) (lambda () ...)))
+
+  (should (equal (use-package--normalize-function 1) 1))
+  (should (equal (use-package--normalize-function "Hello") "Hello"))
+  (should (equal (use-package--normalize-function '(nil . nil)) '(nil . nil))))
+
+;; (ert-deftest use-package-test/:disabled ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:preface ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:pin ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:defer-install ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:ensure ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:if ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:when ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:unless ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:requires ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:load-path ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:no-require ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+(ert-deftest use-package-test-normalize/:bind ()
   (let ((good-values '(:map map-sym
-                       ("str" . sym) ("str" . "str")
-                       ([vec] . sym) ([vec] . "str"))))
+                            ("str" . sym) ("str" . "str")
+                            ([vec] . sym) ([vec] . "str"))))
     (should (equal (use-package-normalize-binder
                     'foopkg :bind good-values)
                    good-values)))
@@ -39,7 +105,27 @@
   (should-error (use-package-normalize-binder
                  'foopkg :bind '(99 . sym))))
 
-(ert-deftest use-package-normalize-mode ()
+;; (ert-deftest use-package-test/:bind ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:bind* ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:bind-keymap ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:bind-keymap* ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:interpreter ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+(ert-deftest use-package-test-normalize/:mode ()
   (should (equal (use-package-normalize-mode 'foopkg :mode '(".foo"))
                  '((".foo" . foopkg))))
   (should (equal (use-package-normalize-mode 'foopkg :mode '(".foo" ".bar"))
@@ -51,7 +137,80 @@
   (should (equal (use-package-normalize-mode 'foopkg :mode '((".foo" . foo) (".bar" . bar)))
                  '((".foo" . foo) (".bar" . bar)))))
 
-(ert-deftest use-package-normalize-delight ()
+;; (ert-deftest use-package-test/:mode ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:magic ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:magic-fallback ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:commands ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:defines ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:functions ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:defer ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:hook ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:custom ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:custom-face ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:init ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+(ert-deftest use-package-test/:after ()
+  (should (equal (macroexpand '(use-package foo :after bar))
+                 '(eval-after-load 'bar
+                    '(require 'foo nil t)))))
+
+;; (ert-deftest use-package-test/:demand ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+;; (ert-deftest use-package-test/:config ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+(ert-deftest use-package-test-normalize/:diminish ()
+  (should (equal (use-package-normalize-diminish 'foopkg :diminish nil)
+                 '(foopkg-mode)))
+  (should (equal (use-package-normalize-diminish 'foopkg :diminish 'bar)
+                 '(bar)))
+  (should (equal (use-package-normalize-diminish 'foopkg :diminish "bar")
+                 '((foopkg-mode . "bar"))))
+  (should (equal (use-package-normalize-diminish 'foopkg :diminish 'foo-mode)
+                 '(foo-mode)))
+  (should (equal (use-package-normalize-diminish 'foopkg :diminish '(foo . "bar"))
+                 '((foo . "bar")))))
+
+;; (ert-deftest use-package-test/:diminish ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
+
+(ert-deftest use-package-test-normalize/:delight ()
   (should (equal `((foo-mode nil foo))
                  (use-package-normalize/:delight 'foo :delight nil)))
   (should (equal `((foo-mode nil foo-mode))
@@ -70,31 +229,9 @@
                                                                  (b-mode " b")))))
   (should-error (use-package-normalize/:delight 'foo :delight '((:eval 1)))))
 
-(ert-deftest use-package-normalize-diminish ()
-  (should (equal (use-package-normalize-diminish 'foopkg :diminish nil)
-                 '(foopkg-mode)))
-  (should (equal (use-package-normalize-diminish 'foopkg :diminish 'bar)
-                 '(bar)))
-  (should (equal (use-package-normalize-diminish 'foopkg :diminish "bar")
-                 '((foopkg-mode . "bar"))))
-  (should (equal (use-package-normalize-diminish 'foopkg :diminish 'foo-mode)
-                 '(foo-mode)))
-  (should (equal (use-package-normalize-diminish 'foopkg :diminish '(foo . "bar"))
-                 '((foo . "bar")))))
-
-(ert-deftest use-package--recognize-function-test ()
-  (should (use-package--recognize-function 'sym))
-  (should (use-package--recognize-function #'sym))
-  (should (use-package--recognize-function (lambda () ...)))
-  (should (use-package--recognize-function '(lambda () ...)))
-  (should (use-package--recognize-function #'(lambda () ...))))
-
-(ert-deftest use-package--normalize-function-test ()
-  (should (equal (use-package--normalize-function 'sym) 'sym))
-  (should (equal (use-package--normalize-function #'sym) 'sym))
-  (should (equal (use-package--normalize-function (lambda () ...)) (lambda () ...)))
-  (should (equal (use-package--normalize-function '(lambda () ...)) (lambda () ...)))
-  (should (equal (use-package--normalize-function #'(lambda () ...)) (lambda () ...))))
+;; (ert-deftest use-package-test/:delight ()
+;;   (should (equal (macroexpand (use-package))
+;;                  '())))
 
 ;; Local Variables:
 ;; indent-tabs-mode: nil
