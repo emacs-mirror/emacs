d19fe4e037baf8a5cd6e8c0609d3999528bb5ac9
Justin Burkett <justin@burkett.cc>
2018-03-01 13:18:43 -0500

    Fix switching to top-level from which-key-undo-key

--- which-key.el
+++ which-key.el
@@ -2246,7 +2246,8 @@ current evil state. "
           (key-lst
            (which-key-reload-key-sequence key-lst)
            (which-key--create-buffer-and-show (apply #'vector key-lst)))
-          (t (which-key-show-top-level)))))
+          (t (setq which-key--automatic-display nil)
+             (which-key-show-top-level)))))
 (defalias 'which-key-undo 'which-key-undo-key)
 
 (defun which-key-abort (&optional _)
