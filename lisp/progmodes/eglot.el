35962402594c6aaf320878e1c5a6433f1d0553d7
João Távora <joaotavora@gmail.com>
2018-05-17 14:09:50 +0100

    * eglot.el (version): bump to 0.3

--- lisp/progmodes/eglot.el
+++ lisp/progmodes/eglot.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2018 Free Software Foundation, Inc.
 
-;; Version: 0.2
+;; Version: 0.3
 ;; Author: João Távora <joaotavora@gmail.com>
 ;; Maintainer: João Távora <joaotavora@gmail.com>
 ;; URL: https://github.com/joaotavora/eglot
