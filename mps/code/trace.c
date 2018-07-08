8d722c4a2bdef0ae6b1aed2d9bef0a3a4a647828
Gareth Rees <gdr@ravenbrook.com>
2018-07-08 08:08:05 +0100

    Better position for comment.
    Update manual.
    
    Copied from Perforce
     Change: 194511

--- mps/code/trace.c
+++ mps/code/trace.c
@@ -1328,11 +1328,11 @@ mps_res_t _mps_fix2(mps_ss_t mps_ss, mps_addr_t *mps_ref_io)
   }
 
   tract = PageTract(&chunk->pageTable[i]);
+  /* See <walk.c#roots-walk.second-stage> for where we arrange to fool
+     this test when walking references in the roots. */
   if (TraceSetInter(TractWhite(tract), ss->traces) == TraceSetEMPTY) {
     /* Reference points to a tract that is not white for any of the
-       active traces. See <design/trace/#fix.tractofaddr>.
-       See also <walk.c#roots-walk.second-stage> for where we arrange
-       to fool this test when walking references in the roots. */
+     * active traces. See <design/trace/#fix.tractofaddr> */
     STATISTIC({
       if (TRACT_SEG(&seg, tract)) {
         ++ss->segRefCount;
