b742e99f96d51db394c2247e5c025a7048e2a555
Gareth Rees <gdr@ravenbrook.com>
2018-07-11 16:24:47 +0100

    Mmqa tests function/{7,21,22}.c now pass on 32-bit platforms.
    MMQA tests function/{8,98}.c now pass on 64-bit platforms.
    
    Copied from Perforce
     Change: 194599

--- mps/test/function/22.c
+++ mps/test/function/22.c
@@ -1,9 +1,10 @@
 /* 
 TEST_HEADER
  id = $Id$
- summary = allocate large promise, make it small, repeat interleaved
+ summary = allocate large object, free its middle, repeat interleaved
  language = c
  link = testlib.o
+ parameters = OBJECTS=2000
 END_HEADER
 */
 
@@ -16,20 +17,22 @@ static void test(void) {
  mps_addr_t q, r;
  int p;
 
- die(mps_arena_create(&arena, mps_arena_class_vm(), mmqaArenaSIZE), "create");
+ die(mps_arena_create_k(&arena, mps_arena_class_vm(), mps_args_none), "create");
 
  die(mps_pool_create(&pool, arena, mps_class_mv(),
-                    (size_t)(1024*32), (size_t)(1024*16), (size_t)(1024*256)),
+                     (size_t)(32), (size_t)(16), (size_t)(256)),
      "create MV pool");
 
- die(mps_alloc(&q, pool, 1024*1024), "alloc");
+ die(mps_alloc(&q, pool, 1024), "alloc");
 
- for (p=0; p<2000; p++) {
-  report("promise", "%i", p);
-  die(mps_alloc(&r, pool, 1024*1024), "alloc");
-  mps_free(pool, q, 256*1024-MPS_PF_ALIGN);
+ for (p=0; p<OBJECTS; p++) {
+  die(mps_alloc(&r, pool, 1024), "alloc");
+  mps_free(pool, q, 256-MPS_PF_ALIGN);
   q = (mps_addr_t) ((char *) r + MPS_PF_ALIGN);
  }
+
+ mps_pool_destroy(pool);
+ mps_arena_destroy(arena);
 }
 
 int main(void) {
