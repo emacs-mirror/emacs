eb279829cb5c11039abda16b92945a882cfcb39d
Michal Krzywkowski <k.michal@zoho.com>
2018-07-28 19:01:10 +0200

    Work around emacs bugs 32237, 32278 ()
    See:
    https://debbugs.gnu.org/cgi/bugreport.cgi?bug=32237
    https://debbugs.gnu.org/cgi/bugreport.cgi?bug=32278
    
    * eglot.el (eglot--apply-text-edits): Inhibit modification hooks and
      call them manually for the changed region.
    
    GitHub-reference: https://github.com/joaotavora/eglot/issues/53

--- lisp/progmodes/eglot.el
+++ lisp/progmodes/eglot.el
@@ -1438,7 +1438,23 @@ If SKIP-SIGNATURE, don't try to send textDocument/signatureHelp."
                       (save-excursion
                         (save-restriction
                           (narrow-to-region beg end)
-                          (replace-buffer-contents temp)))
+
+                          ;; On emacs versions < 26.2,
+                          ;; `replace-buffer-contents' is buggy - it calls
+                          ;; change functions with invalid arguments - so we
+                          ;; manually call the change functions here.
+                          ;;
+                          ;; See emacs bugs #32237, #32278:
+                          ;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=32237
+                          ;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=32278
+                          (let ((inhibit-modification-hooks t)
+                                (length (- end beg)))
+                            (run-hook-with-args 'before-change-functions
+                                                beg end)
+                            (replace-buffer-contents temp)
+                            (run-hook-with-args 'after-change-functions
+                                                beg (+ beg (length newText))
+                                                length))))
                       (progress-reporter-update reporter (cl-incf done)))))))
             (mapcar (jsonrpc-lambda (&key range newText)
                       (cons newText (eglot--range-region range 'markers)))
