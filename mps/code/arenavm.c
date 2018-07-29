f5e1452ea53e5533b9df53ac4c952616318019cf
Gareth Rees <gdr@ravenbrook.com>
2018-07-29 19:37:31 +0100

    Undo change 194718 (merge branch/2016-03-31/page-sparering-elim into the master sources) because of performance regression <https://info.ravenbrook.com/mail/2018/07/29/16-19-19/0/>
    Copied from Perforce
     Change: 194755

--- mps/code/arenavm.c
+++ mps/code/arenavm.c
@@ -569,7 +569,6 @@ static Res VMArenaCreate(Arena *arenaReturn, ArgList args)
     /* Make it easier to write portable programs by rounding up. */
     grainSize = pageSize;
   AVERT(ArenaGrainSize, grainSize);
-  AVER(sizeof(RingStruct) < grainSize); /* .spare-ring.at-base */
 
   if (ArgPick(&arg, args, MPS_KEY_ARENA_SIZE))
     size = arg.val.size;
@@ -792,37 +791,22 @@ static unsigned pageState(VMChunk vmChunk, Index pi)
 }
 
 
-/* sparePageRing -- get the spare ring node for a spare page
- *
- * .spare-ring.at-base: The spare ring node is stored at the base of
- * the spare page. (Since it's spare, the memory is not needed for any
- * other purpose.)
- */
-
-#define sparePageRing(chunk, pi) ((Ring)PageIndexBase(chunk, pi))
-
-
 /* sparePageRelease -- releases a spare page
  *
  * Either to allocate it or to purge it.
- *
- * Temporarily leaves the page table entry in an inconsistent state:
- * the state is PageStateSPARE but the page is no longer on the spare
- * ring.
+ * Temporarily leaves it in an inconsistent state.
  */
-
 static void sparePageRelease(VMChunk vmChunk, Index pi)
 {
   Chunk chunk = VMChunk2Chunk(vmChunk);
   Arena arena = ChunkArena(chunk);
-  Ring spareRing = sparePageRing(chunk, pi);
+  Page page = ChunkPage(chunk, pi);
 
-  AVER(PageState(ChunkPage(chunk, pi)) == PageStateSPARE);
+  AVER(PageState(page) == PageStateSPARE);
   AVER(arena->spareCommitted >= ChunkPageSize(chunk));
-  AVERT(Ring, spareRing);
 
   arena->spareCommitted -= ChunkPageSize(chunk);
-  RingRemove(spareRing);
+  RingRemove(PageSpareRing(page));
 }
 
 
@@ -1033,29 +1017,27 @@ static Size arenaUnmapSpare(Arena arena, Size size, Chunk filter)
 
   /* Start by looking at the oldest page on the spare ring, to try to
      get some LRU behaviour from the spare pages cache. */
-  /* RING_FOR won't work here, because chunkUnmapAroundPage deletes
-     and unmaps "next" and possibly many other entries from the
-     spareRing. However, we know that it won't delete "node", because
-     either "node" is &vmArena->spareRing (which is not in any chunk),
-     or else "node" and "next" are in discontiguous spans of spare
-     pages (otherwise "node" would have been deleted on the previous
-     iteration). */
+  /* RING_FOR won't work here, because chunkUnmapAroundPage deletes many
+     entries from the spareRing, often including the "next" entry.  However,
+     it doesn't delete entries from other chunks, so we can use them to step
+     around the ring. */
   node = &vmArena->spareRing;
   while (RingNext(node) != &vmArena->spareRing && purged < size) {
     Ring next = RingNext(node);
+    Page page = PageOfSpareRing(next);
     Chunk chunk = NULL; /* suppress uninit warning */
-    Bool b = ChunkOfAddr(&chunk, arena, (Addr)next); /* .spare-ring.at-base */
+    Bool b;
+    /* Use the fact that the page table resides in the chunk to find the
+       chunk that owns the page. */
+    b = ChunkOfAddr(&chunk, arena, (Addr)page);
     AVER(b);
     if (filter == NULL || chunk == filter) {
-      Index pi = IndexOfAddr(chunk, (Addr)next);
-      Page page = ChunkPage(chunk, pi);
       purged += chunkUnmapAroundPage(chunk, size - purged, page);
       /* chunkUnmapAroundPage must delete the page it's passed from the ring,
          or we can't make progress and there will be an infinite loop */
       AVER(RingNext(node) != next);
-    } else {
+    } else
       node = next;
-    }
   }
 
   return purged;
@@ -1112,16 +1094,15 @@ static void VMFree(Addr base, Size size, Pool pool)
   for(pi = piBase; pi < piLimit; ++pi) {
     Page page = ChunkPage(chunk, pi);
     Tract tract = PageTract(page);
-    Ring spareRing;
-    
     AVER(TractPool(tract) == pool);
-    TractFinish(tract);
 
+    TractFinish(tract);
     PageSetPool(page, NULL);
     PageSetType(page, PageStateSPARE);
-    spareRing = sparePageRing(chunk, pi);
-    RingInit(spareRing);
-    RingAppend(&vmArena->spareRing, spareRing);
+    /* We must init the page's rings because it is a union with the
+       tract and will contain junk. */
+    RingInit(PageSpareRing(page));
+    RingAppend(&vmArena->spareRing, PageSpareRing(page));
   }
   arena->spareCommitted += ChunkPagesToSize(chunk, piLimit - piBase);
   BTResRange(chunk->allocTable, piBase, piLimit);
