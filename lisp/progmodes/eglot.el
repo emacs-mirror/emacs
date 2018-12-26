1da5b8e1a31f3032baf26573f5ccb09a26a67375
Brady Trainor <mail@bradyt.com>
2018-12-25 16:21:53 -0800

    Add built-in support for dart's dart_language_server
    Closes https://github.com/joaotavora/eglot/issues/194.
    
    Copyright-paperwork-exempt: yes
    
    * README.md (Connecting to a server): Add dart_language_server.
    
    * eglot.el (eglot-server-programs): Add dart_language_server.

--- lisp/progmodes/eglot.el
+++ lisp/progmodes/eglot.el
@@ -100,7 +100,8 @@ language-server/bin/php-language-server.php"))
                                             "-gocodecompletion"))
                                 ((R-mode ess-r-mode) . ("R" "--slave" "-e"
                                                         "languageserver::run()"))
-                                (java-mode . eglot--eclipse-jdt-contact))
+                                (java-mode . eglot--eclipse-jdt-contact)
+                                (dart-mode . ("dart_language_server")))
   "How the command `eglot' guesses the server to start.
 An association list of (MAJOR-MODE . CONTACT) pairs.  MAJOR-MODE
 is a mode symbol, or a list of mode symbols.  The associated
