4c8d5f0b21dbbf888aac988c2370c4626458f713
Radon Rosborough <radon.neon@gmail.com>
2019-07-15 22:11:13 -0700

    Switch from `require' to `load' + `featurep'

--- lisp/use-package/use-package-core.el
+++ lisp/use-package/use-package-core.el
@@ -651,7 +651,8 @@ extending any keys already present."
                     ,(when (eq use-package-verbose 'debug)
                        `(message ,(format "Compiling package %s" name-string)))
                     ,(unless (plist-get args :no-require)
-                       `(require ',name-symbol)))))))))
+                       `(unless (featurep ',name-symbol)
+                          (load ,name-string nil t))))))))))
 
     ;; Certain keywords imply :defer, if :demand was not specified.
     (when (and (not (plist-member args :demand))
