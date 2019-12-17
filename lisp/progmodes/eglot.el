5c235178991116612a8fa51babe332a4e2fa9553
Antoine Kalmbach <ane@iki.fi>
2019-12-17 21:34:29 +0200

    Add metals as the language server for scala
    * README.md (Connecting to a server): Add metals to the list
    
    * eglot.el (eglot-server-programs): Add metals for scala-mode
    
    Copyright-paperwork-exempt: yes
    GitHub-reference: close https://github.com/joaotavora/eglot/issues/376

--- lisp/progmodes/eglot.el
+++ lisp/progmodes/eglot.el
@@ -104,7 +104,8 @@ language-server/bin/php-language-server.php"))
                                 (java-mode . eglot--eclipse-jdt-contact)
                                 (dart-mode . ("dart_language_server"))
                                 (elixir-mode . ("language_server.sh"))
-                                (ada-mode . ("ada_language_server")))
+                                (ada-mode . ("ada_language_server"))
+                                (scala-mode . ("metals-emacs")))
   "How the command `eglot' guesses the server to start.
 An association list of (MAJOR-MODE . CONTACT) pairs.  MAJOR-MODE
 is a mode symbol, or a list of mode symbols.  The associated
