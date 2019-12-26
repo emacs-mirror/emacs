f10409914da0911e149c236f0983f58573f93739
Phillip Lord <phillip.lord@russet.org.uk>
2019-12-26 21:15:32 +0100

    Update for Emacs-28
    * admin/nt/dist-build/build-dep-zips.py: Emacs major version number.

--- admin/nt/dist-build/build-dep-zips.py
+++ admin/nt/dist-build/build-dep-zips.py
@@ -26,7 +26,7 @@ import re
 from subprocess import check_output
 
 ## Constants
-EMACS_MAJOR_VERSION="27"
+EMACS_MAJOR_VERSION="28"
 
 # This list derives from the features we want Emacs to compile with.
 PKG_REQ='''mingw-w64-x86_64-giflib
