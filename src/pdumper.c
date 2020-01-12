6100f9a19e9d8d8e688ad8bbec2233bd6782cbde
Philipp Stephani <phst@google.com>
2020-01-13 00:17:35 +0100

    * src/pdumper.c (dump_vectorlike): Unbreak build after 724af7671590c

--- src/pdumper.c
+++ src/pdumper.c
@@ -2961,7 +2961,7 @@ dump_vectorlike (struct dump_context *ctx,
                  Lisp_Object lv,
                  dump_off offset)
 {
-#if CHECK_STRUCTS && !defined HASH_pvec_type_E55BD36F8E
+#if CHECK_STRUCTS && !defined HASH_pvec_type_A4A6E9984D
 # error "pvec_type changed. See CHECK_STRUCTS comment in config.h."
 #endif
   const struct Lisp_Vector *v = XVECTOR (lv);
