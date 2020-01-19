3d81d2326a210516496ea7b217f30cf48c897bf6
Stefan Kangas <stefankangas@gmail.com>
2020-01-19 15:43:46 +0100

    Make arguments to dired-chage-marks non-optional
    * lisp/dired.el (dired-change-marks): Make arguments
    non-optional.  (Bug#29842)

--- lisp/dired.el
+++ lisp/dired.el
@@ -3858,9 +3858,10 @@ With prefix argument, unmark or unflag these files."
 	    (if fn (backup-file-name-p fn))))
      "backup file")))
 
-(defun dired-change-marks (&optional old new)
+(defun dired-change-marks (old new)
   "Change all OLD marks to NEW marks.
 OLD and NEW are both characters used to mark files."
+  (declare (advertised-calling-convention '(old new) "28.1"))
   (interactive
    (let* ((cursor-in-echo-area t)
 	  (old (progn (message "Change (old mark): ") (read-char)))
