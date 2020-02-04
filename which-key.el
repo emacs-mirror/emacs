45bfcd3041942a3938791bfa34f28f2a9827d42a
Justin Burkett <justin@burkett.cc>
2020-02-03 20:57:53 -0500

    Make which-key-show prefix mode-line work like top and bottom
    Fixes #234

--- which-key.el
+++ which-key.el
@@ -1556,7 +1556,7 @@ no title exists."
             ((not (string-equal repl-res "")) repl-res)
             ((and (eq which-key-show-prefix 'echo) alternate)
              alternate)
-            ((and (member which-key-show-prefix '(bottom top))
+            ((and (member which-key-show-prefix '(bottom top mode-line))
                   (eq which-key-side-window-location 'bottom)
                   echo-keystrokes)
              (if alternate alternate
