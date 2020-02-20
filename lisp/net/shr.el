17691ad2d31251ae7f6a123b65bd2ac2a1a8cc93
Lars Ingebrigtsen <larsi@gnus.org>
2020-02-20 15:15:07 +0100

    shr comment typo fix
    * lisp/net/shr.el (shr-parse-base): Comment typo fix.

--- lisp/net/shr.el
+++ lisp/net/shr.el
@@ -838,7 +838,7 @@ size, and full-buffer size."
   ;; Always chop off anchors.
   (when (string-match "#.*" url)
     (setq url (substring url 0 (match-beginning 0))))
-  ;; NB: <base href="" > URI may itself be relative to the document s URI
+  ;; NB: <base href=""> URI may itself be relative to the document's URI.
   (setq url (shr-expand-url url))
   (let* ((parsed (url-generic-parse-url url))
 	 (local (url-filename parsed)))
