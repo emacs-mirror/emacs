768dd915ac9aadf9163dd32071ee9b3a71b2f883
Andrea Corallo <akrl@sdf.org>
2020-03-14 23:36:49 +0100

    * elisp-benchmarks.el: Increase minor package version

--- elisp-benchmarks/elisp-benchmarks.el
+++ elisp-benchmarks/elisp-benchmarks.el
@@ -4,7 +4,7 @@
 
 ;; Author: Andrea Corallo <akrl@sdf.org>
 ;; Maintainer: Andrea Corallo <akrl@sdf.org>
-;; Version: 1.2
+;; Version: 1.3
 ;; Keywords: languages, lisp
 ;; Package-Type: multi
 ;; Created: 2019-01-12
