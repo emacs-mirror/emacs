0128375a50352720c626fd915312a5c38762d09a
Lars Ingebrigtsen <larsi@gnus.org>
2020-03-19 16:15:04 +0100

    Don't have exif bugging out on short strings
    * lisp/image/exif.el (exif--direct-ascii-value): New function
    (bug#40127).
    (exif--parse-directory): Use it to get the correct values for
    in-directory (i.e., shorter than 4 octets) strings.

--- test/lisp/image/exif-tests.el
+++ test/lisp/image/exif-tests.el
@@ -41,4 +41,15 @@
     (should (equal (exif-elem exif 'orientation) 1))
     (should (equal (exif-elem exif 'x-resolution) '(180 . 1)))))
 
+(ert-deftest test-exif-parse-short ()
+  (let ((exif (exif-parse-file (test-image-file "black-short.jpg"))))
+    (should (equal (exif-elem exif 'make) "thr"))
+    (should (equal (exif-elem exif 'model) "four"))
+    (should (equal (exif-elem exif 'software) "em"))
+    (should (equal (exif-elem exif 'artist) "z"))))
+
+(ert-deftest test-exit-direct-ascii-value ()
+  (equal (exif--direct-ascii-value 28005 2 t) (string ?e ?m 0))
+  (equal (exif--direct-ascii-value 28005 2 nil) (string ?m ?e 0)))
+
 ;;; exif-tests.el ends here
