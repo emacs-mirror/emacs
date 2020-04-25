f691af80f1c2073e610a382029790f7c6f97dd5d
Andrea Corallo <akrl@sdf.org>
2020-04-25 18:10:06 +0100

    * src/comp.h (load_comp_unit): Fix declaration style.

--- src/comp.h
+++ src/comp.h
@@ -66,8 +66,9 @@ XNATIVE_COMP_UNIT (Lisp_Object a)
 
 extern void hash_native_abi (void);
 
-void load_comp_unit (struct Lisp_Native_Comp_Unit *comp_u, bool loading_dump,
-		     bool late_load);
+extern void load_comp_unit (struct Lisp_Native_Comp_Unit *comp_u,
+			    bool loading_dump, bool late_load);
+
 extern void syms_of_comp (void);
 
 extern void maybe_defer_native_compilation (Lisp_Object function_name,
