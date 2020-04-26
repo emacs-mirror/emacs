763df4bc171c9742cf04b029ddfe495d52461bcf
Stefan Kangas <stefankangas@gmail.com>
2020-04-26 16:22:34 +0200

    Use lexical-binding for themes
    * etc/themes/adwaita-theme.el:
    * etc/themes/deeper-blue-theme.el:
    * etc/themes/dichromacy-theme.el:
    * etc/themes/light-blue-theme.el:
    * etc/themes/manoj-dark-theme.el:
    * etc/themes/misterioso-theme.el:
    * etc/themes/tango-dark-theme.el:
    * etc/themes/tango-theme.el:
    * etc/themes/tsdh-dark-theme.el:
    * etc/themes/tsdh-light-theme.el:
    * etc/themes/wheatgrass-theme.el:
    * etc/themes/whiteboard-theme.el:
    * etc/themes/wombat-theme.el: Use lexical-binding.

--- etc/themes/misterioso-theme.el
+++ etc/themes/misterioso-theme.el
@@ -1,4 +1,4 @@
-;;; misterioso-theme.el --- Custom face theme for Emacs
+;;; misterioso-theme.el --- Custom face theme for Emacs  -*- lexical-binding:t -*-
 
 ;; Copyright (C) 2011-2020 Free Software Foundation, Inc.
 
