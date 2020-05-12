f97e9aa75c794f11442167c239439244c88d343f
Dan Davison <dandavison7@gmail.com>
2020-05-12 11:50:56 -0400

    Don't call flymake report function if flymake is disabled
    Also fix https://github.com/joaotavora/eglot/issues/472.
    
    Copyright-paperwork-exempt: yes
    
    * eglot.el (eglot-handle-notification): Check that flymake-mode is
      active before calling flymake report function.
    
    GitHub-reference: fix https://github.com/joaotavora/eglot/issues/468

--- lisp/progmodes/eglot.el
+++ lisp/progmodes/eglot.el
@@ -1580,7 +1580,7 @@ COMMAND is a symbol naming the command."
                                              (t          'eglot-note))
                                        message `((eglot-lsp-diag . ,diag-spec)))))
          into diags
-         finally (cond (eglot--current-flymake-report-fn
+         finally (cond ((and flymake-mode eglot--current-flymake-report-fn)
                         (funcall eglot--current-flymake-report-fn diags
                                  ;; If the buffer hasn't changed since last
                                  ;; call to the report function, flymake won't
