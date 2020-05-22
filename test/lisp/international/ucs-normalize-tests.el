c5cf630ecd467fdcac13928f7e240cfc98cedc7a
Mattias Engdegård <mattiase@acm.org>
2020-05-22 12:21:28 +0200

    Don't clobber match data in utf-8-hfs conversion (bug#41445)
    Reported by Ture Pålsson.
    
    * lisp/international/ucs-normalize.el
    (ucs-normalize-hfs-nfd-post-read-conversion)
    (ucs-normalize-hfs-nfd-pre-write-conversion):
    Use save-match-data to avoid match data clobber in normalisation.
    * test/lisp/international/ucs-normalize-tests.el
    (ucs-normalize-save-match-data): New test.

--- test/lisp/international/ucs-normalize-tests.el
+++ test/lisp/international/ucs-normalize-tests.el
@@ -341,4 +341,15 @@ implementations:
           (display-buffer (current-buffer)))
       (message "No changes to failing lines needed"))))
 
+(ert-deftest ucs-normalize-save-match-data ()
+  "Verify that match data isn't clobbered (bug#41445)"
+  (string-match (rx (+ digit)) "a47b")
+  (should (equal (match-data t) '(1 3)))
+  (should (equal
+           (decode-coding-string
+            (encode-coding-string "Käsesoßenrührlöffel" 'utf-8-hfs)
+            'utf-8-hfs)
+           "Käsesoßenrührlöffel"))
+  (should (equal (match-data t) '(1 3))))
+
 ;;; ucs-normalize-tests.el ends here
