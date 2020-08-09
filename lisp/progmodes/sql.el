8b4e022c5ec8a2f11fee5ec3438f7e91f09c6cc5
Kristian Hole <kristian@hole.priv.no>
2020-08-09 20:55:52 +0200

    Adds backslash as escape character to mysql syntax-alist
    * lisp/progmodes/sql.el (sql-product-alist): The \ character is an
    escape character in mysql (bug#37459).
    (sql-mode): Changes the example from the incorrect use of
    punctuation rule, to the escape character rule.
    
    Copyright-paperwork-exempt: yes

--- lisp/progmodes/sql.el
+++ lisp/progmodes/sql.el
@@ -455,7 +455,7 @@ file.  Since that is a plaintext file, this could be dangerous."
      :prompt-regexp "^mysql> "
      :prompt-length 6
      :prompt-cont-regexp "^    -> "
-     :syntax-alist ((?# . "< b"))
+     :syntax-alist ((?# . "< b") (?\\ . "\\"))
      :input-filter sql-remove-tabs-filter)
 
     (oracle
@@ -4203,7 +4203,7 @@ must tell Emacs.  Here's how to do that in your init file:
 
 \(add-hook \\='sql-mode-hook
           (lambda ()
-	    (modify-syntax-entry ?\\\\ \".\" sql-mode-syntax-table)))"
+	    (modify-syntax-entry ?\\\\ \"\\\\\" sql-mode-syntax-table)))"
   :abbrev-table sql-mode-abbrev-table
 
   (if sql-mode-menu
