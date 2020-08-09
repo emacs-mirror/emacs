781bcce1196376b3db26a1c298f72779e819db76
Stefan Kangas <stefankangas@gmail.com>
2020-08-09 18:23:45 +0200

    Revert obsoletion of manual-entry
    Ref: https://lists.gnu.org/archive/html/emacs-devel/2020-08/msg00167.html
    
    * lisp/man.el (manual-entry): Revert obsoletion of this alias.

--- lisp/man.el
+++ lisp/man.el
@@ -838,7 +838,7 @@ POS defaults to `point'."
 
 ;; For compatibility with older versions.
 ;;;###autoload
-(define-obsolete-function-alias 'manual-entry 'man "28.1")
+(defalias 'manual-entry 'man)
 
 (defvar Man-completion-cache nil
   ;; On my machine, "man -k" is so fast that a cache makes no sense,
