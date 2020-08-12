a459e5256c2cf2a313db503c2b3f565b03694332
Lars Ingebrigtsen <larsi@gnus.org>
2020-08-12 18:29:05 +0200

    Remove some compat code from idlwave.el
    * lisp/progmodes/idlwave.el: Remove some checks for functions that
    always exist.

--- lisp/progmodes/idlwave.el
+++ lisp/progmodes/idlwave.el
@@ -154,21 +154,6 @@
 (eval-when-compile (require 'cl-lib))
 (require 'idlw-help)
 
-;; For XEmacs
-(unless (fboundp 'line-beginning-position)
-  (defalias 'line-beginning-position 'point-at-bol))
-(unless (fboundp 'line-end-position)
-  (defalias 'line-end-position 'point-at-eol))
-(unless (fboundp 'char-valid-p)
-  (defalias 'char-valid-p 'characterp))
-(unless (fboundp 'match-string-no-properties)
-  (defalias 'match-string-no-properties 'match-string))
-
-(if (not (fboundp 'cancel-timer))
-    (condition-case nil
-	(require 'timer)
-      (error nil)))
-
 (declare-function idlwave-shell-get-path-info "idlw-shell")
 (declare-function idlwave-shell-temp-file "idlw-shell")
 (declare-function idlwave-shell-is-running "idlw-shell")
