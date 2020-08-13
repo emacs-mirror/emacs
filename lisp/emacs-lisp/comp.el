f6502f959253b8f705e324e137c2933c5a668f62
Andrea Corallo <akrl@sdf.org>
2020-08-13 09:45:16 +0200

    ; * lisp/emacs-lisp/comp.el (comp-deferred-compilation): Fix doc.

--- lisp/emacs-lisp/comp.el
+++ lisp/emacs-lisp/comp.el
@@ -42,7 +42,7 @@
   :group 'lisp)
 
 (defcustom comp-deferred-compilation nil
-  "If t compile asyncronously all lexically bound .elc files being loaded.
+  "If non-nil compile asyncronously all .elc files being loaded.
 Once compilation happened each function definition is updated to
 the native compiled one."
   :type 'boolean
