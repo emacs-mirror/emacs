97896e68f9a8749381d023c75db5df352455f76a
Paul Eggert <eggert@cs.ucla.edu>
2020-08-12 18:38:52 -0700

    mml-secure-en-decrypt-sign-2 is unstable
    * test/lisp/gnus/mml-sec-tests.el (mml-secure-en-decrypt-sign-2):
    Mark as unstable (Bug#42720).

--- test/lisp/gnus/mml-sec-tests.el
+++ test/lisp/gnus/mml-sec-tests.el
@@ -681,6 +681,7 @@ In this test, just multiple encryption and signing keys may be available."
 (ert-deftest mml-secure-en-decrypt-sign-2 ()
   "Sign and encrypt message; then decrypt and test for expected result.
 In this test, lists of encryption and signing keys are customized."
+  :tags '(:unstable)
   (skip-unless (test-conf))
   (mml-secure-test-key-fixture
    (lambda ()
