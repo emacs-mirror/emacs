e0c77bb62c1c950a82ea0517646d989dc5c1fe27
ej-32u <ej32u@protonmail.com>
2020-09-05 14:12:22 +0200

    Display name with with spaces, but keep symbol name underneath
    * lisp/cus-edit.el (custom-unlispify-menu-entry): Display the
    pretty name, but keep the real symbol name as the value (bug#41905).
    
    Copyright-paperwork-exempt: yes

--- lisp/cus-edit.el
+++ lisp/cus-edit.el
@@ -561,7 +561,8 @@ value unless you are sure you know what it does."
 	   (unless no-suffix
 	     (goto-char (point-max))
 	     (insert "..."))
-	   (buffer-string)))))
+	   (propertize (symbol-name symbol)
+                       'display (buffer-string))))))
 
 (defcustom custom-unlispify-tag-names t
   "Display tag names as words instead of symbols if non-nil."
