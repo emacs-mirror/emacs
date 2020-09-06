670c41cf1270ca4e91c11a9f75af742b23a87260
Lars Ingebrigtsen <larsi@gnus.org>
2020-09-07 00:45:04 +0200

    Add edebug form to subr--with-wrapper-hook-no-warnings
    * lisp/subr.el (subr--with-wrapper-hook-no-warnings): Add a debug
    form to allow edebugging some stuff.

--- lisp/subr.el
+++ lisp/subr.el
@@ -1823,6 +1823,7 @@ FUN is then called once."
 
 (defmacro subr--with-wrapper-hook-no-warnings (hook args &rest body)
   "Like (with-wrapper-hook HOOK ARGS BODY), but without warnings."
+  (declare (debug (form sexp body)))
   ;; We need those two gensyms because CL's lexical scoping is not available
   ;; for function arguments :-(
   (let ((funs (make-symbol "funs"))
