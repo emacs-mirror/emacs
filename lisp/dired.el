227f93abcc8144e47feced18e750f687244abddc
Lars Ingebrigtsen <larsi@gnus.org>
2020-09-12 15:01:32 +0200

    dired-omit-mode may not be defined in dired-jump
    * lisp/dired.el (dired-jump): dired-omit-mode is in dired-x, so it
    may not be defined in dired.

--- lisp/dired.el
+++ lisp/dired.el
@@ -4528,7 +4528,7 @@ Interactively with prefix argument, read FILE-NAME."
                   (dired-insert-subdir (file-name-directory file))
                   (dired-goto-file file))
                 ;; Toggle omitting, if it is on, and try again.
-                (when dired-omit-mode
+                (when (bound-and-true-p dired-omit-mode)
                   (dired-omit-mode)
                   (dired-goto-file file)))))))))
 
