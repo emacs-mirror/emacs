cc24931b2f7cc01274e98258799d36f264ae0f34
Eli Zaretskii <eliz@gnu.org>
2020-09-13 18:27:44 +0300

    ; * lisp/tab-line.el (tab-line-switch-cycling): Fix last change.

--- lisp/tab-line.el
+++ lisp/tab-line.el
@@ -647,7 +647,7 @@ using the `previous-buffer' command."
 If non-nil, `tab-line-switch-to-prev-tab' in the first tab
 switches to the last tab and `tab-line-switch-to-next-tab' in the
 last tab switches to the first tab.  This variable is not consulted
-when `tab-line-tabs-function' is `tab-line-tabs-window-buffers'"
+when `tab-line-tabs-function' is `tab-line-tabs-window-buffers'."
   :type 'boolean
   :group 'tab-line
   :version "28.1")
