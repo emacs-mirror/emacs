d11627a7cadfe7db9415269135a8f4bec543ccd7
Alex Bochannek <alex@bochannek.com>
2020-10-01 02:44:58 +0200

    Make gnus-base64-repad a bit stricter again
    * lisp/gnus/gnus-util.el (gnus-base64-repad): Make the code a bit
    stricter again.

--- lisp/gnus/gnus-util.el
+++ lisp/gnus/gnus-util.el
@@ -1368,7 +1368,11 @@ CRLF (RFC 5321 SMTP)."
   ;;   input (3.1, 3.3)
   ;; - if line-length is set, error on input exceeding the limit (3.1)
   ;; - reject characters outside base encoding (3.3, also section 12)
-  (let ((splitstr (split-string str "[\n\r \t]+" t)))
+  ;;
+  ;; RFC 5322 section 2.2.3 consideration:
+  ;; Because base 64-encoded strings can appear in long header fields, remove
+  ;; folding whitespace while still observing the RFC 4648 decisions above.
+  (let ((splitstr (split-string str "[ \t]*[\r\n]+[ \t]?" t)))
     (when (and reject-newlines (> (length splitstr) 1))
       (error "Invalid Base64 string"))
     (dolist (substr splitstr)
