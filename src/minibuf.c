f43e9ad524b5d23dd9434e5d70bd95142207d601
Eli Zaretskii <eliz@gnu.org>
2020-11-11 21:46:43 +0200

    Avoid crashes in the daemon due to user interaction
    * src/minibuf.c (read_minibuf): Avoid crashes in the daemon if the
    init file invokes some kind of minibuffer interaction, by not
    updating the selected frame if it's the initial frame.
    (Bug#44583)

--- src/minibuf.c
+++ src/minibuf.c
@@ -666,7 +666,8 @@ read_minibuf (Lisp_Object map, Lisp_Object initial, Lisp_Object prompt,
   /* If cursor is on the minibuffer line,
      show the user we have exited by putting it in column 0.  */
   if (XWINDOW (minibuf_window)->cursor.vpos >= 0
-      && !noninteractive)
+      && !noninteractive
+      && !FRAME_INITIAL_P (SELECTED_FRAME ()))
     {
       XWINDOW (minibuf_window)->cursor.hpos = 0;
       XWINDOW (minibuf_window)->cursor.x = 0;
