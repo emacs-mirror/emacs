58bdfd7c540c49f3727c517c3599c9d601696caf
Alan Mackenzie <acm@muc.de>
2020-12-31 20:32:14 +0000

    CC Mode: increment version numbers to 5.35 due to standalone release
    * doc/misc/cc-mode.texi.  Increment the mode to 5.35, twice.
    
    * lisp/progmodes/cc-defs.el.  Increment the mode to "5.35.1".

--- lisp/progmodes/cc-defs.el
+++ lisp/progmodes/cc-defs.el
@@ -87,7 +87,7 @@
 
 ;;; Variables also used at compile time.
 
-(defconst c-version "5.34.2"
+(defconst c-version "5.35.1"
   "CC Mode version number.")
 
 (defconst c-version-sym (intern c-version))
