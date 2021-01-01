33d159c36f42cadccb02d95099bafac26458d3e8
Paul Eggert <eggert@cs.ucla.edu>
2021-01-01 00:33:28 -0800

    Fix copyright years by hand
    These are dates that admin/update-copyright did not update.

--- lisp/net/tramp-sh.el
+++ lisp/net/tramp-sh.el
@@ -778,7 +778,7 @@ on the remote host.")
 (defconst tramp-perl-encode
   "%s -e '
 # This script contributed by Juanma Barranquero <lektu@terra.es>.
-# Copyright (C) 2002-2020 Free Software Foundation, Inc.
+# Copyright (C) 2002-2021 Free Software Foundation, Inc.
 use strict;
 
 my %%trans = do {
@@ -817,7 +817,7 @@ This string is passed to `format', so percent characters need to be doubled.")
 (defconst tramp-perl-decode
   "%s -e '
 # This script contributed by Juanma Barranquero <lektu@terra.es>.
-# Copyright (C) 2002-2020 Free Software Foundation, Inc.
+# Copyright (C) 2002-2021 Free Software Foundation, Inc.
 use strict;
 
 my %%trans = do {
