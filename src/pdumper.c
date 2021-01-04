56556b5f4d73d9c3683fa7573e6bd89f2ef37902
Basil L. Contovounesios <contovob@tcd.ie>
2021-01-04 13:09:40 +0000

    Fix build for --enable-checking=structs
    The last change to lisp.h only added comments in Lisp_String, so the
    portable dumper need not be changed.
    
    * src/pdumper.c (dump_string): Update hash for Lisp_String.

--- src/pdumper.c
+++ src/pdumper.c
@@ -2058,7 +2058,7 @@ dump_interval_tree (struct dump_context *ctx,
 static dump_off
 dump_string (struct dump_context *ctx, const struct Lisp_String *string)
 {
-#if CHECK_STRUCTS && !defined (HASH_Lisp_String_86FEA6EC7C)
+#if CHECK_STRUCTS && !defined (HASH_Lisp_String_348C2B2FDB)
 # error "Lisp_String changed. See CHECK_STRUCTS comment in config.h."
 #endif
   /* If we have text properties, write them _after_ the string so that
