b2e6ed40268e7339be501b3481774773179ca476
Eli Zaretskii <eliz@gnu.org>
2021-01-18 19:17:59 +0200

    Fix recent changes for Cham script
    * lisp/language/cham.el ("Cham"): Fix sample-text.
    * lisp/leim/quail/cham.el: Really install this new file.

--- lisp/language/cham.el
+++ lisp/language/cham.el
@@ -36,7 +36,7 @@
 	      (coding-system utf-8)
 	      (coding-priority utf-8)
               (input-method . "cham")
-              (sample-text . "Cham (ꨌꩌ)\tꨦꨤꩌ ꨦꨰꨁ")
+              (sample-text . "Cham (ꨌꩌ)\tꨦꨤꩌ ꨦꨁꨰ")
               (documentation . "\
 The Cham script is a Brahmic script used to write Cham,
 an Austronesian language spoken by some 245,000 Chams
