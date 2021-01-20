1a6ed932d9d9779419f403e32e911f86657cb9f7
Lars Ingebrigtsen <larsi@gnus.org>
2021-01-20 22:11:38 +0100

    Revert "Always send Lisp words to checkdoc-ispell-init"
    This reverts commit 93141d581330d94e7eec9f114def2bec15f87866.
    
    This would make checkdoc words be used in other flyspell
    buffers.

--- lisp/emacs-lisp/checkdoc.el
+++ lisp/emacs-lisp/checkdoc.el
@@ -2106,14 +2106,12 @@ nil."
   (unless ispell-process
     (condition-case nil
 	(progn
-          ;; Initialize variables and dict alists.
-          (ispell-set-spellchecker-params)
-          ;; Use the correct dictionary.
-          (ispell-accept-buffer-local-defs))
-      (error (setq checkdoc-spellcheck-documentation-flag nil))))
-  ;; This code copied in part from ispell.el Emacs 19.34
-  (dolist (w checkdoc-ispell-lisp-words)
-    (process-send-string ispell-process (concat "@" w "\n"))))
+          (ispell-set-spellchecker-params)    ; Initialize variables and dict alists.
+          (ispell-accept-buffer-local-defs)   ; Use the correct dictionary.
+	  ;; This code copied in part from ispell.el Emacs 19.34
+	  (dolist (w checkdoc-ispell-lisp-words)
+	    (process-send-string ispell-process (concat "@" w "\n"))))
+      (error (setq checkdoc-spellcheck-documentation-flag nil)))))
 
 (defun checkdoc-ispell-docstring-engine (end &optional take-notes)
   "Run the Ispell tools on the doc string between point and END.
