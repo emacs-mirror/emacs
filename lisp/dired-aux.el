7cc970e7e3939672ec8ae490fff8300395e16b76
Jean Louis <bugs@gnu.support>
2021-01-24 00:34:44 +0100

    Add support for dired compressing .lz/.lzo files
    * lisp/dired-aux.el (dired-compress-files-alist): Add support for
    .lz/.lzo files (bug#44901).

--- lisp/dired-aux.el
+++ lisp/dired-aux.el
@@ -1168,6 +1168,8 @@ ARGS are command switches passed to PROGRAM.")
     ("\\.tar\\.bz2\\'" . "tar -cf - %i | bzip2 -c9 > %o")
     ("\\.tar\\.xz\\'" . "tar -cf - %i | xz -c9 > %o")
     ("\\.tar\\.zst\\'" . "tar -cf - %i | zstd -19 -o %o")
+    ("\\.tar\\.lz\\'" . "tar -cf - %i | lzip -c9 > %o")
+    ("\\.tar\\.lzo\\'" . "tar -cf - %i | lzop -c9 > %o")
     ("\\.zip\\'" . "zip %o -r --filesync %i")
     ("\\.pax\\'" . "pax -wf %o %i"))
   "Control the compression shell command for `dired-do-compress-to'.
