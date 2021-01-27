ae361b0a49d0db70aedfa481070d88752fe62f35
ssnnoo <43703153+ssnnoo@users.noreply.github.com>
2021-01-27 09:10:07 +0000

    Add fortls for fotran (f90-mode)
    * eglot.el (eglot-server-programs): Add fortls
    
    * README.md: mention fortls
    
    Copyright-paperwork-exempt: yes
    GitHub-reference: close https://github.com/joaotavora/eglot/issues/603

--- lisp/progmodes/eglot.el
+++ lisp/progmodes/eglot.el
@@ -124,7 +124,8 @@ language-server/bin/php-language-server.php"))
                                  . ("digestif"))
                                 (erlang-mode . ("erlang_ls" "--transport" "stdio"))
                                 (nix-mode . ("rnix-lsp"))
-                                (gdscript-mode . ("localhost" 6008)))
+                                (gdscript-mode . ("localhost" 6008))
+                                (f90-mode . ("fortls")))
   "How the command `eglot' guesses the server to start.
 An association list of (MAJOR-MODE . CONTACT) pairs.  MAJOR-MODE
 is a mode symbol, or a list of mode symbols.  The associated
