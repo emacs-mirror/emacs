1275dc4711af77c9c223063dcd149d782d497463
Lars Ingebrigtsen <larsi@gnus.org>
2021-01-29 07:40:06 +0100

    Improve fontifying of #| ... |# in `lisp-mode'
    * lisp/emacs-lisp/lisp-mode.el (lisp-mode): Fontify the end
    delimiter in #| ... |# correctly (bug#39820).

--- lisp/emacs-lisp/lisp-mode.el
+++ lisp/emacs-lisp/lisp-mode.el
@@ -775,6 +775,7 @@ or to switch back to an existing one."
   (setq-local find-tag-default-function 'lisp-find-tag-default)
   (setq-local comment-start-skip
 	      "\\(\\(^\\|[^\\\n]\\)\\(\\\\\\\\\\)*\\)\\(;+\\|#|\\) *")
+  (setq-local comment-end "|#")
   (setq imenu-case-fold-search t))
 
 (defun lisp-find-tag-default ()
