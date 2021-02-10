4fba79feee58e074d112bb47467913f9aec089c7
Andrea Corallo <akrl@sdf.org>
2021-02-10 21:48:19 +0100

    Add late load pdumper hooks so these can call into Lisp
    	* src/pdumper.h (pdumper_do_now_and_after_late_load): New function.
    	* src/pdumper.c (dump_late_hooks, nr_dump_late_hooks): New static
    	variables.
    	(dump_metadata_for_pdumper): Add support for late load hooks.
    	(pdumper_do_now_and_after_late_load_impl): New functions.
    	(pdumper_load): Add support for late load hooks.
    	* src/window.c (init_window_once): Register
    	'init_window_once_for_pdumper' to be executed after late load.

--- src/pdumper.h
+++ src/pdumper.h
@@ -81,12 +81,25 @@ pdumper_remember_lv_ptr_raw (void *ptr, enum Lisp_Type type)
 
 typedef void (*pdumper_hook)(void);
 extern void pdumper_do_now_and_after_load_impl (pdumper_hook hook);
+extern void pdumper_do_now_and_after_late_load_impl (pdumper_hook hook);
 
 INLINE void
 pdumper_do_now_and_after_load (pdumper_hook hook)
 {
 #ifdef HAVE_PDUMPER
   pdumper_do_now_and_after_load_impl (hook);
+#else
+  hook ();
+#endif
+}
+
+/* Same as 'pdumper_do_now_and_after_load' but for hooks running code
+   that can call into Lisp.  */
+INLINE void
+pdumper_do_now_and_after_late_load (pdumper_hook hook)
+{
+#ifdef HAVE_PDUMPER
+  pdumper_do_now_and_after_late_load_impl (hook);
 #else
   hook ();
 #endif
