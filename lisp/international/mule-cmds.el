6ad9b8d677fe136b9a0489eef0c2dd6a1f63917d
Stefan Monnier <monnier@iro.umontreal.ca>
2021-03-01 11:42:04 -0500

    * src/eval.c (init_eval_once): Bump max_specpdl_size (bug46818)
    Further testing seems to confirm my suspicion that the increase in the
    specpdl comes from the recent change to `pcase--if`.
    
    * lisp/international/mule-cmds.el (update-leim-list-file): Revert workaround.

--- lisp/international/mule-cmds.el
+++ lisp/international/mule-cmds.el
@@ -1312,13 +1312,8 @@ Each function is called with one arg, LEIM directory name.")
 
 (defun update-leim-list-file (&rest dirs)
   "Update LEIM list file in directories DIRS."
-  ;; bug#46818: This `let'-binding is not necessary, but
-  ;; it reduces the recursion depth during bootstrap (at which
-  ;; point some of the core ELisp files haven't been byte-compiled
-  ;; yet, which causes deeper-than-normal recursion).
-  (let ((vc-handled-backends nil))
-    (dolist (function update-leim-list-functions)
-      (apply function dirs))))
+  (dolist (function update-leim-list-functions)
+    (apply function dirs)))
 
 (defvar-local current-input-method nil
   "The current input method for multilingual text.
