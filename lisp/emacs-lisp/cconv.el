009bc7c9d8bd0074a78ebef73102f600a514172c
Stefan Monnier <monnier@iro.umontreal.ca>
2021-03-11 22:27:41 -0500

    * lisp/emacs-lisp/cconv.el (cconv--analyze-use): Tune down the warning
    Don't warn for always-nil bindings if the binding is made explicit.
    Fixes bug#47080.

--- lisp/emacs-lisp/cconv.el
+++ lisp/emacs-lisp/cconv.el
@@ -602,7 +602,8 @@ FORM is the parent form that binds this var."
      (byte-compile-warn
       "%s `%S' not left unused" varkind var))
     ((and (let (or 'let* 'let) (car form))
-          `(,(or `(,var) `(,var nil)) t nil ,_ ,_))
+          `(,`(,var) ;; (or `(,var nil) : Too many false positives: bug#47080
+            t nil ,_ ,_))
      ;; FIXME: Convert this warning to use `macroexp--warn-wrap'
      ;; so as to give better position information.
      (unless (not (intern-soft var))
