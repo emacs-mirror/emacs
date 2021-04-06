2f5f30671a122aeed0a78dbee8f541dda5a6d4c2
Damien Cassou <damien@cassou.me>
2021-04-06 19:31:56 +0200

    Fix broken links in autorevert.el
    * lisp/autorevert.el (global-auto-revert-non-file-buffers):
    Fix broken links.  (Bug#47621)

--- lisp/autorevert.el
+++ lisp/autorevert.el
@@ -227,10 +227,10 @@ modes, etc., of files.  You may still sometimes want to revert
 them manually.
 
 Use this option with care since it could lead to excessive auto-reverts.
-For more information, see Info node `(emacs)Autorevert'."
+For more information, see Info node `(emacs)Auto Revert'."
   :group 'auto-revert
   :type 'boolean
-  :link '(info-link "(emacs)Autorevert"))
+  :link '(info-link "(emacs)Auto Revert"))
 
 (defcustom global-auto-revert-ignore-modes ()
   "List of major modes Global Auto-Revert Mode should not check."
