1054525ae3795b71fe09dafcbb86d3cdb1d7f875
Dmitry Gutov <dgutov@yandex.ru>
2021-04-29 04:59:28 +0300

    * lisp/progmodes/project.el: Also bump version.

--- lisp/progmodes/project.el
+++ lisp/progmodes/project.el
@@ -1,7 +1,7 @@
 ;;; project.el --- Operations on the current project  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2015-2021 Free Software Foundation, Inc.
-;; Version: 0.5.4
+;; Version: 0.6.0
 ;; Package-Requires: ((emacs "26.1") (xref "1.0.2"))
 
 ;; This is a GNU ELPA :core package.  Avoid using functionality that
