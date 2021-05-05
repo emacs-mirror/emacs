3481903d136af4380b5ee22879500e0295da8eba
Andrea Corallo <akrl@sdf.org>
2021-05-05 17:00:59 +0200

    * Better identify native compiler presence in two tests.
    	* test/lisp/help-fns-tests.el (help-fns-test-lisp-defun)
    	(help-fns-test-lisp-defsubst): Better identify native-comp
    	presence.

--- test/lisp/help-fns-tests.el
+++ test/lisp/help-fns-tests.el
@@ -62,14 +62,14 @@ Return first line of the output of (describe-function-1 FUNC)."
     (should (string-match regexp result))))
 
 (ert-deftest help-fns-test-lisp-defun ()
-  (let ((regexp (if (boundp 'comp-ctxt)
+  (let ((regexp (if (featurep 'nativecomp)
                     "a native compiled Lisp function in .+subr\\.el"
                   "a compiled Lisp function in .+subr\\.el"))
         (result (help-fns-tests--describe-function 'last)))
     (should (string-match regexp result))))
 
 (ert-deftest help-fns-test-lisp-defsubst ()
-  (let ((regexp (if (boundp 'comp-ctxt)
+  (let ((regexp (if (featurep 'nativecomp)
                     "a native compiled Lisp function in .+subr\\.el"
                   "a compiled Lisp function in .+subr\\.el"))
         (result (help-fns-tests--describe-function 'posn-window)))
