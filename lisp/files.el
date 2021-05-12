ec574a72f7198d9793b466f33382fff397ac4ce1
Tom Gillespie <tgbugs@gmail.com>
2021-05-12 23:31:23 +0200

    Fix evaluation order for hack-local-variables
    * lisp/files.el (hack-local-variables): Fix the ordering which
    local variables are evaluated by `hack-local-variables' so that
    prop-line local variables are evaluated first. There is a hidden
    nreverse lurking in `hack-local-variables-apply' which means that
    the prop line variables must come second in order to be evaluated
    before the end of file variables.

--- lisp/files.el
+++ lisp/files.el
@@ -3771,8 +3771,8 @@ inhibited."
       (with-demoted-errors "Directory-local variables error: %s"
 	;; Note this is a no-op if enable-local-variables is nil.
 	(hack-dir-local-variables))
-      (let ((result (append (hack-local-variables-prop-line)
-                            (hack-local-variables--find-variables))))
+      (let ((result (append (hack-local-variables--find-variables)
+                            (hack-local-variables-prop-line))))
         (if (and enable-local-variables
                  (not (inhibit-local-variables-p)))
             (progn
