fc87cdf95986667bf083fdab01df20a6a07d5d0f
Michael Albinus <michael.albinus@gmx.de>
2021-05-23 12:34:36 +0200

    Adapt tramp-archive-autoload-file-name-handler
    * lisp/net/tramp-archive.el (tramp-archive-autoload-file-name-handler):
    Bind `default-directory' to safe value.

--- lisp/net/tramp-archive.el
+++ lisp/net/tramp-archive.el
@@ -347,10 +347,15 @@ arguments to pass to the OPERATION."
 ;;;###autoload
 (progn (defun tramp-archive-autoload-file-name-handler (operation &rest args)
   "Load Tramp archive file name handler, and perform OPERATION."
-  (if tramp-archive-enabled
-      (let ((tramp-archive-autoload t))
-        tramp-archive-autoload ; Silence byte compiler.
-        (apply #'tramp-autoload-file-name-handler operation args)))))
+  (when tramp-archive-enabled
+    ;; We cannot use `tramp-compat-temporary-file-directory' here due
+    ;; to autoload.  When installing Tramp's GNU ELPA package, there
+    ;; might be an older, incompatible version active.  We try to
+    ;; overload this.
+    (let ((default-directory temporary-file-directory)
+          (tramp-archive-autoload t))
+      tramp-archive-autoload ; Silence byte compiler.
+      (apply #'tramp-autoload-file-name-handler operation args)))))
 
 ;;;###autoload
 (progn (defun tramp-register-archive-file-name-handler ()
