3988411f6c6a1170bca0831874233ff4b739009c
Philipp Stephani <phst@google.com>
2021-05-30 22:38:48 +0200

    Give 'syntax-propertize-wholelines' a docstring.
    This function is generally useful.
    
    * lisp/emacs-lisp/syntax.el (syntax-propertize-wholelines): Add
    docstring.

--- lisp/emacs-lisp/syntax.el
+++ lisp/emacs-lisp/syntax.el
@@ -125,6 +125,10 @@ otherwise nil.  That construct can be a two character comment
 delimiter or an Escaped or Char-quoted character."))
 
 (defun syntax-propertize-wholelines (start end)
+  "Extend the region delimited by START and END to whole lines.
+This function is useful for
+`syntax-propertize-extend-region-functions';
+see Info node `(elisp) Syntax Properties'."
   (goto-char start)
   (cons (line-beginning-position)
         (progn (goto-char end)
