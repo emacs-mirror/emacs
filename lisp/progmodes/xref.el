6ec3cf1ccb5380acc376e89140b8d3a7fa4e471a
Dmitry Gutov <dgutov@yandex.ru>
2021-07-06 01:55:26 +0300

    (xref--insert-xrefs): Fix printing of line numbers
    * lisp/progmodes/xref.el (xref--insert-xrefs):
    Fix printing of line numbers when we have multiple files with
    (e.g.) single match on the same line.

--- lisp/progmodes/xref.el
+++ lisp/progmodes/xref.el
@@ -959,7 +959,9 @@ GROUP is a string for decoration purposes and XREF is an
                              (prefix
                               (cond
                                ((not line) "  ")
-                               ((equal line prev-line) "")
+                               ((and (equal line prev-line)
+                                     (equal prev-group group))
+                                "")
                                (t (propertize (format line-format line)
                                               'face 'xref-line-number)))))
                         ;; Render multiple matches on the same line, together.
