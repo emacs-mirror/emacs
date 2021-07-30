02cbb37de73d563149389615ee44741322007108
Lars Ingebrigtsen <larsi@gnus.org>
2021-07-30 18:25:12 +0200

    Allow nil initializers in define-minor-mode
    * lisp/emacs-lisp/easy-mmode.el (define-minor-mode): Make the
    meaning of :initialize nil and a missing :initialize the same.

--- lisp/emacs-lisp/easy-mmode.el
+++ lisp/emacs-lisp/easy-mmode.el
@@ -251,7 +251,9 @@ INIT-VALUE LIGHTER KEYMAP.
                    (setq getter `(default-value ',mode))))
 	(:extra-args (setq extra-args (pop body)))
 	(:set (setq set (list :set (pop body))))
-	(:initialize (setq initialize (list :initialize (pop body))))
+	(:initialize
+         (when-let ((val (pop body)))
+           (setq initialize (list :initialize val))))
 	(:type (setq type (list :type (pop body))))
 	(:keymap (setq keymap (pop body)))
 	(:interactive (setq interactive (pop body)))
