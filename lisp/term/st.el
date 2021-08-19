6276f73115a0e976ea1e1cfdec231ca6c2396c88
Lars Ingebrigtsen <larsi@gnus.org>
2021-08-19 14:18:27 +0200

    stterm doesn't understand all codes that xterm does
    * lisp/term/st.el (xterm-st-extra-capabilities): New user option
    (bug#50119).
    (terminal-init-st): Use it to avoid a two-second timeout.

--- lisp/term/st.el
+++ lisp/term/st.el
@@ -11,9 +11,20 @@
 
 (require 'term/xterm)
 
+(defcustom xterm-st-extra-capabilities '( modifyOtherKeys getSelection
+                                          setSelection)
+  "Extra capabilities supported under \"stterm\".
+Some features of screen depend on the terminal emulator in which
+it runs, which can change when the screen session is moved to another tty."
+  :version "28.1"
+  :type xterm--extra-capabilities-type
+  :group 'xterm)
+
 (defun terminal-init-st ()
   "Terminal initialization function for st."
-  (tty-run-terminal-initialization (selected-frame) "xterm"))
+  ;; Using `check' leads to a two-second timeout.
+  (let ((xterm-extra-capabilities xterm-st-extra-capabilities))
+    (tty-run-terminal-initialization (selected-frame) "xterm")))
 
 (provide 'term/st)
 
