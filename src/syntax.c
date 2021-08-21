5da710344392ac59a814b8841d31a562823737f0
Lars Ingebrigtsen <larsi@gnus.org>
2021-08-21 16:07:44 +0200

    Tweak the comment-start-skip example in the manual
    * doc/emacs/programs.texi (Options for Comments): Tweak the
    example regexp for comment-start-skip (bug#15006).

--- src/syntax.c
+++ src/syntax.c
@@ -17,6 +17,7 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.  */
 
+// foo
 
 #include <config.h>
 
