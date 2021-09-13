79eb840753edb2dd9e0bf1d4befb8e3e1275e009
João Távora <joaotavora@gmail.com>
2021-09-13 17:43:43 +0100

    Bump lisp/progmodes/project.el version to 0.7.1
    Amont other things exposes the new project-buffers generic function to
    ELPA users.
    
    * lisp/progmodes/project.el (Version): Bump to 0.7.1

--- lisp/progmodes/project.el
+++ lisp/progmodes/project.el
@@ -1,7 +1,7 @@
 ;;; project.el --- Operations on the current project  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2015-2021 Free Software Foundation, Inc.
-;; Version: 0.6.1
+;; Version: 0.7.1
 ;; Package-Requires: ((emacs "26.1") (xref "1.0.2"))
 
 ;; This is a GNU ELPA :core package.  Avoid using functionality that
