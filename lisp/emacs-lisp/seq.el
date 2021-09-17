3e5298fc96c98d2296432d31ee1d3de86a4c466c
Stefan Kangas <stefan@marxist.se>
2021-09-17 14:01:20 +0200

    Improve performance of seq-union
    * lisp/emacs-lisp/seq.el (seq-union): Improve performance by using
    nreverse instead of seq-reverse.

--- lisp/emacs-lisp/seq.el
+++ lisp/emacs-lisp/seq.el
@@ -471,13 +471,13 @@ negative integer or 0, nil is returned."
 (cl-defgeneric seq-union (sequence1 sequence2 &optional testfn)
   "Return a list of all elements that appear in either SEQUENCE1 or SEQUENCE2.
 Equality is defined by TESTFN if non-nil or by `equal' if nil."
-  (let ((accum (lambda (acc elt)
-                 (if (seq-contains-p acc elt testfn)
-                     acc
-                   (cons elt acc)))))
-    (seq-reverse
-     (seq-reduce accum sequence2
-                 (seq-reduce accum sequence1 '())))))
+  (let* ((accum (lambda (acc elt)
+                  (if (seq-contains-p acc elt testfn)
+                      acc
+                    (cons elt acc))))
+         (result (seq-reduce accum sequence2
+                          (seq-reduce accum sequence1 '()))))
+    (nreverse result)))
 
 ;;;###autoload
 (cl-defgeneric seq-intersection (sequence1 sequence2 &optional testfn)
