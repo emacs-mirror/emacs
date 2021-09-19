9ad962e118a17daf073ef5308233f9301755035d
João Távora <joaotavora@gmail.com>
2021-09-19 17:08:41 +0100

    Expand and improve electric-pair-mode and Python testing (bug#49518)
    In python-tests.el, the triple-quote pairing tests are passing
    incorrectly, i.e. the auto-pairing functionality they purport to guard
    isn't really working for users trying it interactively.  Added a new
    failing test, soon to be fixed.
    
    In electric-tests.el, added tests for Python, too.
    
    * test/lisp/electric-tests.el (define-electric-pair-test): Also run
    main tests for python-mode.  (pair-some-quotes-skip-others): Test
    another slightly different pairing.
    
    * test/lisp/progmodes/python-tests.el
    (python-triple-double-quote-pairing): Rename from
    python-triple-quote-pairing.
    (python-triple-single-quote-pairing): New test.

--- test/lisp/electric-tests.el
+++ test/lisp/electric-tests.el
@@ -174,7 +174,7 @@ The buffer's contents should %s:
           expected-string
           expected-point
           bindings
-          (modes '(quote (ruby-mode js-mode)))
+          (modes '(quote (ruby-mode js-mode python-mode)))
           (test-in-comments t)
           (test-in-strings t)
           (test-in-code t)
@@ -297,7 +297,7 @@ The buffer's contents should %s:
 ;;; Quotes
 ;;;
 (define-electric-pair-test pair-some-quotes-skip-others
-  " \"\"      " "-\"\"-----" :skip-pair-string "-ps------"
+  " \"\"      " "-\"\"-\"---" :skip-pair-string "-ps-p----"
   :test-in-strings nil
   :bindings `((electric-pair-text-syntax-table
                . ,prog-mode-syntax-table)))
