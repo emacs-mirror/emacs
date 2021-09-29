7c79eea51dbbe84e6ee01db30dae87ff373b7f3b
Protesilaos Stavrou <info@protesilaos.com>
2021-09-29 11:40:32 +0300

    Update modus-themes to version 1.6.0
    * doc/misc/modus-themes.org (Customization Options): Document new user
    options in code sample.
    (Option for inhibiting theme reload): Remove trailing space.
    (Option for mode line presentation): Include new possible value.
    (Option for Org agenda constructs): Include new symbols for the
    'modus-themes-org-agenda' alist.
    (Control the scale of headings): Document 'modus-themes-scale-small'.
    (Override color saturation (DIY)): Fix markup for proper texi output.
    (Custom Org user faces (DIY)): Update code samples.
    (Full support for packages or face groups): Note names of newly
    supported packages or face groups.
    (Indirectly covered packages): Document indirectly supported packages.
    (Note on highlight-parentheses.el): Provide detailed instructions on
    how to use 'highlight-parentheses' with the Modus themes.
    (Note on prism.el): Refine code samples.
    (What is the best setup for legibility?): Minor rewording.
    (Sources of the themes): Mention only Emacs28 without explaining that
    it is the development target---in preparation of the emacs-28 branch
    cut.
    (Acknowledgements): Name new contributors to code/ideas.  Stephen
    Gildea's patch was a couple lines long.  The others have assigned
    copyright to the FSF.
    (Meta): Include another link to the development notes of the themes
    about 'modus-themes-org-agenda'.
    
    * etc/themes/modus-operandi-theme.el,
      etc/themes/modus-vivendi-theme.el: Bump file version.
    
    * etc/themes/modus-themes.el (modus-themes-operandi-colors)
    (modus-themes-vivendi-colors): Recalibrate some colour values and add
    a few new ones.
    (modus-themes-slanted-constructs): Remove obsolete user option.
    Superseded by the alias 'modus-themes-italic-constructs'.
    (modus-themes-org-agenda, modus-themes-mode-line): Update user option.
    (modus-themes-scale-headings, modus-themes-scale-4): Update doc
    string.
    (modus-themes-scale-5): Remove obsolete user option.  Superseded by
    the alias 'modus-themes-scale-title'.
    (modus-themes-scale-small, modus-themes-tabs-accented): Add new user
    option.
    (modus-themes--agenda-date, modus-themes--mode-line-attrs)
    (modus-themes--tab): Update internal functions.
    (modus-themes-faces): Update variousface attributes.
    
    Detailed change log here:
    <https://protesilaos.com/codelog/2021-09-29-modus-themes-1-6-0/>.

--- etc/themes/modus-vivendi-theme.el
+++ etc/themes/modus-vivendi-theme.el
@@ -4,7 +4,7 @@
 
 ;; Author: Protesilaos Stavrou <info@protesilaos.com>
 ;; URL: https://gitlab.com/protesilaos/modus-themes
-;; Version: 1.5.0
+;; Version: 1.6.0
 ;; Package-Requires: ((emacs "26.1"))
 ;; Keywords: faces, theme, accessibility
 
