ca3d7234d39fd55e6cd4521e5e583aba12434402
Amin Bandali <bandali@gnu.org>
2021-10-15 16:51:59 -0400

    Release ERC 5.4.1
    * doc/misc/erc.texi (ERCVER): Bump to 5.4.1.
    * etc/ERC-NEWS: Add entry for 5.4.1 with explanations.
    * lisp/erc/erc.el (Version, erc-version): Bump to 5.4.1.
    (customize-package-emacs-version-alist): Add entry for 5.4.1.

--- lisp/erc/erc.el
+++ lisp/erc/erc.el
@@ -12,7 +12,7 @@
 ;;               David Edmondson (dme@dme.org)
 ;;               Michael Olson (mwolson@gnu.org)
 ;;               Kelvin White (kwhite@gnu.org)
-;; Version: 5.4
+;; Version: 5.4.1
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: IRC, chat, client, Internet
 ;; URL: https://www.gnu.org/software/emacs/erc.html
@@ -69,7 +69,7 @@
 (require 'iso8601)
 (eval-when-compile (require 'subr-x))
 
-(defconst erc-version "5.4"
+(defconst erc-version "5.4.1"
   "This version of ERC.")
 
 (defvar erc-official-location
@@ -83,7 +83,8 @@
  'customize-package-emacs-version-alist
  '(ERC ("5.2" . "22.1")
        ("5.3" . "23.1")
-       ("5.4" . "28.1")))
+       ("5.4" . "28.1")
+       ("5.4.1" . "29.1")))
 
 (defgroup erc nil
   "Emacs Internet Relay Chat client."
