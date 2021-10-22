51719617eb19833056618ebac403cdcaf711551a
Alan Mackenzie <acm@muc.de>
2021-10-22 19:55:01 +0000

    CC Mode: Fontify "found types" which are recognized after being first scanned
    This aims to fix the scenario where on jit-lock's first scan of a type, it is
    not recognized as such, and only later does this happen.  The fontification of
    such found types is now done by background scanning in short time slices
    immediately after initialising the mode.
    
    * lisp/progmodes/cc-engine.el (c-add-type-1): New function.
    (c-add-type): Extract c-add-type-1 from it, and reformulate the mechanism for
    protecting c-found-types from excessive partial identifiers.
    
    * lisp/progmodes/cc-fonts.el (c-font-lock-complex-decl-prepare): Remove the
    code which cleared c-found-types on fontification at BOB.
    (c-find-types-background): New function, based on c-font-lock-declarations).
    (c-types-finder-timer-func): New function.
    (c-re-redisplay-timer): New variable.
    (c-force-redisplay, c-fontify-new-found-type): New functions.
    
    * lisp/progmodes/cc-mode.el (c-type-finder-timer): New variable.
    (c-leave-cc-mode-mode): Nullify c-post-command-hook and c-type-finder-timer
    when the last CC Mode buffer of a session is killed.
    (c-type-finder-pos): New variable.
    (c-basic-common-init): Initialize c-type-finder-pos and c-type-finder-timer.
    (c-new-id-start, c-new-id-end, c-new-id-is-type): New variables.
    (c-update-new-id): New function.
    (c-post-command): New post command hook function, used for checking moving
    away from partially typed identifiers, and making them full identifiers.
    (c-before-change): Add code to clear c-found-types on a buffer change at BOB.
    (c-after-change): Call c-update-new-id to keep track of partially typed
    identifiers.
    
    * doc/misc/cc-mode.texi (Found Types): New @section in the @Chapter Font
    Locking.
    
    * lisp/progmodes/cc-vars.el (c-type-finder-time-slot)
    (c-type-finder-repeat-time, c-type-finder-chunk-size): New customizable
    options.

--- lisp/progmodes/cc-vars.el
+++ lisp/progmodes/cc-vars.el
@@ -1524,6 +1524,39 @@ working due to this change."
   :type 'boolean
   :group 'c)
 
+(defcustom c-type-finder-time-slot 0.05
+  "The length in seconds of a background type search time slot.
+
+In CC Mode modes, \"found types\" wouldn't always get cleanly
+fontified without the background searching for them which happens
+in the seconds after starting Emacs or initializing the major
+mode.
+
+This background searching can be disabled by setting this option
+to nil."
+  :type '(choice (const :tag "disabled" nil)
+		 number)
+  :group 'c)
+
+(defcustom c-type-finder-repeat-time 0.1
+  "The interval, in seconds, at which background type searches occur.
+
+This interval must be greater than `c-type-finder-time-slot'."
+  :type 'number
+  :group 'c)
+
+(defcustom c-type-finder-chunk-size 1000
+  "The size, in characters, of a chunk for background type search.
+
+Chunks of this size are searched atomically for \"found types\"
+just after starting Emacs or initializing the major mode.
+
+This chunk size is a balance between efficiency (with larger
+values) and responsiveness of the keyboard (with smaller values).
+See also `c-type-finder-time-slot'."
+  :type 'integer
+  :group 'c)
+
 (define-widget 'c-extra-types-widget 'radio
   "Internal CC Mode widget for the `*-font-lock-extra-types' variables."
   :args '((const :tag "none" nil)
