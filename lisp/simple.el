emacs-mirror/emacs

commit d1aacceae939318f419e0c265358e041c53f1eea

Make `newline' check the argument earlier
* lisp/simple.el (newline): Signal an error earlier to avoid
peculiar behaviour after getting a backtrace (bug#50900).

lisp/simple.el
@@ -589,6 +589,8 @@ text-property `hard'.
 A non-nil INTERACTIVE argument means to run the `post-self-insert-hook'."
   (interactive "*P\np")
   (barf-if-buffer-read-only)
+  (when (< arg 0)
+    (error "Repetition argument has to be non-negative"))
   ;; Call self-insert so that auto-fill, abbrev expansion etc. happen.
   ;; Set last-command-event to tell self-insert what to insert.
   (let* ((was-page-start (and (bolp) (looking-at page-delimiter)))

commit 5dbb04e0ebd2a007b4827138faae2ac0edc06350

Make `C-u RET' work again
* lisp/simple.el (newline): Fix regression introduced by
d1aacceae9 (bug#51459).

lisp/simple.el
@@ -590,7 +590,7 @@ A non-nil INTERACTIVE argument means to run the `post-self-insert-hook'."
   (interactive "*P\np")
   (barf-if-buffer-read-only)
   (when (and arg
-             (< arg 0))
+             (< (prefix-numeric-value arg) 0))
     (error "Repetition argument has to be non-negative"))
   ;; Call self-insert so that auto-fill, abbrev expansion etc. happen.
   ;; Set last-command-event to tell self-insert what to insert.