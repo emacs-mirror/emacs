a1e11502d6c816f8bd577f9ef7c3786cf3ef76c9
Stefan Kangas <stefan@marxist.se>
2021-11-08 06:20:21 +0100

    image-dired: Set window-resize-pixelwise to t
    * lisp/image-dired.el (image-dired-thumbnail-mode): Set
    'window-resize-pixelwise' to t.

--- lisp/image-dired.el
+++ lisp/image-dired.el
@@ -1649,6 +1649,7 @@ You probably want to use this together with
 Use `image-dired-minor-mode' to get a nice setup."
   (buffer-disable-undo)
   (add-hook 'file-name-at-point-functions 'image-dired-file-name-at-point nil t)
+  (setq-local window-resize-pixelwise t)
   (setq-local bookmark-make-record-function #'image-dired-bookmark-make-record)
   ;; Use approximately as much vertical spacing as horizontal.
   (setq-local line-spacing (frame-char-width)))
