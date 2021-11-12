070d6297ca468b197744c94df0c17fb09f47f67a
Po Lu <luangruo@yahoo.com>
2021-11-12 14:16:31 +0800

    Fix doc string for xwidget-webkit-load-html
    * src/xwidget.c (Fxwidget_webkit_load_html): Fix doc string.

--- src/xwidget.c
+++ src/xwidget.c
@@ -2325,7 +2325,7 @@ DEFUN ("xwidget-webkit-load-html", Fxwidget_webkit_load_html,
        doc: /* Make XWIDGET's WebKit widget render TEXT.
 XWIDGET should be a WebKit xwidget, that will receive TEXT.  TEXT
 should be a string that will be displayed by XWIDGET as HTML markup.
-BASE_URI should be a string containing a URI that is used to locate
+BASE-URI should be a string containing a URI that is used to locate
 resources with relative URLs, and if not specified, defaults
 to "about:blank".  */)
   (Lisp_Object xwidget, Lisp_Object text, Lisp_Object base_uri)
