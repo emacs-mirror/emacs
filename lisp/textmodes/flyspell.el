44923722f42c2974c140e385c4c765f60944efe7
Juri Linkov <juri@linkov.net>
2021-11-22 21:05:15 +0200

    * lisp/textmodes/flyspell.el: Pop up the menu under cursor from keyboard.
    * lisp/textmodes/flyspell.el (flyspell-emacs-popup):
    Use popup-menu-normalize-position with point when no mouse is involved,
    instead of the incorrect use of mouse-position (bug#52025).

--- lisp/textmodes/flyspell.el
+++ lisp/textmodes/flyspell.el
@@ -2270,17 +2270,8 @@ If OPOINT is non-nil, restore point there after adjusting it for replacement."
 ;;*---------------------------------------------------------------------*/
 (defun flyspell-emacs-popup (event poss word)
   "The Emacs popup menu."
-  (if (and (not event)
-           (display-mouse-p))
-      (let* ((mouse-pos  (mouse-position))
-	     (mouse-pos  (if (nth 1 mouse-pos)
-			     mouse-pos
-			   (set-mouse-position (car mouse-pos)
-				 	       (/ (frame-width) 2) 2)
-			   (mouse-position))))
-	(setq event (list (list (car (cdr mouse-pos))
-				(1+ (cdr (cdr mouse-pos))))
-			  (car mouse-pos)))))
+  (unless event
+    (setq event (popup-menu-normalize-position (point))))
   (let* ((corrects   (flyspell-sort (car (cdr (cdr poss))) word))
 	 (cor-menu   (if (consp corrects)
 			 (mapcar (lambda (correct)
