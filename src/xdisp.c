833a42fbcf78ec99b84a98dd6bc7c2eea6eeaef6
Lars Ingebrigtsen <larsi@gnus.org>
2021-11-24 20:04:25 +0100

    Fix min-width problem with "overlapping" regions
    * src/xdisp.c (handle_display_prop): Fix problem with overlapping
    regions.

--- src/xdisp.c
+++ src/xdisp.c
@@ -5352,7 +5352,8 @@ handle_display_prop (struct it *it)
 					   Qdisplay, object, &overlay);
 
   /* Handle min-width ends. */
-  if (!NILP (it->min_width_property))
+  if (!NILP (it->min_width_property)
+      && NILP (find_display_property (propval, Qmin_width)))
     display_min_width (it, bufpos, object, Qnil);
 
   if (NILP (propval))
