92cd274ba82b142271d7884795b5f6e9033ab4f9
Yuuki Harano <masm+github@masm11.me>
2021-11-30 00:19:22 +0900

    Remove draw_glyphs_debug
    It is left-over from debugging, and not needed any more.
    
    * src/xdisp.c (draw_glyphs_debug):
    (draw_glyphs):

--- src/xdisp.c
+++ src/xdisp.c
@@ -29341,16 +29341,6 @@ draw_glyphs (struct window *w, int x, struct glyph_row *row,
   return x_reached;
 }
 
-static int draw_glyphs_debug (const char *file, int lineno,
-			     struct window *w, int x, struct glyph_row *row,
-			     enum glyph_row_area area, ptrdiff_t start, ptrdiff_t end,
-			     enum draw_glyphs_face hl, int overlaps)
-{
-  return draw_glyphs(w, x, row, area, start, end, hl, overlaps);
-}
-#define draw_glyphs(w, x, r, a, s, e, h, o) \
-  draw_glyphs_debug (__FILE__, __LINE__, w, x, r, a, s, e, h, o)
-
 /* Find the first glyph in the run of underlined glyphs preceding the
    beginning of glyph string S, and return its font (which could be
    NULL).  This is needed because that font determines the underline
