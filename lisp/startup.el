dbadbb5badb26a006f562374127c180a27c11f70
Lars Ingebrigtsen <larsi@gnus.org>
2021-12-05 01:53:31 +0100

    Tweak fancy-about-text
    * lisp/startup.el (fancy-about-text): Mention that Emacs is an
    editor, because people don't know that (bug#43636).

--- lisp/startup.el
+++ lisp/startup.el
@@ -1636,9 +1636,10 @@ Each element in the list should be a list of strings or pairs
   `((:face (variable-pitch font-lock-comment-face)
      "This is "
      :link ("GNU Emacs"
-	    ,(lambda (_button) (browse-url "https://www.gnu.org/software/emacs/"))
+	    ,(lambda (_button)
+               (browse-url "https://www.gnu.org/software/emacs/"))
 	    "Browse https://www.gnu.org/software/emacs/")
-     ", one component of the "
+     ", a text editor and more.\nIt's a component of the "
      :link
      ,(lambda ()
        (if (eq system-type 'gnu/linux)
