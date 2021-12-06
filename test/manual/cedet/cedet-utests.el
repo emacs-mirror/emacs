af6061894598efdcb1aeebbac2283d6e1d085820
Paul Eggert <eggert@cs.ucla.edu>
2021-12-05 17:58:11 -0800

    Simplify cedet-utest-elapsed-time
    * test/manual/cedet/cedet-utests.el (cedet-utest-elapsed-time):
    Copy newer (circa-2011) implementation from elp.el, so that the
    code matches its documentation again.

--- test/manual/cedet/cedet-utests.el
+++ test/manual/cedet/cedet-utests.el
@@ -252,9 +252,7 @@ Optional argument TITLE is the title of this testing session."
 (defun cedet-utest-elapsed-time (start end)
   "Copied from elp.el.  Was elp-elapsed-time.
 Argument START and END bound the time being calculated."
-  (+ (* (- (car end) (car start)) 65536.0)
-     (- (car (cdr end)) (car (cdr start)))
-     (/ (- (car (cdr (cdr end))) (car (cdr (cdr start)))) 1000000.0)))
+  (float-time (time-subtract start end)))
 
 (defun cedet-utest-log-shutdown (title &optional _errorcondition)
   "Shut-down a larger test suite.
