dda63370669dba6dfda53dec62de8b1c1c275b2d
Paul Eggert <eggert@cs.ucla.edu>
2021-12-09 09:03:10 -0800

    emacsclient takes more care about XDG_RUNTIME_DIR
    * lib-src/emacsclient.c (set_local_socket): Revert to the Emacs 27
    behavior of not trying TMPDIR if XDG_RUNTIME_DIR is set.
    This is one of the suggestions made by Jim Porter and
    independently by Ulrich Mueller in Bug#51327.

--- lib-src/emacsclient.c
+++ lib-src/emacsclient.c
@@ -1456,7 +1456,6 @@ set_local_socket (char const *server_name)
   else
     {
       /* socket_name is a file name component.  */
-      sock_status = ENOENT;
       char const *xdg_runtime_dir = egetenv ("XDG_RUNTIME_DIR");
       if (xdg_runtime_dir)
 	{
@@ -1466,7 +1465,7 @@ set_local_socket (char const *server_name)
 			 ? connect_socket (AT_FDCWD, sockname, s, 0)
 			 : ENAMETOOLONG);
 	}
-      if (sock_status == ENOENT)
+      else
 	{
 	  char const *tmpdir = egetenv ("TMPDIR");
 	  if (tmpdir)
