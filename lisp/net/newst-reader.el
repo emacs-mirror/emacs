26fb4168b884eeb910580c42e87efb73399019c9
Stefan Kangas <stefan@marxist.se>
2021-12-09 16:22:17 +0100

    ; Prefer HTTPS in more URLs

--- lisp/net/newst-reader.el
+++ lisp/net/newst-reader.el
@@ -4,7 +4,7 @@
 
 ;; Author:      Ulf Jasper <ulf.jasper@web.de>
 ;; Filename:    newst-reader.el
-;; URL:         http://www.nongnu.org/newsticker
+;; URL:         https://www.nongnu.org/newsticker
 ;; Package:     newsticker
 
 ;; ======================================================================
