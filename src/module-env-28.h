f88c1d222f17d3483bf6f387dc41be4366a047ab
Philipp Stephani <phst@google.com>
2021-12-14 11:17:37 +0100

    Remove maintainer comment from Emacs 28 module snippet.
    This is the same as 44c13eefe8d30841000a96d82f467fb8d222e365 on
    master.  We should also remove this comment on the release branch,
    otherwise it ends up in the emacs-module.h shipped to users.
    
    * src/module-env-28.h: Remove maintainer comment.

--- src/module-env-28.h
+++ src/module-env-28.h
@@ -1,7 +1,3 @@
-  /* Add module environment functions newly added in Emacs 28 here.
-     Before Emacs 28 is released, remove this comment and start
-     module-env-29.h on the master branch.  */
-
   void (*(*EMACS_ATTRIBUTE_NONNULL (1)
             get_function_finalizer) (emacs_env *env,
                                      emacs_value arg)) (void *) EMACS_NOEXCEPT;
