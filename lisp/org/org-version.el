5f25852e91f167f99988fbe8d1944fe92d70b903
Kyle Meyer <kyle@kyleam.com>
2021-12-18 13:50:40 -0500

    Update to Org 9.5.1-31-ga18849

--- lisp/org/org-version.el
+++ lisp/org/org-version.el
@@ -11,7 +11,7 @@ Inserted by installing Org mode or when a release is made."
 (defun org-git-version ()
   "The Git version of Org mode.
 Inserted by installing Org or when a release is made."
-   (let ((org-git-version "release_9.5.1-25-g9ca3bc"))
+   (let ((org-git-version "release_9.5.1-31-ga18849"))
      org-git-version))
 
 (provide 'org-version)
