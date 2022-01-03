4509095d79b07920268ed21aebf4a26b4b311b83
Ivan Sokolov <ivan-p-sokolov@ya.ru>
2022-01-03 17:21:55 +0300

    Add doc-string indices to easy-mmode-{defmap,defsyntax}
    * lisp/emacs-lisp/easy-mmode.el (easy-mmode-defmap)
    (easy-mmode-defsyntax): Declare third argument as doc-string.
    (Bug#52978)

--- lisp/emacs-lisp/easy-mmode.el
+++ lisp/emacs-lisp/easy-mmode.el
@@ -697,7 +697,7 @@ Valid keywords and arguments are:
   "Define a constant M whose value is the result of `easy-mmode-define-keymap'.
 The M, BS, and ARGS arguments are as per that function.  DOC is
 the constant's documentation."
-  (declare (indent 1))
+  (declare (doc-string 3) (indent 1))
   `(defconst ,m
      (easy-mmode-define-keymap ,bs nil (if (boundp ',m) ,m) ,(cons 'list args))
      ,doc))
@@ -724,7 +724,7 @@ the constant's documentation."
 (defmacro easy-mmode-defsyntax (st css doc &rest args)
   "Define variable ST as a syntax-table.
 CSS contains a list of syntax specifications of the form (CHAR . SYNTAX)."
-  (declare (indent 1))
+  (declare (doc-string 3) (indent 1))
   `(progn
      (autoload 'easy-mmode-define-syntax "easy-mmode")
      (defconst ,st (easy-mmode-define-syntax ,css ,(cons 'list args)) ,doc)))
