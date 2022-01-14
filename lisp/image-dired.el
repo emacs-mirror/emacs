bef9fcc999af5ed5524990c86968be9f9c4497ea
Stefan Kangas <stefan@marxist.se>
2022-01-14 16:17:33 +0100

    image-dired: Use MD5 for thumb filename generation
    * lisp/image-dired.el (image-dired-thumb-name): Revert back to MD5 for
    file name generation to preserve backwards-compatibility.  (Bug#53229)

--- lisp/image-dired.el
+++ lisp/image-dired.el
@@ -609,9 +609,7 @@ See also `image-dired-thumbnail-storage'."
         ((eq 'use-image-dired-dir image-dired-thumbnail-storage)
          (let* ((f (expand-file-name file))
                 (hash
-                 ;; SHA1 is slightly faster than MD5, so let's use it.
-                 ;; (We don't need anything crytographically strong.)
-                 (sha1 (file-name-as-directory (file-name-directory f)))))
+                 (md5 (file-name-as-directory (file-name-directory f)))))
            (format "%s%s%s.thumb.%s"
                    (file-name-as-directory (expand-file-name (image-dired-dir)))
                    (file-name-base f)
