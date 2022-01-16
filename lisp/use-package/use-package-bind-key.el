2203246454fddd41e0a62e78b17befce561998b9
Hugo Heagren <hugo@heagren.com>
2022-01-16 00:21:36 +0000

    bind-keys-form: new keyword :repeat-map, for defining repeat maps
    use-package-normalize/:bind: allow keyword :repeat-map.
    
    bind-keys-form: Add keyword :repeat-map. Specifying a symbol as the
    repeat-map defines a keymap with that name (and with the docstring
    `repeat-doc', if specified). Symbols for functions bound to keys under
    the scope of :repeat-map have their 'repeat-map property set to this
    map. Update docstring (and that of `bind-keys') to reflect changes.
    
    Rename `doc' to `prefix-doc' for clarity and consistency with
    'repeat-doc'.

--- lisp/use-package/use-package-bind-key.el
+++ lisp/use-package/use-package-bind-key.el
@@ -86,6 +86,8 @@ deferred until the prefix key sequence is pressed."
          ;;   :prefix-docstring STRING
          ;;   :prefix-map SYMBOL
          ;;   :prefix STRING
+	 ;;   :repeat-docstring STRING
+         ;;   :repeat-map SYMBOL
          ;;   :filter SEXP
          ;;   :menu-name STRING
          ;;   :package SYMBOL
@@ -93,6 +95,8 @@ deferred until the prefix key sequence is pressed."
               (and (eq x :prefix) (stringp (cadr arg)))
               (and (eq x :prefix-map) (symbolp (cadr arg)))
               (and (eq x :prefix-docstring) (stringp (cadr arg)))
+	      (and (eq x :repeat-map) (symbolp (cadr arg)))
+              (and (eq x :repeat-docstring) (stringp (cadr arg)))
               (eq x :filter)
               (and (eq x :menu-name) (stringp (cadr arg)))
               (and (eq x :package) (symbolp (cadr arg))))
