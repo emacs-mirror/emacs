d25cb37694de446e6dd1095f489d5436ea9e20ae
Eli Zaretskii <eliz@gnu.org>
2022-01-25 19:12:50 +0200

    ; * src/fns.c (concat_strings): Fix comment style.

--- src/fns.c
+++ src/fns.c
@@ -847,8 +847,8 @@ concat_strings (ptrdiff_t nargs, Lisp_Object *args)
 
   if (dest_multibyte && some_unibyte)
     {
-      // Non-ASCII chars in unibyte strings take two bytes when
-      // converted to multibyte -- count them and adjust the total.
+      /* Non-ASCII characters in unibyte strings take two bytes when
+	 converted to multibyte -- count them and adjust the total. */
       for (ptrdiff_t i = 0; i < nargs; i++)
 	{
 	  Lisp_Object arg = args[i];
