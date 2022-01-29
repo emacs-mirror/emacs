bddd9c5f686cd5707fe395bba68c32baf4698cb9
Eli Zaretskii <eliz@gnu.org>
2022-01-29 17:56:30 +0200

    ; Fix formatting of last change.

--- lisp/subr.el
+++ lisp/subr.el
@@ -3249,14 +3249,13 @@ switch back again to the minibuffer before entering the
 character.  This is not possible when using `read-key', but using
 `read-key' may be less confusing to some users.")
 
-
 (defvar from--tty-menu-p nil
   "Non-nil means the current command was invoked from a TTY menu.")
 (defun use-dialog-box-p ()
   "Say whether the current command should promp the user via GUI dialog box."
   (and last-input-event                 ; not during startup
        (or (listp last-nonmenu-event)   ; invoked by a mouse event
-           from--tty-menu-p)             ; invoked via TTY menu
+           from--tty-menu-p)            ; invoked via TTY menu
        use-dialog-box))
 
 (defun y-or-n-p (prompt)
