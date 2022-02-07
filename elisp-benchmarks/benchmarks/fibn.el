8ca8641e1ec7e065833b57ceacebf7ff4d046862
Stefan Monnier <monnier@iro.umontreal.ca>
2022-02-07 16:17:41 -0500

    Add EIEIO benchmark
    * benchmarks/elb-eieio.el: New file.
    * benchmarks/elb-pcase.el: Rename from pcase.el to avoid naming conflict.
    * benchmarks/fibn-tc.el, benchmarks/fibn-rec.el: Fold into benchmarks/fibn.el.
    * benchmarks/fibn.el: Add a `fibn-named-let` entry.  Keep the same
    number of iterations for all the non-naive fibn entries so they can
    be compared more directly.
    
    * elisp-benchmarks.el (elisp-benchmarks-run): Sort entries.

--- elisp-benchmarks/benchmarks/fibn.el
+++ elisp-benchmarks/benchmarks/fibn.el
@@ -1,6 +1,6 @@
 ;; -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2019 Free Software Foundation, Inc.
+;; Copyright (C) 2019, 2022 Free Software Foundation, Inc.
 
 ;; This file is part of GNU Emacs.
 
@@ -19,6 +19,8 @@
 
 ;;; Commentary:
 
+;;; Code:
+
 ;; Adapted to elisp from CL version from:
 ;; https://drmeister.wordpress.com/2015/07/30/timing-data-comparing-cclasp-to-c-sbcl-and-python/
 
@@ -34,7 +36,45 @@
     z))
 
 (defun elb-fibn-entry ()
-  ;; Use 80 to stay in the fixnum range.
-  (elb-fibn 3000000 80))
+  ;; Use 80 to stay in the fixnum range (on 64bit systems).
+  (elb-fibn 1000000 80))
+
+;; Fibonacci sequence tail recursive algo.
+
+(defun elb-fibn-tc (a b count)
+  (if (= count 0)
+      b
+    (elb-fibn-tc (+ a b) a (- count 1))))
+
+(defun elb-fibn-tc-entry ()
+  (dotimes (_ 1000000)
+    (elb-fibn-tc 1 0 80)))
+
+;; Fibonacci sequence with named-let.
+
+(defun elb-fibn-named-let (count)
+  (named-let loop ((a 1)
+                   (b 0)
+                   (count count))
+    (if (= count 0)
+        b
+      (loop (+ a b) a (- count 1)))))
+
+(defun elb-fibn-named-let-entry ()
+  (dotimes (_ 1000000)
+    (elb-fibn-named-let 80)))
+
+;; Fibonacci sequence with the naive recursive algo.
+
+(defun elb-fibn-rec (n)
+  (cond ((= n 0) 0)
+	((= n 1) 1)
+	(t (+ (elb-fibn-rec (- n 1))
+	      (elb-fibn-rec (- n 2))))))
+
+(defun elb-fibn-rec-entry ()
+  (elb-fibn-rec 37))
+
 
 (provide 'elb-fibn)
+;;; elb-fibn ends here.
