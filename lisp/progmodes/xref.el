f0ac4b7797fb16833a7a0c49593f36d88c69d2d5
Dmitry Gutov <dgutov@yandex.ru>
2022-02-21 04:15:39 +0200

    Bump xref.el version.
    * lisp/progmodes/xref.el: Bump version to 1.4.0.

--- lisp/progmodes/xref.el
+++ lisp/progmodes/xref.el
@@ -1,7 +1,7 @@
 ;;; xref.el --- Cross-referencing commands              -*-lexical-binding:t-*-
 
 ;; Copyright (C) 2014-2022 Free Software Foundation, Inc.
-;; Version: 1.3.2
+;; Version: 1.4.0
 ;; Package-Requires: ((emacs "26.1"))
 
 ;; This is a GNU ELPA :core package.  Avoid functionality that is not
