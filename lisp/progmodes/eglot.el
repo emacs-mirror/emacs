c17c3cfcbffc97dfae76d75ab32ec81d893be97c
João Távora <joaotavora@gmail.com>
2022-04-04 11:05:29 +0100

    Check textdocumentsync/willsave cap before sending it
    * eglot.el (eglot--guess-contact): Default language-id to educated
    guess when eglot--lookup-mode returns nil.
    
    GitHub-reference: fix https://github.com/joaotavora/eglot/issues/823

--- lisp/progmodes/eglot.el
+++ lisp/progmodes/eglot.el
@@ -2277,7 +2277,8 @@ When called interactively, use the currently active server"
   "Send textDocument/willSave to server."
   (let ((server (eglot--current-server-or-lose))
         (params `(:reason 1 :textDocument ,(eglot--TextDocumentIdentifier))))
-    (jsonrpc-notify server :textDocument/willSave params)
+    (when (eglot--server-capable :textDocumentSync :willSave)
+      (jsonrpc-notify server :textDocument/willSave params))
     (when (eglot--server-capable :textDocumentSync :willSaveWaitUntil)
       (ignore-errors
         (eglot--apply-text-edits
