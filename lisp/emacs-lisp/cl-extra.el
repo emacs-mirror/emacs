5ee959aa8783689627a1553c678fd4d3720236c8
Lars Ingebrigtsen <larsi@gnus.org>
2022-04-13 06:11:43 +0200

    Add a comment about cl-concatenate
    * lisp/emacs-lisp/cl-extra.el (cl-concatenate): Add a comment.

--- lisp/emacs-lisp/cl-extra.el
+++ lisp/emacs-lisp/cl-extra.el
@@ -553,6 +553,9 @@ too large if positive or too small if negative)."
 			,new)))))
   (seq-subseq seq start end))
 
+;;; This isn't a defalias because autoloading defalises doesn't work
+;;; very well.
+
 ;;;###autoload
 (defun cl-concatenate (type &rest sequences)
   "Concatenate, into a sequence of type TYPE, the argument SEQUENCEs.
