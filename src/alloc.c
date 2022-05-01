8734d60b053852a6cf4dca59da4c5876820fa7d2
Eli Zaretskii <eliz@gnu.org>
2022-05-01 13:06:33 +0300

    Improve documentation of 'malloc-trim'
    * src/alloc.c (Fmalloc_trim): Fix the doc string.
    
    * etc/NEWS: Document which systems support 'malloc-trim'.

--- src/alloc.c
+++ src/alloc.c
@@ -7481,14 +7481,14 @@ arenas.  */)
 
 #ifdef HAVE_MALLOC_TRIM
 DEFUN ("malloc-trim", Fmalloc_trim, Smalloc_trim, 0, 1, "",
-       doc: /* Release free memory from the heap.
-This function asks libc to return unused memory back to the operating
+       doc: /* Release free heap memory to the OS.
+This function asks libc to return unused heap memory back to the operating
 system.  This function isn't guaranteed to do anything, and is mainly
 meant as a debugging tool.
 
 If LEAVE_PADDING is given, ask the system to leave that much unused
-spaced in the heap.  This should be an integer, and if not given,
-defaults to 0.
+space in the heap of the Emacs process.  This should be an integer, and if
+not given, it defaults to 0.
 
 This function returns nil if no memory could be returned to the
 system, and non-nil if some memory could be returned.  */)
