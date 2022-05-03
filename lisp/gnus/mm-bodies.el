emacs-mirror/emacs

commit d90f54db2285002ae969f7c685779c21eaec1605

Avoid regexp overflow in mm-decode-content-transfer-encoding
* lisp/gnus/mm-bodies.el (mm-decode-content-transfer-encoding):
The base64 may be huge, so avoid backtracking (bug#55195).

lisp/gnus/mm-bodies.el
@@ -191,18 +191,17 @@ If TYPE is `text/plain' CRLF->LF translation may occur."
 	   ((eq encoding 'base64)
 	    (base64-decode-region
 	     (point-min)
-	     ;; Some mailers insert whitespace
-	     ;; junk at the end which
-	     ;; base64-decode-region dislikes.
-	     ;; Also remove possible junk which could
-	     ;; have been added by mailing list software.
 	     (save-excursion
+               ;; Some mailers insert whitespace junk at the end which
+	       ;; base64-decode-region dislikes.
 	       (goto-char (point-min))
 	       (while (re-search-forward "^[\t ]*\r?\n" nil t)
 		 (delete-region (match-beginning 0) (match-end 0)))
+	       ;; Also ignore junk which could have been added by
+	       ;; mailing list software by finding the final line with
+	       ;; base64 text.
 	       (goto-char (point-max))
-	       (when (re-search-backward "^[\t ]*[A-Za-z0-9+/]+=*[\t ]*$"
-					 nil t)
+	       (when (re-search-backward "[A-Za-z0-9+/]{3,3}=?[\t ]*$" nil t)
 		 (forward-line))
 	       (point))))
 	   ((memq encoding '(nil 7bit 8bit binary))

commit cf0d289e3838a6978b768b347f9fb5c1c2cad196

Further mm-base64-line-p bug fixes
* lisp/gnus/mm-bodies.el (mm-base64-line-p): Fix parsing error
introduced by d90f54d.

lisp/gnus/mm-bodies.el
@@ -245,9 +245,9 @@ If TYPE is `text/plain' CRLF->LF translation may occur."
   (save-excursion
     (beginning-of-line)
     (skip-chars-forward " \t")
-    (and (looking-at "[A-Za-z0-9+]\\{3\\}")
+    (and (looking-at "[A-Za-z0-9+/]\\{3\\}")
          (progn
-           (skip-chars-forward "A-Za-z0-9+")
+           (skip-chars-forward "A-Za-z0-9+/")
            (skip-chars-forward "=")
            (skip-chars-forward " \t")
            (eolp)))))