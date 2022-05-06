b7e1176323386935e34dbc98ec7d445c728cc83a
Lars Ingebrigtsen <larsi@gnus.org>
2022-05-06 19:09:12 +0200

    Further log-view-file-next fix-ups
    * doc/emacs/maintaining.texi (VC Change Log): Remove the entries
    for commands now removed from most VCs.
    * lisp/vc/log-view.el (log-view-mode-menu): Disable menu entries
    (bug#14531).

--- lisp/vc/log-view.el
+++ lisp/vc/log-view.el
@@ -162,9 +162,15 @@
     ["Previous Log Entry"  log-view-msg-prev
      :help "Go to the previous count'th log message"]
     ["Next File"  log-view-file-next
-     :help "Go to the next count'th file"]
+     :help "Go to the next count'th file"
+     :active (derived-mode-p vc-cvs-log-view-mode
+                             vc-rcs-log-view-mode
+                             vc-sccs-log-view-mode)]
     ["Previous File"  log-view-file-prev
-     :help "Go to the previous count'th file"]))
+     :help "Go to the previous count'th file"
+     :active (derived-mode-p vc-cvs-log-view-mode
+                             vc-rcs-log-view-mode
+                             vc-sccs-log-view-mode)]))
 
 (defvar log-view-mode-hook nil
   "Hook run at the end of `log-view-mode'.")
