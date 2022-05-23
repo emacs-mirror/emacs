8c4498e62ed58ac94cc627d2ab085f2b17abc090
Eli Zaretskii <eliz@gnu.org>
2022-05-23 19:26:18 +0300

    Avoid compiler warnings on macOS (bug#55595)
    * configure.ac: Don't use -Wunknown-pragmas with Clang.
    
    * src/comp.c (load_comp_unit): Avoid Clang compilation warning.

--- src/comp.c
+++ src/comp.c
@@ -5342,7 +5342,7 @@ load_comp_unit (struct Lisp_Native_Comp_Unit *comp_u, bool loading_dump,
 	 are necessary exclusively during the first load.  Once these
 	 are collected we don't have to maintain them in the heap
 	 forever.  */
-      Lisp_Object volatile data_ephemeral_vec;
+      Lisp_Object volatile data_ephemeral_vec = Qnil;
       /* In case another load of the same CU is active on the stack
 	 all ephemeral data is hold by that frame.  Re-writing
 	 'data_ephemeral_vec' would be not only a waste of cycles but
