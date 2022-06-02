f71fad50fc4d10caaf156c37610f78f5edf3a255
Po Lu <luangruo@yahoo.com>
2022-06-02 15:48:12 +0800

    Fix potential NULL pointer dereference on NS
    * src/nsterm.m (ns_mouse_position): Don't test f's tooltip-ness
    if it's NULL.

--- src/nsterm.m
+++ src/nsterm.m
@@ -2365,7 +2365,7 @@ ns_mouse_position (struct frame **fp, int insist, Lisp_Object *bar_window,
   if (!FRAME_NS_P (f))
     f = NULL;
 
-  if (FRAME_TOOLTIP_P (f))
+  if (f && FRAME_TOOLTIP_P (f))
     f = dpyinfo->last_mouse_frame;
 
   /* While dropping, use the last mouse frame only if there is no
