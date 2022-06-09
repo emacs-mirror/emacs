e563020eea738febed38194d9b0984473c852f3f
Po Lu <luangruo@yahoo.com>
2022-06-09 18:20:59 +0800

    Handle monitor attribute updates during drag-and-drop
    * src/xterm.c (x_monitors_changed_cb):
    (handle_one_xevent): Set x_dnd_monitors during monitor changes.

--- src/xterm.c
+++ src/xterm.c
@@ -15626,6 +15626,9 @@ x_monitors_changed_cb (GdkScreen *gscr, gpointer user_data)
     }
 
   dpyinfo->last_monitor_attributes_list = current_monitors;
+
+  if (x_dnd_in_progress && x_dnd_update_tooltip)
+    x_dnd_monitors = current_monitors;
 }
 #endif
 
@@ -21409,6 +21412,9 @@ handle_one_xevent (struct x_display_info *dpyinfo,
 	    inev.ie.kind = NO_EVENT;
 
 	  dpyinfo->last_monitor_attributes_list = current_monitors;
+
+	  if (x_dnd_in_progress && x_dnd_update_tooltip)
+	    x_dnd_monitors = current_monitors;
 	}
 #endif
     OTHER:
