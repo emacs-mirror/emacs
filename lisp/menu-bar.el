9c2b1d37e729f7af9e9661be7ece8348bae70ffa
Gerd Möllmann <gerd@gnu.org>
2022-06-29 07:53:35 +0200

    Ensure correct toggle menu item display
    * lisp/menu-bar.el (menu-bar-make-toggle-command): Call
    force-mode-line-update. (Bug#56155)

--- lisp/menu-bar.el
+++ lisp/menu-bar.el
@@ -749,7 +749,11 @@ by \"Save Options\" in Custom buffers.")
        ;; interactively, because the purpose is to mark the variable as a
        ;; candidate for `Save Options', and we do not want to save options that
        ;; the user has already set explicitly in the init file.
-       (when interactively (customize-mark-as-set ',variable)))
+       (when interactively
+         (customize-mark-as-set ',variable))
+       ;; Toggle menu items must make sure that the menu is updated so
+       ;; that toggle marks are drawn in the right state.
+       (force-mode-line-update t))
      '(menu-item ,item-name ,command :help ,help
                  :button (:toggle . (and (default-boundp ',variable)
                                          (default-value ',variable)))
