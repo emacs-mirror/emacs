7ac9c22636cc2d6c56bf238ca4311924a6ee0cd0
Stefan Kangas <stefan@marxist.se>
2022-07-07 02:09:30 +0200

    End new .dir-locals.el files with a newline
    * lisp/files-x.el (dir-locals-to-string): Add newline at the end of
    newly created .dir-locals.el files.  This avoids git complaining about
    "No newline at end of file".

--- lisp/files-x.el
+++ lisp/files-x.el
@@ -507,19 +507,20 @@ from the MODE alist ignoring the input argument VALUE."
 
 (defun dir-locals-to-string (variables)
   "Output alists of VARIABLES to string in dotted pair notation syntax."
-  (format "(%s)" (mapconcat
-                  (lambda (mode-variables)
-                    (format "(%S . %s)"
-                            (car mode-variables)
-                            (format "(%s)" (mapconcat
-                                            (lambda (variable-value)
-                                              (format "(%S . %s)"
-                                                      (car variable-value)
-                                                      (string-trim-right
-                                                       (pp-to-string
-                                                        (cdr variable-value)))))
-                                            (cdr mode-variables) "\n"))))
-                  variables "\n")))
+  (format "(%s)\n"
+          (mapconcat
+           (lambda (mode-variables)
+             (format "(%S . %s)"
+                     (car mode-variables)
+                     (format "(%s)" (mapconcat
+                                     (lambda (variable-value)
+                                       (format "(%S . %s)"
+                                               (car variable-value)
+                                               (string-trim-right
+                                                (pp-to-string
+                                                 (cdr variable-value)))))
+                                     (cdr mode-variables) "\n"))))
+           variables "\n")))
 
 ;;;###autoload
 (defun add-dir-local-variable (mode variable value)
