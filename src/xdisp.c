9ea9533f1789ce61daf99b9bd484af3ce2780472
Eli Zaretskii <eliz@gnu.org>
2022-07-08 09:48:44 +0300

    * src/xdisp.c (set_vertical_scroll_bar): Remove stale comment.

--- src/xdisp.c
+++ src/xdisp.c
@@ -18891,8 +18891,6 @@ set_vertical_scroll_bar (struct window *w)
 	{
 	  whole = BUF_ZV (buf) - BUF_BEGV (buf);
 	  start = marker_position (w->start) - BUF_BEGV (buf);
-	  /* I don't think this is guaranteed to be right.  For the
-	     moment, we'll pretend it is.  */
 	  end = BUF_Z (buf) - window_end_pos - BUF_BEGV (buf);
 	}
       else
