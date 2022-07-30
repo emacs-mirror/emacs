d34e38f7d14ff45c19102676dc53f5389e8f32f6
Lars Ingebrigtsen <larsi@gnus.org>
2022-07-30 15:36:16 +0200

    Make the symbol icons in outline.el more logical
    * lisp/outline.el (outline-open, outline-close): Use symbols from
    the same series so that it's more likely that they match up (bug#56823).

--- lisp/outline.el
+++ lisp/outline.el
@@ -295,7 +295,7 @@ buffers (yet) -- that will be amended in a future version."
 
 (define-icon outline-open button
   '((emoji "▶️")
-    (symbol " ▶ ")
+    (symbol " ⯈ ")
     (text " open "))
   "Icon used for buttons for opening a section in outline buffers."
   :version "29.1"
@@ -303,7 +303,7 @@ buffers (yet) -- that will be amended in a future version."
 
 (define-icon outline-close button
   '((emoji "🔽")
-    (symbol " ▼ ")
+    (symbol " ⯆ ")
     (text " close "))
   "Icon used for buttons for closing a section in outline buffers."
   :version "29.1"
