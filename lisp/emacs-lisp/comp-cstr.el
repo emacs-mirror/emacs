0bd96806ef1a0f0d2d3f48cdb1204b7e393ab036
Andrea Corallo <akrl@sdf.org>
2022-08-18 23:41:39 +0200

    * Rename `comp--typeof-builtin-types'
    * lisp/emacs-lisp/comp-cstr.el (comp--typeof-builtin-types): Rename.
    (comp-normalize-valset, comp-common-supertype-2): Update.

--- lisp/emacs-lisp/comp-cstr.el
+++ lisp/emacs-lisp/comp-cstr.el
@@ -37,9 +37,9 @@
 
 (require 'cl-lib)
 
-(defconst comp--typeof-types (mapcar (lambda (x)
-                                       (append x '(t)))
-                                     cl--typeof-types)
+(defconst comp--typeof-builtin-types (mapcar (lambda (x)
+                                               (append x '(t)))
+                                             cl--typeof-types)
   ;; TODO can we just add t in `cl--typeof-types'?
   "Like `cl--typeof-types' but with t as common supertype.")
 
@@ -230,7 +230,7 @@ Return them as multiple value."
   (cl-loop
    named outer
    with found = nil
-   for l in comp--typeof-types
+   for l in comp--typeof-builtin-types
    do (cl-loop
        for x in l
        for i from (length l) downto 0
@@ -273,7 +273,7 @@ Return them as multiple value."
                (cl-loop
                 with types = (apply #'append typesets)
                 with res = '()
-                for lane in comp--typeof-types
+                for lane in comp--typeof-builtin-types
                 do (cl-loop
                     with last = nil
                     for x in lane
