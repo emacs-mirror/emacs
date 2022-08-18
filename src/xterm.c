79050eeeaf1ebf36cd2bf8be039f99d1e7e82ca8
Po Lu <luangruo@yahoo.com>
2022-08-18 10:31:02 +0800

    ; * src/xterm.c (xi_select_hierarchy_events): Fix typo.

--- src/xterm.c
+++ src/xterm.c
@@ -27654,6 +27654,8 @@ xi_select_hierarchy_events (struct x_display_info *dpyinfo)
   memset (m, 0, l);
   mask.mask_len = l;
 
+  mask.deviceid = XIAllDevice;
+
   XISetMask (m, XI_PropertyEvent);
   XISetMask (m, XI_HierarchyChanged);
   XISetMask (m, XI_DeviceChanged);
