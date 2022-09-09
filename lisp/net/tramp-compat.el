effb5d75a80b16db636788fb53e4273a1d16d85d
Lars Ingebrigtsen <larsi@gnus.org>
2022-09-09 17:57:32 +0200

    Revert "Fix tramp-loaddefs compilation"
    This reverts commit 43e9c8e9d28dcb659a43e58778e2745d1279051f.
    
    The cookie seemed to go to lisp/loaddefs.el for a reason, but a bootstrap is needed.

--- lisp/net/tramp-compat.el
+++ lisp/net/tramp-compat.el
@@ -220,7 +220,7 @@ CONDITION can also be a list of error conditions."
     (tramp-compat-rx--transform items)))
 
 ;; This is needed for compilation in the Emacs source tree.
-;;;###tramp-autoload (defalias 'tramp-compat-rx #'rx)
+;;;###autoload (defalias 'tramp-compat-rx #'rx)
 
 ;; `file-modes', `set-file-modes' and `set-file-times' got argument
 ;; FLAG in Emacs 28.1.
