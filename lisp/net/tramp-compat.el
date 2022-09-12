2a4f2ba91a1bbcda9c8b452e61f36758527f16ff
Michael Albinus <michael.albinus@gmx.de>
2022-09-12 08:52:08 +0200

    Fix Tramp test
    * lisp/net/tramp-compat.el (tramp-compat-rx):
    Add `tramp-autoload' function property.

--- lisp/net/tramp-compat.el
+++ lisp/net/tramp-compat.el
@@ -222,6 +222,8 @@ CONDITION can also be a list of error conditions."
 ;; This is needed for compilation in the Emacs source tree.
 ;;;###autoload (defalias 'tramp-compat-rx #'rx)
 
+(put #'tramp-compat-rx 'tramp-autoload t)
+
 ;; `file-modes', `set-file-modes' and `set-file-times' got argument
 ;; FLAG in Emacs 28.1.
 (defalias 'tramp-compat-file-modes
