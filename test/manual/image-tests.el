89199f16aef2b1a2e79f22dddd7322f9f4fdd1fc
Stefan Kangas <stefankangas@gmail.com>
2022-09-13 11:06:11 +0200

    ; Fix last change in test/manual/image-tests.el
    * test/manual/image-tests.el (image-skip-unless):
    (image-tests-image-metadata/gif): Fix last change.

--- test/manual/image-tests.el
+++ test/manual/image-tests.el
@@ -32,9 +32,9 @@
 ;;; Code:
 
 (defmacro image-skip-unless (format &rest condition)
-  `(skip-unless (or (and (display-images-p)
-                         (image-type-available-p ,format))
-                    ,@condition)))
+  `(skip-unless (and (and (display-images-p)
+                          (image-type-available-p ,format))
+                     ,@condition)))
 
 (defconst image-tests--images
   `((gif . ,(expand-file-name "test/data/image/black.gif"
@@ -222,7 +222,8 @@
 ;;       contain metadata.
 
 (ert-deftest image-tests-image-metadata/gif ()
-  (image-skip-unless 'gif (not w32-use-native-image-API))
+  (image-skip-unless 'gif
+                (not (bound-and-true-p w32-use-native-image-API)))
   (should (memq 'delay
                 (image-metadata
                  (create-image (cdr (assq 'gif image-tests--images)))))))
