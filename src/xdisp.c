372cc9e88c9dad94a29082269d2424723b28eaee
Eli Zaretskii <eliz@gnu.org>
2022-10-01 09:24:04 +0300

    Fix 'C-a' and 'C-e' under 'visual-line-mode'
    * src/xdisp.c (move_it_vertically_backward): Fix last change.
    (Bug#58210)

--- src/xdisp.c
+++ src/xdisp.c
@@ -10776,7 +10776,9 @@ move_it_vertically_backward (struct it *it, int dy)
   if (dy == 0)
     {
       /* Adjust nlines for increasing it at the beginning.  */
-      nlines -= !!it->bidi_p;
+      if (it2.bidi_p
+	  && !(it2.line_wrap == WORD_WRAP && it2.continuation_lines_width))
+	nlines--;
       /* DY == 0 means move to the start of the screen line.  The
 	 value of nlines is > 0 if continuation lines were involved,
 	 or if the original IT position was at start of a line.  */
