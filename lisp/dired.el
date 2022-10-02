0d0d59b32c66f6b0604cb7c89b875d0407fba41c
Stefan Kangas <stefankangas@gmail.com>
2022-10-03 00:13:56 +0200

    Actually delete markers in dired-do-flagged-delete
    * lisp/dired.el (dired-do-flagged-delete): Actually delete
    markers.  (Bug#58256)

--- lisp/dired.el
+++ lisp/dired.el
@@ -3664,16 +3664,16 @@ non-empty directories is allowed."
 	 case-fold-search markers)
     (if (save-excursion (goto-char (point-min))
 			(re-search-forward regexp nil t))
-	(dired-internal-do-deletions
-         (nreverse
-	  ;; this can't move point since ARG is nil
-	  (dired-map-over-marks (cons (dired-get-filename)
-                                      (let ((m (point-marker)))
-                                        (push m markers)
-                                        m))
-			        nil))
-	 nil t)
-      (dolist (m markers) (set-marker m nil))
+        (progn
+          (dired-internal-do-deletions
+           (nreverse
+            (dired-map-over-marks (cons (dired-get-filename)
+                                   (let ((m (point-marker)))
+                                     (push m markers)
+                                     m))
+                             nil))
+           nil t)
+          (dolist (m markers) (set-marker m nil)))
       (or nomessage
 	  (message "(No deletions requested)")))))
 
