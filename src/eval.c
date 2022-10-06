a112e3f017dd9704f44b4913a2c37ad32d3e73bd
Gerd Möllmann <gerd@gnu.org>
2022-10-06 09:09:04 +0200

    Inhibit GC in probably_quit (bug#58042)

--- src/eval.c
+++ src/eval.c
@@ -1651,10 +1651,12 @@ process_quit_flag (void)
 void
 probably_quit (void)
 {
+  specpdl_ref gc_count = inhibit_garbage_collection ();
   if (!NILP (Vquit_flag) && NILP (Vinhibit_quit))
     process_quit_flag ();
   else if (pending_signals)
     process_pending_signals ();
+  unbind_to (gc_count, Qnil);
 }
 
 DEFUN ("signal", Fsignal, Ssignal, 2, 2, 0,
