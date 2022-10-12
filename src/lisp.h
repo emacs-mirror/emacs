da752c04664c0e22a2f6b4a41dfa1fed4d5276ff
Stefan Monnier <monnier@iro.umontreal.ca>
2022-10-12 17:06:33 -0400

    src/frame.c (do_switch_frame): Fix bug#58343
    * src/frame.c (do_switch_frame): Move call to `get-mru-window` outside
    of the "critical region" where (selected-window) != (frame-selected-window).
    * src/minibuf.c (move_minibuffers_onto_frame): Add arg `frame`.
    Use it instead of `selected_frame`.  Adjust all callers.
    * src/lisp.h (move_minibuffers_onto_frame): Adjust declaration.

--- src/lisp.h
+++ src/lisp.h
@@ -4792,7 +4792,7 @@ extern void clear_regexp_cache (void);
 
 extern Lisp_Object Vminibuffer_list;
 extern Lisp_Object last_minibuf_string;
-extern void move_minibuffers_onto_frame (struct frame *, bool);
+extern void move_minibuffers_onto_frame (struct frame *, Lisp_Object, bool);
 extern bool is_minibuffer (EMACS_INT, Lisp_Object);
 extern EMACS_INT this_minibuffer_depth (Lisp_Object);
 extern EMACS_INT minibuf_level;
