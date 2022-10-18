5ad5b797f78dacb9c901d3c63bee05b1762fa94f
Andrea Corallo <andrea.corallo@arm.com>
2022-10-18 15:14:32 +0200

    Set `comp-no-spawn' earlier using -no-comp-spawn
    * src/emacs.c (standard_args): Add '-no-comp-spawn' cmd line option.
    * lisp/startup.el (command-line): Parse '-no-comp-spawn' cmd line
    option.
    * lisp/emacs-lisp/comp.el (comp-run-async-workers, comp-final):
    Use '-no-comp-spawn'.

--- src/emacs.c
+++ src/emacs.c
@@ -2564,6 +2564,7 @@ static const struct standard_args standard_args[] =
   { "-init-directory", "--init-directory", 30, 1 },
   { "-no-x-resources", "--no-x-resources", 40, 0 },
   { "-no-site-file", "--no-site-file", 40, 0 },
+  { "-no-comp-spawn", "--no-comp-spawn", 60, 0 },
   { "-u", "--user", 30, 1 },
   { "-user", 0, 30, 1 },
   { "-debug-init", "--debug-init", 20, 0 },
