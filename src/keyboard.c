877f706c861ec14a3258e53b82b9c8fd86890ca4
Eli Zaretskii <eliz@gnu.org>
2022-10-27 15:29:46 +0300

    ; * src/keyboard.c (command_loop_1): Remove unused variable.

--- src/keyboard.c
+++ src/keyboard.c
@@ -1268,7 +1268,6 @@ command_loop_1 (void)
 {
   modiff_count prev_modiff = 0;
   struct buffer *prev_buffer = NULL;
-  bool already_adjusted = 0;
 
   kset_prefix_arg (current_kboard, Qnil);
   kset_last_prefix_arg (current_kboard, Qnil);
@@ -1458,8 +1457,6 @@ command_loop_1 (void)
       safe_run_hooks_maybe_narrowed (Qpre_command_hook,
 				     XWINDOW (selected_window));
 
-      already_adjusted = 0;
-
       if (NILP (Vthis_command))
 	/* nil means key is undefined.  */
 	call0 (Qundefined);
@@ -1615,9 +1612,8 @@ command_loop_1 (void)
 		   the automatic composition, we must update the
 		   display.  */
 		windows_or_buffers_changed = 21;
-	      if (!already_adjusted)
-		adjust_point_for_property (last_point_position,
-					   MODIFF != prev_modiff);
+	      adjust_point_for_property (last_point_position,
+					 MODIFF != prev_modiff);
 	    }
 	  else if (PT > BEGV && PT < ZV
 		   && (composition_adjust_point (last_point_position, PT)
