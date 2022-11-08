0e25a39e69acca0324c326ea8e46b1725594bff5
Alexander Adolf <alexander.adolf@condition-alpha.com>
2022-11-08 13:39:19 -0500

    EUDC: Add ecomplete and mailabbrev backends
    * doc/misc/eudc.texi (Overview): Add ecomplete and mailabbrev
    nodes.
    (ecomplete, mailabbrev): New nodes.
    (Installation): Add ecomplete and mailabbrev nodes.
    (LDAP Configuration): Use code formatting instead of quotes.
    (macOS Contacts Configuration): Likewise.
    (ecomplete Configuration): New node.
    (mailabbrev Configuration): Likewise.
    * etc/NEWS (EUDC): Mention ecomplete and mailabbrev backends,
    mention eudc-server-hotlist default change.
    * lisp/net/eudc-vars.el (eudc-known-protocols): Add ecomplete and
    mailabbrev.
    (eudc-server-hotlist): Add entries for ecomplete and mailabbrev.
    * lisp/net/eudcb-ecomplete.el: New EUDC backend file.
    * lisp/net/eudcb-mailabbrev.el: Likewise.
    * test/lisp/net/eudc-resources/ecompleterc,
    test/lisp/net/eudc-resources/mailrc: New eudc-tests resource
    files.
    * test/lisp/net/eudc-tests.el (eudc-test-rfc5322-quote-phrase)
    (eudc-test-make-address, eudcb-ecomplete, eudcb-mailabbrev): New
    test cases.

--- lisp/net/eudc-vars.el
+++ lisp/net/eudc-vars.el
@@ -51,9 +51,10 @@ instead."
 
 ;; Known protocols (used in completion)
 ;; Not to be mistaken with `eudc-supported-protocols'
-(defvar eudc-known-protocols '(bbdb ldap))
+(defvar eudc-known-protocols '(bbdb ldap ecomplete mailabbrev))
 
-(defcustom eudc-server-hotlist nil
+(defcustom eudc-server-hotlist '(("localhost" . ecomplete)
+                                 ("localhost" . mailabbrev))
   "Directory servers to query.
 This is an alist of the form (SERVER . PROTOCOL).  SERVER is the
 host name or URI of the server, PROTOCOL is a symbol representing
