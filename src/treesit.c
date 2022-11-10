98ed6db34f76cfe37d70d8ad82d29fcfa2a56e24
Yuan Fu <casouri@gmail.com>
2022-11-10 14:16:25 -0800

    ; Fix docstring graph alignment in Ftreesit_search_forward
    * src/treesit.c (Ftreesit_search_forward): Replace tabs with spaces.

--- src/treesit.c
+++ src/treesit.c
@@ -2649,15 +2649,15 @@ Return the first matched node, or nil if none matches.
 For a tree like below, where START is marked by S, traverse as
 numbered from 1 to 12:
 
-		12
-		|
+                12
+                |
        S--------3----------11
        |        |          |
   o--o-+--o  1--+--2    6--+-----10
   |  |                  |        |
   o  o                +-+-+   +--+--+
-		      |   |   |  |  |
-		      4   5   7  8  9
+                      |   |   |  |  |
+                      4   5   7  8  9
 
 Note that this function doesn't traverse the subtree of START, and it
 always traverse leaf nodes first, then upwards.  */)
