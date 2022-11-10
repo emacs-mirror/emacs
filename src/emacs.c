25b4cec31d580353995d87fe19ae4dab6e6e37de
Eli Zaretskii <eliz@gnu.org>
2022-11-10 12:12:56 +0200

    Avoid dumping core upon SIGHUP in non-interactive sessions
    * src/emacs.c (terminate_due_to_signal): Don't special-case
    SIGINT.  Patch by Paul Eggert <eggert@cs.ucla.edu>.  (Bug#58956)

--- src/emacs.c
+++ src/emacs.c
@@ -432,9 +432,9 @@ terminate_due_to_signal (int sig, int backtrace_limit)
           if (sig == SIGTERM || sig == SIGHUP || sig == SIGINT)
 	    {
 	      /* Avoid abort in shut_down_emacs if we were interrupted
-		 by SIGINT in noninteractive usage, as in that case we
-		 don't care about the message stack.  */
-	      if (sig == SIGINT && noninteractive)
+		 in noninteractive usage, as in that case we don't
+		 care about the message stack.  */
+	      if (noninteractive)
 		clear_message_stack ();
 	      Fkill_emacs (make_fixnum (sig), Qnil);
 	    }
