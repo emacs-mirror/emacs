756373a772c2c096bad1b26d7e46fa6144086ce1
Po Lu <luangruo@yahoo.com>
2022-11-13 15:34:16 +0800

    Fix warnings without XCB or XFixes
    * src/xterm.c (x_term_init): Don't USE_SAFE_ALLOCA or call
    SAFE_FREE outside of where it is actually used.  For some reason
    the compiler still emitted warnings despite the ((void)
    SAFE_ALLOCA (0)) statement.

--- src/xterm.c
+++ src/xterm.c
@@ -29120,10 +29120,9 @@ x_term_init (Lisp_Object display_name, char *xrm_option, char *resource_name)
 #endif
   int i;
 
+#if defined HAVE_XFIXES && defined USE_XCB
   USE_SAFE_ALLOCA;
-
-  /* Avoid warnings when SAFE_ALLOCA is not actually used.  */
-  ((void) SAFE_ALLOCA (0));
+#endif
 
   block_input ();
 
@@ -29277,7 +29276,9 @@ x_term_init (Lisp_Object display_name, char *xrm_option, char *resource_name)
 
       unblock_input ();
 
+#if defined HAVE_XFIXES && defined USE_XCB
       SAFE_FREE ();
+#endif
       return 0;
     }
 
@@ -29297,7 +29298,9 @@ x_term_init (Lisp_Object display_name, char *xrm_option, char *resource_name)
 
       unblock_input ();
 
+#if defined HAVE_XFIXES && defined USE_XCB
       SAFE_FREE ();
+#endif
       return 0;
     }
 #endif
@@ -30182,7 +30185,9 @@ x_term_init (Lisp_Object display_name, char *xrm_option, char *resource_name)
 
   unblock_input ();
 
+#if defined HAVE_XFIXES && defined USE_XCB
   SAFE_FREE ();
+#endif
   return dpyinfo;
 }
 
