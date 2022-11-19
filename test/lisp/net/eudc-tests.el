a6ae13af42ede6618c326855ea4c95e0298fb75b
Thomas Fitzsimmons <fitzsim@fitzsim.org>
2022-11-19 02:05:36 -0500

    EUDC: Add LDAP tests
    * test/lisp/net/eudc-resources/slapd.conf: New file.
    * test/lisp/net/eudc-resources/dc=gnu,dc=org.ldif: Likewise.
    * test/lisp/net/eudc-resources/dc=gnu,dc=org: New files.
    * test/lisp/net/eudc-tests.el (eudcb-ldap): New test.

--- test/lisp/net/eudc-tests.el
+++ test/lisp/net/eudc-tests.el
@@ -267,5 +267,27 @@
                      '(((email . "Lars Ingebrigtsen <larsi@mail-abbrev.com>, \
 Karl Fogel <kfogel@mail-abbrev.com")))))))))
 
+(require 'ldap)
+(ert-deftest eudcb-ldap ()
+  "Test the LDAP back-end."
+  (when (and (file-exists-p "/usr/sbin/slapd")
+             (file-exists-p "/usr/bin/ldapsearch"))
+    (cd (concat (ert-resource-directory) ".."))
+    (let ((ldap-process
+           (start-process "slapd" "*slapd*" "/usr/sbin/slapd"
+                          "-h" "ldap://127.0.0.1:3899" "-d" "0" "-4"
+                          "-f" (ert-resource-file "slapd.conf")))
+          (ldap-host-parameters-alist '(("ldap://localhost:3899"
+                                         base "dc=gnu,dc=org" auth simple)))
+          (eudc-server-hotlist '(("ldap://localhost:3899" . ldap)))
+          (eudc-ignore-options-file t))
+      (sleep-for 1) ; Wait for slapd to start.
+      (should (equal (with-temp-buffer
+                       (insert "emacs-ert-test-1")
+                       (eudc-expand-try-all)
+                       (buffer-string))
+                     "Emacs ERT1 <emacs-ert-test-1@ldap.gnu.org>"))
+      (kill-process ldap-process))))
+
 (provide 'eudc-tests)
 ;;; eudc-tests.el ends here
