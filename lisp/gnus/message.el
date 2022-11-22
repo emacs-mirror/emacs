b32f50c6d859d86dac6d62dd6ec871f3e5b33f67
Stefan Kangas <stefankangas@gmail.com>
2022-11-22 08:25:32 +0100

    Don't refer to deleted variable 'gnus-parameter-to-list-alist'
    * doc/misc/gnus.texi (Group Parameters):
    * lisp/gnus/message.el (message-recipients-without-full-name):
    Don't refer to deleted variable 'gnus-parameter-to-list-alist'.

--- lisp/gnus/message.el
+++ lisp/gnus/message.el
@@ -8811,8 +8811,6 @@ headers.  If FORCE, insert new field even if NEW-VALUE is empty."
 	"bug-gnu-emacs@gnu.org")
   "Mail addresses that have no full name.
 Used in `message-simplify-recipients'."
-  ;; Maybe the addresses could be extracted from
-  ;; `gnus-parameter-to-list-alist'?
   :type '(choice (const :tag "None" nil)
 		 (repeat string))
   :version "23.1" ;; No Gnus
