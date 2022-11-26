3dbe0cde0e1befb9c7065bdc43f64fdd0aba37b4
Stefan Kangas <stefankangas@gmail.com>
2022-11-26 09:04:39 +0100

    ; * lisp/progmodes/csharp-mode.el: Add Commentary.

--- lisp/progmodes/csharp-mode.el
+++ lisp/progmodes/csharp-mode.el
@@ -24,6 +24,10 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+;;; Commentary:
+
+;; Support for editing C#.
+
 ;;; Code:
 
 (require 'compile)
