c6cb6d8506916dd1c17fba2d24ec63426c4afdbd
Eli Zaretskii <eliz@gnu.org>
2022-11-28 19:47:35 +0200

    Cut the emacs-29 release branch
    * README:
    * configure.ac:
    * nt/README.W32:
    * msdos/sed2v2.inp: Bump Emacs version to 29.0.60.
    
    * admin/admin.el (set-version): Fix regexp for configure.ac.
    
    * lisp/cus-edit.el (customize-changed-options-previous-release):
    Bump last version to 28.2.

--- lisp/cus-edit.el
+++ lisp/cus-edit.el
@@ -1238,7 +1238,7 @@ Show the buffer in another window, but don't select it."
     (unless (eq symbol basevar)
       (message "`%s' is an alias for `%s'" symbol basevar))))
 
-(defvar customize-changed-options-previous-release "28.1"
+(defvar customize-changed-options-previous-release "28.2"
   "Version for `customize-changed' to refer back to by default.")
 
 ;; Packages will update this variable, so make it available.
