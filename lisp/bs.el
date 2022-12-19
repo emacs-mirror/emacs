remacs/remacs

commit 48d33090d0c796fd04b70ddd74ebfa16a94922c9

* erc-ibuffer.el (erc-channel-modes): * bs.el (bs--sort-by-mode, bs--get-mode-name): * imenu.el (imenu-add-to-menubar): * makesum.el (make-command-summary): * mouse.el (mouse-major-mode-menu, mouse-popup-menubar, mouse-buffer-menu): * msb.el (msb--mode-menu-cond): * calc/calc-embed.el (calc-do-embedded): * emacs-lisp/helper.el (Helper-describe-mode): * mail/emacsbug.el (report-emacs-bug): * progmodes/hideshow.el (hs-grok-mode-type): * textmodes/sgml-mode.el (sgml-mode-facemenu-add-face-function): * textmodes/table.el (*table--cell-describe-mode): Pass mode-name through format-mode-line.

lisp/bs.el
@@ -1,7 +1,7 @@
 ;;; bs.el --- menu for selecting and displaying buffers
 
 ;; Copyright (C) 1998, 1999, 2000, 2001, 2002, 2003, 2004,
-;;   2005, 2006, 2007 Free Software Foundation, Inc.
+;;   2005, 2006, 2007, 2008 Free Software Foundation, Inc.
 ;; Author: Olaf Sylvester <Olaf.Sylvester@netsurf.de>
 ;; Maintainer: Olaf Sylvester <Olaf.Sylvester@netsurf.de>
 ;; Keywords: convenience
@@ -369,9 +369,9 @@ A value of `always' means to show buffer regardless of the configuration.")
 
 (defun bs--sort-by-mode (b1 b2)
   "Compare buffers B1 and B2 by mode name."
-  (save-excursion
-    (string< (progn (set-buffer b1) (format "%s" mode-name))
-	     (progn (set-buffer b2) (format "%s" mode-name)))))
+  (save-current-buffer
+    (string< (progn (set-buffer b1) (format-mode-line mode-name nil nil b1))
+	     (progn (set-buffer b2) (format-mode-line mode-name nil nil b2)))))
 
 (defun bs--sort-by-size (b1 b2)
   "Compare buffers B1 and B2 by buffer size."
@@ -1331,7 +1331,7 @@ ALL-BUFFERS is the list of buffers appearing in Buffer Selection Menu."
   "Return the name of mode of current buffer for Buffer Selection Menu.
 START-BUFFER is the buffer where we started buffer selection.
 ALL-BUFFERS is the list of buffers appearing in Buffer Selection Menu."
-  mode-name)
+  (format-mode-line mode-name nil nil buffer))
 
 (defun bs--get-file-name (start-buffer all-buffers)
   "Return string for column 'File' in Buffer Selection Menu.
@@ -1480,5 +1480,5 @@ name of buffer configuration."
 ;; Now provide feature bs
 (provide 'bs)
 
-;;; arch-tag: c0d9ab34-bf06-4368-ae9d-af88878e6802
+;; arch-tag: c0d9ab34-bf06-4368-ae9d-af88878e6802
 ;;; bs.el ends here

lisp/calc/calc-embed.el
@@ -1,7 +1,7 @@
 ;;; calc-embed.el --- embed Calc in a buffer
 
 ;; Copyright (C) 1990, 1991, 1992, 1993, 2001, 2002, 2003, 2004,
-;;   2005, 2006, 2007 Free Software Foundation, Inc.
+;;   2005, 2006, 2007, 2008 Free Software Foundation, Inc.
 
 ;; Author: David Gillespie <daveg@synaptics.com>
 ;; Maintainer: Jay Belanger <jay.p.belanger@gmail.com>
@@ -254,7 +254,7 @@
 	       (set-buffer-modified-p (buffer-modified-p))
                (calc-embedded-restore-original-modes)
 	       (or calc-embedded-quiet
-		   (message "Back to %s mode" mode-name))))
+		   (message "Back to %s mode" (format-mode-line mode-name)))))
 
 	    (t
 	     (if (buffer-name (aref calc-embedded-info 0))
@@ -1374,5 +1374,5 @@ The command \\[yank] can retrieve it from there."
 
 (provide 'calc-embed)
 
-;;; arch-tag: 1b8f311e-fba1-40d3-b8c3-1d6f68fd26fc
+;; arch-tag: 1b8f311e-fba1-40d3-b8c3-1d6f68fd26fc
 ;;; calc-embed.el ends here

lisp/emacs-lisp/helper.el
@@ -1,7 +1,7 @@
 ;;; helper.el --- utility help package supporting help in electric modes
 
 ;; Copyright (C) 1985, 2001, 2002, 2003, 2004, 2005,
-;;   2006, 2007 Free Software Foundation, Inc.
+;;   2006, 2007, 2008 Free Software Foundation, Inc.
 
 ;; Author: K. Shane Hartman
 ;; Maintainer: FSF
@@ -120,10 +120,9 @@
 (defun Helper-describe-mode ()
   "Describe the current mode."
   (interactive)
-  (let ((name mode-name)
+  (let ((name (format-mode-line mode-name))
 	(documentation (documentation major-mode)))
-    (save-excursion
-      (set-buffer (get-buffer-create "*Help*"))
+    (with-current-buffer (get-buffer-create "*Help*")
       (setq buffer-read-only nil)
       (erase-buffer)
       (insert name " Mode\n" documentation)
@@ -158,5 +157,5 @@
 
 (provide 'helper)
 
-;;; arch-tag: a0984577-d3e9-4124-ae0d-c46fe740f6a9
+;; arch-tag: a0984577-d3e9-4124-ae0d-c46fe740f6a9
 ;;; helper.el ends here

lisp/erc/erc-ibuffer.el
@@ -1,6 +1,6 @@
 ;;; erc-ibuffer.el --- ibuffer integration with ERC
 
-;; Copyright (C) 2002, 2004, 2006, 2007 Free Software Foundation, Inc.
+;; Copyright (C) 2002, 2004, 2006, 2007, 2008 Free Software Foundation, Inc.
 
 ;; Author: Mario Lang <mlang@delysid.org>
 ;; Keywords: comm
@@ -158,8 +158,8 @@
 		  (format "l %d" erc-channel-user-limit)
 		"")
 	      ")")
-    (if (not (eq major-mode 'erc-mode))
-	mode-name
+    (if (not (derived-mode-p 'erc-mode))
+	(format-mode-line mode-name nil nil (current-buffer))
       "")))
 
 (define-ibuffer-column erc-nick (:name "Nick")

lisp/imenu.el
@@ -1,7 +1,7 @@
 ;;; imenu.el --- framework for mode-specific buffer indexes
 
 ;; Copyright (C) 1994, 1995, 1996, 1997, 1998, 2001, 2002, 2003, 2004,
-;;   2005, 2006, 2007 Free Software Foundation, Inc.
+;;   2005, 2006, 2007, 2008 Free Software Foundation, Inc.
 
 ;; Author: Ake Stenhoff <etxaksf@aom.ericsson.se>
 ;;         Lars Lindberg <lli@sypro.cap.se>
@@ -954,7 +954,8 @@ See the command `imenu' for more information."
 	  `(menu-item ,name ,(make-sparse-keymap "Imenu")))
 	(use-local-map newmap)
 	(add-hook 'menu-bar-update-hook 'imenu-update-menubar))
-    (error "The mode `%s' does not support Imenu" mode-name)))
+    (error "The mode `%s' does not support Imenu"
+           (format-mode-line mode-name))))
 
 ;;;###autoload
 (defun imenu-add-menubar-index ()

lisp/mail/emacsbug.el
@@ -1,7 +1,7 @@
 ;;; emacsbug.el --- command to report Emacs bugs to appropriate mailing list
 
 ;; Copyright (C) 1985, 1994, 1997, 1998, 2000, 2001, 2002, 2003,
-;;   2004, 2005, 2006, 2007 Free Software Foundation, Inc.
+;;   2004, 2005, 2006, 2007, 2008 Free Software Foundation, Inc.
 
 ;; Author: K. Shane Hartman
 ;; Maintainer: FSF
@@ -168,7 +168,9 @@ usually do not have translators to read other languages for them.\n\n")
 		    default-enable-multibyte-characters))
     (insert "\n")
     (insert (format "Major mode: %s\n"
-		    (buffer-local-value 'mode-name from-buffer)))
+		    (format-mode-line
+                     (buffer-local-value 'mode-name from-buffer)
+                     nil nil from-buffer)))
     (insert "\n")
     (insert "Minor modes in effect:\n")
     (dolist (mode minor-mode-list)

lisp/makesum.el
@@ -1,7 +1,7 @@
 ;;; makesum.el --- generate key binding summary for Emacs
 
 ;; Copyright (C) 1985, 2001, 2002, 2003, 2004, 2005,
-;;   2006, 2007 Free Software Foundation, Inc.
+;;   2006, 2007, 2008 Free Software Foundation, Inc.
 
 ;; Maintainer: FSF
 ;; Keywords: help
@@ -65,7 +65,7 @@ Previous contents of that buffer are killed first."
        (if (re-search-forward "^Local Bindings:" nil t)
 	   (progn
 	    (forward-char -1)
-	    (insert " for " cur-mode " Mode")
+	    (insert " for " (format-mode-line cur-mode) " Mode")
 	    (while (search-forward "??\n" nil t)
 	      (delete-region (point)
 			     (progn
@@ -114,5 +114,5 @@ Previous contents of that buffer are killed first."
 
 (provide 'makesum)
 
-;;; arch-tag: c2383336-fc89-46ad-8110-ded42bffaee3
+;; arch-tag: c2383336-fc89-46ad-8110-ded42bffaee3
 ;;; makesum.el ends here

lisp/mouse.el
@@ -1,7 +1,7 @@
 ;;; mouse.el --- window system-independent mouse support
 
 ;; Copyright (C) 1993, 1994, 1995, 1999, 2000, 2001, 2002, 2003,
-;;   2004, 2005, 2006, 2007 Free Software Foundation, Inc.
+;;   2004, 2005, 2006, 2007, 2008 Free Software Foundation, Inc.
 
 ;; Maintainer: FSF
 ;; Keywords: hardware, mouse
@@ -192,7 +192,8 @@ Default to the Edit menu if the major mode doesn't define a menu."
 	 ;; Make a keymap in which our last command leads to a menu or
 	 ;; default to the edit menu.
 	 (newmap (if ancestor
-		     (make-sparse-keymap (concat mode-name " Mode"))
+		     (make-sparse-keymap (concat (format-mode-line mode-name)
+                                                 " Mode"))
 		   menu-bar-edit-menu)))
     (if ancestor
 	;; Make our menu inherit from the desired keymap which we want
@@ -272,7 +273,8 @@ not it is actually displayed."
     (or (null local-menu)
 	(stringp local-title-or-map)
 	(setq local-menu (cons 'keymap
-			       (cons (concat mode-name " Mode Menu")
+			       (cons (concat (formal-mode-line mode-name)
+                                             " Mode Menu")
 				     (cdr local-menu)))))
     (or (stringp global-title-or-map)
 	(setq global-menu (cons 'keymap
@@ -1861,7 +1863,7 @@ and selects that window."
             (setq elt (list adjusted-major-mode
                             (if (stringp adjusted-major-mode)
                                 adjusted-major-mode
-                                mode-name))
+                              (format-mode-line mode-name nil nil buf)))
                   split-by-major-mode (cons elt split-by-major-mode)))
           (or (memq buf (cdr (cdr elt)))
               (setcdr (cdr elt) (cons buf (cdr (cdr elt))))))))

emacs-mirror/emacs

commit 03648965a027b1a0aa46a2ebfa0dab78ef49b200

bs.el: Use the right buffer context to compute the mode name
* lisp/bs.el (bs--get-mode-name): The function is already called
with the correct buffer as current-buffer; and anyway, START-BUFFER
is usually the wrong one to pass to `format-mode-line'.  This fixes
a bug introduced in 48d33090d0 and c2699583be (back in 2008-01-04).

lisp/bs.el
@@ -1346,11 +1346,11 @@ ALL-BUFFERS is the list of buffers appearing in Buffer Selection Menu."
               'help-echo "mouse-2: select this buffer, mouse-3: select in other frame"
               'mouse-face 'highlight))
 
-(defun bs--get-mode-name (start-buffer _all-buffers)
+(defun bs--get-mode-name (_start-buffer _all-buffers)
   "Return the name of mode of current buffer for Buffer Selection Menu.
 START-BUFFER is the buffer where we started buffer selection.
 ALL-BUFFERS is the list of buffers appearing in Buffer Selection Menu."
-  (format-mode-line mode-name nil nil start-buffer))
+  (format-mode-line mode-name nil nil nil))
 
 (defun bs--get-file-name (_start-buffer _all-buffers)
   "Return string for column `File' in Buffer Selection Menu.