2b1fdbffcb595bcd72fa9aa3db674c6985042bcb
Dmitry Gutov <dgutov@yandex.ru>
2022-12-19 21:01:27 +0200

    ruby-method-params-indent: New user option
    * lisp/progmodes/ruby-mode.el (ruby-method-params-indent):
    New option (bug#60110).
    
    (ruby-smie-rules): Use it.
    
    * etc/NEWS: Mention it.
    
    * test/lisp/progmodes/ruby-mode-resources/ruby.rb:
    Ensure the var's value is default.
    
    * test/lisp/progmodes/ruby-mode-resources/ruby-method-params-indent.rb:
    New file.
    
    * test/lisp/progmodes/ruby-mode-tests.el (ruby-deftest-indent):
    New macro, use it to run the indentation test using the new file.
    Disable the :expensive-test tag, because neither runs for "longer
    than some few seconds", both take significantly below 1s.

---
+++ test/lisp/progmodes/ruby-mode-resources/ruby-method-params-indent.rb
@@ -0,0 +1,18 @@
+class C
+  def self.foo(
+    baz,
+    bar
+  ) =
+    what
+
+  def foo=(
+    baz,
+    bar
+  )
+    hello
+  end
+end
+
+# Local Variables:
+# ruby-method-params-indent: 0
+# End:
