6c00d126e7fe1f6e42a0c9454c2ab4a29dcd5989
Gregory Heytings <gregory@heytings.org>
2022-12-26 01:21:16 +0000

    Remove remaining mentions of 'eval-current-buffer'
    * lisp/emacs-lisp/edebug.el (edebug-all-defs):
    * doc/lispref/eval.texi (Eval):
    * doc/lispref/edebug.texi (Instrumenting, Edebug Options):
    Remove remaining mentions of 'eval-current-buffer', obsoleted in
    Emacs 22 and removed in Emacs 26.

--- lisp/emacs-lisp/edebug.el
+++ lisp/emacs-lisp/edebug.el
@@ -92,9 +92,9 @@ using, but only when you also use Edebug."
 ;;;###autoload
 (defcustom edebug-all-defs nil
   "If non-nil, evaluating defining forms instruments for Edebug.
-This applies to `eval-defun', `eval-region', `eval-buffer', and
-`eval-current-buffer'.  `eval-region' is also called by
-`eval-last-sexp', and `eval-print-last-sexp'.
+This applies to `eval-defun', `eval-region' and `eval-buffer'.
+`eval-region' is also called by `eval-last-sexp', and
+`eval-print-last-sexp'.
 
 You can use the command `edebug-all-defs' to toggle the value of this
 variable.  You may wish to make it local to each buffer with
