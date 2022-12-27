41f12e1019bb96e424e27c2290b285bf7899de80
Stefan Kangas <stefankangas@gmail.com>
2022-12-27 17:28:08 +0100

    ; * lisp/elide-head.el (elide-head): Doc fix to silence checkdoc.

--- lisp/elide-head.el
+++ lisp/elide-head.el
@@ -147,10 +147,11 @@ mode hooks."
 (defun elide-head (&optional arg)
   "Hide header material in buffer according to `elide-head-headers-to-hide'.
 
-The header is made invisible with an overlay.  With a prefix arg, show
-an elided material again.
+The header is made invisible with an overlay.  With a prefix
+argument ARG, show an elided material again.
 
-This is suitable as an entry on `find-file-hook' or appropriate mode hooks."
+This is suitable as an entry on `find-file-hook' or appropriate
+mode hooks."
   (declare (obsolete elide-head-mode "29.1"))
   (interactive "P")
   (if arg
