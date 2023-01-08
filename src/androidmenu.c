e816e570393a964383e8b42330f5582c4c0a54f2
Po Lu <luangruo@yahoo.com>
2023-01-08 15:37:30 +0800

    Check in new file androidmenu.c
    * src/androidmenu.c: New file.

---
+++ src/androidmenu.c
@@ -0,0 +1,38 @@
+/* Communication module for Android terminals.
+
+Copyright (C) 2023 Free Software Foundation, Inc.
+
+This file is part of GNU Emacs.
+
+GNU Emacs is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or (at
+your option) any later version.
+
+GNU Emacs is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.  */
+
+#include <config.h>
+
+#include "lisp.h"
+#include "androidterm.h"
+
+#ifndef ANDROID_STUBIFY
+
+/* Flag indicating whether or not a popup menu has been posted and not
+   yet popped down.  */
+
+static int popup_activated_flag;
+
+int
+popup_activated (void)
+{
+  return popup_activated_flag;
+}
+
+#endif
