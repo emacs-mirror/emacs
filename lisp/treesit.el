faee7e1f1bd0167e455a0e1e5fe02e21d23fd77f
Yuan Fu <casouri@gmail.com>
2023-01-17 22:52:22 -0800

    ; * lisp/treesit.el (treesit-font-lock-fontify-region): Minor fix.

--- lisp/treesit.el
+++ lisp/treesit.el
@@ -987,8 +987,7 @@ If LOUDLY is non-nil, display some debugging information."
                  (end-time (current-time)))
             ;; If for any query the query time is strangely long,
             ;; switch to fast mode (see comments above).
-            (when (and (eq 'undecided treesit--font-lock-fast-mode)
-                       (> (time-to-seconds
+            (when (and (> (time-to-seconds
                            (time-subtract end-time start-time))
                           0.01))
               (if (> treesit--font-lock-fast-mode-grace-count 0)
