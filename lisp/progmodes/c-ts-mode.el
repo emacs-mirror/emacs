3f43a16bc63eac12db5707b26da2507077b4f13c
Eli Zaretskii <eliz@gnu.org>
2023-03-02 09:33:04 +0200

    ; Avoid byte-compilation warning in c-ts-mode.el
    * lisp/progmodes/c-ts-mode.el (treesit-node-first-child-for-pos):
    Declare.

--- lisp/progmodes/c-ts-mode.el
+++ lisp/progmodes/c-ts-mode.el
@@ -78,6 +78,7 @@
 (declare-function treesit-node-child-by-field-name "treesit.c")
 (declare-function treesit-node-type "treesit.c")
 (declare-function treesit-node-prev-sibling "treesit.c")
+(declare-function treesit-node-first-child-for-pos "treesit.c")
 
 ;;; Custom variables
 
