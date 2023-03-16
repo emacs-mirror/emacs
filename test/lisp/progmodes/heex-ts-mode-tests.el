eba7d7970f330068d3332419623f87caf6953e6c
Wilhelm H Kirschbaum <wkirschbaum@gmail.com>
2023-03-16 22:11:59 +0200

    ; Fix source file headers for some test files (bug#62227)
    * test/lisp/progmodes/elixir-ts-mode-tests.el: Fix
    source file header.
    * test/lisp/progmodes/heex-ts-mode-tests.el: Add source
    file header and footer.

--- test/lisp/progmodes/heex-ts-mode-tests.el
+++ test/lisp/progmodes/heex-ts-mode-tests.el
@@ -1,3 +1,24 @@
+;;; heex-ts-mode-tests.el --- Tests for heex-ts-mode         -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023 Free Software Foundation, Inc.
+
+;; This file is part of GNU Emacs.
+
+;; GNU Emacs is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
+
 (require 'ert)
 (require 'ert-x)
 (require 'treesit)
@@ -7,3 +28,4 @@
   (ert-test-erts-file (ert-resource-file "indent.erts")))
 
 (provide 'heex-ts-mode-tests)
+;;; heex-ts-mode-tests.el ends here
