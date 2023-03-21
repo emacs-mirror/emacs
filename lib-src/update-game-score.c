640fd9b594fa376e6493efbe0cf083e1270ddd3f
Paul Eggert <eggert@cs.ucla.edu>
2023-03-21 14:38:23 -0700

    Sleep less in update-game-score
    This is mostly to avoid interface hassles with time/srand/rand.
    * lib-src/update-game-score.c (main): Don’t use ‘srand’ or ‘time’.
    (lock_file): Don’t sleep if we unlocked the lock file.  When
    sleeping, always just sleep 1 s.  This avoids the need for calling
    ‘time’ and ‘rand’, the utility of which was dubious anyway.

--- lib-src/update-game-score.c
+++ lib-src/update-game-score.c
@@ -185,8 +185,6 @@ main (int argc, char **argv)
   ptrdiff_t scorecount, scorealloc;
   ptrdiff_t max_scores = MAX_SCORES;
 
-  srand (time (0));
-
   while ((c = getopt (argc, argv, "hrm:d:")) != -1)
     switch (c)
       {
@@ -485,8 +483,8 @@ lock_file (const char *filename, void **state)
 	    return -1;
 	  attempts = 0;
 	}
-
-      sleep ((rand () & 1) + 1);
+      else
+	sleep (1);
     }
 
   close (fd);
