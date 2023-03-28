6e6e8b5c974202d2691c1971be66c1cb3788b7c1
Earl Hyatt <okamsn@protonmail.com>
2023-03-27 20:57:31 -0400

    Add more documentation for the keys of `package-vc-selected-packages`.
    * doc/emacs/package.texi (Specifying Package Sources): List the
    accepted keys in a new subsection of Fetching Package Sources.
    
    * lisp/emacs-lisp/package-vc.el (package-vc-selected-packages):
    Mention the `:doc` key.  Add the `:doc` key to the Customize form,
    mention the new Info node, correct "TexInfo" to "Texinfo", avoid
    Git-specific terms for the description of `:branch`, mention guessing
    `:vc-backend` based on the URL.

--- lisp/emacs-lisp/package-vc.el
+++ lisp/emacs-lisp/package-vc.el
@@ -142,32 +142,9 @@ is a symbol designating the package and SPEC is one of:
 
 - nil, if any package version can be installed;
 - a version string, if that specific revision is to be installed;
-- a property list, describing a package specification.  Valid
-  key/value pairs are
-
-   `:url' (string)
-      The URL of the repository used to fetch the package source.
-
-   `:branch' (string)
-      If given, the name of the branch to checkout after cloning the directory.
-
-   `:lisp-dir' (string)
-      The repository-relative name of the directory to use for loading the Lisp
-      sources.  If not given, the value defaults to the root directory
-      of the repository.
-
-   `:main-file' (string)
-      The main file of the project, relevant to gather package metadata.
-      If not given, the assumed default is the package name with \".el\"
-      appended to it.
-
-   `:vc-backend' (symbol)
-      A symbol of the VC backend to use for cloning the package.  The
-      value ought to be a member of `vc-handled-backends'.  If omitted,
-      `vc-clone' will fall back onto the archive default or on
-      `package-vc-default-backend'.
-
-  All other keys are ignored.
+- a property list, describing a package specification.  For more
+  details, please consult the subsection \"Specifying Package
+  Sources\" in the Info node `(emacs)Fetching Package Sources'.
 
 This user option will be automatically updated to store package
 specifications for packages that are not specified in any
@@ -181,6 +158,7 @@ archive."
                                          (:branch string)
                                          (:lisp-dir string)
                                          (:main-file string)
+                                         (:doc string)
                                          (:vc-backend symbol)))))
   :version "29.1")
 
