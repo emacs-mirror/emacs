5a1c9aace70b1f26b6cce38dbd79270c854bc080
Yuan Fu <casouri@gmail.com>
2023-04-02 15:09:41 -0700

    ; Add a bit more docstring to tsx-ts-mode (bug#62429)
    * lisp/progmodes/typescript-ts-mode.el:
    (tsx-ts-mode): Explain a bit more.

--- lisp/progmodes/typescript-ts-mode.el
+++ lisp/progmodes/typescript-ts-mode.el
@@ -396,7 +396,10 @@ Argument LANGUAGE is either `typescript' or `tsx'."
 This major mode defines two additional JSX-specific faces:
 `typescript-ts-jsx-attribute-face' and
 `typescript-ts-jsx-attribute-face' that are used for HTML tags
-and attributes, respectively."
+and attributes, respectively.
+
+The JSX-specific faces are used when `treesit-font-lock-level' is
+at least 3 (which is the default value)."
   :group 'typescript
   :syntax-table typescript-ts-mode--syntax-table
 
