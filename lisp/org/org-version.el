479626dbac97d4415ae861c3f66fc33c5fd6fa14
Kyle Meyer <kyle@kyleam.com>
2023-04-02 23:03:32 -0400

    Update to Org 9.6.3-2-gf2949d

--- lisp/org/org-version.el
+++ lisp/org/org-version.el
@@ -5,13 +5,13 @@
 (defun org-release ()
   "The release version of Org.
 Inserted by installing Org mode or when a release is made."
-   (let ((org-release "9.6.2"))
+   (let ((org-release "9.6.3"))
      org-release))
 ;;;###autoload
 (defun org-git-version ()
   "The Git version of Org mode.
 Inserted by installing Org or when a release is made."
-   (let ((org-git-version "release_9.6.2"))
+   (let ((org-git-version "release_9.6.3-2-gf2949d"))
      org-git-version))
 
 (provide 'org-version)
