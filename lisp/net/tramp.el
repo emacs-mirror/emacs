30692f16b1527b364f2b5a242c67e54c0fa83328
Michael Albinus <michael.albinus@gmx.de>
2023-04-04 13:37:18 +0200

    Improve Tramp robustness
    * lisp/net/tramp-sh.el (tramp-open-connection-setup-interactive-shell):
    Check for `tramp-string-empty-or-nil-p'.

--- lisp/net/tramp.el
+++ lisp/net/tramp.el
@@ -4793,7 +4793,7 @@ Do not set it manually, it is used buffer-local in `tramp-get-lock-pid'.")
 	(signal 'file-error `("Cannot remove lock file for" ,file)))
     ;; `userlock--handle-unlock-error' exists since Emacs 28.1.  It
     ;; checks for `create-lockfiles' since Emacs 30.1, we don't need
-    ;; this chweck here, then.
+    ;; this check here, then.
     (error (unless (or (not create-lockfiles)
                        (bound-and-true-p remote-file-name-inhibit-locks))
              (tramp-compat-funcall 'userlock--handle-unlock-error err)))))
