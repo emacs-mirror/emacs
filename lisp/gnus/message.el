f261226d9be4630572df322b2c4f48713c9c2fce
Andrew G Cohen <cohen@andy.bu.edu>
2023-05-03 11:37:45 +0800

    Allow X-Message-SMTP-Method to include more MTAs
    * lisp/gnus/message.el (message-multi-smtp-send-mail): Allow the
    X-Message-SMTP-Method header to override the default mailer with not
    only smtp and sendmail but other MTAs as well.
    * doc/misc/message.texi: Document changes to the usage of
    X-Message-SMTP-Method.

--- lisp/gnus/message.el
+++ lisp/gnus/message.el
@@ -5009,30 +5009,34 @@ Each line should be no more than 79 characters long."
   "Send the current buffer to `message-send-mail-function'.
 Or, if there's a header that specifies a different method, use
 that instead."
-  (let ((method (message-field-value "X-Message-SMTP-Method")))
+  (let ((method (message-field-value "X-Message-SMTP-Method"))
+        send-function)
     (if (not method)
-	(funcall message-send-mail-function)
+        (funcall message-send-mail-function)
       (message-remove-header "X-Message-SMTP-Method")
       (setq method (split-string method))
+      (setq send-function
+            (symbol-function
+             (intern-soft (format "message-send-mail-with-%s" (car method)))))
       (cond
-       ((equal (car method) "sendmail")
-	(message-send-mail-with-sendmail))
        ((equal (car method) "smtp")
-	(require 'smtpmail)
-	(let* ((smtpmail-store-queue-variables t)
+        (require 'smtpmail)
+        (let* ((smtpmail-store-queue-variables t)
                (smtpmail-smtp-server (nth 1 method))
-	       (service (nth 2 method))
-	       (port (string-to-number service))
-	       ;; If we're talking to the TLS SMTP port, then force a
-	       ;; TLS connection.
-	       (smtpmail-stream-type (if (= port 465)
-					 'tls
-				       smtpmail-stream-type))
-	       (smtpmail-smtp-service (if (> port 0) port service))
-	       (smtpmail-smtp-user (or (nth 3 method) smtpmail-smtp-user)))
-	  (message-smtpmail-send-it)))
+               (service (nth 2 method))
+               (port (string-to-number service))
+               ;; If we're talking to the TLS SMTP port, then force a
+               ;; TLS connection.
+               (smtpmail-stream-type (if (= port 465)
+                                         'tls
+                                       smtpmail-stream-type))
+               (smtpmail-smtp-service (if (> port 0) port service))
+               (smtpmail-smtp-user (or (nth 3 method) smtpmail-smtp-user)))
+          (message-smtpmail-send-it)))
+       (send-function
+        (funcall send-function))
        (t
-	(error "Unknown method %s" method))))))
+        (error "Unknown mail method %s" method))))))
 
 (defun message-send-mail-with-sendmail ()
   "Send off the prepared buffer with sendmail."
