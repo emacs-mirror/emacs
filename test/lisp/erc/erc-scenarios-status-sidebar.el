ded35c2da4da52641ec99927347cd50b736b9577
F. Jason Park <jp@neverwas.me>
2023-05-04 00:01:11 -0700

    Add erc-status-sidebar integration to erc-speedbar
    * lisp/erc/erc-speedbar.el: Require `erc-button' atop file and don't
    bother loading `dframe', which `speedbar' handles for us.
    (erc-speedbar): Explain that `nickbar' is the module for this group
    and library for the benefit of those who run M-x customize-group.
    (erc-speedbar-nicknames-window-width): New option.
    (erc-speedbar-hide-mode-topic): New option determining whether to hide
    the mode and topic.
    (erc-speedbar-my-nick-face): New option for determining face to use
    when displaying user's current nick.
    (erc-speedbar-browser): Call `erc-install-speedbar-variables'
    explicitly and remove top-level `with-eval-after-load'.
    (erc-speedbar-insert-target): Add parenthesized channel count after
    channel name in server and channel views.
    (erc-speedbar-expand-channel): Hide mode and topic depending on option
    `erc-speedbar-hide-mode-topic' and pass buffer to
    `erc-speedbar-insert-user'.
    (erc-speedbar--nick-face-function): New internal function-valued
    variable.
    (erc-speedbar--highlight-self-and-ops): New function to serve as
    default value for `erc-speedbar--nick-face-function'.
    (erc-speedbar--on-click): Dispatch `erc-nick-popup' after trimming
    status chars.
    (erc-speedbar-insert-user): Revise doc string.  Call
    `erc-speedbar--nick-face-function' to determine face.  Change
    token for both expansion and on-click text props.  Assign
    `erc-speedbar--on-click' as the mouse handler for nick items.
    (erc-speedbar--buffer-options): Variable to override options locally
    in speedbar buffer.
    (erc-speedbar--hidden-speedbar-frame): Add variable to hold original
    `speedbar-frame' before spoofing by setting to selected frame
    containing window showing ERC buffer.
    (erc-speedbar--emulate-sidebar-set-window-preserve-size,
    erc-speedbar--status-sidebar-mode--unhook): Add function
    to ensure status sidebar is showing correctly and helper to
    unregister from hook on teardown.
    (erc-speedbar--emulate-sidebar): Add function to control sidebar
    nicknames setup.
    (erc-speedbar--toggle-nicknames-sidebar): Add toggle function
    for speedbar or emulated sidebar.
    (erc-speedbar--ensure): Add helper function to show speedbar if it's
    hidden or create one if none exists.
    (erc-nickbar-mode, erc-nickbar-enable, erc-nickbar-disable):
    Add new mini module.
    (erc-speedbar--dframe-controlled) Add function to overwrite
    `speedbar-frame-mode' as `dframe-controlled' in speedbar buffer.
    (erc-speedbar-toggle-nicknames-window-lock,
    erc-speedbar-close-nicknames-window): Add commands to close speedbar
    window and toggle its cyclability.
    (erc-speedbar--compose-nicks-face): Add helper for nicks integration.
    * test/lisp/erc/erc-scenarios-status-sidebar.el
    (erc-scenarios-status-sidebar--nickbar): New test.  (Bug#63595)

--- test/lisp/erc/erc-scenarios-status-sidebar.el
+++ test/lisp/erc/erc-scenarios-status-sidebar.el
@@ -90,4 +90,80 @@
         (erc-status-sidebar-kill)
         (should-not (get-buffer "*ERC Status*"))))))
 
+;; We can't currently run this on EMBA because it needs a usable
+;; terminal, and we lack a fixture for that.  Please try running this
+;; test interactively with both graphical Emacs and non.
+(declare-function erc-nickbar-mode "erc-speedbar" (arg))
+(declare-function erc-speedbar-close-nicknames-window "erc-speedbar" (kill))
+(declare-function speedbar-timer-fn "speedbar" nil)
+(defvar erc-nickbar-mode)
+(defvar speedbar-buffer)
+
+(ert-deftest erc-scenarios-status-sidebar--nickbar ()
+  :tags '(:unstable :expensive-test)
+  (when noninteractive (ert-skip "Interactive only"))
+
+  (erc-scenarios-common-with-cleanup
+      ((erc-scenarios-common-dialog "base/gapless-connect")
+       (erc-server-flood-penalty 0.1)
+       (erc-server-flood-penalty erc-server-flood-penalty)
+       (erc-modules `(nickbar ,@erc-modules))
+       (dumb-server (erc-d-run "localhost" t 'foonet 'barnet))
+       (port (process-contact dumb-server :service))
+       (expect (erc-d-t-make-expecter)))
+
+    (ert-info ("Connect to two different endpoints")
+      (with-current-buffer (erc :server "127.0.0.1"
+                                :port port
+                                :nick "tester"
+                                :password "foonet:changeme"
+                                :full-name "tester")
+        (funcall expect 10 "MOTD File is missing"))
+      (with-current-buffer (erc :server "127.0.0.1"
+                                :port port
+                                :nick "tester"
+                                :password "barnet:changeme"
+                                :full-name "tester")
+        (funcall expect 10 "marked as being away")))
+
+    (erc-d-t-wait-for 20 (get-buffer "#bar"))
+    (with-current-buffer (pop-to-buffer "#bar")
+      (funcall expect 10 "was created on")
+      (funcall expect 2 "his second fit")
+      (erc-d-t-wait-for 10 (and speedbar-buffer (get-buffer speedbar-buffer)))
+      (speedbar-timer-fn)
+      (with-current-buffer speedbar-buffer
+        (funcall expect 10 "#bar (3)")
+        (funcall expect 10 '(| "@mike" "joe"))
+        (funcall expect 10 '(| "@mike" "joe"))
+        (funcall expect 10 "tester")))
+
+    (erc-d-t-wait-for 20 (get-buffer "#foo"))
+    (with-current-buffer (pop-to-buffer "#foo")
+      (delete-other-windows)
+      (funcall expect 10 "was created on")
+      (funcall expect 2 "no use of him")
+      (speedbar-timer-fn)
+      (with-current-buffer speedbar-buffer
+        (funcall expect 10 "#foo (3)")
+        (funcall expect 10 '(| "alice" "@bob"))
+        (funcall expect 10 '(| "alice" "@bob"))
+        (funcall expect 10 "tester")))
+
+    (with-current-buffer "#foo"
+      (ert-info ("Core toggle and kill commands work")
+        ;; Avoid using API, e.g., `erc-status-sidebar-buffer-exists-p',
+        ;; etc. for testing commands that call those same functions.
+        (erc-nickbar-mode -1)
+        (should-not (and speedbar-buffer
+                         (get-buffer-window speedbar-buffer)))
+        (erc-nickbar-mode +1)
+        (should (and speedbar-buffer
+                     (get-buffer-window speedbar-buffer)))
+        (should (get-buffer " SPEEDBAR"))
+        (erc-speedbar-close-nicknames-window 'kill)
+        (should-not (get-buffer " SPEEDBAR"))
+        (should-not erc-nickbar-mode)
+        (should-not (cdr (frame-list)))))))
+
 ;;; erc-scenarios-status-sidebar.el ends here
