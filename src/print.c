85c4efc9f415e76244580dc85cce99c4df6ea4e8
Paul Eggert <eggert@cs.ucla.edu>
2023-05-14 18:51:24 -0700

    Prefer UINTMAX_WIDTH in print_object
    * src/print.c (print_object):
    Prefer UINTMAX_WIDTH to rolling our own substitute.

--- src/print.c
+++ src/print.c
@@ -2202,7 +2202,7 @@ print_object (Lisp_Object obj, Lisp_Object printcharfun, bool escapeflag)
   char buf[max (sizeof "from..to..in " + 2 * INT_STRLEN_BOUND (EMACS_INT),
 		max (sizeof " . #" + INT_STRLEN_BOUND (intmax_t),
 		     max ((sizeof " with data 0x"
-			   + (sizeof (uintmax_t) * CHAR_BIT + 4 - 1) / 4),
+			   + (UINTMAX_WIDTH + 4 - 1) / 4),
 			  40)))];
   current_thread->stack_top = NEAR_STACK_TOP (buf);
 
