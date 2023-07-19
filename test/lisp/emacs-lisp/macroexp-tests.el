ca4bc9baf9d2c861ad776da07e56381da8e3722a
Stefan Monnier <monnier@iro.umontreal.ca>
2023-07-19 11:29:32 -0400

    macroexp.el: Fix missing warning for intermediate expansions
    When a macro expanded to a call to an obsolete macro, we failed
    to emit a warning for that use of the obsolete macro.
    
    * lisp/emacs-lisp/macroexp.el (macroexp-macroexpand):
    Use `macroexpand-1` to check obsolecence of intermediate expansions.
    
    * test/lisp/emacs-lisp/macroexp-tests.el
    (macroexp--test-obsolete-macro): New test.

--- test/lisp/emacs-lisp/macroexp-tests.el
+++ test/lisp/emacs-lisp/macroexp-tests.el
@@ -124,4 +124,20 @@
                      (dyn dyn dyn dyn)
                      (dyn dyn dyn lex))))))
 
+(defmacro macroexp--test-macro1 ()
+  (declare (obsolete "new-replacement" nil))
+  1)
+
+(defmacro macroexp--test-macro2 ()
+  '(macroexp--test-macro1))
+
+(ert-deftest macroexp--test-obsolete-macro ()
+  (should
+   (let ((res
+          (cl-letf (((symbol-function 'message) #'user-error))
+            (condition-case err
+                (macroexpand-all '(macroexp--test-macro2))
+              (user-error (error-message-string err))))))
+     (should (and (stringp res) (string-match "new-replacement" res))))))
+
 ;;; macroexp-tests.el ends here
