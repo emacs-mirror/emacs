191aef4f618408c1be9f57459eaaa91b955fec18
Moritz Maxeiner <mm@ucw.sh>
2023-07-22 16:55:07 +0200

    Implement new option 'mouse-prefer-closest-glyph'
    * src/dispnew.c (mouse_prefer_closest_glyph): New global
    variable.
    (buffer_posn_from_coords):
    * src/xdisp.c (remember_mouse_glyph): Respect
    'mouse_prefer_closest_glyph'.
    (mouse_fine_grained_tracking): Update documentation to
    include 'mouse_prefer_closest_glyph' effects.
    
    * doc/lispref/commands.texi (Accessing Mouse): Update
    documentation to say what the new option does when enabled.
    * lisp/cus-start.el (standard): New user option
    'mouse-prefer-closest-glyph'.

--- src/dispnew.c
+++ src/dispnew.c
@@ -5636,6 +5636,15 @@ buffer_posn_from_coords (struct window *w, int *x, int *y, struct display_pos *p
      argument is ZV to prevent move_it_in_display_line from matching
      based on buffer positions.  */
   move_it_in_display_line (&it, ZV, to_x, MOVE_TO_X);
+  if (mouse_prefer_closest_glyph)
+    {
+      int next_x = it.current_x + it.pixel_width;
+      int before_dx = to_x - it.current_x;
+      int after_dx = next_x - to_x;
+      if (before_dx > after_dx)
+        move_it_in_display_line (&it, ZV, next_x, MOVE_TO_X);
+    }
+
   bidi_unshelve_cache (itdata, 0);
 
   Fset_buffer (old_current_buffer);
@@ -6812,6 +6821,15 @@ predicates which report frame's specific UI-related capabilities.  */);
 
   DEFVAR_BOOL ("cursor-in-echo-area", cursor_in_echo_area,
 	       doc: /* Non-nil means put cursor in minibuffer, at end of any message there.  */);
+
+  DEFVAR_BOOL ("mouse-prefer-closest-glyph", mouse_prefer_closest_glyph,
+	       doc: /* Non-nil means mouse position lists are reported relative
+to the glyph closest to their coordinates.
+
+ When non-nil, mouse position lists will be reported with their
+`posn-point' set to the position of the glyph closest to the mouse
+pointer, instead of the glyph immediately under.  */);
+  mouse_prefer_closest_glyph = false;
 
   DEFVAR_LISP ("glyph-table", Vglyph_table,
 	       doc: /* Table defining how to output a glyph code to the frame.
