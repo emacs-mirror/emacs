de1effd73b4dfa8891e35905d20803b9e76c4a13
Michael Albinus <michael.albinus@gmx.de>
2023-08-06 15:46:58 +0200

    ; Fix last change

--- test/lisp/net/tramp-tests.el
+++ test/lisp/net/tramp-tests.el
@@ -2687,7 +2687,9 @@ This checks also `file-name-as-directory', `file-name-directory',
 	      (let ((bfcs buffer-file-coding-system))
 		(should (buffer-modified-p))
 		(should (null (save-buffer)))
-		(should (eq buffer-file-coding-system bfcs)))))
+		(should
+                 (eq (coding-system-get buffer-file-coding-system :mime-charset)
+                     (coding-system-get  bfcs :mime-charset))))))
 
 	;; Cleanup.
 	(ignore-errors (delete-file tmp-name))))))
