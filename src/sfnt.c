b7c194c1ead2ed5018b05482ef9e17a6a7b6f417
Po Lu <luangruo@yahoo.com>
2023-08-13 10:55:48 +0800

    ; Fix FSF address in some files' copyright notices
    * src/sfnt.c:
    * src/sfnt.h:
    * src/sfntfont-android.c:
    * src/sfntfont.c:
    * src/sfntfont.h: Replace the old FSF address with a link to
    gnu.org/licenses.  These addresses were inadvertently imported
    when sfnt.c was copied to src.

--- src/sfnt.c
+++ src/sfnt.c
@@ -15,8 +15,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with GNU Emacs.  If not, write to the Free Software Foundation,
-Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  */
+along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.  */
 
 #include <config.h>
 
