6412ba2f1ad93bf641b86ba7633beb4f6dda89ea
Stefan Kangas <stefankangas@gmail.com>
2023-08-14 01:59:10 +0200

    ; Delete commented out variable in png_load_body
    * src/image.c (png_load_body): Delete commented out variable.

--- src/image.c
+++ src/image.c
@@ -8212,7 +8212,6 @@ png_load_body (struct frame *f, struct image *img, struct png_load_context *c)
      simple transparency, we prefer a clipping mask.  */
   if (!transparent_p)
     {
-      /* png_color_16 *image_bg; */
       Lisp_Object specified_bg
 	= image_spec_value (img->spec, QCbackground, NULL);
       Emacs_Color color;
