89833430f1ff941df88a0453268e06d4904e171a
Stefan Kangas <stefankangas@gmail.com>
2023-08-19 12:25:25 +0200

    Remove Emacs 24 compat code from reftex
    * lisp/textmodes/reftex-index.el (reftex-display-index):
    * lisp/textmodes/reftex-toc.el (reftex-toc): Assume that we have
    'cursor-intangible-mode', which was added in Emacs 25.

--- lisp/textmodes/reftex-toc.el
+++ lisp/textmodes/reftex-toc.el
@@ -265,11 +265,7 @@ SPC=view TAB=goto RET=goto+hide [q]uit [r]escan [l]abels [f]ollow [x]r [?]Help
 
       (if (reftex-use-fonts)
           (put-text-property (point-min) (point) 'font-lock-face reftex-toc-header-face))
-      (if (fboundp 'cursor-intangible-mode)
-          (cursor-intangible-mode 1)
-        ;; If `cursor-intangible' is not available, fallback on the old
-        ;; intrusive `intangible' property.
-        (put-text-property (point-min) (point) 'intangible t))
+      (cursor-intangible-mode 1)
       (add-text-properties (point-min) (point)
                            '(cursor-intangible t
                              front-sticky (cursor-intangible)
