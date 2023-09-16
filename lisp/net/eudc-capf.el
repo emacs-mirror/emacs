9aab258b80af29865a89ef3e7615251bc250358e
Stefan Kangas <stefankangas@gmail.com>
2023-09-16 14:34:20 +0200

    Add missing builtin package declarations
    * lisp/finder.el (finder--builtins-alist): Add new package
    directories 'leim' and 'obsolete' as part of the 'emacs' package.
    Add new package directory 'use-package' as part of the
    'use-package' package.
    * lisp/net/eudc-capf.el:
    * lisp/net/eudcb-ecomplete.el:
    * lisp/net/eudcb-macos-contacts.el:
    * lisp/net/eudcb-mailabbrev.el: Declare library as part of the
    'eudc' package.
    * lisp/mail/ietf-drums-date.el: Declare library as part of the
    'ietf-drums' package.
    * lisp/image/image-dired-dired.el:
    * lisp/image/image-dired-external.el:
    * lisp/image/image-dired-tags.el:
    * lisp/image/image-dired-util.el: Declare library as part of the
    'image-dired' package.
    * lisp/emacs-lisp/oclosure.el:
    * lisp/keymap.el:
    * lisp/progmodes/c-ts-common.el: Declare library as part of the
    'emacs' package.  (Bug#62751)
    
    (cherry picked from commit 94b1de2774b5c1fa3c28285229900657638f5c3f)

--- lisp/net/eudc-capf.el
+++ lisp/net/eudc-capf.el
@@ -1,21 +1,22 @@
 ;;; eudc-capf.el --- EUDC - completion-at-point bindings  -*- lexical-binding:t -*-
 
 ;; Copyright (C) 2022-2023 Free Software Foundation, Inc.
-;;
+
 ;; Author: Alexander Adolf
-;;
+;; Package: eudc
+
 ;; This file is part of GNU Emacs.
-;;
+
 ;; GNU Emacs is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
-;;
+
 ;; GNU Emacs is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
-;;
+
 ;; You should have received a copy of the GNU General Public License
 ;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
 
