6f07ec1913dc2a1d5a0faa87d506f8fefb45ca35
Stefan Kangas <stefankangas@gmail.com>
2023-10-24 00:07:41 +0200

    ; Fix broken link

--- lisp/term/xterm.el
+++ lisp/term/xterm.el
@@ -725,7 +725,7 @@ Return the pasted text as a string."
         ;; `tty-set-up-initial-frame-faces' only once, but that
         ;; caused the light background faces to be computed
         ;; incorrectly.  See:
-        ;; http://permalink.gmane.org/gmane.emacs.devel/119627
+        ;; https://lists.gnu.org/r/emacs-devel/2010-01/msg00439.html
         (when recompute-faces
           (tty-set-up-initial-frame-faces))))))
 
