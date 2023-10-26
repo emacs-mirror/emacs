8c9e544501ae26ae328deefc05b5bf4874ead2d1
Ihor Radchenko <yantar92@posteo.net>
2023-10-26 14:52:32 +0300

    * src/pdumper.c (dump_buffer): Print message when aborting (bug#66743)
    When the buffer contains overlays, it cannot be dumped.  Print a
    clear message describing the reason, instead of just aborting.

--- src/pdumper.c
+++ src/pdumper.c
@@ -2862,8 +2862,10 @@ dump_buffer (struct dump_context *ctx, const struct buffer *in_buffer)
   DUMP_FIELD_COPY (out, buffer, long_line_optimizations_p);
 
   if (!itree_empty_p (buffer->overlays))
-    /* We haven't implemented the code to dump overlays.  */
-    emacs_abort ();
+    {
+      /* We haven't implemented the code to dump overlays.  */
+      error ("Dumping overlays in buffers is not yet implemented.  Aborting...");
+    }
   else
     out->overlays = NULL;
 
