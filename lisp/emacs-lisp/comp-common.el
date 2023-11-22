366c316a2f869f287f6c5d5e9b3819bac6625caf
Andrea Corallo <acorallo@gnu.org>
2023-11-22 16:37:16 +0100

    * Update 'native-comp-never-optimize-functions' version
    * lisp/emacs-lisp/comp-common.el (native-comp-never-optimize-functions):
    Update version.

--- lisp/emacs-lisp/comp-common.el
+++ lisp/emacs-lisp/comp-common.el
@@ -60,7 +60,7 @@ Primitive functions included in this list will not be called
 directly by the natively-compiled code, which makes trampolines for
 those primitives unnecessary in case of function redefinition/advice."
   :type '(repeat symbol)
-  :version "28.1")
+  :version "30.1")
 
 (defcustom native-comp-async-env-modifier-form nil
   "Form evaluated before compilation by each asynchronous compilation subprocess.
