6b6e770a1f55da0580706c7ec21a409437d19ab4
Niall Dooley <dooleyn@gmail.com>
2023-11-24 16:52:36 +0100

    Eglot: Add ruff-lsp as an alternative Python server
    ruff-lsp [1] is an LSP server for Ruff [2], [3], a fast Python linter
    and code formatter.
    
    It supports surfacing Ruff diagnostics and providing Code Actions to
    fix them, but is intended to be used alongside another Python LSP in
    order to support features like navigation and autocompletion.
    
    [1]: https://github.com/astral-sh/ruff-lsp
    [2]: https://github.com/astral-sh/ruff
    [3]: https://docs.astral.sh/ruff/
    
    * lisp/progmodes/eglot.el (eglot-server-programs): Add ruff-lsp.
    
    Copyright-paperwork-exempt: yes

--- lisp/progmodes/eglot.el
+++ lisp/progmodes/eglot.el
@@ -188,7 +188,7 @@ chosen (interactively or automatically)."
                                 (vimrc-mode . ("vim-language-server" "--stdio"))
                                 ((python-mode python-ts-mode)
                                  . ,(eglot-alternatives
-                                     '("pylsp" "pyls" ("pyright-langserver" "--stdio") "jedi-language-server")))
+                                     '("pylsp" "pyls" ("pyright-langserver" "--stdio") "jedi-language-server" "ruff-lsp")))
                                 ((js-json-mode json-mode json-ts-mode)
                                  . ,(eglot-alternatives '(("vscode-json-language-server" "--stdio")
                                                           ("vscode-json-languageserver" "--stdio")
