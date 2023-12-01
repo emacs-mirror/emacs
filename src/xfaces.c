3cbe263b081eaa40753e122e5b98d74d9d9d0c9f
Eli Zaretskii <eliz@gnu.org>
2023-12-01 14:15:30 +0200

    Fix mode-line-inactive face
    * src/xfaces.c (Finternal_set_lisp_face_attribute): Fix validation
    of the :style attribute of :box.  Previously, nil for :style was not
    accepted, which causes 'mode-line-inactive' face to be rejected.
    (Bug#67567)

--- src/xfaces.c
+++ src/xfaces.c
@@ -3388,8 +3388,10 @@ FRAME 0 means change the face on all frames, and change the default
 		}
 	      else if (EQ (k, QCstyle))
 		{
-		  if (!EQ (v, Qpressed_button) && !EQ (v, Qreleased_button)
-		      && !EQ(v, Qflat_button))
+		  if (!NILP (v)
+		      && !EQ (v, Qpressed_button)
+		      && !EQ (v, Qreleased_button)
+		      && !EQ (v, Qflat_button))
 		    break;
 		}
 	      else
