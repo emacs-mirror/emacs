emacs-mirror/emacs

commit f616edb4ccce5b9d60e3ff42806bd2131989cd1e

macroexp-parse-body: correct parsing of empty body (bug#66136)
* lisp/emacs-lisp/macroexp.el (macroexp-parse-body):
Return an empty body even when there are declarations present.
Previously, the last declaration was considered part of the body,
which is only correct if the input consists of a single string.

Reported by Jens Schmidt.

lisp/emacs-lisp/macroexp.el
@@ -525,12 +525,17 @@ definitions to shadow the loaded ones for use in file byte-compilation."
 (defun macroexp-parse-body (body)
   "Parse a function BODY into (DECLARATIONS . EXPS)."
   (let ((decls ()))
-    (while (and (cdr body)
-                (let ((e (car body)))
-                  (or (stringp e)
-                      (memq (car-safe e)
-                            '(:documentation declare interactive cl-declare)))))
-      (push (pop body) decls))
+    ;; If there is only a string literal with nothing following, we
+    ;; consider this to be part of the body (the return value) rather
+    ;; than a declaration at this point.
+    (unless (and (null (cdr body)) (stringp (car body)))
+      (while
+          (and body
+               (let ((e (car body)))
+                 (or (stringp e)
+                     (memq (car-safe e)
+                           '(:documentation declare interactive cl-declare)))))
+        (push (pop body) decls)))
     (cons (nreverse decls) body)))
 
 (defun macroexp-progn (exps)

commit 9c1f24d7a497ee8b9c1ec3f1161a3ed7d6e34bd0

* lisp/emacs-lisp/macroexp.el (macroexp-parse-body): Fix bug#67568
This fixes a regression introduced in commit f616edb4ccce.

lisp/emacs-lisp/macroexp.el
@@ -533,17 +533,18 @@ definitions to shadow the loaded ones for use in file byte-compilation."
 (defun macroexp-parse-body (body)
   "Parse a function BODY into (DECLARATIONS . EXPS)."
   (let ((decls ()))
-    ;; If there is only a string literal with nothing following, we
-    ;; consider this to be part of the body (the return value) rather
-    ;; than a declaration at this point.
-    (unless (and (null (cdr body)) (stringp (car body)))
-      (while
-          (and body
-               (let ((e (car body)))
-                 (or (stringp e)
-                     (memq (car-safe e)
-                           '(:documentation declare interactive cl-declare)))))
-        (push (pop body) decls)))
+    (while
+        (and body
+             (let ((e (car body)))
+               (or (and (stringp e)
+                        ;; If there is only a string literal with
+                        ;; nothing following, we consider this to be
+                        ;; part of the body (the return value) rather
+                        ;; than a declaration at this point.
+                        (cdr body))
+                   (memq (car-safe e)
+                         '(:documentation declare interactive cl-declare)))))
+      (push (pop body) decls))
     (cons (nreverse decls) body)))
 
 (defun macroexp-progn (exps)