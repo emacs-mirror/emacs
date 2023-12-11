9874561f39e62c1c9fada6c2e013f93d9ea65729
Yuan Fu <casouri@gmail.com>
2023-12-10 16:23:44 -0800

    Fix treesit-node-field-name and friends (bug#66674)
    So turns out ts_node_field_name_for_child takes a named node index,
    but we were passing it normal index that counts both named and
    anonymous nodes.  That's what makes the field name all wrong in
    treesit explorer.
    
    * doc/lispref/parsing.texi:
    (Accessing Node Information): Update docstring.
    * lisp/treesit.el (treesit-node-index): Add some unrelated comment.
    (treesit-node-field-name): Get named node index rather than all node
    index.
    * src/treesit.c (Ftreesit_node_field_name_for_child): Update
    docstring, use ts_node_named_child_count.

--- lisp/treesit.el
+++ lisp/treesit.el
@@ -360,6 +360,7 @@ If NAMED is non-nil, collect named child only."
   "Return the index of NODE in its parent.
 If NAMED is non-nil, count named child only."
   (let ((count 0))
+    ;; TODO: Use next-sibling as it's more efficient.
     (while (setq node (treesit-node-prev-sibling node named))
       (cl-incf count))
     count))
@@ -367,7 +368,7 @@ If NAMED is non-nil, count named child only."
 (defun treesit-node-field-name (node)
   "Return the field name of NODE as a child of its parent."
   (when-let ((parent (treesit-node-parent node))
-             (idx (treesit-node-index node)))
+             (idx (treesit-node-index node t)))
     (treesit-node-field-name-for-child parent idx)))
 
 ;;; Query API supplement
