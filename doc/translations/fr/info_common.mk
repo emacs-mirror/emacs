886307ec48e354e45d56298acc7035ad849d01b8
Vincent Belaïche <vincentb1@users.sourceforge.net>
2023-12-31 17:37:09 +0100

    Generate manual for other languages than default.
    * doc/translations/fr/info_common.mk:
    * doc/translations/default/info_common.mk: New file.
    
    * doc/misc/Makefile.in: Get INFO_COMMON from language respective
    info_common.mk helper script.
    (INFO_TARGETS): Make it depend from info_common.mk settings.
    (ORG_SETUP, ORG_SRC): Make it point at language specific sub-directory.
    (texi_sources, DVI_TARGETS, HTML_TARGETS, PDF_TARGETS)
    (PS_TARGETS, echo-info, info_template,efaq%$(lang_suffix).dvi)
    (efaq%$(lang_suffix).pdf, efaq%$(lang_suffix).texi)
    (efaq%$(lang_suffix).html): Make it use language specific suffix.
    (${buildinfodir}/%$(lang_suffix).info, %$(lang_suffix).dvi)
    (%$(lang_suffix).pdf, %$(lang_suffix).html, %$(lang_suffix).ps):
    Make it use language specific suffix/subdirectory.
    
    * Makefile.in (DOCLANGS): New variable, list of languages.
    (install-info, uninstall): List targets into 'info_misc' for all languages.
    (TAGS tags): Make tags for all languages.
    ($(DOCS)): Create target for all languages.
    (texi_misc_fr, texi_misc_default): Set macro for all languages.
    (srcdir_doc_info_dir_inputs): Macro concatenate texi_misc_lll for
    all language lll.

---
+++ doc/translations/fr/info_common.mk
@@ -0,0 +1,8 @@
+## Info files to build and install on all platforms (onl ses has been
+## translated to Frenchj)
+INFO_COMMON = ses
+
+## efac-w32 has not been translated to French
+DOCMISC_W32:=#
+
+DOCMISC_W32_TARGET:=#
