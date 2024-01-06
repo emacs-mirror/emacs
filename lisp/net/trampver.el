e48a396d4ba1694e083f900dda1f41cc41d00ead
Michael Albinus <michael.albinus@gmx.de>
2024-01-06 18:12:47 +0100

    Adapt Tramp version
    * doc/misc/trampver.texi:
    * lisp/net/trampver.el (tramp-version): Adapt Tramp versions.

--- lisp/net/trampver.el
+++ lisp/net/trampver.el
@@ -7,7 +7,7 @@
 ;; Maintainer: Michael Albinus <michael.albinus@gmx.de>
 ;; Keywords: comm, processes
 ;; Package: tramp
-;; Version: 2.7.0-pre
+;; Version: 2.7.0
 ;; Package-Requires: ((emacs "27.1"))
 ;; Package-Type: multi
 ;; URL: https://www.gnu.org/software/tramp/
@@ -40,7 +40,7 @@
 ;; ./configure" to change them.
 
 ;;;###tramp-autoload
-(defconst tramp-version "2.7.0-pre"
+(defconst tramp-version "2.7.0"
   "This version of Tramp.")
 
 ;;;###tramp-autoload
@@ -78,7 +78,7 @@
 ;; Check for Emacs version.
 (let ((x   (if (not (string-version-lessp emacs-version "27.1"))
       "ok"
-    (format "Tramp 2.7.0-pre is not fit for %s"
+    (format "Tramp 2.7.0 is not fit for %s"
             (replace-regexp-in-string "\n" "" (emacs-version))))))
   (unless (string-equal "ok" x) (error "%s" x)))
 
