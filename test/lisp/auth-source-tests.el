5df57f1792ee31fd3a00734dd754cc11bba9dd9c
Michael Albinus <michael.albinus@gmx.de>
2024-01-11 12:45:03 +0100

    Adapt test names in auth-source-tests.el
    * test/lisp/auth-source-tests.el (auth-source-test-netrc-credentials)
    (auth-source-test-netrc-credentials-2)
    (auth-source-test-macos-keychain-search): Adapt test names.

--- test/lisp/auth-source-tests.el
+++ test/lisp/auth-source-tests.el
@@ -411,7 +411,7 @@ machine c1 port c2 user c3 password c4\n"
       ;; this is actually the same as `auth-source-search'.
       (should (equal found expected)))))
 
-(ert-deftest test-netrc-credentials ()
+(ert-deftest auth-source-test-netrc-credentials ()
   (let ((data (auth-source-netrc-parse-all (ert-resource-file "authinfo"))))
     (should data)
     (let ((imap (seq-find (lambda (elem)
@@ -427,7 +427,7 @@ machine c1 port c2 user c3 password c4\n"
       (should (equal (cdr (assoc "login" imap)) "jrh"))
       (should (equal (cdr (assoc "password" imap)) "*baz*")))))
 
-(ert-deftest test-netrc-credentials-2 ()
+(ert-deftest auth-source-test-netrc-credentials-2 ()
   (let ((data (auth-source-netrc-parse-all
                (ert-resource-file "netrc-folding"))))
     (should
@@ -435,7 +435,7 @@ machine c1 port c2 user c3 password c4\n"
             '((("machine" . "XM") ("login" . "XL") ("password" . "XP"))
               (("machine" . "YM") ("login" . "YL") ("password" . "YP")))))))
 
-(ert-deftest test-macos-keychain-search ()
+(ert-deftest auth-source-test-macos-keychain-search ()
   "Test if the constructed command line arglist is correct."
   (let ((auth-sources '(macos-keychain-internet macos-keychain-generic)))
     ;; Redefine `call-process' to check command line arguments.
