7d869a04029798410773fe6071e76b556d3a9ee2
Stefan Kangas <stefankangas@gmail.com>
2024-01-14 14:47:39 +0100

    Doc fix in auth-source-read-char-choice
    * lisp/auth-source.el (auth-source-read-char-choice): Don't
    document 'dropdown-list', which was removed in 2011.

--- lisp/auth-source.el
+++ lisp/auth-source.el
@@ -330,7 +330,6 @@ If the value is not a list, symmetric encryption will be used."
 
 (defun auth-source-read-char-choice (prompt choices)
   "Read one of CHOICES by `read-char-choice', or `read-char'.
-`dropdown-list' support is disabled because it doesn't work reliably.
 Only one of CHOICES will be returned.  The PROMPT is augmented
 with \"[a/b/c] \" if CHOICES is \(?a ?b ?c)."
   (when choices
