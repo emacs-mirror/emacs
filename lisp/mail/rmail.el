314ac2e4317650a5135b950374118bbc38e8207f
Eli Zaretskii <eliz@gnu.org>
2024-01-18 08:29:34 +0200

    ; * lisp/mail/rmail.el (rmail-show-message-verbose-min): Doc fix (bug#68369).

--- lisp/mail/rmail.el
+++ lisp/mail/rmail.el
@@ -2684,7 +2684,9 @@ N defaults to the current message."
 	  (and (string-match text-regexp content-type-header) t)))))
 
 (defcustom rmail-show-message-verbose-min 200000
-  "Message size at which to show progress messages for displaying it."
+  "Message size at which to show progress messages for displaying it.
+Messages longer than this (in characters) will produce echo-area
+messages when Rmail processes such a message for display."
   :type 'integer
   :group 'rmail
   :version "23.1")
