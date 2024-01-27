fa7543eeb72342544d324a54010b6cb96c246733
Eli Zaretskii <eliz@gnu.org>
2024-01-27 11:44:54 +0200

    Minor fix in 'describe-language-environment'
    * lisp/international/mule-cmds.el (describe-language-environment):
    Use 'current-language-environment' as DEFAULT in the prompt.
    Patch by Thierry Volpiatto <thievol@posteo.net>.  (Bug#68602)

--- lisp/international/mule-cmds.el
+++ lisp/international/mule-cmds.el
@@ -2159,7 +2159,9 @@ See `set-language-info-alist' for use in programs."
   (interactive
    (list (read-language-name
 	  'documentation
-	  (format-prompt "Describe language environment" current-language-environment))))
+	  (format-prompt "Describe language environment"
+                         current-language-environment)
+          current-language-environment)))
   (let ((help-buffer-under-preparation t))
     (if (null language-name)
 	(setq language-name current-language-environment))
