e3620796ffc65c36697bced54988a1a383a4deeb
Daniel Brooks <db48x@db48x.net>
2024-01-28 00:17:50 -0800

    Fix 'calc-math-read-preprocess-string' test (bug#66944).
    Copyright-paperwork-exempt: yes

--- test/lisp/calc/calc-tests.el
+++ test/lisp/calc/calc-tests.el
@@ -861,7 +861,7 @@ An existing calc stack is reused, otherwise a new one is created."
   ;; exponent/subscript
   (should (string= (concat "+/-*:-/*inf<=>=<=>=μ(1:4)(1:2)(3:4)(1:3)(2:3)"
                            "(1:5)(2:5)(3:5)(4:5)(1:6)(5:6)"
-                           "(1:8)(3:8)(5:8)(7:8)1:^(0123456789+-()ni)"
+                           "(1:8)(3:8)(5:8)(7:8)1::^(0123456789+-()ni)"
                            "_(0123456789+-())")
                    (math-read-preprocess-string
                     (mapconcat #'car math-read-replacement-list))))
