56d0fbd99a87858717e08488df57db7fc08a2891
Stefan Kangas <stefankangas@gmail.com>
2024-02-04 10:28:18 +0100

    Add alias progress-reporter-make
    * lisp/subr.el (progress-reporter-make): New alias for
    'make-progress-reporter'.

--- lisp/subr.el
+++ lisp/subr.el
@@ -1,7 +1,6 @@
 ;;; subr.el --- basic lisp subroutines for Emacs  -*- lexical-binding:t -*-
 
-;; Copyright (C) 1985-1986, 1992, 1994-1995, 1999-2024 Free Software
-;; Foundation, Inc.
+;; Copyright (C) 1985-2024 Free Software Foundation, Inc.
 
 ;; Maintainer: emacs-devel@gnu.org
 ;; Keywords: internal
@@ -6735,6 +6734,8 @@ effectively rounded up."
     ;; Force a call to `message' now.
     (progress-reporter-update reporter (or current-value min-value))
     reporter))
+
+(defalias 'progress-reporter-make #'make-progress-reporter)
 
 (defun progress-reporter-force-update (reporter &optional value new-message suffix)
   "Report progress of an operation in the echo area unconditionally.
