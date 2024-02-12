2f7d662dd4636a84e157a2af8f843c0589bc5dda
Basil L. Contovounesios <basil@contovou.net>
2024-02-12 12:07:37 +0100

    ; Update Lisp_Hash_Table hash for CHECK_STRUCTS
    This follows commit 05e3183ede of 2024-02-06
    "Rearrange and pack hash table fields to reduce space".

--- src/pdumper.c
+++ src/pdumper.c
@@ -2719,7 +2719,7 @@ dump_hash_table_contents (struct dump_context *ctx, struct Lisp_Hash_Table *h)
 static dump_off
 dump_hash_table (struct dump_context *ctx, Lisp_Object object)
 {
-#if CHECK_STRUCTS && !defined HASH_Lisp_Hash_Table_313A489F0A
+#if CHECK_STRUCTS && !defined HASH_Lisp_Hash_Table_0360833954
 # error "Lisp_Hash_Table changed. See CHECK_STRUCTS comment in config.h."
 #endif
   const struct Lisp_Hash_Table *hash_in = XHASH_TABLE (object);
