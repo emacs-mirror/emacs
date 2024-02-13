18457c64b064e3853511d20a6d1d5d3c0f136733
Philip Kaludercic <philipk@posteo.net>
2024-02-13 10:52:24 +0100

    ; * .dir-locals.el: Set 'vc-default-patch-addressee'

--- .dir-locals.el
+++ .dir-locals.el
@@ -14,7 +14,8 @@
             ("/[ \t]*DEFVAR_[A-Z_ \t(]+\"\\([^\"]+\\)\"/\\1/"
              "/[ \t]*DEFVAR_[A-Z_ \t(]+\"[^\"]+\",[ \t]\\([A-Za-z0-9_]+\\)/\\1/"))))
          (etags-regen-ignores . ("test/manual/etags/"))
-         (vc-prepare-patches-separately . nil)))
+         (vc-prepare-patches-separately . nil)
+         (vc-default-patch-addressee . "bug-gnu-emacs@gnu.org")))
  (c-mode . ((c-file-style . "GNU")
             (c-noise-macro-names . ("INLINE" "NO_INLINE" "ATTRIBUTE_NO_SANITIZE_UNDEFINED"
                                     "UNINIT" "CALLBACK" "ALIGN_STACK" "ATTRIBUTE_MALLOC"
