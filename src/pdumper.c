42c6cf4e5804312defa9d9caac8882500bd38179
Paul Eggert <eggert@cs.ucla.edu>
2024-02-17 23:38:30 -0800

    Remove no-longer-needed pdumper_load workaround
    * src/pdumper.c (pdumper_load): Revert my commit "Pacify GCC
    12.1.1 in default developer build" dated 2022-06-13 13:21:18 -07,
    as GCC bug 105961 is fixed, and this workaround is not needed for
    unfixed GCC as these builds should not use --enable-gcc-warnings.

--- src/pdumper.c
+++ src/pdumper.c
@@ -5593,10 +5593,7 @@ pdumper_load (const char *dump_filename, char *argv0)
 
   struct dump_header header_buf = { 0 };
   struct dump_header *header = &header_buf;
-  struct dump_memory_map sections[NUMBER_DUMP_SECTIONS];
-
-  /* Use memset instead of "= { 0 }" to work around GCC bug 105961.  */
-  memset (sections, 0, sizeof sections);
+  struct dump_memory_map sections[NUMBER_DUMP_SECTIONS] = { 0 };
 
   const struct timespec start_time = current_timespec ();
   char *dump_filename_copy;
