0530800175913769cb55ae7997ee4487a755a0a4
Eli Zaretskii <eliz@gnu.org>
2024-02-24 15:12:57 +0200

    Fix infinite recursion in gdb-mi.el
    * lisp/progmodes/gdb-mi.el: (gdb-clear-partial-output)
    (gdb-clear-inferior-io):  Set inhibit-read-only, to avoid
    signaling errors in process filter.  (Bug#69327)

--- lisp/progmodes/gdb-mi.el
+++ lisp/progmodes/gdb-mi.el
@@ -1849,7 +1849,8 @@ this trigger is subscribed to `gdb-buf-publisher' and called with
 
 (defun gdb-clear-inferior-io ()
   (with-current-buffer (gdb-get-buffer-create 'gdb-inferior-io)
-    (erase-buffer)))
+    (let ((inhibit-read-only t))
+      (erase-buffer))))
 
 
 (defconst breakpoint-xpm-data
@@ -2819,7 +2820,8 @@ current thread and update GDB buffers."
 
 (defun gdb-clear-partial-output ()
   (with-current-buffer (gdb-get-buffer-create 'gdb-partial-output-buffer)
-    (erase-buffer)))
+    (let ((inhibit-read-only t))
+      (erase-buffer))))
 
 ;; Parse GDB/MI result records: this process converts
 ;;  list      [...]      ->  list
