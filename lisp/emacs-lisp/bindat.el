7231a89524f280c51278c3c74c6ae2215a307f0f
Stefan Monnier <monnier@iro.umontreal.ca>
2024-03-15 12:45:09 -0400

    * lisp/emacs-lisp/bindat.el (sint): Burp in dynbind (bug#69749)

--- lisp/emacs-lisp/bindat.el
+++ lisp/emacs-lisp/bindat.el
@@ -944,9 +944,13 @@ a bindat type expression."
 (bindat-defmacro sint (bitlen le)
   "Signed integer of size BITLEN.
 Big-endian if LE is nil and little-endian if not."
+  (unless lexical-binding
+    (error "The `sint' type requires 'lexical-binding'"))
   (let ((bl (make-symbol "bitlen"))
         (max (make-symbol "max"))
         (wrap (make-symbol "wrap")))
+    ;; FIXME: This `let*' around the `struct' results in code which the
+    ;; byte-compiler does not handle efficiently. 🙁
     `(let* ((,bl ,bitlen)
             (,max (ash 1 (1- ,bl)))
             (,wrap (+ ,max ,max)))
