77a86d738eebc7a80b7d4a6357a5fa675df9de8c
Dmitry Gutov <dmitry@gutov.dev>
2024-03-15 04:28:45 +0200

    (project--value-in-dir): Ensure that the global value is still honored
    * lisp/progmodes/project.el (project--value-in-dir):
    Ensure that the global value of the variable is still honored,
    when the variable is not in dir-locals.

--- lisp/progmodes/project.el
+++ lisp/progmodes/project.el
@@ -810,7 +810,8 @@ DIRS must contain directory names."
     (let ((enable-local-variables :all))
       (hack-dir-local-variables))
     ;; Don't use `hack-local-variables-apply' to avoid setting modes.
-    (alist-get var file-local-variables-alist)))
+    (alist-get var file-local-variables-alist
+               (symbol-value var))))
 
 (cl-defmethod project-buffers ((project (head vc)))
   (let* ((root (expand-file-name (file-name-as-directory (project-root project))))
