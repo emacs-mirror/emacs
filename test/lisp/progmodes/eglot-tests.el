94ed2df02fa1841095041c8c26ad243052638e22
Michael Albinus <michael.albinus@gmx.de>
2024-04-23 17:08:06 +0200

    Revert skip on eglot-tests.el
    * test/lisp/progmodes/eglot-tests.el (eglot-test-basic-symlink):
    Comment out skip on EMBA.

--- test/lisp/progmodes/eglot-tests.el
+++ test/lisp/progmodes/eglot-tests.el
@@ -441,7 +441,7 @@ directory hierarchy."
   (skip-unless (executable-find "clangd"))
   ;; This test is said to fail due to bug#70408.  So we skip it at least
   ;; on EMBA, because it poisons the test jobs.
-  (skip-when (getenv "EMACS_EMBA_CI"))
+  ;(skip-when (getenv "EMACS_EMBA_CI"))
   ;; MS-Windows either fails symlink creation or pops up UAC prompts.
   (skip-when (eq system-type 'windows-nt))
   (eglot--with-fixture
