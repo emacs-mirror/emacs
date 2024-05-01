f804a492bf890b53d704809bd8cf6306f9c17d76
Basil L. Contovounesios <basil@contovou.net>
2024-05-01 13:07:17 +0200

    ; Update pvec_type hash for CHECK_STRUCTS
    This follows commit 2fa839c1886 of 2024-04-28
    "(COMPILED): Rename to CLOSURE".

--- src/pdumper.c
+++ src/pdumper.c
@@ -3050,7 +3050,7 @@ dump_vectorlike (struct dump_context *ctx,
                  Lisp_Object lv,
                  dump_off offset)
 {
-#if CHECK_STRUCTS && !defined HASH_pvec_type_2D583AC566
+#if CHECK_STRUCTS && !defined HASH_pvec_type_99104541E2
 # error "pvec_type changed. See CHECK_STRUCTS comment in config.h."
 #endif
   const struct Lisp_Vector *v = XVECTOR (lv);
