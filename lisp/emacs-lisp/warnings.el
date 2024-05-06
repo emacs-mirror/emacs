446031722546513ace2dc6f3ee86999bb7e17b39
Juri Linkov <juri@linkov.net>
2024-05-06 20:00:23 +0300

    * lisp/emacs-lisp/warnings.el (display-warning): Add guardrails for window.
    Check for non-nil 'window' returned from 'display-buffer' for the case when
    users decide to use 'allow-no-window' in 'display-buffer-alist' (#70795).

--- lisp/emacs-lisp/warnings.el
+++ lisp/emacs-lisp/warnings.el
@@ -377,10 +377,10 @@ entirely by setting `warning-suppress-types' or
 					(window-height . (lambda (window)
 					  (fit-window-to-buffer window 10)))
 					(category . warning))))))
-		       (when (and (markerp warning-series)
+		       (when (and window (markerp warning-series)
 				  (eq (marker-buffer warning-series) buffer))
 			 (set-window-start window warning-series))
-		       (when warning-display-at-bottom
+		       (when (and window warning-display-at-bottom)
 			 (with-selected-window window
 			   (goto-char (point-max))
 			   (forward-line -1)
