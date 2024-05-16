6ee9e1f16520e880467540b15c6c159bc7ca73ae
Eli Zaretskii <eliz@gnu.org>
2024-05-16 11:30:35 +0300

    ; * src/igc.c (mirror_glyph_matrix): Fix a typo that broke the build.

--- src/igc.c
+++ src/igc.c
@@ -3569,7 +3569,7 @@ mirror_buffer (struct igc_mirror *m, struct buffer *b)
 }
 
 static void
-mirror_göyph_matrix (struct igc_mirror *m, struct glyph_matrix *g)
+mirror_glyph_matrix (struct igc_mirror *m, struct glyph_matrix *g)
 {
   emacs_abort ();
 }
