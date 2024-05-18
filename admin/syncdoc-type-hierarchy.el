bbb3038ad9feb1f37e4bef7151b2bc6f18c4123d
Andrea Corallo <acorallo@gnu.org>
2024-05-18 22:17:58 +0200

    * admin/syncdoc-type-hierarchy.el (syncdoc-make-type-table): Clean-up quote.

--- admin/syncdoc-type-hierarchy.el
+++ admin/syncdoc-type-hierarchy.el
@@ -110,7 +110,7 @@
                            do (cl-incf x (1+ child-len)) )
                do (insert "\n")))
     (require 'org-table)
-    (declare-function 'org-table-align "org")
+    (declare-function org-table-align "org")
     (org-table-align)))
 
 (defun syncdoc-update-type-hierarchy0 ()
