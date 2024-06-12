5fc75b051fc623a4f614a7abe09f08d127ed9ccd
Po Lu <luangruo@yahoo.com>
2024-06-12 13:52:33 +0800

    Improve compression of Emacs packages on Android 2.2
    * configure.ac (ZIP): Search for a `zip' executable and
    substitute the same if targeting SDK 8.
    
    * java/INSTALL: Document new requirement.
    
    * java/Makefile.in (GZIP_PROG, ZIP): New substitutions.
    (.PHONY): Delete references to directory-tree.
    
    (install_temp): On SDK 8 and earlier, compress large *.el and
    Info files with gzip, and divide install_temp into three
    separate directories, with one for large files, another for
    gzipped files, and one more for the remainder.
    (install_temp/assets/directory-tree, install_temp/assets/version)
    (install_temp/assets/build_info): Merge these rules into
    install_tmp, that they may execute before the assets directory
    is split.
    (emacs.apk-in): Package each directory separately, accounting
    for their varied compression requirements.
    
    * lisp/term/android-win.el (input-decode-map): Define select to
    return in input-decode-map.

--- lisp/term/android-win.el
+++ lisp/term/android-win.el
@@ -627,6 +627,9 @@ accessible to other programs."
 (global-set-key [\83] #'ignore) ; KEYCODE_NOTIFICATION on pre-Honeycomb
                                 ; releases.
 
+;; `select' key, which I suppose amounts to return.
+(define-key input-decode-map [select] [return])
+
 
 (provide 'android-win)
 ;; android-win.el ends here.
