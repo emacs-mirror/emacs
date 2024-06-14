1079d333ab7cb24b81e7ebc4087d2a11baa0097e
Arsen Arsenović <arsen@aarsen.me>
2024-06-15 02:18:56 +0300

    typescript/tsx-ts-mode: Indentation for type parameters
    * lisp/progmodes/typescript-ts-mode.el
    (typescript-ts-mode--indent-rules): Add rule for type_parameters.

--- lisp/progmodes/typescript-ts-mode.el
+++ lisp/progmodes/typescript-ts-mode.el
@@ -125,6 +125,7 @@ Argument LANGUAGE is either `typescript' or `tsx'."
      ((parent-is "switch_case") parent-bol typescript-ts-mode-indent-offset)
      ((parent-is "switch_default") parent-bol typescript-ts-mode-indent-offset)
      ((parent-is "type_arguments") parent-bol typescript-ts-mode-indent-offset)
+     ((parent-is "type_parameters") parent-bol typescript-ts-mode-indent-offset)
      ((parent-is ,(rx (or "variable" "lexical") "_" (or "declaration" "declarator")))
       typescript-ts-mode--anchor-decl 1)
      ((parent-is "arguments") parent-bol typescript-ts-mode-indent-offset)
