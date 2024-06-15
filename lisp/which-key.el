4b2781f16d72985dc8ba5d14cd6821c8f1cd1439
Philip Kaludercic <philipk@posteo.net>
2024-06-15 14:48:58 +0200

    * lisp/which-key.el: Clean up file header

--- lisp/which-key.el
+++ lisp/which-key.el
@@ -5,8 +5,9 @@
 ;; Author: Justin Burkett <justin@burkett.cc>
 ;; Maintainer: Justin Burkett <justin@burkett.cc>
 ;; Version: 3.6.0
-;; Keywords:
 ;; Package-Requires: ((emacs "24.4"))
+
+;; This file is part of GNU Emacs.
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
@@ -23,16 +24,15 @@
 
 ;;; Commentary:
 
-;; which-key provides the minor mode which-key-mode for Emacs.  The mode displays
-;; the key bindings following your currently entered incomplete command (a
-;; prefix) in a popup.  For example, after enabling the minor mode if you enter
-;; C-x and wait for the default of 1 second the minibuffer will expand with all
-;; of the available key bindings that follow C-x (or as many as space allows
-;; given your settings).  This includes prefixes like C-x 8 which are shown in a
-;; different face.  Screenshots of what the popup will look like along with
-;; information about additional features can be found at
-;; https://github.com/justbur/emacs-which-key.
+;; The `which-key' mode displays the key bindings following your
+;; currently entered incomplete command (a prefix) in a popup.  For
+;; example, after enabling the minor mode if you enter C-x and wait for
+;; the default of 1 second the minibuffer will expand with all of the
+;; available key bindings that follow C-x (or as many as space allows
+;; given your settings).
 ;;
+;; This includes prefixes like C-x 8 which are shown in a different
+;; face.
 
 ;;; Code:
 
