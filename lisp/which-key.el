emacs-mirror/emacs

commit 768e92b9c0214a2aa1be2afbee48c455583d3110

Update options that depend on 'which-key-dont-use-unicode'
* lisp/which-key.el (which-key-dont-use-unicode): Add a custom setter
that re-evaluates a manual list of options use
'which-key-dont-use-unicode' to determine their default value.

https://lists.gnu.org/archive/html/help-gnu-emacs/2024-06/msg00130.html

lisp/which-key.el
@@ -128,6 +128,11 @@ of the which-key popup."
   "If non-nil, don't use any unicode characters in default setup.
 For affected settings, see `which-key-replacement-alist', `which-key-ellipsis'
 `which-key-separator'."
+  :set (lambda (sym val)
+         (custom-set-default sym val)
+         (mapc #'custom-reevaluate-setting
+               '(which-key-separator
+                 which-key-ellipsis)))
   :type 'boolean
   :package-version "1.0" :version "30.1")
 

commit 6ad6507532bc653c13531112e9a5467e4370929a

; * lisp/which-key.el (which-key-dont-use-unicode): Add :initialize
This fixes the change from 768e92b9, which accidentally would call
'custom-reevaluate-setting' on user options before they were registered,
initialising their default value to nil.  When the 'defcustom'
expressions were evaluated later on, they were not properly initialised
as their variables were already bound to some value (nil).

https://lists.gnu.org/archive/html/emacs-devel/2024-06/msg00825.html

lisp/which-key.el
@@ -133,6 +133,7 @@ For affected settings, see `which-key-replacement-alist', `which-key-ellipsis'
          (mapc #'custom-reevaluate-setting
                '(which-key-separator
                  which-key-ellipsis)))
+  :initialize #'custom-initialize-changed
   :type 'boolean
   :package-version "1.0" :version "30.1")
 