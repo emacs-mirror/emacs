84552ff395c133bebfa4b542485317236230f1f5
Stefan Kangas <stefankangas@gmail.com>
2024-07-08 12:15:05 +0200

    Revert "Fix link to major mode variable in docstring"
    This reverts commit 73c1252bb6b7cc61d9f992818568d3c57de4ff67.
    
    This will sometimes say
    "the variable `(default-value 'global-auto-revert-mode)'".
    Problem reported by Eshel Yaron <me@eshelyaron.com>.

--- lisp/emacs-lisp/easy-mmode.el
+++ lisp/emacs-lisp/easy-mmode.el
@@ -91,7 +91,7 @@ Enable the mode if ARG is nil, omitted, or is a positive number.
 Disable the mode if ARG is a negative number.
 
 To check whether the minor mode is enabled in the current buffer,
-evaluate the variable `%s'.
+evaluate `%s'.
 
 The mode's hook is called both when the mode is enabled and when
 it is disabled.")
