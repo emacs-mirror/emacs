emacs-mirror/emacs

commit bd86a6c4fde1aa42ea9e8b5434f0cb63f8d52684

perl-mode.el: Improve indent after hanging open-paren
Make the indentation of the code right after an open paren/brace
use the same rule as already used to indent the corresponding
matching paren/brace.

* lisp/progmodes/perl-mode.el (perl-calculate-indent):
Use `perl-indent-new-calculate`s "virtual" indentation instead of
approximating it with `current-indentation`.

lisp/progmodes/perl-mode.el
@@ -1130,14 +1130,7 @@ Returns (parse-state) if line starts inside a string."
             ;; add the perl-brace-imaginary-offset.
             (progn (skip-chars-backward " \t")
                    (if (bolp) 0 perl-brace-imaginary-offset))
-            ;; If the openbrace is preceded by a parenthesized exp,
-            ;; move to the beginning of that;
-            ;; possibly a different line
-            (progn
-              (if (eq (preceding-char) ?\))
-                  (forward-sexp -1))
-              ;; Get initial indentation of the line we are on.
-              (current-indentation)))))))))
+            (perl-indent-new-calculate 'virtual))))))))
 
 (defun perl-backward-to-noncomment ()
   "Move point backward to after the first non-white-space, skipping comments."

test/manual/indent/perl.perl
@@ -5,6 +5,10 @@ sub add_funds($) {
     return 0;
 }
 
+find ({ wanted => sub {
+            return;
+        }, follow => 1 }, '/tmp');
+
 # qw(...) is a quoted list of words, so we can and should indent its content!
 my @tutu = qw[
     tata

commit dd8d5d57df51bcd066323d068b852e9373871584

perl-mode: fix bug introduced in bd86a6c4fde
* lisp/progmodes/perl-mode.el (perl-calculate-indent): Use
`save-excursion' around code that modifies point.

lisp/progmodes/perl-mode.el
@@ -1128,8 +1128,8 @@ Returns (parse-state) if line starts inside a string."
             ;; Move back over whitespace before the openbrace.
             ;; If openbrace is not first nonwhite thing on the line,
             ;; add the perl-brace-imaginary-offset.
-            (progn (skip-chars-backward " \t")
-                   (if (bolp) 0 perl-brace-imaginary-offset))
+            (save-excursion (skip-chars-backward " \t")
+                            (if (bolp) 0 perl-brace-imaginary-offset))
             (perl-indent-new-calculate 'virtual))))))))
 
 (defun perl-backward-to-noncomment ()