15afa72460b4a0ec910749646cb9852b4c578f5e
Eli Zaretskii <eliz@gnu.org>
2024-08-03 10:29:38 +0300

    Fix 'script-representative-chars' for the 'han' script
    * lisp/international/fontset.el (script-representative-chars):
    Remove from 'han' codepoints that belong to 'cjk-misc'.

--- lisp/international/fontset.el
+++ lisp/international/fontset.el
@@ -208,8 +208,7 @@
 	(kana #x304B)
 	(bopomofo #x3105)
 	(kanbun #x319D)
-	(han #x2e90 #x2f00 #x3010 #x3200 #x3300 #x3400 #x31c0 #x4e10
-             #x5B57 #xfe30 #xf900)
+	(han #x2e90 #x2f00 #x3200 #x3300 #x3400 #x4e10 #x5B57 #xfe30 #xf900)
 	(yi #xA288)
         (syloti-nagri #xA807 #xA823 #xA82C)
         (rejang #xA930 #xA947 #xA95F)
