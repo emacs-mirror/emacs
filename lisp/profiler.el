d6726e6dfc7f0a5133a0489bcdc21f10844692f2
Stefan Monnier <monnier@iro.umontreal.ca>
2024-08-17 13:35:07 -0400

    Further fix of reading and writing profiler data
    * lisp/profiler.el (profiler-report-make-entry-part): Print
    strings as-is.  (Bug#72559)

--- lisp/profiler.el
+++ lisp/profiler.el
@@ -452,6 +452,11 @@ Do not touch this variable directly.")
   (let ((string (cond
 		 ((eq entry t)
 		  "Others")
+		 ;; When we save profile data into a file, the function
+                 ;; objects are replaced with their "names".  When we see
+                 ;; a string here, that's presumably why, so just print
+                 ;; it as-is.
+		 ((stringp entry) entry)
 		 (t (propertize (help-fns-function-name entry)
 		                ;; Override the `button-map' which
 		                ;; otherwise adds RET, mouse-1, and TAB
