cfcba7ddc41b84f108809bdd8d7201c181fd62ab
João Távora <joaotavora@gmail.com>
2024-08-22 17:27:20 +0100

    Eglot: bump version to 1.17.30 and update EGLOT-NEWS
    This is a change specific to emacs-30.  Don't merge to master.
    
    * lisp/progmodes/eglot.el (Version): Mark it 1.17.30.
    
    * etc/EGLOT-NEWS (1.17.30): Fill in section.

--- lisp/progmodes/eglot.el
+++ lisp/progmodes/eglot.el
@@ -2,7 +2,7 @@
 
 ;; Copyright (C) 2018-2024 Free Software Foundation, Inc.
 
-;; Version: 1.17
+;; Version: 1.17.30
 ;; Author: João Távora <joaotavora@gmail.com>
 ;; Maintainer: João Távora <joaotavora@gmail.com>
 ;; URL: https://github.com/joaotavora/eglot
