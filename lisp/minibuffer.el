3cda1fdc3b755dd329617cabc266e2b86894d0cb
Spencer Baugh <sbaugh@janestreet.com>
2024-08-26 10:12:51 -0400

    Correctly include fixed strings before a prefix wildcard in PCM
    In 03ac16ece40ba3e3ba805d6a61cc457d84bf3792 I fixed a bug with the
    PCM implementation of substring completion, relating to the handling
    of PCM wildcards.
    However, this fix was incomplete.  This change completes the fix by
    also including a fixed string if it appears before a 'prefix'
    wildcard, even if 'try-completion' doesn't discover that fixed
    string grows to a unique completion.
    I discovered this bug while working on enhancements to PCM
    completion related to 'completion-pcm-leading-wildcard'.
    * lisp/minibuffer.el (completion-pcm--merge-completions): Include
    fixed strings before 'prefix wildcard.  (Bug#72819)
    * test/lisp/minibuffer-tests.el (completion-substring-test-5): Add a
    test for this behavior.

--- lisp/minibuffer.el
+++ lisp/minibuffer.el
@@ -4384,18 +4384,21 @@ the same set of elements."
                      (unique (or (and (eq prefix t) (setq prefix fixed))
                                  (and (stringp prefix)
                                       (eq t (try-completion prefix comps))))))
-                ;; if the common prefix is unique, it also is a common
-                ;; suffix, so we should add it for `prefix' elements
-                (unless (or (and (eq elem 'prefix) (not unique))
-                            (equal prefix ""))
-                  (push prefix res))
                 ;; If there's only one completion, `elem' is not useful
                 ;; any more: it can only match the empty string.
                 ;; FIXME: in some cases, it may be necessary to turn an
                 ;; `any' into a `star' because the surrounding context has
                 ;; changed such that string->pattern wouldn't add an `any'
                 ;; here any more.
-                (unless unique
+                (if unique
+                    ;; if the common prefix is unique, it also is a common
+                    ;; suffix, so we should add it for `prefix' elements
+                    (push prefix res)
+                  ;; `prefix' only wants to include the fixed part before the
+                  ;; wildcard, not the result of growing that fixed part.
+                  (when (eq elem 'prefix)
+                    (setq prefix fixed))
+                  (push prefix res)
                   (push elem res)
                   ;; Extract common suffix additionally to common prefix.
                   ;; Don't do it for `any' since it could lead to a merged
