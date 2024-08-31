6c15b7710d4bfc201afbafd2daec74c2baec9102
Aaron Jensen <aaronjensen@gmail.com>
2024-08-31 19:31:20 -0400

    Add new option ruby-bracketed-args-indent
    * lisp/progmodes/ruby-mode.el (ruby-bracketed-args-indent): New option.
    (ruby-smie-rules): Use it (bug#60321).
    * test/lisp/progmodes/ruby-mode-resources/ruby-bracketed-args-indent.rb:
    New file.
    * test/lisp/progmodes/ruby-mode-tests.el: Use it for new case.

---
+++ test/lisp/progmodes/ruby-mode-resources/ruby-bracketed-args-indent.rb
@@ -0,0 +1,32 @@
+update({
+  key => value,
+  other_key:
+}, {
+  key => value,
+  other_key:
+})
+
+update([
+  1,
+  2
+], [
+  3,
+  4
+])
+
+update([{
+  key: "value"
+}, {
+  key: "value"
+}])
+
+update(arg1, {
+  foo: "bar"
+}, [
+  1,
+  2
+], arg2)
+
+# Local Variables:
+# ruby-bracketed-args-indent: nil
+# End:
