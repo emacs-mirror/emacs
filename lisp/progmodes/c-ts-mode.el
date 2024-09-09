b0523dffabbec8acd9c5c92711af849a11867884
Vincenzo Pupillo <v.pupillo@gmail.com>
2024-09-09 09:20:42 +0200

    Add user option to enable Doxygen syntax highlighting (bug#72814)
    Both 'c-ts-mode' and 'java-ts-mode' have a new user option,
    'c-ts-mode-enable-doxygen' and 'java-ts-mode-enable-doxygen'
    (defaults to nil) which allow to enable syntax highlighting
    of comment blocks based on the Doxygen grammar.
    
    * lisp/progmodes/c-ts-mode.el: Add the 'c-ts-mode-enable-doxygen'
    user option variable to disable doxygen grammar.
    Notifies the user if doxygen grammar is not available.
    * lisp/progmodes/c-ts-mode.el (c-ts-mode, c++-ts-mode): Use the
    new 'c-ts-mode-enable-doxygen' option.
    * lisp/progmodes/java-ts-mode.el: Add the
    'java-ts-mode-enable-doxygen' user option variable to disable
    doxygen grammar.  Notifies the user if doxygen grammar is not
    available.
    * lisp/progmodes/java-ts-mode.el (java-ts-mode): Use the new
    'java-ts-mode-enable-doxygen' option.
    
    * etc/NEWS: Document the change.

--- lisp/progmodes/c-ts-mode.el
+++ lisp/progmodes/c-ts-mode.el
@@ -65,7 +65,7 @@
 ;; libraries installed.
 ;;
 ;; If the tree-sitter doxygen grammar is available, then the comment
-;; blocks will be highlighted according to this grammar.
+;; blocks can be highlighted according to this grammar.
 
 ;;; Code:
 
@@ -212,6 +212,17 @@ This needs to be set before enabling `c-ts-mode'; if you change
 the value after enabling `c-ts-mode', toggle the mode off and on
 again."
   :version "29.1"
+  :type 'boolean
+  :safe 'booleanp
+  :group 'c)
+
+(defcustom c-ts-mode-enable-doxygen nil
+  "Enable doxygen syntax highlighting.
+If Non-nil, enable doxygen based font lock for comment blocks.
+This needs to be set before enabling `c-ts-mode'; if you change
+the value after enabling `c-ts-mode', toggle the mode off and on
+again."
+  :version "31.1"
   :type 'boolean
   :safe 'booleanp
   :group 'c)
@@ -1365,7 +1376,7 @@ in your init files."
         (treesit-font-lock-recompute-features '(emacs-devel)))
 
       ;; Inject doxygen parser for comment.
-      (when (treesit-ready-p 'doxygen t)
+      (when (and c-ts-mode-enable-doxygen (treesit-ready-p 'doxygen t))
         (setq-local treesit-primary-parser primary-parser)
         (setq-local treesit-font-lock-settings
                     (append
@@ -1426,7 +1437,7 @@ recommended to enable `electric-pair-mode' with this mode."
                     #'c-ts-mode--emacs-current-defun-name))
 
       ;; Inject doxygen parser for comment.
-      (when (treesit-ready-p 'doxygen t)
+      (when (and c-ts-mode-enable-doxygen (treesit-ready-p 'doxygen t))
         (setq-local treesit-primary-parser primary-parser)
         (setq-local treesit-font-lock-settings
                     (append
@@ -1537,6 +1548,9 @@ the code is C or C++, and based on that chooses whether to enable
 (when (and (treesit-ready-p 'cpp)
            (treesit-ready-p 'c))
   (add-to-list 'major-mode-remap-defaults '(c-or-c++-mode . c-or-c++-ts-mode)))
+
+(when (and c-ts-mode-enable-doxygen (not (treesit-ready-p 'doxygen t)))
+  (message "Doxygen syntax highlighting can't be enabled, please install the language grammar."))
 
 (provide 'c-ts-mode)
 (provide 'c++-ts-mode)
