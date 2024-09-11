ee3e3a6311196129104881d6e9097bb54d8843af
Po Lu <luangruo@yahoo.com>
2024-09-11 12:53:29 +0800

    ; Update version number of exec/configure.ac
    * admin/admin.el (set-version): Update AC_INIT expansion in
    exec/configure.ac.
    
    * exec/configure.ac: Update version to 30.0.90.

--- admin/admin.el
+++ admin/admin.el
@@ -112,6 +112,10 @@ Root must be the root of an Emacs source tree."
 		       (rx (and "AC_INIT" (1+ (not (in ?,)))
                                 ?, (0+ space) ?\[
                                 (submatch (1+ (in "0-9."))))))
+  (set-version-in-file root "exec/configure.ac" version
+		       (rx (and "AC_INIT" (1+ (not (in ?,)))
+                                ?, (0+ space) ?\[
+                                (submatch (1+ (in "0-9."))))))
   (set-version-in-file root "nt/README.W32" version
 		       (rx (and "version" (1+ space)
 				(submatch (1+ (in "0-9."))))))
