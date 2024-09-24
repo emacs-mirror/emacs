a06a7209028363a1bb2f727ffaecdf4d02296b2e
Robert Pluim <rpluim@gmail.com>
2024-09-24 10:52:35 +0200

    ; Really disable 'button-mode' in 'emacs-news-mode'
    * lisp/textmodes/emacs-news-mode.el (emacs-news-mode): Call
    'button-mode' with -1 arg.

--- lisp/textmodes/emacs-news-mode.el
+++ lisp/textmodes/emacs-news-mode.el
@@ -114,7 +114,7 @@
 (define-derived-mode emacs-news-mode text-mode "NEWS"
   "Major mode for editing the Emacs NEWS file."
   ;; Disable buttons.
-  (button-mode nil)
+  (button-mode -1)
   ;; And make the buffer writable.  This is used when toggling
   ;; emacs-news-mode.
   (setq buffer-read-only nil)
