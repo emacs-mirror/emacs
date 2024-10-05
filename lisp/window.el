688201ef18df468dc8c7e8eb2ce6a96a9c625517
Eli Zaretskii <eliz@gnu.org>
2024-10-05 13:30:30 +0300

    ; * lisp/window.el (switch-to-prev-buffer-skip-regexp): Doc fix (bug#73545).

--- lisp/window.el
+++ lisp/window.el
@@ -4617,7 +4617,8 @@ Also see `switch-to-prev-buffer-skip-regexp'."
   "Buffers that `switch-to-prev-buffer' and `switch-to-next-buffer' should skip.
 The value can either be a regexp or a list of regexps.  Buffers whose
 names match these regexps are skipped by `switch-to-prev-buffer'
-and `switch-to-next-buffer'.
+and `switch-to-next-buffer', unless there's no other buffer to
+switch to.
 
 Also see `switch-to-prev-buffer-skip'."
   :type '(choice regexp
