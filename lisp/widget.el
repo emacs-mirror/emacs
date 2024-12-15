32060cef1cb58e53896f4fea82d2d30d35501d93
Stefan Kangas <stefankangas@gmail.com>
2024-12-15 19:15:55 +0100

    ; * lisp/widget.el: Delete commented out code.

--- lisp/widget.el
+++ lisp/widget.el
@@ -1,6 +1,6 @@
 ;;; widget.el --- a library of user interface components  -*- lexical-binding: t; -*-
 ;;
-;; Copyright (C) 1996-1997, 2001-2024 Free Software Foundation, Inc.
+;; Copyright (C) 1996-2024 Free Software Foundation, Inc.
 ;;
 ;; Author: Per Abrahamsen <abraham@dina.kvl.dk>
 ;; Keywords: help, extensions, faces, hypermedia
@@ -32,42 +32,9 @@
 
 ;;; Code:
 
-;; Doing this is unnecessary in Emacs 20.  Kept as dummy in case
-;; external libraries call it.  We save a kb or two of purespace by
-;; dummying-out such definitions generally.
 (defmacro define-widget-keywords (&rest _keys)
-  ;; ;; Don't use backquote, since that makes trouble trying to
-  ;; ;; re-bootstrap from just the .el files.
-  ;; (list 'eval-and-compile
-  ;;   (list 'let (list (list 'keywords (list 'quote keys)))
-  ;;     (list 'while 'keywords
-  ;; 	(list 'or (list 'boundp (list 'car 'keywords))
-  ;; 	  (list 'set (list 'car 'keywords) (list 'car 'keywords)))
-  ;; 	(list 'setq 'keywords (list 'cdr 'keywords)))))
   (declare (obsolete nil "27.1") (indent defun))
   nil)
-
-;;(define-widget-keywords :documentation-indent
-;;  :complete-function :complete :button-overlay
-;;  :field-overlay
-;;  :documentation-shown :button-prefix
-;;  :button-suffix :mouse-down-action :glyph-up :glyph-down :glyph-inactive
-;;  :prompt-internal :prompt-history :prompt-match
-;;  :prompt-value  :deactivate :active
-;;  :inactive :activate :sibling-args :delete-button-args
-;;  :insert-button-args :append-button-args :button-args
-;;  :tag-glyph :off-glyph :on-glyph :valid-regexp
-;;  :secret :sample-face :sample-face-get :case-fold
-;;  :create :convert-widget :format :value-create :offset :extra-offset
-;;  :tag :doc :from :to :args :value :action
-;;  :value-set :value-delete :match :parent :delete :menu-tag-get
-;;  :value-get :choice :void :menu-tag :on :off :on-type :off-type
-;;  :notify :entry-format :button :children :buttons :insert-before
-;;  :delete-at :format-handler :widget :value-pos :value-to-internal
-;;  :indent :size :value-to-external :validate :error :directory
-;;  :must-match :type-error :value-inline :inline :match-inline :greedy
-;;  :button-face-get :button-face :value-face :keymap :entry-from
-;;  :entry-to :help-echo :documentation-property :tab-order)
 
 (defun define-widget (name class doc &rest args)
   "Define a new widget type named NAME from CLASS.
@@ -84,7 +51,6 @@ create identical widgets:
 
 The third argument DOC is a documentation string for the widget."
   (declare (doc-string 3) (indent defun))
-  ;;
   (unless (or (null doc) (stringp doc))
     (error "Widget documentation must be nil or a string"))
   (put name 'widget-type (cons class args))
