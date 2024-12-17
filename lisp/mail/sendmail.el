22806c65f4f8d68547d33997d017039763b4bf6a
Stefan Monnier <monnier@iro.umontreal.ca>
2024-12-17 17:07:45 -0500

    * lisp/mail/sendmail.el (mail-citation-prefix-regexp): Fix bug#24081

--- lisp/mail/sendmail.el
+++ lisp/mail/sendmail.el
@@ -258,7 +258,9 @@ regardless of what part of it (if any) is included in the cited text.")
 
 ;;;###autoload
 (defcustom mail-citation-prefix-regexp
-  (purecopy "\\([ \t]*\\(\\w\\|[_.]\\)+>+\\|[ \t]*[>|]\\)+")
+  ;; Use [[:word:]] rather than \w so we don't get tripped up if one
+  ;; of those chars has a weird `syntax-table' text property.
+  (purecopy "\\([ \t]*\\([[:word:]]\\|[_.]\\)+>+\\|[ \t]*[>|]\\)+")
   "Regular expression to match a citation prefix plus whitespace.
 It should match whatever sort of citation prefixes you want to handle,
 with whitespace before and after; it should also match just whitespace.
