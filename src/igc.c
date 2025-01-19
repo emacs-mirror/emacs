89b7cfbb647173efc27b0aa734ce6656be85029e
Gerd Möllmann <gerd@gnu.org>
2025-01-19 15:52:40 +0100

    ; Fix CHECK_STRUCT hashes

--- src/igc.c
+++ src/igc.c
@@ -125,7 +125,7 @@ along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>. */
 # ifndef HASH_vectorlike_header_AF1B22D957
 #  error "struct vectorlike_header changed"
 # endif
-# ifndef HASH_Lisp_Hash_Table_18BE341ECB
+# ifndef HASH_Lisp_Hash_Table_C3E7F39721
 #  error "struct Lisp_Hash_Table changed"
 # endif
 # ifndef HASH_Lisp_Weak_Hash_Table_7C5D3EDAD7
