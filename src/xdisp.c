0fd5b2d146e3578820446aa69b30d93958d9b1a5
Gerd Möllmann <gerd@gnu.org>
2025-01-21 18:31:10 +0100

    Don't use a redisplay optimization in a certain case on ttys
    * src/dispnew.c (is_tty_root_frame_with_visible_child): New function.
    * src/dispextern.h: Declare it.
    * src/xdisp.c (redisplay_internal): Don't use optimization 1 for tty root
    frames with a visible child frame.

--- src/xdisp.c
+++ src/xdisp.c
@@ -17279,7 +17279,11 @@ redisplay_internal (void)
 		 line and this line is the current one, because
 		 display_line above is not informed about the
 		 current-line's vpos, and cannot DTRT in that case.  */
-	      && !hscrolling_current_line_p (w))
+	      && !hscrolling_current_line_p (w)
+	      /* A root frame may have visible children displayed in its
+		 current matrix, so that we can't do the below with its
+		 current matrix.  */
+	      && !is_tty_root_frame_with_visible_child (it.f))
 	    {
  	      /* If this is not the window's last line, we must adjust
  		 the charstarts of the lines below.  */
