bf55eb1a8ae95051ac5c8a646b2a106053d7b1b7
Pip Cet <pipcet@protonmail.com>
2025-01-22 19:50:31 +0000

    ; * src/pdumper.c (dump_hash_table): Adjust hash; bitfields changed.

--- src/pdumper.c
+++ src/pdumper.c
@@ -2736,7 +2736,7 @@ dump_hash_table_contents (struct dump_context *ctx, struct Lisp_Hash_Table *h)
 static dump_off
 dump_hash_table (struct dump_context *ctx, Lisp_Object object)
 {
-#if CHECK_STRUCTS && !defined HASH_Lisp_Hash_Table_0360833954
+#if CHECK_STRUCTS && !defined HASH_Lisp_Hash_Table_267C58D687
 # error "Lisp_Hash_Table changed. See CHECK_STRUCTS comment in config.h."
 #endif
   const struct Lisp_Hash_Table *hash_in = XHASH_TABLE (object);
