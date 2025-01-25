f37559a1ee035d184bc4db6e4f597281d3ac6879
Gerd Möllmann <gerd@gnu.org>
2025-01-25 07:33:45 +0100

    Fix mouse position handling for nested tty child frames
    * src/dispnew.c (child_xy): New function.
    * src/dispextern.h: Declare it.
    * src/term.c (tty_frame_at): Return child-relative (x, y) in output
    parameters.
    (Ftty_frame_at): Return a list (FRAME CHILD-X CHILD-Y).
    (handle_one_term_event): Adapt use of tty_frame_at.
    * lisp/xt-mouse.el (xterm-mouse-event): Use new tty-frame-at.

--- src/dispextern.h
+++ src/dispextern.h
@@ -3959,6 +3959,7 @@ void combine_updates_for_frame (struct frame *f, bool inhibit_id_p);
 void tty_raise_lower_frame (struct frame *f, bool raise);
 int max_child_z_order (struct frame *parent);
 void root_xy (struct frame *f, int x, int y, int *rx, int *ry);
+void child_xy (struct frame *f, int x, int y, int *cx, int *cy);
 bool is_frame_ancestor (struct frame *f1, struct frame *f2);
 
 INLINE_HEADER_END
