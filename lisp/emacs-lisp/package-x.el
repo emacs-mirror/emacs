cdf9f3ae19d9b5f82a3677f4a9b7585e956cded6
Stefan Kangas <stefankangas@gmail.com>
2025-02-12 17:32:03 +0100

    ; Fix indentation to appease git hooks
    * lisp/emacs-lisp/package-x.el (package-upload-buffer-internal):
    Fix indentation.

--- lisp/emacs-lisp/package-x.el
+++ lisp/emacs-lisp/package-x.el
@@ -245,9 +245,9 @@ if it exists."
 	    ;; If there is a commentary section, write it.
 	    (when commentary
 	      (write-region commentary nil
-	        	    (expand-file-name
-	        	     (concat (symbol-name pkg-name) "-readme.txt")
-	        	     package-archive-upload-base)))
+                            (expand-file-name
+                             (concat (symbol-name pkg-name) "-readme.txt")
+                             package-archive-upload-base)))
 
 	    (set-buffer (if (eq file-type 'tar) tar-data-buffer pkg-buffer))
 	    (write-region (point-min) (point-max)
