35d4316101752fc45bfd2301d891fb7b64060500
Elías Gabriel Pérez <eg642616@gmail.com>
2025-02-16 13:21:52 -0600

    Add gtk-redo icon to x-gtk-stock-map to x-win.el
    * lisp/term/x-win.el (x-gtk-stock-map): Add gtk-redo.

--- lisp/term/x-win.el
+++ lisp/term/x-win.el
@@ -1383,6 +1383,7 @@ This returns an error if any Emacs frames are X frames."
     ("etc/images/save" . ("document-save" "gtk-save"))
     ("etc/images/saveas" . ("document-save-as" "gtk-save-as"))
     ("etc/images/undo" . ("edit-undo" "gtk-undo"))
+    ("etc/images/redo" . ("edit-redo" "gtk-redo"))
     ("etc/images/cut" . ("edit-cut" "gtk-cut"))
     ("etc/images/copy" . ("edit-copy" "gtk-copy"))
     ("etc/images/paste" . ("edit-paste" "gtk-paste"))
