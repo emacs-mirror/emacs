230ecb1e273e3fec6400cfad76e9b13f648d9055
Ulrich Müller <ulm@gentoo.org>
2025-02-21 10:36:52 +0100

    ; Skip shr-test/zoom-image test if png or libxml support is missing
    * test/lisp/net/shr-tests.el (shr-test/zoom-image): Skip if png
    images or libxml are not supported.  (Bug#76464)

--- test/lisp/net/shr-tests.el
+++ test/lisp/net/shr-tests.el
@@ -135,7 +135,9 @@ settings, then once more for each (OPTION . VALUE) pair.")
 
 (ert-deftest shr-test/zoom-image ()
   "Test that `shr-zoom-image' properly replaces the original image."
-  (skip-unless (bound-and-true-p image-types))
+  (skip-unless (and (bound-and-true-p image-types)
+                    (image-type-available-p 'png)
+                    (fboundp 'libxml-parse-html-region)))
   (let* ((image (expand-file-name "data/image/blank-100x200.png"
                                   (getenv "EMACS_TEST_DIRECTORY")))
          (image-url (concat "file://" (if (string-prefix-p "/" image)
