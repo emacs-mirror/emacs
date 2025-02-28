8b9194ae03214dd5169bd84f7d83bbe660dd9f90
Eshel Yaron <me@eshelyaron.com>
2025-02-28 12:07:31 +0100

    Make 'completion-preview-sort-function' a defcustom
    * lisp/completion-preview.el
    (completion-preview-sort-function): Define and document it
    as a user option.
    * etc/NEWS: Announce.

--- lisp/completion-preview.el
+++ lisp/completion-preview.el
@@ -226,8 +226,20 @@ affect the background color, for example with `hl-line-mode'."
                  (const :tag "Disable" nil))
   :version "31.1")
 
-(defvar completion-preview-sort-function #'minibuffer--sort-by-length-alpha
-  "Sort function to use for choosing a completion candidate to preview.")
+(defcustom completion-preview-sort-function #'minibuffer--sort-by-length-alpha
+  "Sort function to use for choosing a completion candidate to preview.
+
+Completion Preview mode calls the function that this option specifies to
+sort completion candidates.  The function takes one argument, the list
+of candidates, and returns the list sorted.
+
+The default sort function sorts first by length, then alphabetically.
+To disable sorting, set this option to `identity'.
+
+If the completion table that produces the candidates already specifies a
+sort function, it takes precedence over this option."
+  :type 'function
+  :version "31.1")
 
 (defface completion-preview
   '((t :inherit shadow))
