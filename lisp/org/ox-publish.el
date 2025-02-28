becdfb11a5b13bc773729236763e99e513806451
Stefan Kangas <stefankangas@gmail.com>
2025-02-28 03:02:15 +0100

    Remove Emacs 23 compat code from ox-publish.el
    * lisp/org/ox-publish.el (org-publish-timestamp-filename): Remove
    Emacs 23 compat code.

--- lisp/org/ox-publish.el
+++ lisp/org/ox-publish.el
@@ -365,7 +365,7 @@ The timestamp file name is constructed using FILENAME, publishing
 directory PUB-DIR, and PUB-FUNC publishing function."
   (setq filename (concat filename "::" (or pub-dir "") "::"
 			 (format "%s" (or pub-func ""))))
-  (concat "X" (if (fboundp 'sha1) (sha1 filename) (md5 filename))))
+  (concat "X" (sha1 filename)))
 
 (defun org-publish-needed-p
     (filename &optional pub-dir pub-func _true-pub-dir base-dir)
