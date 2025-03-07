6f19715937bbeaec5fb80ee9dad0d14b7712a989
Daniel Colascione <dancol@dancol.org>
2025-03-07 11:10:09 -0800

    Stop moving markers during erase-in-line
    We shouldn't move a line marker just because we cleared a line next to
    it.  clear-in-line is defined not to affect line structure.
    
    * lisp/term.el (term-erase-in-line): use insert-before-markers

--- lisp/term.el
+++ lisp/term.el
@@ -4062,7 +4062,10 @@ all pending output has been dealt with."))
       ;; extra space when wrapped is false.
       (when wrapped
 	(insert ? ))
-      (insert ?\n)
+      ;; If there's a marker at the start of the next line, we shouldn't
+      ;; disturb it: erase-in-line doesn't change logical
+      ;; line structure.
+      (insert-before-markers ?\n)
       (put-text-property saved-point (point) 'font-lock-face 'default)
       (goto-char saved-point))))
 
