1a9c2a9505a9e006955294fe2a9a2f93e76d712c
Daniel Colascione <dancol@dancol.org>
2025-03-08 07:04:18 -0800

    Fix documentation for windmove commands
    * lisp/windmove.el (windmove-left, windmove-up, windmove-right)
    (windmove-down): Mention correct variable names and align descriptions.

--- lisp/windmove.el
+++ lisp/windmove.el
@@ -435,7 +435,11 @@ With no prefix argument, or with prefix argument equal to zero,
 it is relative to the top edge (for positive ARG) or the bottom edge
 \(for negative ARG) of the current window.
 If no window is at the desired location, an error is signaled
-unless `windmove-create-window' is non-nil and a new window is created."
+unless `windmove-create-window' is non-nil and a new window is created.
+
+If `windmove-allow-repeated-command-override' is true and this commnad
+stopped because it wouldn't move into a window marked with
+`no-other-window', repeating the command will move into that window."
   (interactive "P\np")
   (windmove-do-window-select
    'left (and arg (prefix-numeric-value arg)) nil
@@ -451,10 +455,9 @@ negative ARG) of the current window.
 If no window is at the desired location, an error is signaled
 unless `windmove-create-window' is non-nil and a new window is created.
 
-When this command is used twice in quick succession (within
-`windmove-double-command-timeout' seconds), the second invocation
-will override the `no-other-window' property of windows, allowing
-you to move to windows that would normally be skipped."
+If `windmove-allow-repeated-command-override' is true and this commnad
+stopped because it wouldn't move into a window marked with
+`no-other-window', repeating the command will move into that window."
   (interactive "P\np")
   (windmove-do-window-select
    'up (and arg (prefix-numeric-value arg)) nil
@@ -470,9 +473,9 @@ bottom edge (for negative ARG) of the current window.
 If no window is at the desired location, an error is signaled
 unless `windmove-create-window' is non-nil and a new window is created.
 
-If `windmove-double-command-timeout' is set and a window exists in the
-requested direction that is marked with the no-other-window property, you
-can press this command twice in quick succession to override that property."
+If `windmove-allow-repeated-command-override' is true and this commnad
+stopped because it wouldn't move into a window marked with
+`no-other-window', repeating the command will move into that window."
   (interactive "P\np")
   (windmove-do-window-select
    'right (and arg (prefix-numeric-value arg)) nil
@@ -486,7 +489,11 @@ With no prefix argument, or with prefix argument equal to zero,
 it is relative to the left edge (for positive ARG) or the right edge
 \(for negative ARG) of the current window.
 If no window is at the desired location, an error is signaled
-unless `windmove-create-window' is non-nil and a new window is created."
+unless `windmove-create-window' is non-nil and a new window is created.
+
+If `windmove-allow-repeated-command-override' is true and this commnad
+stopped because it wouldn't move into a window marked with
+`no-other-window', repeating the command will move into that window."
   (interactive "P\np")
   (windmove-do-window-select
    'down (and arg (prefix-numeric-value arg)) nil
