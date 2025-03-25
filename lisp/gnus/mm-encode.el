a15534f32e1a978dee6ec96d6b60b5e755666e70
Michael Albinus <michael.albinus@gmx.de>
2025-03-25 15:13:16 +0100

    * lisp/gnus/mm-encode.el (mm-default-buffer-type): Check `major-mode'.

--- lisp/gnus/mm-encode.el
+++ lisp/gnus/mm-encode.el
@@ -109,10 +109,8 @@ This variable should never be set directly, but bound before a call to
 
 (defun mm-default-buffer-type (buffer)
   "Return a default content type for BUFFER, a buffer name."
-  (if (and (stringp buffer)
-           (string-match-p
-            (rx (| "*Diff*" "*vc-diff*" "*ediff-diff*" "*ediff-custom-diff*"))
-            buffer))
+  (if-let* ((buf (get-buffer buffer))
+            ((eq (buffer-local-value 'major-mode buf) 'diff-mode)))
       "text/x-patch" "text/plain"))
 
 (defun mm-safer-encoding (encoding &optional type)
