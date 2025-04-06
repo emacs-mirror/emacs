4b43d080bbddcd66c67256dc75a444b871d2845e
Jeremy Bryant <jb@jeremybryant.net>
2025-04-06 21:27:00 +0100

    ; * src/igc.c (make_igc): Correct typo in comment (bug#77586).

--- src/igc.c
+++ src/igc.c
@@ -4909,7 +4909,7 @@ make_igc (void)
   struct igc *gc = xzalloc (sizeof *gc);
   make_arena (gc);
 
-  /* We cannot let the GC run until at least all staticpros haven been
+  /* We cannot let the GC run until at least all staticpros have been
      processed.  Otherwise we might allocate objects that are not
      protected by anything.  */
   arena_park (gc);
