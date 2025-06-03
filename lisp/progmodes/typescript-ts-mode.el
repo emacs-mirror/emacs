6b668c2213432db04d29d36b8bf25a8f3f853c62
Juri Linkov <juri@linkov.net>
2025-06-03 18:41:01 +0300

    Restore c-ts-common-comment-setup in js-ts-mode and typescript-ts-mode.
    * lisp/progmodes/js.el (js-ts-mode):
    * lisp/progmodes/typescript-ts-mode.el (typescript-ts-base-mode):
    Add 'c-ts-common-comment-setup' back removed in 8bccccedb65a
    since it does more than setting comment variables (bug#78680).

--- lisp/progmodes/typescript-ts-mode.el
+++ lisp/progmodes/typescript-ts-mode.el
@@ -661,6 +661,7 @@ This mode is intended to be inherited by concrete major modes."
   :syntax-table typescript-ts-mode--syntax-table
 
   ;; Comments.
+  (c-ts-common-comment-setup)
   (setq-local comment-setup-function #'js--treesit-comment-setup)
 
   ;; Electric
