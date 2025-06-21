1406e583fe63c6d0731a731cf67e574be0d8d4d8
Eli Zaretskii <eliz@gnu.org>
2025-06-21 12:33:12 +0300

    Workaround for "M-x man" on macOS 15 and later
    * lisp/man.el (Man-init-defvars): Workaround for macOS Sed.  Do
    not merge to master.  (Bug#77944)

--- lisp/man.el
+++ lisp/man.el
@@ -650,9 +650,12 @@ This is necessary if one wants to dump man.el with Emacs."
 	     (if Man-sed-script
 		 (concat "-e '" Man-sed-script "'")
 	       "")
-             ;; Use octal numbers.  Otherwise, \032 (Ctrl-Z) would
-             ;; suspend remote connections.
-	     "-e '/^[\\o001-\\o032][\\o001-\\o032]*$/d'"
+             (if (eq system-type 'darwin)
+                 ;; macOS Sed doesn't support \o notation.
+                 "-e '/^[[:cntrl:]][[:cntrl:]]*$/d'"
+               ;; Use octal numbers.  Otherwise, \032 (Ctrl-Z) would
+               ;; suspend remote connections.
+	       "-e '/^[\\o001-\\o032][\\o001-\\o032]*$/d'")
 	     "-e '/\e[789]/s///g'"
 	     "-e '/Reformatting page.  Wait/d'"
 	     "-e '/Reformatting entry.  Wait/d'"
