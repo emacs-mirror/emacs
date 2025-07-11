8ff6e7fe58466e413b5eed22ea93869e60da8d3d
Sean Whitton <spwhitton@spwhitton.name>
2025-07-11 11:08:10 +0100

    ; vc-git-find-revision: Fix last change to this function.

--- lisp/vc/vc-git.el
+++ lisp/vc/vc-git.el
@@ -1336,7 +1336,7 @@ It is based on `log-edit-mode', and has Git-specific extensions."
 	    (if (string= fn "")
 		(file-relative-name file (vc-git-root default-directory))
 	      (substring fn 0 -1)))))
-    (vc-git-command buffer 0 nil "cat-file" "--filters" "blob"
+    (vc-git-command buffer 0 nil "cat-file" "--filters"
                     (concat (or rev "HEAD") ":" fullname))))
 
 (defun vc-git-find-ignore-file (file)
