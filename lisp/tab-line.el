32ba66d97d77a8f2ad5d8c6e2262fd934a42a657
Eli Zaretskii <eliz@gnu.org>
2025-08-05 15:30:17 +0300

    ; Improve doc strings in tab-line.el
    * lisp/tab-line.el (tab-line-tab-modified-p)
    (tab-line-tab-face-special, tab-line-tab-face-modified)
    (tab-line-tab-face-group, tab-line-tab-face-functions): Doc fixes.

--- lisp/tab-line.el
+++ lisp/tab-line.el
@@ -43,7 +43,8 @@
 Each function is called with five arguments: the tab, a list of
 all tabs, the face returned by the previously called modifier,
 whether the tab is a buffer (when nil, the buffer is extracted from
-the association list key `buffer'), and whether the tab is selected."
+the association list using the key `buffer'), and whether the tab
+is selected."
   :type '(repeat
           (choice (function-item tab-line-tab-face-special)
                   (function-item tab-line-tab-face-modified)
@@ -420,9 +421,8 @@ Used only for `tab-line-tabs-mode-buffers' and `tab-line-tabs-buffer-groups'.")
 
 (defun tab-line-tab-modified-p (tab buffer-p)
   "Return t if TAB's buffer is modified.
-BUFFER-P specifies whether the tab is a buffer.
-When nil, the buffer is extracted from the association list
-key `buffer'."
+BUFFER-P specifies whether the tab is a buffer; if nil, the buffer
+is extracted from the association list TAB using the key `buffer'."
   (let ((buffer (if buffer-p tab (cdr (assq 'buffer tab)))))
     (when (and buffer (buffer-file-name buffer) (buffer-modified-p buffer))
       t)))
@@ -717,9 +717,11 @@ inherit from `tab-line-tab-inactive-alternate'.  For use in
 
 (defun tab-line-tab-face-special (tab _tabs face buffer-p _selected-p)
   "Return FACE for TAB according to whether its buffer is special.
-When TAB is a non-file-visiting buffer, make FACE inherit from
-`tab-line-tab-special'.  For use in
-`tab-line-tab-face-functions'."
+TAB is either a buffer (if BUFFER-P is non-nil), or an association
+list with the buffer given by the key `buffer'.
+When TAB specifies a non-file-visiting buffer, make FACE inherit
+from `tab-line-tab-special'.
+For use in `tab-line-tab-face-functions'."
   (let ((buffer (if buffer-p tab (cdr (assq 'buffer tab)))))
     (when (and buffer (not (buffer-file-name buffer)))
       (setf face `(:inherit (tab-line-tab-special ,face)))))
@@ -727,15 +729,19 @@ When TAB is a non-file-visiting buffer, make FACE inherit from
 
 (defun tab-line-tab-face-modified (tab _tabs face buffer-p _selected-p)
   "Return FACE for TAB according to whether its buffer is modified.
+TAB is either a buffer (if BUFFER-P is non-nil), or an association
+list with the buffer given by the key `buffer'.
 When TAB's buffer is a modified, file-backed buffer, make FACE inherit
-from `tab-line-tab-modified'.  For use in
-`tab-line-tab-face-functions'."
+from `tab-line-tab-modified'.
+For use in `tab-line-tab-face-functions'."
   (when (tab-line-tab-modified-p tab buffer-p)
     (setf face `(:inherit (tab-line-tab-modified ,face))))
   face)
 
 (defun tab-line-tab-face-group (tab _tabs face _buffer-p _selected-p)
   "Return FACE for TAB according to whether it's a group tab.
+TAB is either a buffer (if BUFFER-P is non-nil), or an association
+list with the buffer given by the key `buffer'.
 For use in `tab-line-tab-face-functions'."
   (when (alist-get 'group-tab tab)
     (setf face `(:inherit (tab-line-tab-group ,face))))
