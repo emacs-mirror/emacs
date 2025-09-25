e223d2f33986480d294988c8c27f9d7722d8a666
Eli Zaretskii <eliz@gnu.org>
2025-09-25 11:31:27 +0300

    Fix igc tests on MS-Windows
    * src/igc.c (Figc_info): Fix "commit-limit" value when there's no
    limit, and 'size_t' is narrower than 'intmax_t'.  Doc fix.
    
    * test/src/igc-tests.el (set-commit-limit-test): Fix test for
    32-bit builds.

--- test/src/igc-tests.el
+++ test/src/igc-tests.el
@@ -32,8 +32,11 @@
                  '("commit-limit" 1 1073741824 0)))
   (should-error (igc--set-commit-limit -1)
                 :type 'args-out-of-range)
-  (should-error (igc--set-commit-limit (- (ash 1 64) 1))
-                :type 'args-out-of-range)
+  (should-error (igc--set-commit-limit
+                 (if (< #x1fffffff most-positive-fixnum)
+                     (- (ash 1 64) 1)
+                   (- (ash 1 32) 1))
+                :type 'args-out-of-range))
   (should (equal (igc--set-commit-limit nil) nil))
   (should (equal (assoc-string "commit-limit" (igc-info))
                  '("commit-limit" 1 -1 0))))
