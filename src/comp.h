a93ee02873cca9ae6417bd77ada7d9388fd22876
Gerd Möllmann <gerd.moellmann@gmail.com>
2025-10-17 09:04:47 +0200

    Alternative implementation of constants in .eln files
    * src/comp.h (USE_POINTER_TO_CONSTANT_VECTOR): If defined, don't
    use vectors.
    (Lisp_Native_Comp_Unit): Make data_relocs and data_eph_relocs
    pointers to Lisp_Object pointers.
    
    * src/comp.c (ABI_VERSION): Increase to 13.
    (check_comp_unit_relocs): Don't check if pointers are used.
    * src/comp.c (load_comp_unit, Fcomp__register_lambda): Handle
    case that constant vectors are pointers.
    * src/comp.c (emit_static_object): Add some FIXME comments.
    (declare_imported_data_relocs): Handle case of pointers to
    constants vectors differently.
    * src/comp.c (declare_imported_data_relocs): Make array elemen
    type volatile.
    * src/comp.c (comp_t): Add n_data_relocs and
    n_data_relocs_ephemeral.
    (obj_to_reloc): Set array length correctly.
    (declare_imported_data_relocs): Return # of constants.
    (declare_imported_data): Set comp's number of constants.
    
    * src/igc.c (root_create_exact_ptr, igc_root_create_exact_ptr):
    Return root list node.
    * src/igc.h (igc_root_create_exact_ptr): Change return type.
    
    * src/pdumper.c (dump_do_dump_relocation): Don't do fixup if
    using pointers to constants vectors.
    
    * src/comp.h (struct Lisp_Native_Comp_Unit): Add member
    data_eph_vec.
    * src/comp.c (load_comp_unit): Use data_eph_vec to protect the
    eph constants.

--- src/comp.h
+++ src/comp.h
@@ -20,44 +20,111 @@ along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.  */
 #ifndef COMP_H
 #define COMP_H
 
+// clang-format off
+
 #include <dynlib.h>
 # include "lisp.h"
 
-# ifdef HAVE_MPS
-struct igc_root_list;
-# endif
+#ifdef HAVE_MPS
+typedef struct igc_root_list *gc_root_t;
+#else
+typedef void *gc_root_t;
+#endif
+
+/* Shared .eln objects contain printed representations of Lisp vectors
+   representing constant Lisp objects used in native-compiled
+   code. There are two of these vectors: one for top-level code, which
+   is called "ephemeral", and one for other code.
+
+   When an .eln is loaded, the Lisp reader is used to construct Lisp
+   vectors from these printed representations (which are C strings in
+   the text segment). This Lisp vector is then saved to protect the
+   constant objects from GC, and its contents are additionally copied to
+   vectors in the data segment of the .eln to avoid an additional
+   indirection when accessing them.
+
+   With igc, the vectors in the data segment are made exact roots, and
+   there are quite a lot of them. With my init file, this add up to
+   around 1.5 MB. To avoid these roots, an alternative can be used:
+
+   Compile the shared object to contain a pointer to the contents member
+   of the Lisp vectors that has been read, instead of copying the
+   vector's contents to the data segment. Access to constants then
+   requires an additional indirection, but the GC latency is less. It's
+   something like the difference between
+
+   static Lisp_Object constants[42];
+   Lisp_Object constant = constants[17];
+
+   and
+
+   static Lisp_Object **constants;
+   Lisp_Object constant = (*constants)[17];
+
+   The advantage of the first method is slightly faster native code, the
+   advantage of the second method is slightly less latency of
+   incremental GC.
+
+   Define USE_POINTER_TO_CONSTANTS to use the second method. */
+
+#ifdef USE_POINTER_TO_CONSTANTS
+typedef Lisp_Object **comp_data_vector_t;
+#else
+typedef Lisp_Object *comp_data_vector_t;
+#endif
 
 struct Lisp_Native_Comp_Unit
 {
   struct vectorlike_header header;
+
   /* The original eln file loaded.  In the pdumper file this is stored
      as a cons cell of 2 alternative file names: the car is the
      filename relative to the directory of an installed binary, the
      cdr is the filename relative to the directory of an uninstalled
      binary.  This is arranged in loadup.el.  */
   Lisp_Object file;
+
   Lisp_Object optimize_qualities;
+
   /* Guard anonymous lambdas against Garbage Collection and serve
      sanity checks.  */
   Lisp_Object lambda_gc_guard_h;
+
   /* Hash c_name -> d_reloc index.  */
   Lisp_Object lambda_c_name_idx_h;
+
   /* Hash doc-idx -> function documentation.  */
   Lisp_Object data_fdoc_v;
-  /* Analogous to the constant vector but per compilation unit.  Must be
-     last.  */
+
+  /* Temporarily used to GC protect a vector of constants used
+     during the execution of top-level code.  */
+  Lisp_Object data_eph_vec;
+
+  /* A Lisp vector read from a string contained in the text segment of
+     the .eln (TEXT_DATA_RELOC_SYM). The elements of the vector are
+     constants used in the native code. */
   Lisp_Object data_vec;
-  /* STUFFS WE DO NOT DUMP!!  */
-# ifdef HAVE_MPS
-  size_t n_data_relocs;
-  Lisp_Object *data_eph_relocs;
-  size_t n_data_eph_relocs;
+
+  /* STUFF WE DO NOT DUMP!!  */
+
+  /* Pointer into the data segment where the compilation unit is
+     stored (COMP_UNIT_SYM), and an exact root for it.  */
   Lisp_Object *comp_unit;
-  struct igc_root_list *data_relocs_root;
-  struct igc_root_list *data_eph_relocs_root;
-  struct igc_root_list *comp_unit_root;
-# endif
-  Lisp_Object *data_relocs;
+  gc_root_t comp_unit_root;
+
+  /* Pointers into data segment where constant vectors are found. */
+  comp_data_vector_t data_relocs;
+  comp_data_vector_t data_eph_relocs;
+
+  /* Size of the vectors above. 1 in the USE_POINTER_TO_CONSTANTS
+     case. */
+  size_t n_data_relocs;
+  size_t n_data_eph_relocs;
+
+  /* Exact roots for the vectors. */
+  gc_root_t data_relocs_root;
+  gc_root_t data_eph_relocs_root;
+
   bool loaded_once;
   bool load_ongoing;
   dynlib_handle_ptr handle;
