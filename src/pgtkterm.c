e1713417ab72384d6c12e5e0240e873e3602cade
Pip Cet <pipcet@protonmail.com>
2025-10-26 14:42:27 +0000

    Remove glib_user_data in PGTK code
    * src/pgtkmenu.c (menu_highlight_callback, menubar_selection_callback)
    (popup_selection_callback):
    * src/pgtkterm.c (size_allocate, pgtk_set_event_handler): Use
    'gc_handle' or raw pointer, not 'glib_user_data'.

--- src/pgtkterm.c
+++ src/pgtkterm.c
@@ -5120,7 +5120,7 @@ size_allocate (GtkWidget *widget, GtkAllocation *alloc,
   struct frame *f = pgtk_any_window_to_frame (gtk_widget_get_window (widget));
 
   if (!f)
-    f = get_glib_user_data (user_data);
+    f = XFRAME (gc_handle_value (user_data));
 
   if (f)
     {
@@ -6863,8 +6863,9 @@ pgtk_set_event_handler (struct frame *f)
   g_signal_connect (G_OBJECT (FRAME_GTK_WIDGET (f)), "map-event",
 		    G_CALLBACK (map_event), NULL);
   g_signal_connect_data (G_OBJECT (FRAME_GTK_WIDGET (f)), "size-allocate",
-			 G_CALLBACK (size_allocate), glib_user_data (f),
-			 free_glib_user_data, 0);
+			 G_CALLBACK (size_allocate),
+			 gc_handle_for_pvec (&f->header),
+			 free_glib_gc_handle, 0);
   g_signal_connect (G_OBJECT (FRAME_GTK_WIDGET (f)), "key-press-event",
 		    G_CALLBACK (key_press_event), NULL);
   g_signal_connect (G_OBJECT (FRAME_GTK_WIDGET (f)), "key-release-event",
