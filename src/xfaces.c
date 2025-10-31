19305982253e248dfa29ec20a89b0f1792106ad2
Eli Zaretskii <eliz@gnu.org>
2025-10-31 17:24:58 +0200

    Avoid infinite recursion in 'face_inheritance_cycle'
    * src/xfaces.c (face_inheritance_cycle)
    (Finternal_set_lisp_face_attribute): Prevent infinite recursion if
    a face is defined to inherit from itself (whaaat? how??!).

--- src/xfaces.c
+++ src/xfaces.c
@@ -2444,12 +2444,14 @@ face_inheritance_cycle (struct frame *f, Lisp_Object face, Lisp_Object child)
       Lisp_Object tail;
       for (tail = face; CONSP (tail); tail = XCDR (tail))
 	{
-	  ok = get_lface_attributes (NULL, f, XCAR (tail), face_attrs,
+	  Lisp_Object member_face = XCAR (tail);
+	  ok = get_lface_attributes (NULL, f, member_face, face_attrs,
 				     false, NULL);
 	  if (!ok)
 	    break;
 	  parent_face = face_attrs[LFACE_INHERIT_INDEX];
-	  if (EQ (parent_face, child))
+	  if (EQ (parent_face, member_face)
+	      || EQ (parent_face, child))
 	    cycle_found = true;
 	  else if (!NILP (parent_face)
 		   && !UNSPECIFIEDP (parent_face)
@@ -2466,7 +2468,8 @@ face_inheritance_cycle (struct frame *f, Lisp_Object face, Lisp_Object child)
       if (ok)
 	{
 	  parent_face = face_attrs[LFACE_INHERIT_INDEX];
-	  if (EQ (parent_face, child))
+	  if (EQ (parent_face, face)
+	      || EQ (parent_face, child))
 	    cycle_found = true;
 	  else if (!NILP (parent_face)
 		   && !UNSPECIFIEDP (parent_face)
@@ -3707,7 +3710,7 @@ FRAME 0 means change the face on all frames, and change the default
 	for (tail = value; CONSP (tail); tail = XCDR (tail))
 	  if (!SYMBOLP (XCAR (tail)))
 	    break;
-      if (face_inheritance_cycle (f, value, face))
+      if (EQ (value, face) || face_inheritance_cycle (f, value, face))
 	signal_error ("Face inheritance results in inheritance cycle", value);
       else if (NILP (tail))
 	ASET (lface, LFACE_INHERIT_INDEX, value);
