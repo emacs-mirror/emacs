1b931fbe42bdad1507768872dfe034caa236adab
Philip Kaludercic <philipk@posteo.net>
2025-11-07 23:27:20 +0100

    Prepare and load user scripts at startup
    * doc/emacs/custom.texi (Early Init File): Document feature and
    related user options.
    * etc/NEWS: Mention new feature.
    * lisp/startup.el (user-lisp-auto-scrape, user-lisp-directory)
    (user-lisp-ignored-directories): New user options.
    (prepare-user-lisp): New command.
    (command-line): Invoke 'prepare-user-lisp' during startup if a
    user-lisp directory exists and if not disabled per
    'user-lisp-auto-scrape'.

--- lisp/startup.el
+++ lisp/startup.el
@@ -1190,6 +1190,92 @@ This function is called from `load' via `load-path-filter-function'."
                   (directory-files dir nil rx t)))))
            path))))))
 
+(defcustom user-lisp-auto-scrape t
+  "Enable auto-scraping of `user-lisp-directory' at startup.
+If you customize this to nil, you can still invoke the auto-scraping
+with `prepare-user-lisp'.
+
+Note that this variable must be set in your early-init file, as the
+variable's value is used before loading the regular init file.
+Therefore, if you customize it via Customize, you should save your
+customized setting into your `early-init-file'."
+  :type 'boolean
+  :version "31.1")
+
+(defcustom user-lisp-directory
+  (locate-user-emacs-file "user-lisp/")
+  "Activate all Lisp files in this directory, if it exists.
+All regular files below directories are byte-compiled, scraped for
+autoload cookies and ensured to be in `load-path' at startup.  To
+restrict what subdirectories to process, see
+`user-lisp-ignored-directories'.  Note that byte-compilation and
+autoload scraping is lazy, occurring only if the file timestamps
+indicate that it is necessary.  For details on how to override this
+behavior, consult `prepare-user-lisp'.
+
+If you need Emacs to pick up on updates to this directory that occur
+after startup, you can also invoke the `prepare-user-lisp' manually.  To
+disable auto-scraping, see `user-lisp-auto-scrape'.
+
+Note that this variable must be set in your early-init file, as the
+variable's value is used before loading the regular init file.
+Therefore, if you customize it via Customize, you should save your
+customized setting into your `early-init-file'."
+  :initialize #'custom-initialize-delay
+  :type 'directory
+  :version "31.1")
+
+(defcustom user-lisp-ignored-directories
+  '(".git" ".hg" "RCS" "CVS" ".svn" "_svn" ".bzr")
+  "List of directory names for `prepare-user-lisp' to not descend into.
+Each entry of the list is a string that denotes the file name without a
+directory component.  If during recursion any single entry matches the
+file name of any directory, `prepare-user-lisp' will ignore the contents
+of the directory.  This option is most useful to exclude administrative
+directories that do not contain Lisp files."
+  :type '(choice (repeat (string :tag "Directory name")))
+  :version "31.1")
+
+(defun prepare-user-lisp (&optional just-activate autoload-file force)
+  "Byte-compile, scrape autoloads and prepare files in `user-lisp-directory'.
+Write the autoload file to AUTOLOAD-FILE.  If JUST-ACTIVATE is non-nil,
+then the more expensive operations (byte-compilation and autoload
+scraping) are skipped, in effect only processing any previous autoloads.
+If AUTOLOAD-FILE is nil, store the autoload data in a file next to DIR.
+If FORCE is non-nil, or if invoked interactively with a prefix argument,
+re-create the entire autoload file and byte-compile everything
+unconditionally."
+  (interactive (list nil current-prefix-arg))
+  (unless (file-directory-p user-lisp-directory)
+    (error "No such directory: %S" user-lisp-directory))
+  (unless autoload-file
+    (setq autoload-file (expand-file-name ".user-lisp-autoloads.el"
+                                          user-lisp-directory)))
+  (let* ((pred
+          (let ((ignored
+                 (concat "\\`" (regexp-opt user-lisp-ignored-directories) "\\'")))
+            (lambda (dir)
+              (not (string-match-p ignored (file-name-nondirectory dir))))))
+         (dir (expand-file-name user-lisp-directory))
+         (backup-inhibited t)
+         (dirs (list dir)))
+    (add-to-list 'load-path dir)
+    (dolist (file (directory-files-recursively dir "" t pred))
+      (cond
+       ((and (file-regular-p file) (string-suffix-p ".el" file))
+        (unless just-activate
+          (with-demoted-errors "Error while compiling: %S"
+            (byte-recompile-file file force 0)
+            (when (native-comp-available-p)
+              (native-compile-async file)))))
+       ((file-directory-p file)
+        (add-to-list 'load-path file)
+        (push file dirs))))
+    (unless just-activate
+      (loaddefs-generate dirs autoload-file nil nil nil force))
+    (when (file-exists-p autoload-file)
+      (load autoload-file nil t))))
+
 (defun command-line ()
   "A subroutine of `normal-top-level'.
 Amongst another things, it parses the command-line arguments."
@@ -1471,6 +1557,12 @@ please check its value")
                                 subdir))))
 		   (throw 'package-dir-found t)))))))
        (package-activate-all))
+
+  ;; If it enabled and the directory exists, process the contents of the
+  ;; user-lisp/ directory.
+  (when (and init-file-user
+             (file-directory-p user-lisp-directory))
+    (prepare-user-lisp (not user-lisp-auto-scrape)))
 
   ;; Make sure window system's init file was loaded in loadup.el if
   ;; using a window system.
