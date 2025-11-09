04831782198bfe49620e981012d7d73bd4945280
Sean Whitton <spwhitton@spwhitton.name>
2025-11-09 11:58:39 +0000

    ; * lisp/vc/vc-dispatcher.el (vc-do-command): Reduce indentation.

--- lisp/vc/vc-dispatcher.el
+++ lisp/vc/vc-dispatcher.el
@@ -394,101 +394,102 @@ that is inserted into the command line before the filename.
 
 Return the return value of the slave command in the synchronous
 case, and the process object in the asynchronous case."
-  (let (;; Keep entire commands in *Messages* but avoid resizing the
-        ;; echo area.  Messages in this function are formatted in
-        ;; a such way that the important parts are at the beginning,
-        ;; due to potential truncation of long messages.
-        (message-truncate-lines t)
-        (vc-inhibit-message
-         (or (eq vc-command-messages 'log)
-             (eq (selected-window) (active-minibuffer-window)))))
+  (pcase-let (;; Keep entire commands in *Messages* but avoid resizing the
+              ;; echo area.  Messages in this function are formatted in
+              ;; a such way that the important parts are at the beginning,
+              ;; due to potential truncation of long messages.
+              (message-truncate-lines t)
+              (vc-inhibit-message
+               (or (eq vc-command-messages 'log)
+                   (eq (selected-window) (active-minibuffer-window))))
+              (`(,command ,file-or-list ,flags)
+               (funcall vc-filter-command-function
+                        command file-or-list flags)))
     (save-current-buffer
       (unless (or (eq buffer t)
                   (eq (current-buffer) (get-buffer buffer)))
         (vc-setup-buffer buffer))
-      (cl-destructuring-bind (command file-or-list flags)
-          (funcall vc-filter-command-function command file-or-list flags)
-        (when vc-tor
-          (push command flags)
-          (setq command "torsocks"))
-        (let* (;; FIXME: file-relative-name can return a bogus result
-               ;; because it doesn't look at the actual file-system to
-               ;; see if symlinks come into play.
-               (files
-                (mapcar (lambda (f)
-                          (file-relative-name (expand-file-name f)))
-                        (ensure-list file-or-list)))
-               (full-command
-                (concat (if (equal (substring command -1) "\n")
-                            (substring command 0 -1)
-                          command)
-                        " " (vc-delistify flags)
-                        (and files (concat " " (vc-delistify files)))))
-               (squeezed (remq nil flags))
-               (inhibit-read-only t)
-               (status 0))
-          ;; If there's some previous async process still running,
-          ;; just kill it.
-          (when files
-            (setq squeezed (nconc squeezed files)))
-          (let (;; Since some functions need to parse the output
-                ;; from external commands, set LC_MESSAGES to C.
-                (process-environment
-                 (cons "LC_MESSAGES=C" process-environment))
-                (w32-quote-process-args t))
-            (if (eq okstatus 'async)
-                ;; Run asynchronously.
-                (let ((proc
-                       (let (process-connection-type)
-                         (apply #'start-file-process command
-                                (current-buffer) command squeezed))))
-                  (when vc-command-messages
-                    (let ((inhibit-message vc-inhibit-message))
-                      (message "Running in background: %s"
-                               full-command)))
-                  ;; Get rid of the default message insertion, in case
-                  ;; we don't set a sentinel explicitly.
-                  (set-process-sentinel proc #'ignore)
-                  (set-process-filter proc #'vc-process-filter)
-                  (setq status proc)
-                  (when vc-command-messages
-                    (vc-run-delayed
-                      (let ((message-truncate-lines t)
-                            (inhibit-message vc-inhibit-message))
-                        (message "Done in background: %s"
-                                 full-command)))))
-              ;; Run synchronously
-              (when vc-command-messages
-                (let ((inhibit-message vc-inhibit-message))
-                  (message "Running in foreground: %s" full-command)))
-              (let ((buffer-undo-list t))
-                (setq status (apply #'process-file
-                                    command nil t nil squeezed)))
-              (when (and (not (eq t okstatus))
-                         (or (not (integerp status))
-                             (and okstatus (< okstatus status))))
-                (unless (eq ?\s (aref (buffer-name (current-buffer)) 0))
-                  (pop-to-buffer (current-buffer))
-                  (goto-char (point-min))
-                  (shrink-window-if-larger-than-buffer))
-                (when-let* (noninteractive
-                            (out (string-trim (buffer-string)))
-                            (_ (not (string-empty-p out))))
-                  (with-current-buffer buffer
-                    (message "%s" out)))
-                (error "Failed (%s): %s"
-                       (if (integerp status)
-                           (format "status %d" status)
-                         status)
-                       full-command))
-              (when vc-command-messages
-                (let ((inhibit-message vc-inhibit-message))
-                  (message "Done (status=%d): %s"
-                           status full-command)))))
-          (vc-run-delayed
-            (run-hook-with-args 'vc-post-command-functions
-                                command file-or-list flags))
-          status)))))
+      (when vc-tor
+        (push command flags)
+        (setq command "torsocks"))
+      (let* (;; FIXME: file-relative-name can return a bogus result
+             ;; because it doesn't look at the actual file-system to
+             ;; see if symlinks come into play.
+             (files
+              (mapcar (lambda (f)
+                        (file-relative-name (expand-file-name f)))
+                      (ensure-list file-or-list)))
+             (full-command
+              (concat (if (equal (substring command -1) "\n")
+                          (substring command 0 -1)
+                        command)
+                      " " (vc-delistify flags)
+                      (and files (concat " " (vc-delistify files)))))
+             (squeezed (remq nil flags))
+             (inhibit-read-only t)
+             (status 0))
+        ;; If there's some previous async process still running,
+        ;; just kill it.
+        (when files
+          (setq squeezed (nconc squeezed files)))
+        (let (;; Since some functions need to parse the output
+              ;; from external commands, set LC_MESSAGES to C.
+              (process-environment
+               (cons "LC_MESSAGES=C" process-environment))
+              (w32-quote-process-args t))
+          (if (eq okstatus 'async)
+              ;; Run asynchronously.
+              (let ((proc
+                     (let (process-connection-type)
+                       (apply #'start-file-process command
+                              (current-buffer) command squeezed))))
+                (when vc-command-messages
+                  (let ((inhibit-message vc-inhibit-message))
+                    (message "Running in background: %s"
+                             full-command)))
+                ;; Get rid of the default message insertion, in case
+                ;; we don't set a sentinel explicitly.
+                (set-process-sentinel proc #'ignore)
+                (set-process-filter proc #'vc-process-filter)
+                (setq status proc)
+                (when vc-command-messages
+                  (vc-run-delayed
+                    (let ((message-truncate-lines t)
+                          (inhibit-message vc-inhibit-message))
+                      (message "Done in background: %s"
+                               full-command)))))
+            ;; Run synchronously
+            (when vc-command-messages
+              (let ((inhibit-message vc-inhibit-message))
+                (message "Running in foreground: %s" full-command)))
+            (let ((buffer-undo-list t))
+              (setq status (apply #'process-file
+                                  command nil t nil squeezed)))
+            (when (and (not (eq t okstatus))
+                       (or (not (integerp status))
+                           (and okstatus (< okstatus status))))
+              (unless (eq ?\s (aref (buffer-name (current-buffer)) 0))
+                (pop-to-buffer (current-buffer))
+                (goto-char (point-min))
+                (shrink-window-if-larger-than-buffer))
+              (when-let* (noninteractive
+                          (out (string-trim (buffer-string)))
+                          (_ (not (string-empty-p out))))
+                (with-current-buffer buffer
+                  (message "%s" out)))
+              (error "Failed (%s): %s"
+                     (if (integerp status)
+                         (format "status %d" status)
+                       status)
+                     full-command))
+            (when vc-command-messages
+              (let ((inhibit-message vc-inhibit-message))
+                (message "Done (status=%d): %s"
+                         status full-command)))))
+        (vc-run-delayed
+          (run-hook-with-args 'vc-post-command-functions
+                              command file-or-list flags))
+        status))))
 
 (defvar vc--inhibit-async-window nil)
 
