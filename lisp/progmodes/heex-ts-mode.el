d059e7516e5a70b049fe99284260af47ba0c1c10
Juri Linkov <juri@linkov.net>
2025-11-10 20:46:40 +0200

    Improve hideshow support for elixir-ts-mode and heex-ts-mode
    * lisp/progmodes/elixir-ts-mode.el (elixir-ts--thing-settings):
    Add 'defun' thing.
    (elixir-ts-mode): Set 'hs-treesit-things'.
    * lisp/progmodes/heex-ts-mode.el (heex-ts-mode):
    Set 'hs-treesit-things'.  Use bos/eos instead of bol/eol.

--- lisp/progmodes/heex-ts-mode.el
+++ lisp/progmodes/heex-ts-mode.el
@@ -209,7 +209,7 @@ Return nil if NODE is not a defun node or doesn't have a name."
 
     ;; Navigation.
     (setq-local treesit-defun-type-regexp
-                (rx bol (or "component" "tag" "slot") eol))
+                (rx bos (or "component" "tag" "slot") eos))
     (setq-local treesit-defun-name-function #'heex-ts--defun-name)
 
     ;; Imenu
@@ -261,7 +261,8 @@ Return nil if NODE is not a defun node or doesn't have a name."
 
     ;; Enable the 'sexp' navigation by default
     (setq-local forward-sexp-function #'treesit-forward-sexp
-                treesit-sexp-thing 'sexp)))
+                treesit-sexp-thing 'sexp
+                hs-treesit-things 'list)))
 
 (derived-mode-add-parents 'heex-ts-mode '(heex-mode))
 
