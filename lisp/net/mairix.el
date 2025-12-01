02afd1305b4921d209aa95c1728768e514d37f8a
Paul Nelson <ultrono@gmail.com>
2025-12-01 09:04:04 +0100

    Use dedicated minibuffer history for mairix-search
    * lisp/net/mairix.el (mairix-search-history): New variable.
    (mairix-search): Use it (bug#79926).

--- lisp/net/mairix.el
+++ lisp/net/mairix.el
@@ -200,6 +200,8 @@ Currently there are `threads' and `flags'.")
 
 ;;;; Internal variables
 
+(defvar mairix-search-history nil
+  "Minibuffer history for `mairix-search'.")
 (defvar mairix-last-search nil)
 (defvar mairix-searches-changed nil)
 
@@ -334,7 +336,7 @@ If THREADS is non-nil, also display whole threads of found
 messages.  Results will be put into the default search file."
   (interactive
    (list
-    (read-string "Query: ")
+    (read-string "Query: " nil 'mairix-search-history)
     (y-or-n-p "Include threads? ")))
   (when (mairix-call-mairix
 	 (split-string search)
