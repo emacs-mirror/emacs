b7601bfb70ee06e7ed97c5f80bfc8c8e0dd80d6c
Eli Zaretskii <eliz@gnu.org>
2025-12-17 15:14:25 +0200

    Fix 'mode-line-format-right-align' in Olivetti mode
    * lisp/bindings.el (mode--line-format-right-align): Account for
    the left fringe.  (Bug#80019)

--- lisp/bindings.el
+++ lisp/bindings.el
@@ -383,6 +383,7 @@ the symbol `mode-line-format-right-align' is processed by
                                          (window-right-divider-width)
                                          (* (or (car (window-margins)) 0)
                                             (frame-char-width))
+                                         (car (window-fringes))
                                          ;; Manually account for value of
                                          ;; `mode-line-right-align-edge' even
                                          ;; when display is non-graphical
