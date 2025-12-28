8f2557844d2f0e7a87b4c94d7a28bc6e4207d9da
Yuan Fu <casouri@gmail.com>
2025-12-28 15:11:27 -0800

    Use variable-use-face for assignment in c-ts-mode (bug#79623)
    * lisp/progmodes/c-ts-mode.el (c-ts-mode--font-lock-settings):
    Use variable-use face for the LHS of assignment statements.

--- lisp/progmodes/c-ts-mode.el
+++ lisp/progmodes/c-ts-mode.el
@@ -128,6 +128,7 @@ just toggles it when zero or omitted."
       (c-ts-mode-set-modeline))))
 
 (defun c-ts-mode-set-modeline ()
+  "Set major mode name to either C or C++ in mode-line."
   (setq mode-name
         (concat (if (eq major-mode 'c-ts-mode) "C" "C++")
                 (string-trim-right comment-start)))
@@ -827,15 +828,15 @@ MODE is either `c' or `cpp'."
    ;; expressions, see `c-ts-mode--fontify-declarator' for
    ;; inspiration.
    '((assignment_expression
-      left: (identifier) @font-lock-variable-name-face)
+      left: (identifier) @font-lock-variable-use-face)
      (assignment_expression
       left: (field_expression field: (_) @font-lock-property-use-face))
      (assignment_expression
       left: (pointer_expression
-             (identifier) @font-lock-variable-name-face))
+             (identifier) @font-lock-variable-use-face))
      (assignment_expression
       left: (subscript_expression
-             (identifier) @font-lock-variable-name-face))
+             (identifier) @font-lock-variable-use-face))
      (init_declarator declarator: (_) @c-ts-mode--fontify-declarator))
 
    :feature 'function
