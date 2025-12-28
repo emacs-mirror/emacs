1b23b4a5ad31a13199b356ad2dd6465a462fbab8
Sean Whitton <spwhitton@spwhitton.name>
2025-12-28 12:33:25 +0000

    ; * lisp/ldefs-boot.el: Restore no-byte-compile local variable.

--- lisp/ldefs-boot.el
+++ lisp/ldefs-boot.el
@@ -40081,6 +40081,7 @@ run a specific program.  The program must be a member of
 (provide 'loaddefs)
 
 ;; Local Variables:
+;; no-byte-compile: t
 ;; version-control: never
 ;; no-update-autoloads: t
 ;; no-native-compile: t
