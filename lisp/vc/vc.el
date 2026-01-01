47dd565d39d03a2c6b2a9d89ca574ad8740089a5
Sean Whitton <spwhitton@spwhitton.name>
2026-01-01 11:05:56 +0000

    ; * lisp/vc/vc.el (dir-status-files): Mention vc-dir-status-files.

--- lisp/vc/vc.el
+++ lisp/vc/vc.el
@@ -182,10 +182,11 @@
 ;;   `dir-printer', and `extra-dir-menu'.
 ;;
 ;;   NOTE: project.el includes a similar method `project-list-files'
-;;   that has slightly different return value and performance tradeoffs.
-;;   If you want to use it in your code and it suits your needs better
-;;   than `dir-status-files', consider contacting the development list
-;;   about changes or having it promoted to the core VC.
+;;   that has a slightly different return value and performance
+;;   trade-offs.  If you want to use it in your code and it suits your
+;;   needs better than `dir-status-files', consider contacting the
+;;   development list about changes or having it promoted to the core
+;;   VC.  See also `vc-dir-status-files'.
 ;;
 ;; - dir-extra-headers (dir)
 ;;
