0054a5ff0cbb06fea6035a0eeab7bcc110c90767
Philip Kaludercic <philipk@posteo.net>
2026-02-01 19:43:26 +0100

    Clarify that package suggestions have to be confirmed
    * lisp/emacs-lisp/package.el (package--autosugest-prompt):
    Adjust message to indicate that no changes have yet occurred.
    
    Co-Developed-By: Jens Schmidt <jschmidt4gnu@vodafonemail.de>

--- lisp/emacs-lisp/package.el
+++ lisp/emacs-lisp/package.el
@@ -4672,8 +4672,8 @@ if affirmative, otherwise nil"
        "The buffer \""
        (buffer-name buf)
        "\" currently lacks any language-specific support.
-The package manager has detected that by installing a third-party package,
-Emacs can provide the editor support for these kinds of files:" nl)
+The Emacs package manager can provide the editor support for these kinds
+of files by installing a third-party package:" nl)
 
       (when (length> packages 1)
         (insert nl "(Note that there are multiple candidate packages,
