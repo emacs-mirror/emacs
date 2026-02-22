74c2fce343720906c57b897b4b42341f848231c7
Helmut Eller <eller.helmut@gmail.com>
2026-02-22 19:14:25 +0100

    Simplify set_header a bit.
    * src/igc.c (set_header): Use uint64_t to avoid compiler warnings on
    32-bit targets.

--- src/igc.c
+++ src/igc.c
@@ -731,11 +731,7 @@ static void
 set_header (union igc_header *h, enum igc_obj_type type,
 	    mps_word_t nbytes, mps_word_t hash)
 {
-#if IGC_HEADER_NWORDS_BITS >= 32 && INTPTR_MAX > INT_MAX
-  /* On 32-bit architecture the assertion below is redundant and
-     causes compiler warnings.  */
-  igc_assert (nbytes < ((size_t) 1 << IGC_HEADER_NWORDS_BITS));
-#endif
+  igc_assert (nbytes < ((uint64_t) 1 << IGC_HEADER_NWORDS_BITS));
   igc_assert (type == IGC_OBJ_PAD
 	      || nbytes >= sizeof (struct igc_fwd));
   union igc_header val = { .s = { .tag = IGC_TAG_OBJ,
