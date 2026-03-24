bb0a5dd6e7df2c7f46c39283d7e0faea54d9ee76
Vincent Belaïche <vincentb1@users.sourceforge.net>
2026-03-24 18:12:38 +0100

    Fix SES tests indentation.
    * test/lisp/ses-tests.el (ses-jump-B2-lowcase)
    (ses-jump-B2-lowcase-keys): Fix indentation.

--- test/lisp/ses-tests.el
+++ test/lisp/ses-tests.el
@@ -202,23 +202,23 @@ to `ses--bar' and inserting a row, makes A2 value empty, and `ses--bar' equal to
 
 (ert-deftest ses-jump-B2-lowcase ()
   "Test jumping to cell B2 by use of lowercase cell name string"
-    (let ((ses-initial-size '(3 . 3))
-          ses-after-entry-functions)
-      (with-temp-buffer
-        (ses-mode)
-        (funcall-interactively 'ses-jump "b2")
-        (ses-command-hook)
-        (should (eq (ses--cell-at-pos (point)) 'B2)))))
+  (let ((ses-initial-size '(3 . 3))
+        ses-after-entry-functions)
+    (with-temp-buffer
+      (ses-mode)
+      (funcall-interactively 'ses-jump "b2")
+      (ses-command-hook)
+      (should (eq (ses--cell-at-pos (point)) 'B2)))))
 
 (ert-deftest ses-jump-B2-lowcase-keys ()
   "Test jumping to cell B2 by use of lowercase cell name string with simulating keys"
-    (let ((ses-initial-size '(3 . 3))
-          ses-after-entry-functions)
-      (with-temp-buffer
-        (ses-mode)
-        (ert-simulate-keys [ ?b ?2 return] (ses-jump))
-        (ses-command-hook)
-        (should (eq (ses--cell-at-pos (point)) 'B2)))))
+  (let ((ses-initial-size '(3 . 3))
+        ses-after-entry-functions)
+    (with-temp-buffer
+      (ses-mode)
+      (ert-simulate-keys [ ?b ?2 return] (ses-jump))
+      (ses-command-hook)
+      (should (eq (ses--cell-at-pos (point)) 'B2)))))
 
 (ert-deftest ses-jump-B2-symbol ()
   "Test jumping to cell B2 by use of cell name symbol"
