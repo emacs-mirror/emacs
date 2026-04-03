4e855f90d1b1c3996b408d3f5d9e02d0ca7fd792
Dirk-Jan C. Binnema <djcb@djcbsoftware.nl>
2026-04-03 09:41:13 +0300

    xwidgets: re-enable with newer webkit2gtk
    By disabling hw-acceleration we can re-enable xwidgets with newer
    webkit2gtk versions (Bug#80728).
    Newer versions were disallowed (Bug#66068) since they abort when a
    webkit view is created; however, they work fine as long as we disable
    hw-acceleration, since the off-screen windows that xwidgets uses do not
    support that.
    * configure.ac: Remove upper-bound for webkit2gtk version check.
    * src/xwidget.c (Fmake_xwidget): Turn off hardware-acceleration.
    
    Copyright-paperwork-exempt: yes

--- src/xwidget.c
+++ src/xwidget.c
@@ -380,6 +380,15 @@ fails.  */)
 	  g_object_set (G_OBJECT (settings), "enable-developer-extras", TRUE, NULL);
 	  g_object_set (G_OBJECT (settings), "enable-javascript",
 		        (gboolean) (!xwidget_webkit_disable_javascript), NULL);
+#if WEBKIT_CHECK_VERSION (2, 16, 0)
+	  /* the webkit view is created on an *offscreen* window, on
+	     which hardware acceleration is not supported; we need to
+	     tell webkitgtk _not_ to try that, since it will fail & abort()
+	     with recent versions. */
+
+	  webkit_settings_set_hardware_acceleration_policy
+	    (settings, WEBKIT_HARDWARE_ACCELERATION_POLICY_NEVER);
+#endif
 	}
 
       gtk_widget_set_size_request (GTK_WIDGET (xw->widget_osr), xw->width,
