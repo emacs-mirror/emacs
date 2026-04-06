7679855281b0a91fd6acce81555be2807d7fa5aa
Sean Whitton <spwhitton@spwhitton.name>
2026-04-06 12:26:08 +0100

    ; * lisp/vc/vc.el (checkin): Specify the async case (bug#80723).

--- lisp/vc/vc.el
+++ lisp/vc/vc.el
@@ -287,6 +287,11 @@
 ;;   revision argument is only supported with some older VCSes, like
 ;;   RCS and CVS, and is otherwise silently ignored.
 ;;
+;;   If the backend supports async checkins and `vc-async-checkin' is
+;;   non-nil, the implementation should start an asychronous process to
+;;   commit the changes, and return a cons whose car is `async' and
+;;   whose cdr is that process object.
+;;
 ;; - checkin-patch (patch-string comment)
 ;;
 ;;   Commit a single patch PATCH-STRING to this backend, bypassing any
