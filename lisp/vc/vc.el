49420f7033150c94d4828c7f363e043f610e8416
Sean Whitton <spwhitton@spwhitton.name>
2026-04-09 16:22:20 +0100

    with-vc-properties: Don't substitute in a global value
    * lisp/vc/vc.el (with-vc-properties): Don't substitute in the
    value vc-touched-properties happens to have at macro expansion
    time.  This was left over from when vc-touched-properties was
    bound to a local gensym.

--- lisp/vc/vc.el
+++ lisp/vc/vc.el
@@ -1276,7 +1276,7 @@ Return the result of evaluating FORM."
          (dolist (file ,flist)
            (dolist (setting ,settings)
              (let ((property (car setting)))
-               (unless (memq property ,vc-touched-properties)
+               (unless (memq property vc-touched-properties)
                  (put (intern file vc-file-prop-obarray)
                       property (cdr setting))))))))))
 
