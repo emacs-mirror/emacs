99265573fc3a48e676f3ba5cda80f66af6412c17
Vincent Belaïche <vincentb1@users.sourceforge.net>
2026-04-16 23:04:05 +0200

    Handle the info duplicate target issue as close as possible to the conflict.
    The problems are:
    - filtering out the info from info making templates misses some dependencies.
    - naming PHONY target info.info is misleading, as there is also an info.info
      in the info build directory

--- doc/translations/default/info_common.mk
+++ doc/translations/default/info_common.mk
@@ -2,7 +2,7 @@
 INFO_COMMON = auth autotype bovine calc ccmode cl dbus dired-x		\
 	ebrowse ede ediff edt efaq eglot eieio emacs-gnutls		\
 	emacs-mime epa erc ert eshell eudc eww flymake forms gnus	\
-	htmlfontify idlwave ido info.info mairix-el message mh-e	\
+	htmlfontify idlwave ido info mairix-el message mh-e	        \
 	modus-themes newsticker nxml-mode octave-mode org pcl-cvs pgg	\
 	rcirc reftex remember sasl sc semantic ses sieve smtpmail	\
 	speedbar srecode todo-mode tramp transient url use-package	\
