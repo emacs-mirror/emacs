0d287aa2761af39f64d5d0d7f4e6b74430ec8b74
Sean Whitton <spwhitton@spwhitton.name>
2026-05-07 17:46:43 +0100

    Bump master Emacs version to 32.0.50
    * README:
    * configure.ac:
    * etc/refcards/ru-refcard.tex:
    * exec/configure.ac:
    * java/AndroidManifest.xml.in (Version-code):
    * msdos/sed2v2.inp:
    * nt/README.W32: Bump Emacs version to 32.0.50.
    * etc/NEWS: Empty for Emacs 32.
    * lisp/cus-edit.el (customize-changed-options-previous-release):
    Set last version to 31.1.
    * src/emacs-module.in.h (struct emacs_env_32): Add.
    * src/module-env-31.h: Remove header comment.
    * src/module-env-32.h: New file.

---
+++ src/module-env-32.h
@@ -0,0 +1,3 @@
+  /* Add module environment functions newly added in Emacs 32 here.
+     Before Emacs 32 is released, remove this comment and start
+     module-env-33.h on master (see admin/release-branch.txt).  */
