a9064bfdd9c4bf4888a0cf043a0537ada36f454e
Eli Zaretskii <eliz@gnu.org>
2026-05-07 19:05:39 +0300

    ; * lisp/term/x-win.el (icon-map-list): Fix :type (bug#80982)

--- lisp/term/x-win.el
+++ lisp/term/x-win.el
@@ -1491,7 +1491,10 @@ If you don't want stock icons, set the variable to nil."
   :type '(choice (const :tag "Don't use stock icons" nil)
 		 (repeat (choice symbol
 				 (cons (string :tag "Emacs icon")
-				       (string :tag "Stock/named")))))
+                                       (choice
+                                        (group (string "Named")
+                                               (string "Stock"))
+				        (string :tag "Stock/named"))))))
   :group 'x)
 
 (defcustom x-display-cursor-at-start-of-preedit-string nil
