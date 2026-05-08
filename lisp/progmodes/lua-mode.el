2d496b842d6c6f2a6eb0ca891e00396caedb5178
Sean Whitton <spwhitton@spwhitton.name>
2026-05-08 13:47:52 +0100

    ; Fix Gregor Schmid's attribution for lua-mode.el.

--- lisp/progmodes/lua-mode.el
+++ lisp/progmodes/lua-mode.el
@@ -8,10 +8,10 @@
 ;;         2004 various (support for Lua 5 and byte compilation)
 ;;         2001 Christian Vogler <cvogler@gradient.cis.upenn.edu>
 ;;         1997 Bret Mogilefsky <mogul-lua@gelatinous.com>
-;; Bret Mogilefsky started from tcl-mode by
-;;                               Gregor Schmid <schmid@fb3-s7.math.tu-berlin.de>
-;; with tons of assistance from  Paul Du Bois <pld-lua@gelatinous.com>
-;;                          and  Aaron Smith <aaron-lua@gelatinous.com>.
+;;              Gregor Schmid <schmid@fb3-s7.math.tu-berlin.de>
+;; Bret Mogilefsky started from tcl-mode by Gregor Schmid with tons of
+;; assistance from Paul Du Bois <pld-lua@gelatinous.com>
+;; and Aaron Smith <aaron-lua@gelatinous.com>.
 ;; Maintainer: emacs-devel@gnu.org
 ;; Keywords: languages, processes, tools
 
