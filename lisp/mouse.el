79391d3e1941a168e9e5e650fe44f168ae3b8712
Eli Zaretskii <eliz@gnu.org>
2026-06-05 15:05:19 +0300

    ; * lisp/mouse.el (send-to--resolve-handler): Fix last change.

--- lisp/mouse.el
+++ lisp/mouse.el
@@ -30,7 +30,7 @@
 ;;; Code:
 
 (eval-when-compile (require 'rect))
-(require 'send-to)
+(eval-when-compile (require 'send-to))
 
 ;; Indent track-mouse like progn.
 (put 'track-mouse 'lisp-indent-function 0)
@@ -539,6 +539,7 @@ Some context functions add menu items below the separator."
                   (cdr mode))))
   menu)
 
+(autoload 'send-to--resolve-handler "send-to")
 (defun context-menu-send-to (menu _click)
   "Add a \"Send to...\" context MENU entry on supported platforms."
   (run-hooks 'activate-menubar-hook 'menu-bar-update-hook)
