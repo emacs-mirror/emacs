e1a4bb7a3372b6de513a7b0e37543e2e7d24ea99
Eli Zaretskii <eliz@gnu.org>
2026-07-08 16:21:33 +0300

    ; * lisp/mail/rmail.el (rmail): Add comment (bug#81368).

--- lisp/mail/rmail.el
+++ lisp/mail/rmail.el
@@ -903,6 +903,8 @@ If `rmail-display-summary' is non-nil, make a summary for this RMAIL file."
       ;; The mail file is either unchanged or not visited.  Visit it.
       (switch-to-buffer
        (let ((enable-local-variables nil)
+             ;; Prevent recursive invocation of 'rmail' via
+             ;; 'auto-mode-alist'.
 	     (auto-mode-alist nil)
 	     ;; Force no-conversion by default, since that's what
 	     ;; pre-mbox Rmail did with BABYL files (via
