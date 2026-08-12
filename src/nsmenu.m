023b3b46d9775e0a4ae18e3070b42dfc402dfe3b
Yavor Doganov <yavor@gnu.org>
2026-08-12 16:44:33 +0300

    NS: Fix NSRangeException when enabling tool-bar-mode (bug#81610)
    * src/nsmenu.m (free_frame_tool_bar): Don't call EmacsWindow
    -setToolbar: with a nil argument; fixes NSRangeException when
    enabling tool-bar-mode.

--- src/nsmenu.m
+++ src/nsmenu.m
@@ -1110,8 +1110,6 @@ free_frame_tool_bar (struct frame *f)
   [[[view window] toolbar] setVisible:NO];
   f->output_data.ns->in_animation = 0;
 
-  [[view window] setToolbar:nil];
-
   unblock_input ();
 }
 
