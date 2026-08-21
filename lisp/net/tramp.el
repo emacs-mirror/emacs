f3e7104d05bdb8e32ba13bf75604108ad88536dc
Michael Albinus <michael.albinus@gmx.de>
2026-08-21 14:23:38 +0200

    Restrict Tramp user name
    * doc/misc/tramp.texi (File name syntax): The user name can also
    be an environment variable.
    
    * lisp/net/tramp.el (tramp-build-prefix-regexp):
    * lisp/net/tramp-gvfs.el (tramp-gvfs-handle-expand-file-name)
    (tramp-gvfs-get-file-attributes, tramp-gvfs-file-name)
    (tramp-gvfs-url-host, tramp-gvfs-handler-mounted-unmounted)
    (tramp-gvfs-connection-mounted-p, tramp-gvfs-mount-spec-entry)
    (tramp-gvfs-mount-spec):
    * lisp/net/tramp-rclone.el (tramp-rclone-parse-device-names)
    (tramp-rclone-remote-file-name):
    * lisp/net/tramp-smb.el (tramp-smb-get-share, tramp-smb-get-localname):
    Use string-anchored regexp, not line-anchored.
    
    * lisp/net/tramp.el (tramp-prefix-regexp): Adapt docstring.
    (tramp-user-regexp): Exclude shell meta characters.
    (tramp-dissect-file-name, tramp-file-name-handler):
    Expand environment variable.
    (tramp-convert-file-attributes):
    * lisp/net/tramp-gvfs.el (tramp-gvfs-handle-file-attributes):
    Quote symbolic link target with remote file name syntax.
    
    * test/lisp/net/tramp-tests.el (tramp-test01-file-name-syntax)
    (tramp-test01-file-name-syntax-simplified)
    (tramp-test01-file-name-syntax-separate)
    (tramp-test02-file-name-dissect)
    (tramp-test02-file-name-dissect-simplified)
    (tramp-test02-file-name-dissect-separate)
    (tramp-test21-file-links): Extend test.

--- lisp/net/tramp.el
+++ lisp/net/tramp.el
@@ -1024,11 +1024,11 @@ Used in `tramp-make-tramp-file-name'.")
 
 (defun tramp-build-prefix-regexp ()
   "Return `tramp-prefix-regexp'."
-  (rx bol (literal (tramp-build-prefix-format))))
+  (rx bos (literal (tramp-build-prefix-format))))
 
 (defvar tramp-prefix-regexp nil ; Initialized when defining `tramp-syntax'!
   "Regexp matching the very beginning of Tramp file names.
-Should always start with \"^\".  Derived from `tramp-prefix-format'.")
+Should always start with \"\\\\=`\".  Derived from `tramp-prefix-format'.")
 
 (defconst tramp-method-regexp-alist
   `((default . ,(rx (| (literal tramp-default-method-marker) (>= 2 alnum))))
@@ -1070,7 +1070,10 @@ Used in `tramp-make-tramp-file-name'.")
   "Regexp matching delimiter between method and user or host names.
 Derived from `tramp-postfix-method-format'.")
 
-(defconst tramp-user-regexp (rx (+ (not (any "/:|[]" blank))))
+(defconst tramp-user-regexp
+  (rx (| (+ (not (any "/\\^$?*:;|[]{}()<>`'\"" blank)))
+	 ;; Environment variable.
+	 (: "$" (+ (any "_" alnum)))))
   "Regexp matching user names.")
 
 (defconst tramp-prefix-domain-format "%"
@@ -1845,6 +1848,8 @@ default values are used."
 	    (hop       (match-string (nth 5 tramp-file-name-structure) name))
 	    domain port v)
 	(when user
+	  (while (string-match (rx bos "$" (group (+ (any "_" alnum))) eos) user)
+	    (setq user (getenv (match-string 1 user))))
 	  (when (string-match tramp-user-with-domain-regexp user)
 	    (setq domain (match-string 2 user)
 		  user (match-string 1 user))))
@@ -2710,9 +2715,9 @@ Fall back to normal file name handler if no Tramp file name handler exists."
 	;; `file-remote-p' is called for everything, even for symbolic
 	;; links which look remote.  We don't want to get an error.
 	(non-essential (or non-essential (eq operation 'file-remote-p))))
+    (setq filename (tramp-replace-environment-variables filename))
     (if (tramp-tramp-file-p filename)
 	(save-match-data
-          (setq filename (tramp-replace-environment-variables filename))
           (with-parsed-tramp-file-name filename nil
             (let ((current-connection tramp-current-connection)
 		  (foreign
@@ -6957,6 +6962,9 @@ to cache the result.  Return the modified ATTR."
 			       (caar attr))
 			      (decode-coding-string
 			       (match-string 1 (caar attr)) 'utf-8))))
+	       ;; Quote remote-like symlink.
+	       (when (and (stringp (car attr)) (tramp-tramp-file-p (car attr)))
+		 (setcar attr (file-name-quote (car attr) 'top)))
 	       ;; Set file's gid change bit.
 	       (setcar
 		(nthcdr 9 attr)
