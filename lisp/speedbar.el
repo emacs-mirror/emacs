6fc331b5871362572d85abe23c1d6080eae8181d
Vincenzo Pupillo <v.pupillo@gmail.com>
2026-08-25 00:33:33 +0200

    'speedbar-mode' must be enabled in the 'speedbar-window' buffer.
    * lisp/speedbar.el (speedbar-window-mode): Enable 'speedbar-mode'
    major-mode in the 'speedbar-window' buffer.  (Bug#81699)

--- lisp/speedbar.el
+++ lisp/speedbar.el
@@ -1083,6 +1083,7 @@ supported at a time.
 	    speedbar-last-selected-file nil)
 
       (set-buffer speedbar-buffer)
+      (speedbar-mode)
 
       ;; let's create the window
       (setq speedbar--window
