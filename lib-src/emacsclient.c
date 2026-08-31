d3caf5fbd1178612b92e31ce8b6bd35b0f7db06c
Sean Whitton <spwhitton@spwhitton.name>
2026-08-31 11:21:18 +0100

    emacsclient check_socket_timeout: Don't assume EAGAIN==EWOULDBLOCK
    * lib-src/emacsclient.c (check_socket_timeout): Don't assume
    EAGAIN==EWOULDBLOCK.  See also would_block in src/process.c.

--- lib-src/emacsclient.c
+++ lib-src/emacsclient.c
@@ -1993,10 +1993,14 @@ static bool
 check_socket_timeout (ssize_t rl)
 {
 #ifndef WINDOWSNT
-  return (rl == -1)
-    && (errno == EAGAIN)
-    && (errno == EWOULDBLOCK);
-#else
+  if (rl != -1)
+    return false;
+#ifdef EWOULDBLOCK
+  if (EWOULDBLOCK != EAGAIN && errno == EWOULDBLOCK)
+    return true;
+#endif
+  return errno == EAGAIN;
+#else /* WINDOWSNT */
   return (rl == SOCKET_ERROR)
     && (WSAGetLastError() == WSAETIMEDOUT);
 #endif
