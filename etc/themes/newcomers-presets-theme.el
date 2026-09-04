a834ad2edecc8029f910ff66f7ee74199fb1bd55
Sean Whitton <spwhitton@spwhitton.name>
2026-09-04 16:39:34 +0100

    newcomers-presets: Set some more user options
    * etc/themes/newcomers-presets-theme.el (newcomers-presets): Set
    set-mark-command-repeat-pop, kill-do-not-save-duplicates,
    help-window-select and window-combination-resize.

--- etc/themes/newcomers-presets-theme.el
+++ etc/themes/newcomers-presets-theme.el
@@ -124,12 +124,14 @@ This minor mode will enable and disable the theme on startup."
 ;;;; Editing-related options
  '(electric-pair-mode t)
  '(repeat-mode t)
+ '(set-mark-command-repeat-pop t)       ; Given `repeat-mode'.
  '(delete-selection-mode t)
  '(editorconfig-mode t)
  '(indent-tabs-mode nil)
  '(imenu-auto-rescan t)
  '(view-read-only t)
  '(column-number-mode t)
+ '(kill-do-not-save-duplicates t)
 
 ;;;; Directory managment-related options
  '(dired-auto-revert-buffer t)
@@ -162,10 +164,12 @@ This minor mode will enable and disable the theme on startup."
 
 ;;;; Frame- and window-related options
  '(frame-inhibit-implied-resize t)
+ '(help-window-select t)
  '(tab-bar-history-mode t)
  ;; FIXME: If you toggle off the theme, without having run any
  ;; tab-related commands, the tab bar remains visible.
  '(tab-bar-show 0)
+ '(window-combination-resize t)
 
 ;;;; Programming-related options
  '(compilation-scroll-output 'first-error))
