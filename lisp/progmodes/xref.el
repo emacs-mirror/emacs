9c003435ca3321b2047a89edf28c04138680042e
Dmitry Gutov <dmitry@gutov.dev>
2026-09-04 03:33:04 +0300

    xref.el: Rewrite the introduction text in Commentary
    * lisp/progmodes/xref.el:
    Rewrite the introduction text in Commentary (bug#81624).

--- lisp/progmodes/xref.el
+++ lisp/progmodes/xref.el
@@ -24,49 +24,61 @@
 
 ;;; Commentary:
 
-;; This file provides a somewhat generic infrastructure for cross
-;; referencing commands, in particular "find-definition".
+;; This file provides a generic infrastructure for cross referencing
+;; commands, such as "find definition".
 ;;
-;; Some part of the functionality must be implemented in a language
-;; dependent way and that's done by defining an xref backend.
+;; The unique functionality is configured by defining an xref backend.
+;; That consists of a constructor function, and implementations for the
+;; generic functions (using `cl-defmethod').
 ;;
-;; That consists of a constructor function, which should return a
-;; backend value, and a set of implementations for the generic
-;; functions:
+;; The constructor should return a value that `cl-defmethod' can
+;; dispatch on.  Most often it is a plain symbol, such as `elisp'.
+;; A major or minor mode would use `add-hook' to set itself up and add
+;; the backend constructor to `xref-backend-functions'.
+;;
+;; Some important backend methods to define are:
 ;;
 ;; `xref-backend-identifier-at-point',
 ;; `xref-backend-identifier-completion-table',
 ;; `xref-backend-definitions', `xref-backend-references',
-;; `xref-backend-apropos', which see.
+;; `xref-backend-apropos'.
 ;;
-;; A major mode would normally use `add-hook' to add the backend
-;; constructor to `xref-backend-functions'.
+;; And more optional ones:
 ;;
-;; The last three methods operate with "xref" and "location" values.
+;; `xref-backend-identifier-completion-ignore-case',
+;; `xref-backend-identifier-completion-predicate',
+;; `xref-backend-xref-kinds' and `xref-backend-xrefs-by-kind'.
+;;
+;; The methods which return a list of results (such as "definitions")
+;; operate with "xref" and "location" values.  Locations are values that
+;; also can be dispatched on by `cl-defmethod', usually cl-structs.
 ;;
 ;; One would usually call `xref-make' and `xref-make-file-location',
 ;; `xref-make-buffer-location' or `xref-make-bogus-location' to create
-;; them.  More generally, a location must be an instance of a type for
-;; which methods `xref-location-group' and `xref-location-marker' are
-;; implemented.
+;; them.  The latter functions correspond to built-in location types.
 ;;
-;; There's a special kind of xrefs we call "match xrefs", which
-;; correspond to search results.  For these values,
-;; `xref-match-length' must be defined, and `xref-location-marker'
-;; must return the beginning of the match.
+;; More generally, a location must also be values that `cl-defmethod'
+;; can dispatch on, usually cl-strincs.  Each of them implements
+;; `xref-location-group' and `xref-location-marker'.
+;; `xref-location-line' is optional.
 ;;
-;; Each identifier must be represented as a string.  Implementers can
-;; use string properties to store additional information about the
-;; identifier, but they should keep in mind that values returned from
-;; `xref-backend-identifier-completion-table' should still be
-;; distinct, because the user can't see the properties when making the
-;; choice.
+;; There is a special category of xrefs we call "match xrefs", which
+;; correspond to search results that have spans.  For these values,
+;; `xref-match-length' must be defined, and `xref-location-marker' must
+;; return the beginning of the match.
+;;
+;; Each identifier must be represented as a string.  Definitions can use
+;; string properties to store additional information about the
+;; identifier, but strings in `xref-backend-identifier-completion-table'
+;; should still be distinct, because the user can't see the text
+;; properties when making the choice.
 ;;
 ;; Older versions of Xref used EIEIO for implementation of the
 ;; built-in types, and included a class called `xref-location' which
 ;; was supposed to be inherited from.  Neither is true anymore.
 ;;
-;; See the etags and elisp-mode implementations for full examples.
+;; See the implementations in `elisp-mode', `eglot' and `etags' for more
+;; complete examples.
 
 ;;; Code:
 
